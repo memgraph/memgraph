79ec770af39c34f79ee9187f119ee1e2540c2921
Dominik Tomičević <dominik.tomicevic@gmail.com>
2015-07-04 11:54:27 +0200

    cleaning up the repo a bit

--- data_model/data_model.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MEMGRAPH_DATA_MODEL_DATA_MODEL_HPP
-#define MEMGRAPH_DATA_MODEL_DATA_MODEL_HPP
-
-#include "node.hpp"
-#include "edge.hpp"
-
-#endif
--- utils/sync/all.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MEMGRAPH_UTILS_SYNC_ALL_HPP
-#define MEMGRAPH_UTILS_SYNC_ALL_HPP
-
-#include "spinlock.hpp"
-
-#endif
