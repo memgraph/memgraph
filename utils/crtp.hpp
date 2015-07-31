2c9625e480242467b5392beb2ccf765c6264ed2a
Dominik Tomičević <dominik.tomicevic@gmail.com>
2015-07-31 12:35:46 +0200

    extracted crtp code to a separate class

---
+++ utils/crtp.hpp
@@ -0,0 +1,18 @@
+#ifndef MEMGRAPH_UTILS_CRTP_HPP
+#define MEMGRAPH_UTILS_CRTP_HPP
+
+// a helper class for implementing static casting to a derived class using the
+// curiously recurring template pattern
+
+template <class Derived>
+class Crtp
+{
+public:
+
+    Derived& derived()
+    {
+        return *static_cast<Derived*>(this);
+    }
+};
+
+#endif
