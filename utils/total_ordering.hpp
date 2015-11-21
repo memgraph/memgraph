12628f36893d67ee5b22fa64d4a16c1b3aba9e9c
Dominik Tomičević <dominik.tomicevic@gmail.com>
2015-11-21 13:57:15 +0100

    skeleton for total ordering

---
+++ utils/total_ordering.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+template <class Derived>
+struct TotalOrdering
+{
+    friend bool operator!=(const Derived& a, const Derived& b)
+    {
+        return !(a == b);
+    }
+
+    friend bool operator>(const Derived& a, const Derived& b)
+    {
+        return !(a == b);
+    }
+
+};
