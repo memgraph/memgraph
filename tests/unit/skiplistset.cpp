7527e91d560a40a89fde1e81a7543cdb7a1a9a08
Kruno Tomola Fabro <krunotf@memgraph.io>
2016-08-18 16:03:05 +0100

    Build pass changes.

--- tests/unit/skiplistset.cpp
+++ tests/unit/skiplistset.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-#include "data_structures/skiplist/skiplistset.hpp"
+#include "data_structures/concurrent/concurrent_set.hpp"
 
 using std::cout;
 using std::endl;
 
-void print_skiplist(const SkipListSet<int>::Accessor& skiplist)
+void print_skiplist(const ConcurrentSet<int>::Accessor &skiplist)
 {
     cout << "---- skiplist set now has: ";
 
-    for(auto& item : skiplist)
+    for (auto &item : skiplist)
         cout << item << ", ";
 
     cout << "----" << endl;
@@ -17,16 +17,16 @@ void print_skiplist(const SkipListSet<int>::Accessor& skiplist)
 
 int main(void)
 {
-    SkipListSet<int> set;
+    ConcurrentSet<int> set;
     auto accessor = set.access();
 
     cout << std::boolalpha;
 
-    cout << "added non-existing 1? (true) "
-         << accessor.insert(1).second << endl;
+    cout << "added non-existing 1? (true) " << accessor.insert(1).second
+         << endl;
 
-    cout << "added already existing 1? (false) "
-         << accessor.insert(1).second << endl;
+    cout << "added already existing 1? (false) " << accessor.insert(1).second
+         << endl;
 
     accessor.insert(2);
     print_skiplist(accessor);
@@ -34,11 +34,10 @@ int main(void)
     cout << "item 3 doesn't exist? (true) "
          << (accessor.find(3) == accessor.end()) << endl;
 
-    cout << "item 3 exists? (false) "
-         << accessor.contains(3) << endl;
+    cout << "item 3 exists? (false) " << accessor.contains(3) << endl;
 
-    cout << "item 2 exists? (true) "
-         << (accessor.find(2) != accessor.end()) << endl;
+    cout << "item 2 exists? (true) " << (accessor.find(2) != accessor.end())
+         << endl;
 
     cout << "at item 2 is? 2 " << *accessor.find(2) << endl;
 
