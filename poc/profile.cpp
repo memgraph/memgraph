f5cf9c72d2abe3d52fe1959e79d1634fd143416a
Marko Budiselic <mbudiselicbuda@gmail.com>
2016-10-04 15:09:21 +0200

    skiplist benchmark

--- poc/profile.cpp
+++ poc/profile.cpp
@@ -1,46 +1,46 @@
-#include "profile.hpp"
-
-#include "barrier/barrier.cpp"
-
-#include "database/db.hpp"
-#include "database/db_accessor.hpp"
-
 #include <chrono>
 #include <ctime>
 #include <strings.h>
 #include <unistd.h>
 #include <unordered_map>
 
+#include "barrier/barrier.cpp"
+#include "database/db.hpp"
+#include "database/db_accessor.hpp"
 #include "communication/bolt/v1/serialization/bolt_serializer.hpp"
 #include "import/csv_import.hpp"
 #include "logging/default.hpp"
 #include "logging/streams/stdout.hpp"
 #include "utils/command_line/arguments.hpp"
+#include "profile.hpp"
 
 using namespace std;
 
+// (company, {type_name, score})
 using company_profile_type =
     pair<barrier::VertexAccessor, unordered_map<string, double>>;
 
-// Accepts flags for csv import.
+// Accepted flags for CSV import.
 // -db name # will create database with that name.
-// -s true # will create snapshot of the database after import.
+// -s true  # will create snapshot of the database after import.
 int main(int argc, char **argv)
 {
+    // initialize logger
     logging::init_async();
     logging::log->pipe(std::make_unique<Stdout>());
 
+    // read program arguments
     auto para = all_arguments(argc, argv);
     Db db(get_argument(para, "-db", "powerlinks_profile"));
 
+    // import database
     import_csv_from_arguments(db, para);
 
     {
         DbAccessor t(db);
-
         vector<company_profile_type> company_profiles;
 
-        // QUERY BENCHMARK
+        // query benchmark
         auto begin = clock();
         int n = for_all_companys(barrier::trans(t), company_profiles);
         clock_t end = clock();
@@ -51,6 +51,7 @@ int main(int argc, char **argv)
             return 0;
         }
 
+        // performance statistics
         cout << endl
              << "Query duration: " << (elapsed_s / n) * 1000 * 1000 << " [us]"
              << endl;
@@ -67,8 +68,8 @@ int main(int argc, char **argv)
         int company_id = std::stoi(get_argument(para, "-company_id", "230216"));
         for (auto &company_profile : company_profiles) {
             auto prop_vertex_id = t.vertex_property_key<Int64>("company_id");
-            auto db_company_id = *barrier::trans(company_profile.first)
-                                             .at(prop_vertex_id).get();
+            auto db_company_id =
+                *barrier::trans(company_profile.first).at(prop_vertex_id).get();
             if (db_company_id == company_id) {
                 cout << endl << "CompanyID: " << company_id << endl;
                 for (auto e : company_profile.second) {
@@ -77,20 +78,13 @@ int main(int argc, char **argv)
             }
         }
 
-        // double sum = 0;
-        // for (auto r : coll) {
-        //     for (auto e : r.second) {
-        //         sum += e.second;
-        //     }
-        // }
-        // cout << endl << endl << "Compiler sum " << sum << endl;
-
         t.commit();
     }
 
     if (get_argument(para, "-s", "false") == "true") {
         db.snap_engine.make_snapshot();
     }
+
     // usleep(1000 * 1000 * 60);
 
     return 0;
