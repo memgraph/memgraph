2a046bba786dc741433f5b12bd8e2044b31a3a20
Marko Budiselic <marko.budiselic@memgraph.io>
2017-02-19 23:47:09 +0100

    all unit tests are gtests

--- tests/unit/id.cpp
+++ tests/unit/id.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
+#include "gtest/gtest.h"
 
 #include "mvcc/id.hpp"
 
-using std::cout;
-using std::endl;
-
-int main() {
+TEST(IdTest, BasicUsageAndTotalOrdering) {
   Id id0(0);
   Id id1(1);
   Id id2(1);
@@ -13,21 +10,17 @@ int main() {
   Id id4 = id3;
   Id id5(5);
 
-  cout << id5 << " " << id0 << endl;
+  ASSERT_EQ(id0 < id5, true);
+  ASSERT_EQ(id1 == id2, true);
+  ASSERT_EQ(id3 == id4, true);
+  ASSERT_EQ(id5 > id0, true);
+  ASSERT_EQ(id5 > id0, true);
+  ASSERT_EQ(id5 != id3, true);
+  ASSERT_EQ(id1 >= id2, true);
+  ASSERT_EQ(id3 <= id4, true);
+}
 
-  if (id0 < id5) cout << "id0 < id5" << endl;
-
-  if (id1 == id2) cout << "are equal" << endl;
-
-  if (id3 == id4) cout << "id3 == id4" << endl;
-
-  if (id5 > id0) cout << "id5 > id0" << endl;
-
-  if (id5 != id3) cout << "id5 != id3" << endl;
-
-  if (id1 >= id2) cout << "id1 >= id2" << endl;
-
-  if (id3 <= id4) cout << "id3 <= id4" << endl;
-
-  return 0;
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- tests/unit/timestamp.cpp
+++ tests/unit/timestamp.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <thread>
 
+#include "gtest/gtest.h"
 #include "utils/datetime/timestamp.hpp"
 
-int main(void) {
+TEST(TimestampTest, BasicUsage) {
   auto timestamp = Timestamp::now();
 
   std::cout << timestamp << std::endl;
@@ -14,9 +15,16 @@ int main(void) {
 
   std::cout << Timestamp::now().to_iso8601() << std::endl;
 
+  ASSERT_GT(Timestamp::now(), timestamp);
+
   std::cout << std::boolalpha;
 
   std::cout << (timestamp == Timestamp::now()) << std::endl;
 
-  return 0;
+  ASSERT_NE(timestamp, Timestamp::now());
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
