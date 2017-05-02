487f429d5b40492cd158c69517d7ebbe4e54d03c
Dominik Gleich <dominik.gleich@memgraph.io>
2017-05-02 10:43:46 +0200

    Add concurrent benchmarks for skiplist.
    Summary: Skiplist benchmark and refactor testing infrastructure.
    
    Reviewers: mferencevic, buda
    
    Reviewed By: buda
    
    Subscribers: pullbot
    
    Differential Revision: https://phabricator.memgraph.io/D103

--- tests/benchmark/data_structures/concurrent/skiplist_reverse_iteration.cpp
+++ tests/benchmark/data_structures/concurrent/skiplist_reverse_iteration.cpp
@@ -14,41 +14,19 @@
 #include "data_structures/concurrent/skiplist.hpp"
 #include "logging/default.hpp"
 #include "logging/streams/stdout.hpp"
+#include "skiplist_helper.hpp"
 #include "utils/random/random_generator.hpp"
 
 using utils::random::NumberGenerator;
 using IntegerGenerator = NumberGenerator<std::uniform_int_distribution<int>,
                                          std::default_random_engine, int>;
 
-void InsertSkiplist(SkipList<int> *skiplist, int start, int end) {
-  auto accessor = skiplist->access();
-
-  for (int start = 0; start < end; start++) {
-    accessor.insert(std::move(start));
-  }
-}
-
-void InsertConcurrentSkiplist(SkipList<int> *skiplist, int start, int end) {
-  int number_od_threads = std::thread::hardware_concurrency();
-  std::vector<std::thread> threads(number_od_threads);
-
-  for (int i = 0; i < number_od_threads; i++) {
-    int part = (end - start) / number_od_threads;
-    threads[i] = std::thread(InsertSkiplist, skiplist, start + i * part,
-                             start + (i + 1) * part);
-  }
-
-  for (int i = 0; i < number_od_threads; i++) {
-    threads[i].join();
-  }
-}
-
 static void ReverseFromRBegin(benchmark::State &state) {
   while (state.KeepRunning()) {
     state.PauseTiming();
 
     SkipList<int> skiplist;
-    InsertConcurrentSkiplist(&skiplist, 0, state.range(0));
+    SkipListHelper::InsertConcurrentSkiplist(&skiplist, 0, state.range(0));
     int counter = 10;
 
     auto accessor = skiplist.access();
@@ -70,7 +48,7 @@ static void FindFromRBegin(benchmark::State &state) {
     state.PauseTiming();
 
     SkipList<int> skiplist;
-    InsertConcurrentSkiplist(&skiplist, 0, state.range(0));
+    SkipListHelper::InsertConcurrentSkiplist(&skiplist, 0, state.range(0));
     int counter = 10;
 
     auto accessor = skiplist.access();
