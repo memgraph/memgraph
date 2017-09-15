7e5dddecae2f37628b0d12d3cf27b71308b0bde9
Mislav Bradac <mislav.bradac@memgraph.io>
2017-09-15 13:23:05 +0200

    Move harness stuff to parent directory
    Reviewers: buda
    
    Reviewed By: buda
    
    Subscribers: pullbot
    
    Differential Revision: https://phabricator.memgraph.io/D799

--- tests/macro_benchmark/common.py
+++ tests/macro_benchmark/common.py
@@ -8,13 +8,13 @@ DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def get_absolute_path(path, base=""):
     if base == "build":
-        extra = "../../../build"
+        extra = "../../build"
     elif base == "build_release":
-        extra = "../../../build_release"
+        extra = "../../build_release"
     elif base == "libs":
-        extra = "../../../libs"
+        extra = "../../libs"
     elif base == "config":
-        extra = "../../../config"
+        extra = "../../config"
     else:
         extra = ""
     return os.path.normpath(os.path.join(DIR_PATH, extra, path))
