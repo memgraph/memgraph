8c2ca44cb98a2295b072e933c183213b9f32f074
Teon Banek <teon.banek@memgraph.io>
2018-10-10 10:23:10 +0200

    Split manual/query_planner for distributed and single
    Reviewers: mtomic, llugovic, mferencevic
    
    Reviewed By: mferencevic
    
    Subscribers: pullbot
    
    Differential Revision: https://phabricator.memgraph.io/D1644

---
+++ tests/manual/distributed_query_planner.cpp
@@ -0,0 +1,50 @@
+#include "interactive_planning.hpp"
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+#include "database/distributed/distributed_graph_db.hpp"
+#include "database/distributed/graph_db_accessor.hpp"
+#include "query/plan/distributed.hpp"
+#include "query/plan/distributed_pretty_print.hpp"
+
+DECLARE_int32(min_log_level);
+
+DEFCOMMAND(ShowDistributed) {
+  int64_t plan_ix = 0;
+  std::stringstream ss(args[0]);
+  ss >> plan_ix;
+  if (ss.fail() || !ss.eof() || plan_ix >= plans.size()) return;
+  const auto &plan = plans[plan_ix].first;
+  std::atomic<int64_t> plan_id{0};
+  auto distributed_plan = MakeDistributedPlan(*plan, symbol_table, plan_id);
+  {
+    std::cout << "---- Master Plan ---- " << std::endl;
+    query::plan::DistributedPrettyPrint(dba,
+                                        distributed_plan.master_plan.get());
+    std::cout << std::endl;
+  }
+  for (size_t i = 0; i < distributed_plan.worker_plans.size(); ++i) {
+    int64_t id;
+    std::shared_ptr<query::plan::LogicalOperator> worker_plan;
+    std::tie(id, worker_plan) = distributed_plan.worker_plans[i];
+    std::cout << "---- Worker Plan #" << id << " ---- " << std::endl;
+    query::plan::DistributedPrettyPrint(dba, worker_plan.get());
+    std::cout << std::endl;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  FLAGS_min_log_level = google::ERROR;
+  google::InitGoogleLogging(argv[0]);
+  AddCommand("show-distributed",
+             {ShowDistributedCommand, 1,
+              "Show the Nth plan as for distributed execution"});
+  database::Master db;
+  auto dba = db.Access();
+  RunInteractivePlanning(dba.get());
+  db.Shutdown();
+  db.AwaitShutdown();
+  return 0;
+}
---
+++ tests/manual/interactive_planning.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "query/frontend/semantic/symbol_table.hpp"
+#include "query/plan/operator.hpp"
+
+namespace database {
+class GraphDbAccessor;
+}
+
+// Shorthand for a vector of pairs (logical_plan, cost).
+typedef std::vector<
+    std::pair<std::unique_ptr<query::plan::LogicalOperator>, double>>
+    PlansWithCost;
+
+// Encapsulates a consoles command function.
+struct Command {
+  typedef std::vector<std::string> Args;
+  // Function of this command
+  std::function<void(database::GraphDbAccessor &, const query::SymbolTable &,
+                     PlansWithCost &, const Args &)>
+      function;
+  // Number of arguments the function works with.
+  int arg_count;
+  // Explanation of the command.
+  std::string documentation;
+};
+
+#define DEFCOMMAND(Name)                                     \
+  void Name##Command(database::GraphDbAccessor &dba,         \
+                     const query::SymbolTable &symbol_table, \
+                     PlansWithCost &plans, const Command::Args &args)
+
+void AddCommand(const std::string &name, const Command &command);
+
+void RunInteractivePlanning(database::GraphDbAccessor *dba);
