6d21e58b09f593ca163ec18eae4e3016177a37c0
Matej Ferencevic <matej.ferencevic@memgraph.io>
2018-11-12 11:19:27 +0100

    Add ClientPool and ThreadPool tests to RPC benchmark
    Summary: Add script for plotting throughput
    
    Reviewers: teon.banek, mculinovic, buda
    
    Reviewed By: teon.banek
    
    Subscribers: pullbot
    
    Differential Revision: https://phabricator.memgraph.io/D1721

--- tests/benchmark/rpc.cpp
+++ tests/benchmark/rpc.cpp
@@ -44,11 +44,30 @@ DEFINE_bool(run_benchmark, true, "Set to false to only run server");
 
 std::experimental::optional<communication::rpc::Server> server;
 std::experimental::optional<communication::rpc::Client> clients[kThreadsNum];
+std::experimental::optional<communication::rpc::ClientPool> client_pool;
+std::experimental::optional<utils::ThreadPool> thread_pool;
 
 static void BenchmarkRpc(benchmark::State &state) {
-  std::string data('a', state.range(0));
+  std::string data(state.range(0), 'a');
   while (state.KeepRunning()) {
     clients[state.thread_index]->Call<Echo>(data);
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+
+static void BenchmarkRpcPool(benchmark::State &state) {
+  std::string data(state.range(0), 'a');
+  while (state.KeepRunning()) {
+    client_pool->Call<Echo>(data);
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+
+static void BenchmarkRpcPoolAsync(benchmark::State &state) {
+  std::string data(state.range(0), 'a');
+  while (state.KeepRunning()) {
+    auto future = thread_pool->Run([&data] { client_pool->Call<Echo>(data); });
+    future.get();
   }
   state.SetItemsProcessed(state.iterations());
 }
@@ -60,10 +79,25 @@ BENCHMARK(BenchmarkRpc)
     ->Unit(benchmark::kNanosecond)
     ->UseRealTime();
 
+BENCHMARK(BenchmarkRpcPool)
+    ->RangeMultiplier(4)
+    ->Range(4, 1 << 13)
+    ->ThreadRange(1, kThreadsNum)
+    ->Unit(benchmark::kNanosecond)
+    ->UseRealTime();
+
+BENCHMARK(BenchmarkRpcPoolAsync)
+    ->RangeMultiplier(4)
+    ->Range(4, 1 << 13)
+    ->ThreadRange(1, kThreadsNum)
+    ->Unit(benchmark::kNanosecond)
+    ->UseRealTime();
+
 int main(int argc, char **argv) {
+  ::benchmark::Initialize(&argc, argv);
+  gflags::AllowCommandLineReparsing();
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
-  ::benchmark::Initialize(&argc, argv);
 
   if (FLAGS_run_server) {
     server.emplace(
@@ -81,15 +115,34 @@ int main(int argc, char **argv) {
   if (FLAGS_run_benchmark) {
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
+    io::network ::Endpoint endpoint;
+    if (FLAGS_run_server) {
+      endpoint = server->endpoint();
+    } else {
+      endpoint = io::network::Endpoint(FLAGS_server_address, FLAGS_server_port);
+    }
+
     for (int i = 0; i < kThreadsNum; ++i) {
-      if (FLAGS_run_server) {
-        clients[i].emplace(server->endpoint());
-      } else {
-        clients[i].emplace(
-            io::network::Endpoint(FLAGS_server_address, FLAGS_server_port));
-      }
+      clients[i].emplace(endpoint);
       clients[i]->Call<Echo>("init");
     }
+
+    // The client pool connects to the server only when there are no leftover
+    // unused RPC clients (during concurrent execution). To reduce the overhead
+    // of making connections to the server during the benchmark here we
+    // simultaneously call the Echo RPC on the client pool to make the client
+    // pool connect to the server `kThreadsNum` times.
+    client_pool.emplace(endpoint);
+    std::thread threads[kThreadsNum];
+    for (int i = 0; i < kThreadsNum; ++i) {
+      threads[i] =
+          std::thread([] { client_pool->Call<Echo>(std::string(10000, 'a')); });
+    }
+    for (int i = 0; i < kThreadsNum; ++i) {
+      threads[i].join();
+    }
+
+    thread_pool.emplace(kThreadsNum, "RPC client");
 
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
