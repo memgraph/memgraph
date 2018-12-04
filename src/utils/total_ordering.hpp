eabd8320489d19f157b7069aaa04ff53c6e39ac9
Matej Ferencevic <matej.ferencevic@memgraph.io>
2018-12-04 16:54:53 +0100

    Remove vtable from TotalOrdering
    Summary: This change undoes the performance impact that was introduced in D1117
    
    Reviewers: teon.banek
    
    Reviewed By: teon.banek
    
    Subscribers: pullbot
    
    Differential Revision: https://phabricator.memgraph.io/D1756

--- src/utils/total_ordering.hpp
+++ src/utils/total_ordering.hpp
@@ -13,8 +13,10 @@ namespace utils {
  */
 template <typename TLhs, typename TRhs = TLhs, typename TReturn = bool>
 struct TotalOrdering {
-  virtual ~TotalOrdering() {}
+ protected:
+  ~TotalOrdering() {}
 
+ public:
   friend constexpr TReturn operator!=(const TLhs &a, const TRhs &b) {
     return !(a == b);
   }
@@ -28,43 +30,6 @@ struct TotalOrdering {
   }
 
   friend constexpr TReturn operator>=(const TLhs &a, const TRhs &b) {
-    return !(a < b);
-  }
-};
-
-template <class Derived, class T>
-struct TotalOrderingWith {
-  virtual ~TotalOrderingWith() {}
-
-  friend constexpr bool operator!=(const Derived &a, const T &b) {
-    return !(a == b);
-  }
-
-  friend constexpr bool operator<=(const Derived &a, const T &b) {
-    return a < b || a == b;
-  }
-
-  friend constexpr bool operator>(const Derived &a, const T &b) {
-    return !(a <= b);
-  }
-
-  friend constexpr bool operator>=(const Derived &a, const T &b) {
-    return !(a < b);
-  }
-
-  friend constexpr bool operator!=(const T &a, const Derived &b) {
-    return !(a == b);
-  }
-
-  friend constexpr bool operator<=(const T &a, const Derived &b) {
-    return a < b || a == b;
-  }
-
-  friend constexpr bool operator>(const T &a, const Derived &b) {
-    return !(a <= b);
-  }
-
-  friend constexpr bool operator>=(const T &a, const Derived &b) {
     return !(a < b);
   }
 };
