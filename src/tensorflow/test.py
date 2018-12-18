7d305b66f9b028b71a4c097bda11b699e6ee8b1e
Dino Santl <dino.santl@memgraph.io>
2018-12-18 12:35:29 +0100

    Add formated test

--- src/tensorflow/test.py
+++ src/tensorflow/test.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import tensorflow as tf
-memgraph_op_module = tf.load_op_library('/home/dino/git/memgraph/build/src/tensorflow/libmemgraph_op.so')
+
+# Load libmemgraph_op.so
+memgraph_op_module = tf.load_op_library('libmemgraph_op.so')
+
 
 def main():
+    query = """match (u :User)-->(m :Movie)
+              where u.id in $input_list
+              return u.id, m.id;"""
 
-  query = "match (u:User)-->(m) where u.id in $input_list return u.id, m.id;"
-  input_list = [1, 2, 3, 4, 5]
+    # Input list used in query
+    input_list = [1, 2, 3, 4, 5]
 
-  with tf.Session() as sess:
-    query_holder = tf.placeholder(tf.string)
-    input_list_holder = tf.placeholder(tf.int64)
-    
-    memgraph_op = memgraph_op_module.memgraph_op(query_holder, \
-      input_list_holder, \
-      output_dtype=tf.int64, host="10.10.10.10")
-    
-    output = sess.run(memgraph_op, {query_holder: query, \
-      input_list_holder: input_list})
-    
-    for i in output[0]:
-      print(i)
-    for i in output[1]:
-      print(i)
+    # Create tensorflow session
+    with tf.Session() as sess:
+
+        # Query placeholder
+        query_holder = tf.placeholder(tf.string)
+
+        # Input list placeholder
+        input_list_holder = tf.placeholder(tf.int64)
+
+        # Create Memgraph op, and put placeholders for input
+        memgraph_op = memgraph_op_module.memgraph_op(query_holder,
+                                                     input_list_holder,
+                                                     output_dtype=tf.int64)
+
+        # Run Memgraph op
+        output = sess.run(memgraph_op, {query_holder: query,
+                                        input_list_holder: input_list})
+
+        # First output is list of headers
+        print("Headers:")
+        for i in output[0]:
+            print(i)
+
+        # Output matrix (rows), query results
+        print("Rows: ")
+        for i in output[1]:
+            print(i)
 
 if __name__ == "__main__":
-  main()
+    main()
