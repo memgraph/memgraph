dfc546183dddcc6190ffad97d7ca5c60c16db2db
Matej Ferencevic <matej.ferencevic@memgraph.io>
2020-02-14 11:04:14 +0100

    Add community to Apollo build
    Reviewers: teon.banek
    
    Reviewed By: teon.banek
    
    Subscribers: pullbot
    
    Differential Revision: https://phabricator.memgraph.io/D2671

--- apollo_archives.py
+++ apollo_archives.py
@@ -3,17 +3,27 @@ import json
 import os
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-BUILD_OUTPUT_DIR = os.path.join("build_release", "output")
+
+
+def find_packages(build_output_dir):
+    ret = []
+    output_dir = os.path.join(SCRIPT_DIR, build_output_dir)
+    if os.path.exists(output_dir):
+        for fname in os.listdir(output_dir):
+            if fname.startswith("memgraph") and fname.endswith(".deb"):
+                path = os.path.join(build_output_dir, fname)
+                ret.append({
+                    "name": "Release " + fname.split("_")[1] +
+                            " (deb package)",
+                    "archive": path,
+                })
+    return ret
+
 
 archives = []
-output_dir = os.path.join(SCRIPT_DIR, BUILD_OUTPUT_DIR)
-if os.path.exists(output_dir):
-    for fname in os.listdir(output_dir):
-        if fname.startswith("memgraph") and fname.endswith(".deb"):
-            path = os.path.join(BUILD_OUTPUT_DIR, fname)
-            archives = [{
-                "name": "Release " + fname.split("_")[1] + " (deb package)",
-                "archive": path,
-            }]
+# Find enterprise package(s).
+archives += find_packages(os.path.join("build_release", "output"))
+# Find community package(s).
+archives += find_packages(os.path.join("build_community", "output"))
 
 print(json.dumps(archives, indent=4, sort_keys=True))
--- tests/apollo_runs.py
+++ tests/apollo_runs.py
@@ -10,72 +10,107 @@ WORKSPACE_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, "..", ".."))
 TESTS_DIR_REL = os.path.join("..", "build_debug", "tests")
 TESTS_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, TESTS_DIR_REL))
 
+# test ordering: first unit, then concurrent, then everything else
+CTEST_ORDER = {"unit": 0, "concurrent": 1}
+CTEST_DELIMITER = "__"
+
+
+def get_runs(build_dir, include=None, exclude=None, outfile=None,
+             name_prefix=""):
+    tests_dir = os.path.join(build_dir, "tests")
+    tests_dir_abs = os.path.normpath(os.path.join(SCRIPT_DIR, tests_dir))
+    ctest_output = subprocess.run(
+        ["ctest", "-N"], cwd=tests_dir_abs, check=True,
+        stdout=subprocess.PIPE).stdout.decode("utf-8")
+    tests = []
+
+    for row in ctest_output.split("\n"):
+        # Filter rows only containing tests.
+        if not re.match("^\s*Test\s+#", row):
+            continue
+        if not row.count("memgraph"):
+            continue
+        test_name = row.split(":")[1].strip()
+        name = test_name.replace("memgraph" + CTEST_DELIMITER, "")
+        path = os.path.join(
+            tests_dir, name.replace(CTEST_DELIMITER, "/", 1))
+        order = CTEST_ORDER.get(
+            name.split(CTEST_DELIMITER)[0], len(CTEST_ORDER))
+        tests.append((order, name, path))
+
+    tests.sort()
+
+    runs = []
+    for test in tests:
+        order, name, path = test
+        dirname, basename = os.path.split(path)
+        files = [basename]
+
+        # check whether the test should be included
+        if include is not None:
+            should_include = False
+            for inc in include:
+                if name.startswith(inc):
+                    should_include = True
+                    break
+            if not should_include:
+                continue
+
+        # check whether the test should be excluded
+        if exclude is not None:
+            should_exclude = False
+            for exc in exclude:
+                if name.startswith(exc):
+                    should_exclude = True
+                    break
+            if should_exclude:
+                continue
+
+        # larger timeout for benchmark and concurrent tests
+        prefix = ""
+        if name.startswith("benchmark") or name.startswith("concurrent"):
+            prefix = "TIMEOUT=600 "
+
+        # larger timeout for storage_v2_durability unit test
+        if name.endswith("storage_v2_durability"):
+            prefix = "TIMEOUT=300 "
+
+        # get output files
+        outfile_paths = []
+        if outfile:
+            curdir_abs = os.path.normpath(os.path.join(SCRIPT_DIR, dirname))
+            curdir_rel = os.path.relpath(curdir_abs, WORKSPACE_DIR)
+            outfile_paths.append("\./" + curdir_rel.replace(".", "\\.") + "/" +
+                                 outfile.replace(".", "\\."))
+
+        runs.append({
+            "name": name_prefix + name,
+            "cd": dirname,
+            "commands": prefix + "./" + basename,
+            "infiles": files,
+            "outfile_paths": outfile_paths,
+        })
+    return runs
+
+
 # generation mode
 mode = "release"
 if os.environ.get("PROJECT", "") == "mg-master-diff":
     mode = "diff"
 
-# ctest tests
-ctest_output = subprocess.run(["ctest", "-N"], cwd=TESTS_DIR, check=True,
-        stdout=subprocess.PIPE).stdout.decode("utf-8")
-tests = []
+# get unit tests
+runs = get_runs("../build_coverage", include=["unit"],
+                outfile="default.profraw")
 
-# test ordering: first unit, then concurrent, then everything else
-CTEST_ORDER = {"unit": 0, "concurrent": 1}
-CTEST_DELIMITER = "__"
-for row in ctest_output.split("\n"):
-    # Filter rows only containing tests.
-    if not re.match("^\s*Test\s+#", row): continue
-    if not row.count("memgraph"): continue
-    test_name = row.split(":")[1].strip()
-    name = test_name.replace("memgraph" + CTEST_DELIMITER, "")
-    path = os.path.join(TESTS_DIR_REL, name.replace(CTEST_DELIMITER, "/", 1))
-    order = CTEST_ORDER.get(name.split(CTEST_DELIMITER)[0], len(CTEST_ORDER))
-    tests.append((order, name, path))
+# get all other tests except unit and benchmark
+runs += get_runs("../build_debug", exclude=["unit", "benchmark"])
 
-tests.sort()
+# get benchmark tests
+if mode != "diff":
+    runs += get_runs("../build_release", include=["benchmark"])
 
-runs = []
-for test in tests:
-    order, name, path = test
-    dirname, basename = os.path.split(path)
-    files = [basename]
-
-    # skip benchmark tests on diffs
-    if name.startswith("benchmark") and mode == "diff":
-        continue
-
-    # larger timeout for benchmark and concurrent tests
-    prefix = ""
-    if name.startswith("benchmark") or name.startswith("concurrent"):
-        prefix = "TIMEOUT=600 "
-
-    # larger timeout for storage_v2_durability unit test
-    if name.endswith("storage_v2_durability"):
-        prefix = "TIMEOUT=300 "
-
-    outfile_paths = []
-    if name.startswith("unit"):
-        dirname = dirname.replace("/build_debug/", "/build_coverage/")
-        curdir_abs = os.path.normpath(os.path.join(SCRIPT_DIR, dirname))
-        curdir_rel = os.path.relpath(curdir_abs, WORKSPACE_DIR)
-        outfile_paths.append("\./" + curdir_rel.replace(".", "\\.") + "/default\\.profraw")
-    elif name.startswith("benchmark"):
-        dirname = dirname.replace("/build_debug/", "/build_release/")
-
-    runs.append({
-        "name": name,
-        "cd": dirname,
-        "commands": prefix + "./" + basename,
-        "infiles": files,
-        "outfile_paths": outfile_paths,
-    })
-
-runs.append({
-    "name": "test_lcp",
-    "cd": os.path.join(TESTS_DIR_REL, "unit"),
-    "commands": "./test_lcp",
-    "infiles": ["test_lcp"],
-})
+# get community tests
+runs += get_runs("../build_community", include=["unit"],
+                 name_prefix="community__")
 
 print(json.dumps(runs, indent=4, sort_keys=True))
