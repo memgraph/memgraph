memgraph/memgraph

commit 50b2646765027f068d3c77c57786bf968f7e4c82

Compose ExpandCommon instead of inheriting from it
Reviewers: teon.banek, llugovic

Reviewed By: teon.banek

Subscribers: pullbot

Differential Revision: https://phabricator.memgraph.io/D1703

tests/unit/query_plan_match_filter_return.cpp
@@ -6,12 +6,12 @@
 
 #include <fmt/format.h>
 
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include <cppitertools/enumerate.hpp>
 #include <cppitertools/product.hpp>
 #include <cppitertools/range.hpp>
 #include <cppitertools/repeat.hpp>
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
 
 #include "communication/result_stream_faker.hpp"
 #include "database/single_node/graph_db.hpp"
@@ -561,16 +561,16 @@ class QueryPlanExpandVariable : public testing::Test {
       };
 
       return std::make_shared<ExpandVariable>(
-          n_to_sym, edge_sym, EdgeAtom::Type::DEPTH_FIRST, direction,
-          edge_types, is_reverse, convert(lower), convert(upper), filter_op,
-          n_from.sym_, false,
+          filter_op, n_from.sym_, n_to_sym, edge_sym,
+          EdgeAtom::Type::DEPTH_FIRST, direction, edge_types, is_reverse,
+          convert(lower), convert(upper), false,
           ExpansionLambda{symbol_table.CreateSymbol("inner_edge", false),
                           symbol_table.CreateSymbol("inner_node", false),
                           nullptr},
           std::experimental::nullopt, std::experimental::nullopt, graph_view);
     } else
-      return std::make_shared<Expand>(n_to_sym, edge_sym, direction, edge_types,
-                                      filter_op, n_from.sym_, false,
+      return std::make_shared<Expand>(filter_op, n_from.sym_, n_to_sym,
+                                      edge_sym, direction, edge_types, false,
                                       graph_view);
   }
 
@@ -916,9 +916,10 @@ class QueryPlanExpandWeightedShortestPath : public testing::Test {
                         : symbol_table.CreateSymbol("node", true);
     auto edge_list_sym = symbol_table.CreateSymbol("edgelist_", true);
     auto filter_lambda = last_op = std::make_shared<ExpandVariable>(
-        node_sym, edge_list_sym, EdgeAtom::Type::WEIGHTED_SHORTEST_PATH,
-        direction, std::vector<storage::EdgeType>{}, false, nullptr,
-        max_depth ? LITERAL(max_depth.value()) : nullptr, last_op, n.sym_,
+        last_op, n.sym_, node_sym, edge_list_sym,
+        EdgeAtom::Type::WEIGHTED_SHORTEST_PATH, direction,
+        std::vector<storage::EdgeType>{}, false, nullptr,
+        max_depth ? LITERAL(max_depth.value()) : nullptr,
         existing_node_input != nullptr,
         ExpansionLambda{filter_edge, filter_node, where},
         ExpansionLambda{weight_edge, weight_node,
@@ -1339,8 +1340,8 @@ TEST(QueryPlan, OptionalMatchThenExpandToMissingNode) {
   auto node = NODE("n");
   symbol_table[*node->identifier_] = with_n_sym;
   auto expand = std::make_shared<plan::Expand>(
-      with_n_sym, edge_sym, edge_direction, std::vector<storage::EdgeType>{},
-      m.op_, m.sym_, true, GraphView::OLD);
+      m.op_, m.sym_, with_n_sym, edge_sym, edge_direction,
+      std::vector<storage::EdgeType>{}, true, GraphView::OLD);
   // RETURN m
   auto m_ne = NEXPR("m", IDENT("m"));
   symbol_table[*m_ne->expression_] = m.sym_;
@@ -1372,10 +1373,9 @@ TEST(QueryPlan, ExpandExistingNode) {
                           EdgeAtom::Direction::OUT, {}, "n", with_existing,
                           GraphView::OLD);
     if (with_existing)
-      r_n.op_ =
-          std::make_shared<Expand>(n.sym_, r_n.edge_sym_, r_n.edge_->direction_,
-                                   std::vector<storage::EdgeType>{}, n.op_,
-                                   n.sym_, with_existing, GraphView::OLD);
+      r_n.op_ = std::make_shared<Expand>(
+          n.op_, n.sym_, n.sym_, r_n.edge_sym_, r_n.edge_->direction_,
+          std::vector<storage::EdgeType>{}, with_existing, GraphView::OLD);
 
     // make a named expression and a produce
     auto output = NEXPR("n", IDENT("n"));

src/query/plan/distributed.cpp
@@ -157,9 +157,8 @@ class IndependentSubtreeFinder : public DistributedOperatorVisitor {
       branch_.depends_on = lower_depends;
       if (upper_depends || !scan.upper_bound_) {
         // Cases 2) and 1.b)
-        new_scan =
-            std::make_shared<ScanAllByLabel>(scan.input(), scan.output_symbol_,
-                                             scan.label_, scan.graph_view_);
+        new_scan = std::make_shared<ScanAllByLabel>(
+            scan.input(), scan.output_symbol_, scan.label_, scan.graph_view_);
       } else {
         // Case 1.a)
         new_scan = std::make_shared<ScanAllByLabelPropertyRange>(
@@ -193,13 +192,11 @@ class IndependentSubtreeFinder : public DistributedOperatorVisitor {
           // Case 1.a)
           new_scan = std::make_shared<ScanAllByLabelPropertyRange>(
               scan.input(), scan.output_symbol_, scan.label_, scan.property_,
-              scan.lower_bound_, std::experimental::nullopt,
-              scan.graph_view_);
+              scan.lower_bound_, std::experimental::nullopt, scan.graph_view_);
         } else {
           // Case 1.b)
           new_scan = std::make_shared<ScanAllByLabel>(
-              scan.input(), scan.output_symbol_, scan.label_,
-              scan.graph_view_);
+              scan.input(), scan.output_symbol_, scan.label_, scan.graph_view_);
         }
       }
     }
@@ -259,12 +256,12 @@ class IndependentSubtreeFinder : public DistributedOperatorVisitor {
     if (auto found = FindForbidden(exp.input_symbol_)) {
       SetBranch(exp.input(), &exp, *found);
     }
-    if (exp.existing_node_) {
-      if (auto found = FindForbidden(exp.node_symbol_)) {
+    if (exp.common_.existing_node) {
+      if (auto found = FindForbidden(exp.common_.node_symbol)) {
         SetBranch(exp.input(), &exp, *found);
       }
     }
-    CHECK(!FindForbidden(exp.edge_symbol_))
+    CHECK(!FindForbidden(exp.common_.edge_symbol))
         << "Expand uses an already used edge symbol.";
     return true;
   }
@@ -279,12 +276,12 @@ class IndependentSubtreeFinder : public DistributedOperatorVisitor {
     if (auto found = FindForbidden(exp.input_symbol_)) {
       SetBranch(exp.input(), &exp, *found);
     }
-    if (exp.existing_node_) {
-      if (auto found = FindForbidden(exp.node_symbol_)) {
+    if (exp.common_.existing_node) {
+      if (auto found = FindForbidden(exp.common_.node_symbol)) {
         SetBranch(exp.input(), &exp, *found);
       }
     }
-    CHECK(!FindForbidden(exp.edge_symbol_))
+    CHECK(!FindForbidden(exp.common_.edge_symbol))
         << "Expand uses an already used edge symbol.";
     // Check for bounding expressions.
     if (exp.lower_bound_) {
@@ -331,12 +328,12 @@ class IndependentSubtreeFinder : public DistributedOperatorVisitor {
     if (auto found = FindForbidden(exp.input_symbol_)) {
       SetBranch(exp.input(), &exp, *found);
     }
-    if (exp.existing_node_) {
-      if (auto found = FindForbidden(exp.node_symbol_)) {
+    if (exp.common_.existing_node) {
+      if (auto found = FindForbidden(exp.common_.node_symbol)) {
         SetBranch(exp.input(), &exp, *found);
       }
     }
-    CHECK(!FindForbidden(exp.edge_symbol_))
+    CHECK(!FindForbidden(exp.common_.edge_symbol))
         << "Expand uses an already used edge symbol.";
     // Check for bounding expressions.
     if (exp.lower_bound_) {
@@ -959,8 +956,7 @@ class DistributedPlanner : public HierarchicalLogicalOperatorVisitor {
   bool PostVisit(Expand &exp) override {
     prev_ops_.pop_back();
     auto distributed_expand = std::make_unique<DistributedExpand>(
-        exp.node_symbol_, exp.edge_symbol_, exp.direction_, exp.edge_types_,
-        exp.input(), exp.input_symbol_, exp.existing_node_, exp.graph_view_);
+        exp.input(), exp.input_symbol_, exp.common_);
     SetOnPrevious(std::move(distributed_expand));
     return true;
   }
@@ -973,9 +969,7 @@ class DistributedPlanner : public HierarchicalLogicalOperatorVisitor {
     prev_ops_.pop_back();
     if (exp.type_ == EdgeAtom::Type::BREADTH_FIRST) {
       auto distributed_bfs = std::make_unique<DistributedExpandBfs>(
-          exp.node_symbol_, exp.edge_symbol_, exp.direction_,
-          exp.edge_types_, exp.input(), exp.input_symbol_,
-          exp.existing_node_, exp.graph_view_, exp.lower_bound_,
+          exp.input(), exp.input_symbol_, exp.common_, exp.lower_bound_,
           exp.upper_bound_, exp.filter_lambda_);
       SetOnPrevious(std::move(distributed_bfs));
     }

commit 28590aea53cb874876c05517a0f4c5a486310b27

Fix variable expand between same symbol
Summary:
This diff fixes the variable expand operator to work correctly then the start
and destination nodes use the same symbol or when the destination symbol is an
existing symbol.

Previously, the variable expand operator produced paths that were both
completely wrong (they shouldn't have been produced) and nonexistent (they
didn't even exist in the storage). Invalid data was produced because of a
wrong equality check that was introduced in D1703.

This issue was reported externally and the supplied test case was:
```
CREATE (p1:Person {id: 1})-[:KNOWS]->(:Person {id: 2})-[:KNOWS]->(:Person {id: 3})-[:KNOWS]->(:Person {id: 4})-[:KNOWS]->(p1);
MATCH path = (pers:Person {id: 3})-[:KNOWS*2]->(pers) RETURN path;
```

Also, tests have been added so the behavior remains correct.

Reviewers: buda

Reviewed By: buda

Subscribers: pullbot

Differential Revision: https://phabricator.memgraph.io/D2793

src/query/plan/operator.cpp
@@ -533,7 +533,7 @@ bool CheckExistingNode(const VertexAccessor &new_node,
   const TypedValue &existing_node = frame[existing_node_sym];
   if (existing_node.IsNull()) return false;
   ExpectType(existing_node_sym, existing_node, TypedValue::Type::Vertex);
-  return existing_node.ValueVertex() != new_node;
+  return existing_node.ValueVertex() == new_node;
 }
 
 template <class TEdges>
@@ -834,10 +834,11 @@ class ExpandVariableCursor : public Cursor {
         // if lower bound is zero we also yield empty paths
         if (lower_bound_ == 0) {
           auto &start_vertex = frame[self_.input_symbol_].ValueVertex();
-          if (!self_.common_.existing_node ||
-              CheckExistingNode(start_vertex, self_.common_.node_symbol,
-                                frame)) {
+          if (!self_.common_.existing_node) {
             frame[self_.common_.node_symbol] = start_vertex;
+            return true;
+          } else if (CheckExistingNode(start_vertex, self_.common_.node_symbol,
+                                       frame)) {
             return true;
           }
         }
@@ -1016,11 +1017,9 @@ class ExpandVariableCursor : public Cursor {
               ? current_edge.first.From()
               : current_edge.first.To();
 
-      if (self_.common_.existing_node &&
-          !CheckExistingNode(current_vertex, self_.common_.node_symbol, frame))
-        continue;
-
-      frame[self_.common_.node_symbol] = current_vertex;
+      if (!self_.common_.existing_node) {
+        frame[self_.common_.node_symbol] = current_vertex;
+      }
 
       // Skip expanding out of filtered expansion.
       frame[self_.filter_lambda_.inner_edge_symbol] = current_edge.first;
@@ -1038,6 +1037,10 @@ class ExpandVariableCursor : public Cursor {
                                              self_.common_.edge_types, memory));
         edges_it_.emplace_back(edges_.back().begin());
       }
+
+      if (self_.common_.existing_node &&
+          !CheckExistingNode(current_vertex, self_.common_.node_symbol, frame))
+        continue;
 
       // We only yield true if we satisfy the lower bound.
       if (static_cast<int64_t>(edges_on_frame.size()) >= lower_bound_)

tests/qa/tests/memgraph_V1/features/match.feature
@@ -513,3 +513,111 @@ Feature: Match
             | size(path) |
             | 0          |
             | 1          |
+
+    Scenario: Variable expand to existing symbol 1
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(:Person {id: 2})-[:KNOWS]->(:Person {id: 3})-[:KNOWS]->(:Person {id: 4})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 3})-[:KNOWS*2]->(pers) RETURN path;
+            """
+        Then the result should be empty
+
+    Scenario: Variable expand to existing symbol 2
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(:Person {id: 2})-[:KNOWS]->(:Person {id: 3})-[:KNOWS]->(:Person {id: 4})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 3})-[:KNOWS*]->(pers) RETURN path
+            """
+        Then the result should be:
+            | path                                                                                                                      |
+            | <(:Person{id:3})-[:KNOWS]->(:Person{id:4})-[:KNOWS]->(:Person{id:1})-[:KNOWS]->(:Person{id:2})-[:KNOWS]->(:Person{id:3})> |
+
+    Scenario: Variable expand to existing symbol 3
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(:Person {id: 2})-[:KNOWS]->(:Person {id: 3})-[:KNOWS]->(:Person {id: 4})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 3})-[:KNOWS*0..]->(pers) RETURN path
+            """
+        Then the result should be:
+            | path                                                                                                                      |
+            | <(:Person{id:3})>                                                                                                         |
+            | <(:Person{id:3})-[:KNOWS]->(:Person{id:4})-[:KNOWS]->(:Person{id:1})-[:KNOWS]->(:Person{id:2})-[:KNOWS]->(:Person{id:3})> |
+
+    Scenario: Variable expand to existing symbol 4
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(:Person {id: 2})-[:KNOWS]->(:Person {id: 3})-[:KNOWS]->(:Person {id: 4})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 3})-[:KNOWS*2..6]->(pers) RETURN path
+            """
+        Then the result should be:
+            | path                                                                                                                      |
+            | <(:Person{id:3})-[:KNOWS]->(:Person{id:4})-[:KNOWS]->(:Person{id:1})-[:KNOWS]->(:Person{id:2})-[:KNOWS]->(:Person{id:3})> |
+
+    Scenario: Variable expand to existing symbol 5
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(:Person {id: 2})-[:KNOWS]->(:Person {id: 3})-[:KNOWS]->(:Person {id: 4})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 3})-[:KNOWS*5..]->(pers) RETURN path
+            """
+        Then the result should be empty
+
+    Scenario: Variable expand to existing symbol 6
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 1})-[:KNOWS*]->(pers) RETURN path
+            """
+        Then the result should be
+            | path                                        |
+            | <(:Person{id:1})-[:KNOWS]->(:Person{id:1})> |
+
+    Scenario: Variable expand to existing symbol 7
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 1})-[:KNOWS*0..]->(pers) RETURN path
+            """
+        Then the result should be
+            | path                                        |
+            | <(:Person{id:1})>                           |
+            | <(:Person{id:1})-[:KNOWS]->(:Person{id:1})> |
+
+    Scenario: Variable expand to existing symbol 8
+        Given an empty graph
+        And having executed:
+            """
+            CREATE (p1:Person {id: 1})-[:KNOWS]->(p1);
+            """
+        When executing query:
+            """
+            MATCH path = (pers:Person {id: 1})-[:KNOWS*2..]->(pers) RETURN path
+            """
+        Then the result should be empty

tests/unit/query_plan_match_filter_return.cpp
@@ -803,6 +803,247 @@ TEST_F(QueryPlanExpandVariable, NamedPath) {
                                   expected_paths.begin()));
 }
 
+TEST_F(QueryPlanExpandVariable, ExpandToSameSymbol) {
+  auto test_expand = [&](int layer, EdgeAtom::Direction direction,
+                         std::optional<size_t> lower,
+                         std::optional<size_t> upper, bool reverse) {
+    auto e = Edge("r", direction);
+
+    auto node = NODE("n");
+    auto symbol = symbol_table.CreateSymbol("n", true);
+    node->identifier_->MapTo(symbol);
+    auto logical_op =
+        std::make_shared<ScanAll>(nullptr, symbol, storage::View::OLD);
+    auto n_from = ScanAllTuple{node, logical_op, symbol};
+
+    auto filter_op = std::make_shared<Filter>(
+        n_from.op_,
+        storage.Create<query::LabelsTest>(
+            n_from.node_->identifier_, std::vector<LabelIx>{storage.GetLabelIx(
+                                           dba.LabelToName(labels[layer]))}));
+
+    // convert optional ints to optional expressions
+    auto convert = [this](std::optional<size_t> bound) {
+      return bound ? LITERAL(static_cast<int64_t>(bound.value())) : nullptr;
+    };
+
+    return GetEdgeListSizes(
+        std::make_shared<ExpandVariable>(
+            filter_op, symbol, symbol, e, EdgeAtom::Type::DEPTH_FIRST,
+            direction, std::vector<storage::EdgeTypeId>{}, reverse,
+            convert(lower), convert(upper), /* existing = */ true,
+            ExpansionLambda{symbol_table.CreateSymbol("inner_edge", false),
+                            symbol_table.CreateSymbol("inner_node", false),
+                            nullptr},
+            std::nullopt, std::nullopt),
+        e);
+  };
+
+  // The graph is a double chain:
+  // chain 0:   (v:0)-(v:1)-(v:2)
+  //                 X     X
+  // chain 1:   (v:0)-(v:1)-(v:2)
+
+  // Expand from chain 0 v:0 to itself.
+  //
+  // It has a total of 3 cycles:
+  // 1. C0 v:0 -> C0 v:1 -> C1 v:2 -> C1 v:1 -> C0 v:0
+  // 2. C0 v:0 -> C0 v:1 -> C0 v:2 -> C1 v:1 -> C0 v:0
+  // 3. C0 v:0 -> C0 v:1 -> C1 v:0 -> C1 v:1 -> C0 v:0
+  //
+  // Each cycle can be in two directions, also, we have two starting nodes: one
+  // in chain 0 and the other in chain 1.
+  for (auto reverse : {false, true}) {
+    // Tests with both bounds set.
+    for (int lower_bound = 0; lower_bound < 10; ++lower_bound) {
+      for (int upper_bound = lower_bound; upper_bound < 10; ++upper_bound) {
+        map_int expected_directed;
+        map_int expected_undirected;
+        if (lower_bound == 0) {
+          expected_directed.emplace(0, 2);
+          expected_undirected.emplace(0, 2);
+        }
+        if (lower_bound <= 4 && upper_bound >= 4) {
+          expected_undirected.emplace(4, 12);
+        }
+        if (lower_bound <= 8 && upper_bound >= 8) {
+          expected_undirected.emplace(8, 24);
+        }
+
+        EXPECT_EQ(test_expand(0, EdgeAtom::Direction::IN, lower_bound,
+                              upper_bound, reverse),
+                  expected_directed);
+        EXPECT_EQ(test_expand(0, EdgeAtom::Direction::OUT, lower_bound,
+                              upper_bound, reverse),
+                  expected_directed);
+        EXPECT_EQ(test_expand(0, EdgeAtom::Direction::BOTH, lower_bound,
+                              upper_bound, reverse),
+                  expected_undirected);
+      }
+    }
+
+    // Test only upper bound.
+    for (int upper_bound = 0; upper_bound < 10; ++upper_bound) {
+      map_int expected_directed;
+      map_int expected_undirected;
+      if (upper_bound >= 4) {
+        expected_undirected.emplace(4, 12);
+      }
+      if (upper_bound >= 8) {
+        expected_undirected.emplace(8, 24);
+      }
+
+      EXPECT_EQ(test_expand(0, EdgeAtom::Direction::IN, std::nullopt,
+                            upper_bound, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(0, EdgeAtom::Direction::OUT, std::nullopt,
+                            upper_bound, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(0, EdgeAtom::Direction::BOTH, std::nullopt,
+                            upper_bound, reverse),
+                expected_undirected);
+    }
+
+    // Test only lower bound.
+    for (int lower_bound = 0; lower_bound < 10; ++lower_bound) {
+      map_int expected_directed;
+      map_int expected_undirected;
+      if (lower_bound == 0) {
+        expected_directed.emplace(0, 2);
+        expected_undirected.emplace(0, 2);
+      }
+      if (lower_bound <= 4) {
+        expected_undirected.emplace(4, 12);
+      }
+      if (lower_bound <= 8) {
+        expected_undirected.emplace(8, 24);
+      }
+
+      EXPECT_EQ(test_expand(0, EdgeAtom::Direction::IN, lower_bound,
+                            std::nullopt, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(0, EdgeAtom::Direction::OUT, lower_bound,
+                            std::nullopt, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(0, EdgeAtom::Direction::BOTH, lower_bound,
+                            std::nullopt, reverse),
+                expected_undirected);
+    }
+
+    // Test no bounds.
+    EXPECT_EQ(test_expand(0, EdgeAtom::Direction::IN, std::nullopt,
+                          std::nullopt, reverse),
+              (map_int{}));
+    EXPECT_EQ(test_expand(0, EdgeAtom::Direction::OUT, std::nullopt,
+                          std::nullopt, reverse),
+              (map_int{}));
+    EXPECT_EQ(test_expand(0, EdgeAtom::Direction::BOTH, std::nullopt,
+                          std::nullopt, reverse),
+              (map_int{{4, 12}, {8, 24}}));
+  }
+
+  // Expand from chain 0 v:1 to itself.
+  //
+  // It has a total of 6 cycles:
+  // 1. C0 v:1 -> C1 v:0 -> C1 v:1 -> C1 v:2 -> C0 v:1
+  // 2. C0 v:1 -> C1 v:0 -> C1 v:1 -> C0 v:2 -> C0 v:1
+  // 3. C0 v:1 -> C0 v:0 -> C1 v:1 -> C1 v:2 -> C0 v:1
+  // 4. C0 v:1 -> C0 v:0 -> C1 v:1 -> C0 v:2 -> C0 v:1
+  // 5. C0 v:1 -> C1 v:0 -> C1 v:1 -> C0 v:0 -> C0 v:1
+  // 6. C0 v:1 -> C1 v:2 -> C1 v:1 -> C0 v:2 -> C0 v:1
+  //
+  // Each cycle can be in two directions, also, we have two starting nodes: one
+  // in chain 0 and the other in chain 1.
+  for (auto reverse : {false, true}) {
+    // Tests with both bounds set.
+    for (int lower_bound = 0; lower_bound < 10; ++lower_bound) {
+      for (int upper_bound = lower_bound; upper_bound < 10; ++upper_bound) {
+        map_int expected_directed;
+        map_int expected_undirected;
+        if (lower_bound == 0) {
+          expected_directed.emplace(0, 2);
+          expected_undirected.emplace(0, 2);
+        }
+        if (lower_bound <= 4 && upper_bound >= 4) {
+          expected_undirected.emplace(4, 24);
+        }
+        if (lower_bound <= 8 && upper_bound >= 8) {
+          expected_undirected.emplace(8, 48);
+        }
+
+        EXPECT_EQ(test_expand(1, EdgeAtom::Direction::IN, lower_bound,
+                              upper_bound, reverse),
+                  expected_directed);
+        EXPECT_EQ(test_expand(1, EdgeAtom::Direction::OUT, lower_bound,
+                              upper_bound, reverse),
+                  expected_directed);
+        EXPECT_EQ(test_expand(1, EdgeAtom::Direction::BOTH, lower_bound,
+                              upper_bound, reverse),
+                  expected_undirected);
+      }
+    }
+
+    // Test only upper bound.
+    for (int upper_bound = 0; upper_bound < 10; ++upper_bound) {
+      map_int expected_directed;
+      map_int expected_undirected;
+      if (upper_bound >= 4) {
+        expected_undirected.emplace(4, 24);
+      }
+      if (upper_bound >= 8) {
+        expected_undirected.emplace(8, 48);
+      }
+
+      EXPECT_EQ(test_expand(1, EdgeAtom::Direction::IN, std::nullopt,
+                            upper_bound, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(1, EdgeAtom::Direction::OUT, std::nullopt,
+                            upper_bound, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(1, EdgeAtom::Direction::BOTH, std::nullopt,
+                            upper_bound, reverse),
+                expected_undirected);
+    }
+
+    // Test only lower bound.
+    for (int lower_bound = 0; lower_bound < 10; ++lower_bound) {
+      map_int expected_directed;
+      map_int expected_undirected;
+      if (lower_bound == 0) {
+        expected_directed.emplace(0, 2);
+        expected_undirected.emplace(0, 2);
+      }
+      if (lower_bound <= 4) {
+        expected_undirected.emplace(4, 24);
+      }
+      if (lower_bound <= 8) {
+        expected_undirected.emplace(8, 48);
+      }
+
+      EXPECT_EQ(test_expand(1, EdgeAtom::Direction::IN, lower_bound,
+                            std::nullopt, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(1, EdgeAtom::Direction::OUT, lower_bound,
+                            std::nullopt, reverse),
+                expected_directed);
+      EXPECT_EQ(test_expand(1, EdgeAtom::Direction::BOTH, lower_bound,
+                            std::nullopt, reverse),
+                expected_undirected);
+    }
+
+    // Test no bounds.
+    EXPECT_EQ(test_expand(1, EdgeAtom::Direction::IN, std::nullopt,
+                          std::nullopt, reverse),
+              (map_int{}));
+    EXPECT_EQ(test_expand(1, EdgeAtom::Direction::OUT, std::nullopt,
+                          std::nullopt, reverse),
+              (map_int{}));
+    EXPECT_EQ(test_expand(1, EdgeAtom::Direction::BOTH, std::nullopt,
+                          std::nullopt, reverse),
+              (map_int{{4, 24}, {8, 48}}));
+  }
+}
+
 namespace std {
 template <>
 struct hash<std::pair<int, int>> {