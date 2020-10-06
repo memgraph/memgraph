72c31dccc6cec600eadd3ca2d5f2853b08db02f4
Antonio Andelic <antonio.andelic@memgraph.io>
2020-10-06 13:14:54 +0200

    Check for dotnet and go install

--- tests/drivers/csharp/v4_1/run.sh
+++ tests/drivers/csharp/v4_1/run.sh
@@ -3,7 +3,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-#check if dotnet-sdk-2.1 is installed
+# check if dotnet-sdk-2.1 is installed
+for i in dotnet; do
+  if ! which $i >/dev/null; then
+    echo "Please install $i!"
+    exit 1
+  fi
+done
 
 for i in *; do
   if [ ! -d $i ]; then 
