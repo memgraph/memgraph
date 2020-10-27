bc42a09f13322e7957f50d1efc87386574972326
Marko Budiselić <marko.budiselic@memgraph.com>
2020-10-27 10:11:43 +0100

    Add implementation of synchronous replication (#7)
    This implements the initial version of synchronous replication.
    Currently, only one replica is supported and that isn't configurable.
    
    To run the main instance use the following command:
    ```
    ./memgraph \
        --main \
        --data-directory main-data \
        --storage-properties-on-edges \
        --storage-wal-enabled \
        --storage-snapshot-interval-sec 300
    ```
    
    To run the replica instance use the following command:
    ```
    ./memgraph \
        --replica \
        --data-directory replica-data \
        --storage-properties-on-edges \
        --bolt-port 7688
    ```
    
    You can then write/read data to Bolt port 7687 (the main instance) and also you
    can read the data from the replica instance using Bolt port 7688.
    
    NOTE: The main instance *must* be started without any data and the replica
    *must* be started before any data is added to the main instance.
    
    * Add basic synchronous replication test
    * Using RWLock for replication stuff
    
    Co-authored-by: Matej Ferencevic <matej.ferencevic@memgraph.io>
    Co-authored-by: Antonio Andelic <antonio.andelic@memgraph.io>

---
+++ src/storage/v2/replication/replication.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "rpc/client.hpp"
+#include "storage/v2/config.hpp"
+#include "storage/v2/delta.hpp"
+#include "storage/v2/durability/wal.hpp"
+#include "storage/v2/id_types.hpp"
+#include "storage/v2/mvcc.hpp"
+#include "storage/v2/name_id_mapper.hpp"
+#include "storage/v2/property_value.hpp"
+#include "storage/v2/replication/rpc.hpp"
+#include "storage/v2/replication/serialization.hpp"
+
+namespace storage::replication {
+
+class ReplicationClient {
+ public:
+  ReplicationClient(NameIdMapper *name_id_mapper, Config::Items items,
+                    const io::network::Endpoint &endpoint, bool use_ssl)
+      : name_id_mapper_(name_id_mapper),
+        items_(items),
+        rpc_context_(use_ssl),
+        rpc_client_(endpoint, &rpc_context_) {}
+
+  class Handler {
+   private:
+    friend class ReplicationClient;
+
+    /// @throw rpc::RpcFailedException
+    explicit Handler(ReplicationClient *self)
+        : self_(self), stream_(self_->rpc_client_.Stream<AppendDeltasRpc>()) {}
+
+   public:
+    /// @throw rpc::RpcFailedException
+    void AppendDelta(const Delta &delta, const Vertex &vertex,
+                     uint64_t final_commit_timestamp) {
+      Encoder encoder(stream_.GetBuilder());
+      EncodeDelta(&encoder, self_->name_id_mapper_, self_->items_, delta,
+                  vertex, final_commit_timestamp);
+    }
+
+    /// @throw rpc::RpcFailedException
+    void AppendDelta(const Delta &delta, const Edge &edge,
+                     uint64_t final_commit_timestamp) {
+      Encoder encoder(stream_.GetBuilder());
+      EncodeDelta(&encoder, self_->name_id_mapper_, delta, edge,
+                  final_commit_timestamp);
+    }
+
+    /// @throw rpc::RpcFailedException
+    void AppendTransactionEnd(uint64_t final_commit_timestamp) {
+      Encoder encoder(stream_.GetBuilder());
+      EncodeTransactionEnd(&encoder, final_commit_timestamp);
+    }
+
+    /// @throw rpc::RpcFailedException
+    void AppendOperation(durability::StorageGlobalOperation operation,
+                         LabelId label, const std::set<PropertyId> &properties,
+                         uint64_t timestamp) {
+      Encoder encoder(stream_.GetBuilder());
+      EncodeOperation(&encoder, self_->name_id_mapper_, operation, label,
+                      properties, timestamp);
+    }
+
+    /// @throw rpc::RpcFailedException
+    void Finalize() { stream_.AwaitResponse(); }
+
+   private:
+    ReplicationClient *self_;
+    rpc::Client::StreamHandler<AppendDeltasRpc> stream_;
+  };
+
+  Handler ReplicateTransaction() { return Handler(this); }
+
+ private:
+  NameIdMapper *name_id_mapper_;
+  Config::Items items_;
+  communication::ClientContext rpc_context_;
+  rpc::Client rpc_client_;
+};
+
+}  // namespace storage::replication
