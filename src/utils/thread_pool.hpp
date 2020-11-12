f39add8e485b902c45dc96a4672009bb90bd192f
Antonio Andelic <antonio.andelic@memgraph.io>
2020-11-12 14:13:08 +0100

    Define thread pools

---
+++ src/utils/thread_pool.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+#include "utils/spin_lock.hpp"
+#include "utils/synchronized.hpp"
+#include "utils/thread.hpp"
+
+namespace utils {
+
+class ThreadPool {
+  using TaskSignature = std::function<void()>;
+
+ public:
+  explicit ThreadPool(size_t pool_size);
+
+  void AddTask(std::function<void()> new_task);
+
+  void Shutdown();
+
+  ~ThreadPool();
+
+  ThreadPool(const ThreadPool &) = delete;
+  ThreadPool(ThreadPool &&) = delete;
+  ThreadPool &operator=(const ThreadPool &) = delete;
+  ThreadPool &operator=(ThreadPool &&) = delete;
+
+ private:
+  std::unique_ptr<TaskSignature> PopTask();
+
+  void ThreadLoop();
+
+  std::vector<std::thread> thread_pool_;
+
+  std::atomic<bool> terminate_pool_{false};
+  std::atomic<bool> stopped_{false};
+  utils::Synchronized<std::queue<std::unique_ptr<TaskSignature>>,
+                      utils::SpinLock>
+      task_queue_;
+  std::mutex pool_lock_;
+  std::condition_variable queue_cv_;
+};
+
+}  // namespace utils
