b40ce4f6c4782ea1ea53c5e0ac96ee3a211c9eb1
Antonio Andelic <antonio.andelic@memgraph.io>
2020-11-30 13:47:14 +0100

    Add detailed comment

--- src/storage/v2/storage.hpp
+++ src/storage/v2/storage.hpp
@@ -535,11 +535,18 @@ class Storage final {
 
   // UUID to distinguish different main instance runs for replication process
   // on SAME storage.
-  // Multiple instance can have same storage UUID and be MAIN at the same time.
+  // Multiple instances can have same storage UUID and be MAIN at the same time.
   // We cannot compare commit timestamps of those instances if one of them
   // becomes the replica of the other so we use epoch_id_ as additional
-  // property.
-  // Needs to be synchronized because the value can be read at any time
+  // discriminating property.
+  // Example of this:
+  // We have 2 instances of the same storage, S1 and S2.
+  // S1 and S2 are MAIN and accept their own commits and write them to the WAL.
+  // At the moment when S1 commited a transaction with timestamp 20, and S2
+  // a different transaction with timestamp 15, we change S2's role to REPLICA
+  // and register it on S1.
+  // Without using the epoch_id, we don't know that S1 and S2 have completely
+  // different transactions, we think that the S2 is behind only by 5 commits.
   std::string epoch_id_;
   // History of the previous epoch ids.
   // Each value consists of the epoch id along the last commit belonging to that
