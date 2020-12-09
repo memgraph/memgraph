80a213db1df462a24864c9b90dd0d0fc1ab62422
Marko Budiselić <marko.budiselic@memgraph.com>
2020-12-09 17:07:09 +0100

    Add Jepsen initialization (#53)
    Co-authored-by: Antonio Andelic <antonio.andelic@memgraph.io>

---
+++ tests/jepsen/src/jepsen/memgraph/client.clj
@@ -0,0 +1,23 @@
+(ns jepsen.memgraph.client
+  "Neo4j Clojure driver helper functions/macros"
+  (:require [neo4j-clj.core :as dbclient])
+  (:import (java.net URI)))
+
+;; Jepsen related utils.
+(defn instance-url
+  "An URL for connecting to an instance on a particular port"
+  [node port]
+  (str "bolt://" node ":" port))
+
+;; neo4j-clj related utils.
+(defmacro with-session
+  "Execute body expressions by using the same session. Useful when executing
+  multiple queries, each as a separete transaction."
+  [connection session & body]
+  `(with-open [~session (dbclient/get-session ~connection)]
+     ~@body))
+
+(defn open
+  "Open client connection to the node"
+  [node]
+  (dbclient/connect (URI. (instance-url node 7687)) "" ""))
