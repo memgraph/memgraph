fe60866641ab12a8d90029efdeda17dddb53f740
Antonio Andelic <antonio.andelic@memgraph.io>
2021-02-11 15:08:13 +0100

    Add and use jemalloc lib

---
+++ src/utils/new_delete.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <new>
+
+#if USE_JEMALLOC
+#include <jemalloc/jemalloc.h>
+#else
+#include <cstdlib>
+#endif
+
+#include "utils/likely.hpp"
+#include "utils/logging.hpp"
+
+namespace {
+void *newImpl(std::size_t size) {
+  auto *ptr = malloc(size);
+  if (LIKELY(ptr != nullptr)) {
+    return ptr;
+  }
+
+  throw std::bad_alloc{};
+}
+
+void *newNoExcept(std::size_t size) noexcept { return malloc(size); }
+
+void deleteImpl(void *ptr) noexcept { free(ptr); }
+
+#if USE_JEMALLOC
+
+void deleteSized(void *ptr, std::size_t size) noexcept {
+  if (UNLIKELY(ptr == nullptr)) {
+    return;
+  }
+
+  sdallocx(ptr, size, 0);
+}
+
+#else
+
+void deleteSized(void *ptr, std::size_t size) noexcept { free(ptr); }
+
+#endif
+
+}  // namespace
+
+void *operator new(std::size_t size) { return newImpl(size); }
+
+void *operator new[](std::size_t size) { return newImpl(size); }
+
+void *operator new(std::size_t size, const std::nothrow_t &) noexcept {
+  return newNoExcept(size);
+}
+
+void *operator new[](std::size_t size, const std::nothrow_t &) noexcept {
+  return newNoExcept(size);
+}
+
+void operator delete(void *ptr) noexcept { deleteImpl(ptr); }
+
+void operator delete[](void *ptr) noexcept { deleteImpl(ptr); }
+
+void operator delete(void *ptr, std::size_t size) noexcept {
+  deleteSized(ptr, size);
+}
+
+void operator delete[](void *ptr, std::size_t size) noexcept {
+  deleteSized(ptr, size);
+}
