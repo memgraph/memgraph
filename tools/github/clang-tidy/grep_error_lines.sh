1fea9e156f6f989380d548d89196e2e6709bc76e
Benjamin Antal <benjamin.antal@memgraph.io>
2021-03-26 13:21:27 +0100

    Fail build in case of clang-tidy reported any warning

---
+++ tools/github/clang-tidy/grep_error_lines.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Matches timestamp like "2021-03-25T17:06:42.2621697Z"
+TIMESTAMP_PATTERN="\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{7}Z"
+
+# Matches absolute file pathes with line and column identifier like
+# "/opt/actions-runner/_work/memgraph/memgraph/src/utils/exceptions.hpp:71:11:"
+FILE_ABSOLUTE_PATH_PATTERN="/[^:]+:\d+:\d+:"
+
+ERROR_OR_WARNING_PATTERN="(error|warning):"
+
+grep -P "^($TIMESTAMP_PATTERN )?$FILE_ABSOLUTE_PATH_PATTERN $ERROR_OR_WARNING_PATTERN.*$"
