fcaada4bae832464d82523560ec2485877b5f557
Marko Budiselic <marko.budiselic@memgraph.io>
2021-03-27 23:26:11 +0100

    Add wget toolchain and unpacking

--- release/legacy/ops.sh
+++ release/legacy/ops.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-# TODO(gitbuda): Docker file for each legacy OS. During build install deps.
-# TODO(gitbuda): Docker compose to mange the cluster of Docker build containers.
-# TODO(gitbuda): The ops.sh script should manage the cluster. If cluster is up, do nothing. If there is, e.g., --override, reboot the cluster.
-# TODO(gitbuda): The ops.sh should be parametrized by Memgraph build type and target operating system.
+# TODO(gitbuda): Install toolchain deps somehow during image build phase.
+# TODO(gitbuda): To build Docker image, just use Debian10 package + the release/docker/package_deb_docker.
 
-docker-compose build
-docker-compose up -d
+make_package () {
+    offering="$1"
+    os="$2"
+    build_container="legacy-mgbuild_$os"
+    echo "TODO(gitbuda): Build $offering package for $os inside $build_container."
+    # TODO(gitbuda): During runtime install deps from scratch because deps might change.
+    exit 1
+}
+
+case "$1" in
+    init)
+        docker-compose build
+        docker-compose up -d
+    ;;
+    package)
+        shift 1
+        offering="$1" # TODO(gitbuda): Validate.
+        shift 1
+        os="$1" # TODO(gitbuda): Validate.
+        shift
+        make_package "$offering" "$os"
+    ;;
+    *)
+        echo "$0 init"
+        exit 1
+    ;;
+esac
