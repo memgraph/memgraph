2b08ddf76725316bca5da387781d9e6497845d03
antonio2368 <antonio2368@users.noreply.github.com>
2021-04-15 10:49:40 +0200

    Initial trigger definition (#133)
    * Pull out cypher query parsing logic
    
    * Define trigger structure
    
    * Run triggers before commit
    
    * Use skip list for saving triggers

--- src/query/interpreter.hpp
+++ src/query/interpreter.hpp
@@ -3,6 +3,7 @@
 #include <gflags/gflags.h>
 
 #include "query/context.hpp"
+#include "query/cypher_query_interpreter.hpp"
 #include "query/db_accessor.hpp"
 #include "query/exceptions.hpp"
 #include "query/frontend/ast/ast.hpp"
@@ -12,6 +13,7 @@
 #include "query/plan/operator.hpp"
 #include "query/plan/read_write_type_checker.hpp"
 #include "query/stream.hpp"
+#include "query/trigger.hpp"
 #include "query/typed_value.hpp"
 #include "utils/event_counter.hpp"
 #include "utils/logging.hpp"
@@ -20,9 +22,6 @@
 #include "utils/spin_lock.hpp"
 #include "utils/timer.hpp"
 #include "utils/tsc.hpp"
-
-DECLARE_bool(query_cost_planner);
-DECLARE_int32(query_plan_cache_ttl);
 
 namespace EventCounter {
 extern const Event FailedQuery;
@@ -139,64 +138,6 @@ struct PreparedQuery {
   plan::ReadWriteTypeChecker::RWType rw_type;
 };
 
-// TODO: Maybe this should move to query/plan/planner.
-/// Interface for accessing the root operator of a logical plan.
-class LogicalPlan {
- public:
-  virtual ~LogicalPlan() {}
-
-  virtual const plan::LogicalOperator &GetRoot() const = 0;
-  virtual double GetCost() const = 0;
-  virtual const SymbolTable &GetSymbolTable() const = 0;
-  virtual const AstStorage &GetAstStorage() const = 0;
-};
-
-class CachedPlan {
- public:
-  explicit CachedPlan(std::unique_ptr<LogicalPlan> plan);
-
-  const auto &plan() const { return plan_->GetRoot(); }
-  double cost() const { return plan_->GetCost(); }
-  const auto &symbol_table() const { return plan_->GetSymbolTable(); }
-  const auto &ast_storage() const { return plan_->GetAstStorage(); }
-
-  bool IsExpired() const { return cache_timer_.Elapsed() > std::chrono::seconds(FLAGS_query_plan_cache_ttl); };
-
- private:
-  std::unique_ptr<LogicalPlan> plan_;
-  utils::Timer cache_timer_;
-};
-
-struct CachedQuery {
-  AstStorage ast_storage;
-  Query *query;
-  std::vector<AuthQuery::Privilege> required_privileges;
-};
-
-struct QueryCacheEntry {
-  bool operator==(const QueryCacheEntry &other) const { return first == other.first; }
-  bool operator<(const QueryCacheEntry &other) const { return first < other.first; }
-  bool operator==(const uint64_t &other) const { return first == other; }
-  bool operator<(const uint64_t &other) const { return first < other; }
-
-  uint64_t first;
-  // TODO: Maybe store the query string here and use it as a key with the hash
-  // so that we eliminate the risk of hash collisions.
-  CachedQuery second;
-};
-
-struct PlanCacheEntry {
-  bool operator==(const PlanCacheEntry &other) const { return first == other.first; }
-  bool operator<(const PlanCacheEntry &other) const { return first < other.first; }
-  bool operator==(const uint64_t &other) const { return first == other; }
-  bool operator<(const uint64_t &other) const { return first < other; }
-
-  uint64_t first;
-  // TODO: Maybe store the query string here and use it as a key with the hash
-  // so that we eliminate the risk of hash collisions.
-  std::shared_ptr<CachedPlan> second;
-};
-
 /**
  * Holds data shared between multiple `Interpreter` instances (which might be
  * running concurrently).
@@ -205,7 +146,10 @@ struct PlanCacheEntry {
  * been passed to an `Interpreter` instance.
  */
 struct InterpreterContext {
-  explicit InterpreterContext(storage::Storage *db) : db(db) {}
+  explicit InterpreterContext(storage::Storage *db) : db(db) {
+    // auto triggers_acc = triggers.access();
+    // triggers_acc.insert(Trigger{"Creator", "CREATE (:CREATED)", &ast_cache, &antlr_lock});
+  }
 
   storage::Storage *db;
 
@@ -225,6 +169,9 @@ struct InterpreterContext {
 
   utils::SkipList<QueryCacheEntry> ast_cache;
   utils::SkipList<PlanCacheEntry> plan_cache;
+
+  // use a thread safe container
+  utils::SkipList<Trigger> triggers;
 };
 
 /// Function that is used to tell all active interpreters that they should stop
