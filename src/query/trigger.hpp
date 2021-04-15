e417417e48e1bb14638622c6cfb7163922b12247
antonio2368 <antonio2368@users.noreply.github.com>
2021-04-15 10:49:40 +0200

    Initial trigger definition (#133)
    * Pull out cypher query parsing logic
    
    * Define trigger structure
    
    * Run triggers before commit
    
    * Use skip list for saving triggers

---
+++ src/query/trigger.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "query/cypher_query_interpreter.hpp"
+#include "query/frontend/ast/ast.hpp"
+
+namespace query {
+struct Trigger {
+  explicit Trigger(std::string name, std::string query, utils::SkipList<QueryCacheEntry> *cache,
+                   utils::SpinLock *antlr_lock);
+
+  void Execute(utils::SkipList<PlanCacheEntry> *plan_cache, DbAccessor *dba,
+               utils::MonotonicBufferResource *execution_memory, double tsc_frequency, double max_execution_time_sec,
+               std::atomic<bool> *is_shutting_down) const;
+
+  bool operator==(const Trigger &other) const { return name_ == other.name_; }
+  // NOLINTNEXTLINE (modernize-use-nullptr)
+  bool operator<(const Trigger &other) const { return name_ < other.name_; }
+  bool operator==(const std::string &other) const { return name_ == other; }
+  // NOLINTNEXTLINE (modernize-use-nullptr)
+  bool operator<(const std::string &other) const { return name_ < other; }
+
+ private:
+  std::string name_;
+  ParsedQuery parsed_statements_;
+};
+}  // namespace query
