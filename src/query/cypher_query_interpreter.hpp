69a7f425e3de114038d4adba3ff400b078329fbe
antonio2368 <antonio2368@users.noreply.github.com>
2021-05-04 13:00:07 +0200

    Support removed vertices in the triggers (#140)

--- src/query/cypher_query_interpreter.hpp
+++ src/query/cypher_query_interpreter.hpp
@@ -129,6 +129,10 @@ class SingleNodeLogicalPlan final : public LogicalPlan {
   SymbolTable symbol_table_;
 };
 
+std::unique_ptr<LogicalPlan> MakeLogicalPlan(AstStorage ast_storage, CypherQuery *query, const Parameters &parameters,
+                                             DbAccessor *db_accessor,
+                                             const std::vector<Identifier *> &predefined_identifiers);
+
 /**
  * Return the parsed *Cypher* query's AST cached logical plan, or create and
  * cache a fresh one if it doesn't yet exist.
@@ -139,7 +143,7 @@ class SingleNodeLogicalPlan final : public LogicalPlan {
  */
 std::shared_ptr<CachedPlan> CypherQueryToPlan(uint64_t hash, AstStorage ast_storage, CypherQuery *query,
                                               const Parameters &parameters, utils::SkipList<PlanCacheEntry> *plan_cache,
-                                              DbAccessor *db_accessor, bool is_cacheable = true,
+                                              DbAccessor *db_accessor,
                                               const std::vector<Identifier *> &predefined_identifiers = {});
 
 }  // namespace query
