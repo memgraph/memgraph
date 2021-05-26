dc297705033ca7c452ef2c9dc4767e06d85b1f6b
Antonio Andelic <antonio.andelic@memgraph.io>
2021-05-26 10:20:37 +0200

    Update mgconsole commit

--- libs/setup.sh
+++ libs/setup.sh
@@ -214,7 +214,7 @@ pymgclient_tag="4f85c179e56302d46a1e3e2cf43509db65f062b3" # (2021-01-15)
 repo_clone_try_double "${primary_urls[pymgclient]}" "${secondary_urls[pymgclient]}" "pymgclient" "$pymgclient_tag"
 
 # mgconsole
-mgconsole_tag="6301b1f420d587b5defa63c34724967fa0fc6836" # (2021-05-25)
+mgconsole_tag="5377df371375a73f5d7eb1d3a839dfcfd356438d" # (2021-05-25)
 repo_clone_try_double "${primary_urls[mgconsole]}" "${secondary_urls[mgconsole]}" "mgconsole" "$mgconsole_tag"
 
 spdlog_tag="46d418164dd4cd9822cf8ca62a116a3f71569241" # (2020-12-01)
