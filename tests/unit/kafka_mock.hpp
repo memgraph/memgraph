44062b43dc43ca32c10ce1bc1008fb6a3bd3ee64
Benjamin Antal <benjamin.antal@memgraph.io>
2021-06-04 18:05:40 +0200

    Add simple unit test for consumer

---
+++ tests/unit/kafka_mock.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <librdkafka/rdkafka.h>
+#include <librdkafka/rdkafka_mock.h>
+#include <librdkafka/rdkafkacpp.h>
+
+// Based on https://github.com/edenhill/librdkafka/issues/2693
+namespace details {
+struct RdKafkaDeleter {
+  void operator()(rd_kafka_t *rd);
+};
+struct RdKafkaMockClusterDeleter {
+  void operator()(rd_kafka_mock_cluster_t *rd);
+};
+}  // namespace details
+
+using RdKafkaUniquePtr = std::unique_ptr<rd_kafka_t, details::RdKafkaDeleter>;
+using RdKafkaMockClusterUniquePtr = std::unique_ptr<rd_kafka_mock_cluster_t, details::RdKafkaMockClusterDeleter>;
+
+class KafkaClusterMock {
+ public:
+  explicit KafkaClusterMock(const std::vector<std::string> &topics);
+
+  std::string Bootstraps() const;
+  void SeedTopic(const std::string &topic_name, std::string_view message);
+
+ private:
+  RdKafkaUniquePtr rk_{nullptr};
+  RdKafkaMockClusterUniquePtr cluster_{nullptr};
+};
