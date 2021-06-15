76ffd3664ff767791ad5cfc29730737002917594
Kostas <kostaskyrim@gmail.com>
2021-06-15 18:30:27 +0300

    simplified mgp_kafka_c_api test

--- tests/unit/mgp_kafka_c_api.cpp
+++ tests/unit/mgp_kafka_c_api.cpp
@@ -3,6 +3,7 @@
 #include <exception>
 #include <iterator>
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "gtest/gtest.h"
@@ -21,7 +22,7 @@ class MockedRdKafkaMessage : public RdKafka::Message {
   explicit MockedRdKafkaMessage(std::string key, std::string payload)
       : key_(std::move(key)), payload_(std::move(payload)) {
     message_.err = rd_kafka_resp_err_t::RD_KAFKA_RESP_ERR__BEGIN;
-    message_.key = static_cast<void *>(&key_[0]);
+    message_.key = static_cast<void *>(key_.data());
     message_.key_len = key_.size();
     message_.offset = 0;
     message_.payload = static_cast<void *>(payload_.data());
@@ -44,42 +45,44 @@ class MockedRdKafkaMessage : public RdKafka::Message {
 
   RdKafka::ErrorCode err() const override { return RdKafka::ErrorCode::ERR_NO_ERROR; }
 
-  [[noreturn]] std::string errstr() const override { throw_error(); }
+  [[noreturn]] std::string errstr() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] RdKafka::Topic *topic() const override { throw_error(); }
+  [[noreturn]] RdKafka::Topic *topic() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] std::string topic_name() const override { throw_error(); }
+  [[noreturn]] std::string topic_name() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] int32_t partition() const override { throw_error(); }
+  [[noreturn]] int32_t partition() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] void *payload() const override { throw_error(); }
+  [[noreturn]] void *payload() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] size_t len() const override { throw_error(); }
+  [[noreturn]] size_t len() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] const std::string *key() const override { throw_error(); }
+  [[noreturn]] const std::string *key() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] const void *key_pointer() const override { throw_error(); }
+  [[noreturn]] const void *key_pointer() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] size_t key_len() const override { throw_error(); }
+  [[noreturn]] size_t key_len() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] int64_t offset() const override { throw_error(); }
+  [[noreturn]] int64_t offset() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] RdKafka::MessageTimestamp timestamp() const override { throw_error(); }
+  [[noreturn]] RdKafka::MessageTimestamp timestamp() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] void *msg_opaque() const override { throw_error(); }
+  [[noreturn]] void *msg_opaque() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] int64_t latency() const override { throw_error(); }
+  [[noreturn]] int64_t latency() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] Status status() const override { throw_error(); }
+  [[noreturn]] Status status() const override { ThrowIllegalCallError(); }
 
-  [[noreturn]] RdKafka::Headers *headers() override { throw_error(); }
+  [[noreturn]] RdKafka::Headers *headers() override { ThrowIllegalCallError(); }
 
-  [[noreturn]] RdKafka::Headers *headers(RdKafka::ErrorCode *err) override { throw_error(); }
+  [[noreturn]] RdKafka::Headers *headers(RdKafka::ErrorCode *err) override { ThrowIllegalCallError(); }
 
-  [[noreturn]] int32_t broker_id() const override { throw_error(); }
+  [[noreturn]] int32_t broker_id() const override { ThrowIllegalCallError(); }
 
  private:
-  [[noreturn]] void throw_error() const { throw std::logic_error("This function should not have been called"); }
+  [[noreturn]] void ThrowIllegalCallError() const {
+    throw std::logic_error("This function should not have been called");
+  }
 
   std::string key_;
   rd_kafka_t *rd_kafka_;
@@ -94,54 +97,57 @@ std::string MockedRdKafkaMessage::mocked_topic_name = "Topic1";
 class MgpApiTest : public ::testing::Test {
  public:
   using Message = integrations::kafka::Message;
-  using KMessage = MockedRdKafkaMessage;
-  MgpApiTest() : msgs_storage_(utils::NewDeleteResource()) {
-    messages_ = std::make_unique<mgp_messages>(CreateMockedBatch());
-  }
-  mgp_messages *Messages() { return messages_.get(); }
+  using KafkaMessage = MockedRdKafkaMessage;
+
+  MgpApiTest() {}
+
+  mgp_messages &Messages() { return *messages_; }
+
+ protected:
+  struct ExpectedResult {
+    const char *payload;
+    const char key;
+    const char *topic_name;
+    const size_t payload_size;
+  };
+
+  static constexpr size_t sample_size = 2;
+
+  const std::array<ExpectedResult, sample_size> expected = {ExpectedResult{"payload1", '1', "Topic1", 8},
+                                                            ExpectedResult{"payload2", '2', "Topic1", 8}};
 
  private:
   utils::pmr::vector<mgp_message> CreateMockedBatch() {
-    for (int i = 0; i < ExpectedResults::sample_size; ++i)
+    for (int i = 0; i < sample_size; ++i)
       msgs_storage_.push_back(
-          Message(std::make_unique<KMessage>(std::string(1, expected.key[i]), expected.payload[i])));
+          Message(std::make_unique<KafkaMessage>(std::string(1, expected[i].key), expected[i].payload)));
     auto v = utils::pmr::vector<mgp_message>(utils::NewDeleteResource());
     std::transform(msgs_storage_.begin(), msgs_storage_.end(), std::back_inserter(v),
                    [](auto &msgs) { return mgp_message{&msgs}; });
     return v;
   }
 
-  utils::pmr::vector<Message> msgs_storage_;
-  std::unique_ptr<mgp_messages> messages_;
-
- protected:
-  struct ExpectedResults {
-    static constexpr size_t sample_size = 2;
-    const std::array<const char *, sample_size> payload{"payload1", "payload2"};
-    const std::array<char, sample_size> key{'1', '2'};
-    const std::array<const char *, sample_size> topic_name{"Topic1", "Topic1"};
-    const std::array<size_t, sample_size> payload_size{8, 8};
-  };
-  ExpectedResults expected;
+  utils::pmr::vector<Message> msgs_storage_{utils::NewDeleteResource()};
+  std::optional<mgp_messages> messages_{std::make_optional<mgp_messages>(CreateMockedBatch())};
 };
 
 TEST_F(MgpApiTest, TEST_ALL_MGP_KAFKA_C_API) {
-  const mgp_messages *messages = Messages();
-  EXPECT_EQ(mgp_messages_size(messages), ExpectedResults::sample_size);
+  const mgp_messages &messages = Messages();
+  EXPECT_EQ(mgp_messages_size(&messages), sample_size);
   // Test for keys
-  const auto msgs = std::array<const mgp_message *, ExpectedResults::sample_size>{mgp_messages_at(messages, 0),
-                                                                                  mgp_messages_at(messages, 1)};
-  for (int i = 0; i < ExpectedResults::sample_size; ++i) {
+  const auto msgs =
+      std::array<const mgp_message *, sample_size>{mgp_messages_at(&messages, 0), mgp_messages_at(&messages, 1)};
+  for (int i = 0; i < sample_size; ++i) {
     // Test for key and key size. Key size is always 1 in this test.
     EXPECT_EQ(mgp_message_key_size(msgs[i]), 1);
-    EXPECT_EQ(*mgp_message_key(msgs[i]), expected.key[i]);
+    EXPECT_EQ(*mgp_message_key(msgs[i]), expected[i].key);
 
     // Test for payload size
-    EXPECT_EQ(mgp_message_get_payload_size(msgs[i]), expected.payload_size[i]);
+    EXPECT_EQ(mgp_message_get_payload_size(msgs[i]), expected[i].payload_size);
     // Test for payload
-    EXPECT_FALSE(std::strcmp(mgp_message_get_payload(msgs[i]), expected.payload[i]));
+    EXPECT_FALSE(std::strcmp(mgp_message_get_payload(msgs[i]), expected[i].payload));
     // Test for topic name
-    EXPECT_FALSE(std::strcmp(mgp_message_topic_name(msgs[i]), expected.topic_name[i]));
+    EXPECT_FALSE(std::strcmp(mgp_message_topic_name(msgs[i]), expected[i].topic_name));
   }
 
   // Unfortunately, we can't test timestamp here because we can't mock (as explained above)
