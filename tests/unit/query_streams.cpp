348614bef48354193cd2342e4b7a538d9e0007a9
Benjamin Antal <benjamin.antal@memgraph.io>
2021-06-16 10:15:12 +0200

    Adjust tests to work without single stream show

--- tests/unit/query_streams.cpp
+++ tests/unit/query_streams.cpp
@@ -15,6 +15,12 @@ using StreamStatus = query::StreamStatus;
 namespace {
 const static std::string kTopicName{"TrialTopic"};
 
+struct StreamCheckData {
+  std::string name;
+  StreamInfo info;
+  bool is_running;
+};
+
 std::string GetDefaultStreamName() {
   return std::string{::testing::UnitTest::GetInstance()->current_test_info()->name()};
 }
@@ -30,17 +36,7 @@ StreamInfo CreateDefaultStreamInfo() {
   };
 }
 
-void CheckStreamStatus(const StreamInfo &expected_info, const bool expected_is_running, const StreamStatus &status) {
-  // the order don't have to be strictly the same, but based on the implementation it shouldn't change
-  EXPECT_TRUE(std::equal(expected_info.topics.begin(), expected_info.topics.end(), status.info.topics.begin(),
-                         status.info.topics.end()));
-  EXPECT_EQ(expected_info.consumer_group, status.info.consumer_group);
-  EXPECT_EQ(expected_info.batch_interval, status.info.batch_interval);
-  EXPECT_EQ(expected_info.batch_size, status.info.batch_size);
-  // TODO(antaljanosbenjamin) Add proper reference once Streams supports that
-  EXPECT_EQ(expected_info.transformation_name, status.info.transformation_name);
-  EXPECT_EQ(expected_is_running, status.is_running);
-}
+StreamCheckData CreateDefaultStreamCheckData() { return {GetDefaultStreamName(), CreateDefaultStreamInfo(), false}; }
 
 }  // namespace
 
@@ -56,22 +52,49 @@ class StreamsTest : public ::testing::Test {
   std::filesystem::path streams_data_directory_{data_directory_ / "separate-dir-for-test"};
   std::optional<Streams> streams_{std::in_place, &interpreter_context_, mock_cluster_.Bootstraps(),
                                   streams_data_directory_};
+
+  void CheckStreamStatus(const StreamCheckData &check_data) {
+    const auto &stream_statuses = streams_->Show();
+    auto it = std::find_if(stream_statuses.begin(), stream_statuses.end(), [&check_data](const auto &name_and_status) {
+      return name_and_status.first == check_data.name;
+    });
+    ASSERT_NE(it, stream_statuses.end());
+    const auto &status = it->second;
+    // the order don't have to be strictly the same, but based on the implementation it shouldn't change
+    EXPECT_TRUE(std::equal(check_data.info.topics.begin(), check_data.info.topics.end(), status.info.topics.begin(),
+                           status.info.topics.end()));
+    EXPECT_EQ(check_data.info.consumer_group, status.info.consumer_group);
+    EXPECT_EQ(check_data.info.batch_interval, status.info.batch_interval);
+    EXPECT_EQ(check_data.info.batch_size, status.info.batch_size);
+    // TODO(antaljanosbenjamin) Add proper reference once Streams supports that
+    EXPECT_EQ(check_data.info.transformation_name, status.info.transformation_name);
+    EXPECT_EQ(check_data.is_running, status.is_running);
+  }
 };
 
 TEST_F(StreamsTest, SimpleStreamManagement) {
-  auto stream_info = CreateDefaultStreamInfo();
-  auto stream_name = GetDefaultStreamName();
-  streams_->Create(stream_name, stream_info);
-  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(stream_info, false, streams_->Show(stream_name)));
-  streams_->Start(stream_name);
-  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(stream_info, true, streams_->Show(stream_name)));
+  auto check_data = CreateDefaultStreamCheckData();
+  streams_->Create(check_data.name, check_data.info);
+  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(check_data));
+
+  streams_->Start(check_data.name);
+  check_data.is_running = true;
+  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(check_data));
+
   streams_->StopAll();
-  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(stream_info, false, streams_->Show(stream_name)));
+  check_data.is_running = false;
+  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(check_data));
+
   streams_->StartAll();
-  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(stream_info, true, streams_->Show(stream_name)));
-  streams_->Stop(stream_name);
-  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(stream_info, false, streams_->Show(stream_name)));
-  streams_->Drop(stream_name);
+  check_data.is_running = true;
+  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(check_data));
+
+  streams_->Stop(check_data.name);
+  check_data.is_running = false;
+  EXPECT_NO_FATAL_FAILURE(CheckStreamStatus(check_data));
+
+  streams_->Drop(check_data.name);
+  EXPECT_TRUE(streams_->Show().empty());
 }
 
 TEST_F(StreamsTest, CreateAlreadyExisting) {
@@ -96,20 +119,6 @@ TEST_F(StreamsTest, DropNotExistingStream) {
   try {
     streams_->Drop(not_existing_stream_name);
     FAIL() << "Dropping not existing stream should throw\n";
-  } catch (query::StreamsException &exception) {
-    EXPECT_EQ(exception.what(), fmt::format("Couldn't find stream '{}'", not_existing_stream_name));
-  }
-}
-
-TEST_F(StreamsTest, ShowNotExisting) {
-  const auto stream_info = CreateDefaultStreamInfo();
-  const auto stream_name = GetDefaultStreamName();
-  const std::string not_existing_stream_name{"ThisDoesn'tExists"};
-  streams_->Create(stream_name, stream_info);
-
-  try {
-    streams_->Show(not_existing_stream_name);
-    FAIL() << "Getting the status of non existing stream should throw\n";
   } catch (query::StreamsException &exception) {
     EXPECT_EQ(exception.what(), fmt::format("Couldn't find stream '{}'", not_existing_stream_name));
   }
