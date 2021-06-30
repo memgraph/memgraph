d97b07323d67b1d3d391450313dd03f948ba4aa9
Antonio Andelic <antonio.andelic@memgraph.io>
2021-06-30 15:41:47 +0200

    Increase default timeout to 10 minutes

--- src/query/config.hpp
+++ src/query/config.hpp
@@ -6,7 +6,7 @@ struct InterpreterConfig {
     bool allow_load_csv{true};
   } query;
 
-  // The default execution timeout is 3 minutes.
-  double execution_timeout_sec{180.0};
+  // The default execution timeout is 10 minutes.
+  double execution_timeout_sec{600.0};
 };
 }  // namespace query
