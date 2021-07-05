25991da3e6a793eca180a0dd12ed0715dab0738e
Benjamin Antal <benjamin.antal@memgraph.io>
2021-07-06 00:34:14 +0200

    Use bash

--- tests/e2e/streams/streams_test_runner.sh
+++ tests/e2e/streams/streams_test_runner.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 # This workaround is necessary to run in the same virtualenv as the e2e runner.py
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 python3 "$DIR/streams_tests.py"
