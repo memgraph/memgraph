d206c71548506a6923464219ae71e8262214d877
Benjamin Antal <benjamin.antal@memgraph.io>
2021-07-16 11:29:35 +0200

    Modularize streams e2e tests

---
+++ tests/e2e/streams/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
+
+from common import execute_and_fetch_all, connect, NAME
+
+# To run these test locally a running Kafka sever is necessery. The test tries
+# to connect on localhost:9092.
+
+
+@pytest.fixture(autouse=True)
+def connection():
+    connection = connect()
+    yield connection
+    cursor = connection.cursor()
+    execute_and_fetch_all(cursor, "MATCH (n) DETACH DELETE n")
+    stream_infos = execute_and_fetch_all(cursor, "SHOW STREAMS")
+    for stream_info in stream_infos:
+        execute_and_fetch_all(cursor, f"DROP STREAM {stream_info[NAME]}")
+
+
+@pytest.fixture(scope="function")
+def topics():
+    admin_client = KafkaAdminClient(
+        bootstrap_servers="localhost:9092", client_id='test')
+
+    topics = []
+    topics_to_create = []
+    for index in range(3):
+        topic = f"topic_{index}"
+        topics.append(topic)
+        topics_to_create.append(NewTopic(name=topic,
+                                num_partitions=1, replication_factor=1))
+
+    admin_client.create_topics(new_topics=topics_to_create, timeout_ms=5000)
+    yield topics
+    admin_client.delete_topics(topics=topics, timeout_ms=5000)
+
+
+@pytest.fixture(scope="function")
+def producer():
+    yield KafkaProducer(bootstrap_servers="localhost:9092")
--- tests/e2e/streams/streams_test_runner.sh
+++ tests/e2e/streams/streams_test_runner.sh
@@ -3,4 +3,4 @@
 # This workaround is necessary to run in the same virtualenv as the e2e runner.py
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-python3 "$DIR/streams_tests.py"
+python3 "$DIR/$1"
