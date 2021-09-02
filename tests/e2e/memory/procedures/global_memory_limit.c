caf58f1cf4d0c9fd33a8be609156f12b2fcf7a0e
Benjamin Antal <benjamin.antal@memgraph.io>
2021-09-02 16:00:00 +0200

    Update global memory limit e2e tests

--- tests/e2e/memory/procedures/global_memory_limit.c
+++ tests/e2e/memory/procedures/global_memory_limit.c
@@ -2,33 +2,40 @@
 
 int *gVal = NULL;
 
-// void set_error(struct mgp_result *result) { mgp_result_set_error_msg(result, "Something went wrong"); }
+void set_error(struct mgp_result *result) { mgp_result_set_error_msg(result, "Something went wrong"); }
 
-// static void procedure(const struct mgp_list *args, const struct mgp_graph *graph, struct mgp_result *result,
-//                       struct mgp_memory *memory) {
-//   struct mgp_result_record *record = mgp_result_new_record(result);
-//   if (record == NULL) return set_error(result);
+static void procedure(struct mgp_list *args, struct mgp_graph *graph, struct mgp_result *result,
+                      struct mgp_memory *memory) {
+  struct mgp_result_record *record = NULL;
+  const enum mgp_error_code new_record_err = mgp_result_new_record(result, &record);
+  if (new_record_err != MGP_ERROR_NO_ERROR) return set_error(result);
 
-//   struct mgp_value *result_msg = mgp_value_make_string("mgp_init_module allocation works", memory);
-//   if (result_msg == NULL) return set_error(result);
+  struct mgp_value *result_msg = NULL;
+  const enum mgp_error_code make_string_err =
+      mgp_value_make_string("mgp_init_module allocation works", memory, &result_msg);
+  if (make_string_err != MGP_ERROR_NO_ERROR) return set_error(result);
 
-//   int result_inserted = mgp_result_record_insert(record, "result", result_msg);
-//   mgp_value_destroy(result_msg);
-//   if (!result_inserted) return set_error(result);
-// }
+  const enum mgp_error_code result_inserted = mgp_result_record_insert(record, "result", result_msg);
+  mgp_value_destroy(result_msg);
+  if (result_inserted != MGP_ERROR_NO_ERROR) return set_error(result);
+}
 
-// int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
-//   const size_t one_gb = 1 << 30;
-//   gVal = mgp_global_alloc(one_gb);
-//   if (!gVal) return 1;
+int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
+  const size_t one_gb = 1 << 30;
+  const enum mgp_error_code alloc_err = mgp_global_alloc(one_gb, &gVal);
+  if (alloc_err != MGP_ERROR_NO_ERROR) return 1;
 
-//   struct mgp_proc *proc = mgp_module_add_read_procedure(module, "procedure", procedure);
-//   if (!proc) return 1;
+  struct mgp_proc *proc = NULL;
+  const enum mgp_error_code proc_err = mgp_module_add_read_procedure(module, "procedure", procedure, &proc);
+  if (proc_err != MGP_ERROR_NO_ERROR) return 1;
 
-//   if (!mgp_proc_add_result(proc, "result", mgp_type_string())) return 1;
+  const struct mgp_type *string_type = NULL;
+  const enum mgp_error_code string_type_err = mgp_type_string(&string_type);
+  if (string_type_err != MGP_ERROR_NO_ERROR) return 1;
+  if (mgp_proc_add_result(proc, "result", string_type) != MGP_ERROR_NO_ERROR) return 1;
 
-//   return 0;
-// }
+  return 0;
+}
 
 int mgp_shutdown_module() {
   if (gVal) mgp_global_free(gVal);
