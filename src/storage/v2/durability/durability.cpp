2f44e3f615df83eb0fc41e14e97d314c92ad451c
Antonio Andelic <antonio.andelic@memgraph.io>
2021-09-24 11:31:38 +0200

    Fix comment and log

--- src/storage/v2/durability/durability.cpp
+++ src/storage/v2/durability/durability.cpp
@@ -276,11 +276,11 @@ std::optional<RecoveryInfo> RecoverData(const std::filesystem::path &snapshot_di
               "recovered without them!");
         } else if (first_wal.from_timestamp >= *snapshot_timestamp) {
           // We recovered from a snapshot and we must have at least one WAL file
-          // whose all deltas were created before the snapshot in order to
+          // that has at least one delta that was created before the snapshot in order to
           // verify that nothing is missing from the beginning of the WAL chain.
           LOG_FATAL(
-              "You must have at least one WAL file that contains "
-              "deltas that were created before the snapshot file!");
+              "You must have at least one WAL file that contains at least one "
+              "delta that was created before the snapshot file!");
         }
       }
     }
