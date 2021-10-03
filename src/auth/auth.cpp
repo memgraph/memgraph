512412b451b81c62ac663e909c99e288e14bb9c4
Antonio Andelic <antonio.andelic@memgraph.io>
2021-10-03 10:25:11 +0200

    Add header to all cpp files

--- src/auth/auth.cpp
+++ src/auth/auth.cpp
@@ -1,3 +1,11 @@
+// Copyright 2021 Memgraph Ltd.
+//
+// Licensed as a Memgraph Enterprise file under the Memgraph Enterprise
+// License (the "License"); by using this file, you agree to be bound by the terms of the License, and you may not use
+// this file except in compliance with the License. You may obtain a copy of the License at https://memgraph.com/legal.
+//
+//
+
 #include "auth/auth.hpp"
 
 #include <cstring>
