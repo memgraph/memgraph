36d518fe12cd08a922cceab8351db850a6c4a155
Jure Bajic <jure.bajic@memgraph.io>
2021-10-15 18:02:40 +0200

    Refactor init

--- src/communication/bolt/v1/states/init.hpp
+++ src/communication/bolt/v1/states/init.hpp
@@ -12,6 +12,7 @@
 #pragma once
 
 #include <fmt/format.h>
+#include <optional>
 
 #include "communication/bolt/v1/codes.hpp"
 #include "communication/bolt/v1/state.hpp"
@@ -22,9 +23,44 @@
 
 namespace communication::bolt {
 
-namespace detail {
+namespace details {
 template <typename TSession>
-std::optional<Value> StateInitRunV1(TSession &session, const Marker marker) {
+std::optional<State> AuthenticateUser(TSession &session, Value &metadata) {
+  // Get authentication data.
+  auto &data = metadata.ValueMap();
+  if (!data.count("scheme")) {
+    spdlog::warn("The client didn't supply authentication information!");
+    return State::Close;
+  }
+  std::string username;
+  std::string password;
+  if (data["scheme"].ValueString() == "basic") {
+    if (!data.count("principal") || !data.count("credentials")) {
+      spdlog::warn("The client didn't supply authentication information!");
+      return State::Close;
+    }
+    username = data["principal"].ValueString();
+    password = data["credentials"].ValueString();
+  } else if (data["scheme"].ValueString() != "none") {
+    spdlog::warn("Unsupported authentication scheme: {}", data["scheme"].ValueString());
+    return State::Close;
+  }
+
+  // Authenticate the user.
+  if (!session.Authenticate(username, password)) {
+    if (!session.encoder_.MessageFailure(
+            {{"code", "Memgraph.ClientError.Security.Unauthenticated"}, {"message", "Authentication failure"}})) {
+      spdlog::trace("Couldn't send failure message to the client!");
+    }
+    // Throw an exception to indicate to the network stack that the session
+    // should be closed and cleaned up.
+    throw SessionClosedException("The client is not authenticated!");
+  }
+  return std::nullopt;
+}
+
+template <typename TSession>
+std::optional<Value> GetMetadataV1(TSession &session, const Marker marker) {
   if (UNLIKELY(marker != Marker::TinyStruct2)) {
     spdlog::trace("Expected TinyStruct2 marker, but received 0x{:02X}!", utils::UnderlyingCast(marker));
     spdlog::trace(
@@ -53,7 +89,7 @@ std::optional<Value> StateInitRunV1(TSession &session, const Marker marker) {
 }
 
 template <typename TSession>
-std::optional<Value> StateInitRunV4(TSession &session, const Marker marker) {
+std::optional<Value> GetMetadataV4(TSession &session, const Marker marker) {
   if (UNLIKELY(marker != Marker::TinyStruct1)) {
     spdlog::trace("Expected TinyStruct1 marker, but received 0x{:02X}!", utils::UnderlyingCast(marker));
     spdlog::trace(
@@ -80,70 +116,21 @@ std::optional<Value> StateInitRunV4(TSession &session, const Marker marker) {
 
   return metadata;
 }
-}  // namespace detail
 
-/**
- * Init state run function.
- * This function runs everything to initialize a Bolt session with the client.
- * @param session the session that should be used for the run.
- */
-template <typename Session>
-State StateInitRun(Session &session) {
-  DMG_ASSERT(!session.encoder_buffer_.HasData(), "There should be no data to write in this state");
-
-  Marker marker;
-  Signature signature;
-  if (!session.decoder_.ReadMessageHeader(&signature, &marker)) {
-    spdlog::trace("Missing header data!");
-    return State::Close;
-  }
-
-  if (UNLIKELY(signature == Signature::Noop && session.version_.major == 4 && session.version_.minor == 1)) {
-    SPDLOG_DEBUG("Received NOOP message");
-    return State::Init;
-  }
-
+template <typename TSession>
+State StateInitRunV1(TSession &session, const Marker marker, const Signature signature) {
   if (UNLIKELY(signature != Signature::Init)) {
     spdlog::trace("Expected Init signature, but received 0x{:02X}!", utils::UnderlyingCast(signature));
     return State::Close;
   }
 
-  auto maybeMetadata =
-      session.version_.major == 1 ? detail::StateInitRunV1(session, marker) : detail::StateInitRunV4(session, marker);
+  auto maybeMetadata = GetMetadataV1(session, marker);
 
   if (!maybeMetadata) {
     return State::Close;
   }
-
-  // Get authentication data.
-  std::string username;
-  std::string password;
-  auto &data = maybeMetadata->ValueMap();
-  if (!data.count("scheme")) {
-    spdlog::warn("The client didn't supply authentication information!");
-    return State::Close;
-  }
-  if (data["scheme"].ValueString() == "basic") {
-    if (!data.count("principal") || !data.count("credentials")) {
-      spdlog::warn("The client didn't supply authentication information!");
-      return State::Close;
-    }
-    username = data["principal"].ValueString();
-    password = data["credentials"].ValueString();
-  } else if (data["scheme"].ValueString() != "none") {
-    spdlog::warn("Unsupported authentication scheme: {}", data["scheme"].ValueString());
-    return State::Close;
-  }
-
-  // Authenticate the user.
-  if (!session.Authenticate(username, password)) {
-    if (!session.encoder_.MessageFailure(
-            {{"code", "Memgraph.ClientError.Security.Unauthenticated"}, {"message", "Authentication failure"}})) {
-      spdlog::trace("Couldn't send failure message to the client!");
-    }
-    // Throw an exception to indicate to the network stack that the session
-    // should be closed and cleaned up.
-    throw SessionClosedException("The client is not authenticated!");
+  if (auto result = AuthenticateUser(session, *maybeMetadata)) {
+    return result.value();
   }
 
   // Return success.
@@ -165,5 +152,81 @@ State StateInitRun(Session &session) {
   }
 
   return State::Idle;
+}
+
+template <typename TSession, int bolt_minor = 0>
+State StateInitRunV4(TSession &session, Marker marker, Signature signature) {
+  if constexpr (bolt_minor > 0) {
+    if (UNLIKELY(signature == Signature::Noop)) {
+      SPDLOG_DEBUG("Received NOOP message");
+      return State::Init;
+    }
+  }
+
+  if (UNLIKELY(signature != Signature::Init)) {
+    spdlog::trace("Expected Init signature, but received 0x{:02X}!", utils::UnderlyingCast(signature));
+    return State::Close;
+  }
+
+  auto maybeMetadata = GetMetadataV4(session, marker);
+
+  if (!maybeMetadata) {
+    return State::Close;
+  }
+  if (auto result = AuthenticateUser(session, *maybeMetadata)) {
+    return result.value();
+  }
+
+  // Return success.
+  {
+    bool success_sent = false;
+    // Neo4j's Java driver 4.1.1+ requires connection_id.
+    // The only usage in the mentioned version is for logging purposes.
+    // Because it's not critical for the regular usage of the driver
+    // we send a hardcoded value for now.
+    std::map<std::string, Value> metadata{{"connection_id", "bolt-1"}};
+    if (auto server_name = session.GetServerNameForInit(); server_name) {
+      metadata.insert({"server", *server_name});
+    }
+    success_sent = session.encoder_.MessageSuccess(metadata);
+    if (!success_sent) {
+      spdlog::trace("Couldn't send success message to the client!");
+      return State::Close;
+    }
+  }
+
+  return State::Idle;
+}
+}  // namespace details
+
+/**
+ * Init state run function.
+ * This function runs everything to initialize a Bolt session with the client.
+ * @param session the session that should be used for the run.
+ */
+template <typename TSession>
+State StateInitRun(TSession &session) {
+  DMG_ASSERT(!session.encoder_buffer_.HasData(), "There should be no data to write in this state");
+
+  Marker marker;
+  Signature signature;
+  if (!session.decoder_.ReadMessageHeader(&signature, &marker)) {
+    spdlog::trace("Missing header data!");
+    return State::Close;
+  }
+
+  switch (session.version_.major) {
+    case 1: {
+      return details::StateInitRunV1<TSession>(session, marker, signature);
+    }
+    case 4: {
+      if (session.version_.minor > 0) {
+        return details::StateInitRunV4<TSession, 1>(session, marker, signature);
+      }
+      return details::StateInitRunV4<TSession>(session, marker, signature);
+    }
+  }
+  spdlog::trace("Unsupported bolt version:{}.{})!", session.version_.major, session.version_.minor);
+  return State::Close;
 }
 }  // namespace communication::bolt
