277ee3d4dc8c6e7a58fff919bfb4de31def51678
Antonio Andelic <antonio2368@users.noreply.github.com>
2021-11-15 16:23:49 +0100

    Pulsar e2e tests (#296)

--- tests/e2e/streams/streams_owner_tests.py
+++ tests/e2e/streams/streams_owner_tests.py
@@ -32,20 +32,20 @@ def create_stream_user(cursor, stream_user):
         cursor, f"GRANT STREAM TO {stream_user}")
 
 
-def test_ownerless_stream(producer, topics, connection):
-    assert len(topics) > 0
+def test_ownerless_stream(kafka_producer, kafka_topics, connection):
+    assert len(kafka_topics) > 0
     userless_cursor = connection.cursor()
     common.execute_and_fetch_all(userless_cursor,
                                  "CREATE KAFKA STREAM ownerless "
-                                 f"TOPICS {topics[0]} "
-                                 f"TRANSFORM transform.simple")
+                                 f"TOPICS {kafka_topics[0]} "
+                                 f"TRANSFORM kafka_transform.simple")
     common.start_stream(userless_cursor, "ownerless")
     time.sleep(1)
 
     admin_user = "admin_user"
     create_admin_user(userless_cursor, admin_user)
 
-    producer.send(topics[0], b"first message").get(timeout=60)
+    kafka_producer.send(kafka_topics[0], b"first message").get(timeout=60)
     assert common.timed_wait(
         lambda: not common.get_is_running(userless_cursor, "ownerless"))
 
@@ -57,16 +57,16 @@ def test_ownerless_stream(producer, topics, connection):
     time.sleep(1)
 
     second_message = b"second message"
-    producer.send(topics[0], second_message).get(timeout=60)
-    common.check_vertex_exists_with_topic_and_payload(
-        userless_cursor, topics[0], second_message)
+    kafka_producer.send(kafka_topics[0], second_message).get(timeout=60)
+    common.kafka_check_vertex_exists_with_topic_and_payload(
+        userless_cursor, kafka_topics[0], second_message)
 
     assert len(common.execute_and_fetch_all(
         userless_cursor, "MATCH (n) RETURN n")) == 1
 
 
-def test_owner_is_shown(topics, connection):
-    assert len(topics) > 0
+def test_owner_is_shown(kafka_topics, connection):
+    assert len(kafka_topics) > 0
     userless_cursor = connection.cursor()
 
     stream_user = "stream_user"
@@ -74,15 +74,15 @@ def test_owner_is_shown(topics, connection):
     stream_cursor = get_cursor_with_user(stream_user)
 
     common.execute_and_fetch_all(stream_cursor, "CREATE KAFKA STREAM test "
-                                 f"TOPICS {topics[0]} "
-                                 f"TRANSFORM transform.simple")
+                                 f"TOPICS {kafka_topics[0]} "
+                                 f"TRANSFORM kafka_transform.simple")
 
     common.check_stream_info(userless_cursor, "test", ("test", None, None,
-        "transform.simple", stream_user, False))
+        "kafka_transform.simple", stream_user, False))
 
 
-def test_insufficient_privileges(producer, topics, connection):
-    assert len(topics) > 0
+def test_insufficient_privileges(kafka_producer, kafka_topics, connection):
+    assert len(kafka_topics) > 0
     userless_cursor = connection.cursor()
 
     admin_user = "admin_user"
@@ -95,15 +95,15 @@ def test_insufficient_privileges(producer, topics, connection):
 
     common.execute_and_fetch_all(stream_cursor,
                                  "CREATE KAFKA STREAM insufficient_test "
-                                 f"TOPICS {topics[0]} "
-                                 f"TRANSFORM transform.simple")
+                                 f"TOPICS {kafka_topics[0]} "
+                                 f"TRANSFORM kafka_transform.simple")
 
     # the stream is started by admin, but should check against the owner
     # privileges
     common.start_stream(admin_cursor, "insufficient_test")
     time.sleep(1)
 
-    producer.send(topics[0], b"first message").get(timeout=60)
+    kafka_producer.send(kafka_topics[0], b"first message").get(timeout=60)
     assert common.timed_wait(
         lambda: not common.get_is_running(userless_cursor, "insufficient_test"))
 
@@ -116,16 +116,16 @@ def test_insufficient_privileges(producer, topics, connection):
     time.sleep(1)
 
     second_message = b"second message"
-    producer.send(topics[0], second_message).get(timeout=60)
-    common.check_vertex_exists_with_topic_and_payload(
-        userless_cursor, topics[0], second_message)
+    kafka_producer.send(kafka_topics[0], second_message).get(timeout=60)
+    common.kafka_check_vertex_exists_with_topic_and_payload(
+        userless_cursor, kafka_topics[0], second_message)
 
     assert len(common.execute_and_fetch_all(
         userless_cursor, "MATCH (n) RETURN n")) == 1
 
 
-def test_happy_case(producer, topics, connection):
-    assert len(topics) > 0
+def test_happy_case(kafka_producer, kafka_topics, connection):
+    assert len(kafka_topics) > 0
     userless_cursor = connection.cursor()
 
     admin_user = "admin_user"
@@ -140,17 +140,17 @@ def test_happy_case(producer, topics, connection):
 
     common.execute_and_fetch_all(stream_cursor,
                                  "CREATE KAFKA STREAM insufficient_test "
-                                 f"TOPICS {topics[0]} "
-                                 f"TRANSFORM transform.simple")
+                                 f"TOPICS {kafka_topics[0]} "
+                                 f"TRANSFORM kafka_transform.simple")
 
     common.start_stream(stream_cursor, "insufficient_test")
     time.sleep(1)
 
     first_message = b"first message"
-    producer.send(topics[0], first_message).get(timeout=60)
+    kafka_producer.send(kafka_topics[0], first_message).get(timeout=60)
 
-    common.check_vertex_exists_with_topic_and_payload(
-        userless_cursor, topics[0], first_message)
+    common.kafka_check_vertex_exists_with_topic_and_payload(
+        userless_cursor, kafka_topics[0], first_message)
 
     assert len(common.execute_and_fetch_all(
         userless_cursor, "MATCH (n) RETURN n")) == 1
