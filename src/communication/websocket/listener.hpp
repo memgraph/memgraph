ed5af9a22f5f2226dc70363803fb748619e856b7
Antonio Andelic <antonio2368@users.noreply.github.com>
2022-01-04 12:13:48 +0100

    Add websocket server and log stream (#314)

---
+++ src/communication/websocket/listener.hpp
@@ -0,0 +1,51 @@
+// Copyright 2021 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+#pragma once
+
+#include <list>
+#include <memory>
+
+#include <spdlog/spdlog.h>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+
+#include "communication/websocket/session.hpp"
+#include "utils/spin_lock.hpp"
+#include "utils/synchronized.hpp"
+
+namespace communication::websocket {
+class Listener : public std::enable_shared_from_this<Listener> {
+  using tcp = boost::asio::ip::tcp;
+
+ public:
+  template <typename... Args>
+  static std::shared_ptr<Listener> Create(Args &&...args) {
+    return std::shared_ptr<Listener>{new Listener(std::forward<Args>(args)...)};
+  }
+
+  // Start accepting incoming connections
+  void Run();
+  void WriteToAll(std::shared_ptr<std::string> message);
+
+ private:
+  Listener(boost::asio::io_context &ioc, tcp::endpoint endpoint);
+
+  void DoAccept();
+  void OnAccept(boost::beast::error_code ec, tcp::socket socket);
+
+  boost::asio::io_context &ioc_;
+  tcp::acceptor acceptor_;
+  utils::Synchronized<std::list<std::shared_ptr<Session>>, utils::SpinLock> sessions_;
+};
+}  // namespace communication::websocket
---
+++ src/communication/websocket/session.cpp
@@ -0,0 +1,102 @@
+// Copyright 2021 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+#define BOOST_ASIO_USE_TS_EXECUTOR_AS_DEFAULT
+
+#include "communication/websocket/session.hpp"
+
+#include <boost/asio/bind_executor.hpp>
+
+#include "utils/logging.hpp"
+
+namespace communication::websocket {
+namespace {
+void LogError(boost::beast::error_code ec, const std::string_view what) {
+  spdlog::warn("Websocket session failed on {}: {}", what, ec.message());
+}
+}  // namespace
+
+void Session::Run() {
+  ws_.set_option(boost::beast::websocket::stream_base::timeout::suggested(boost::beast::role_type::server));
+
+  ws_.set_option(boost::beast::websocket::stream_base::decorator(
+      [](boost::beast::websocket::response_type &res) { res.set(boost::beast::http::field::server, "Memgraph WS"); }));
+
+  // Accept the websocket handshake
+  boost::beast::error_code ec;
+  ws_.accept(ec);
+  if (ec) {
+    return LogError(ec, "accept");
+  }
+  connected_.store(true, std::memory_order_relaxed);
+
+  // run on the strand
+  boost::asio::dispatch(strand_, [shared_this = shared_from_this()] { shared_this->DoRead(); });
+}
+
+void Session::Write(std::shared_ptr<std::string> message) {
+  if (!connected_.load(std::memory_order_relaxed)) {
+    return;
+  }
+  boost::asio::dispatch(strand_, [message = std::move(message), shared_this = shared_from_this()]() mutable {
+    shared_this->messages_.push_back(std::move(message));
+
+    if (shared_this->messages_.size() > 1) {
+      return;
+    }
+    shared_this->DoWrite();
+  });
+}
+
+bool Session::IsConnected() const { return connected_.load(std::memory_order_relaxed); }
+
+void Session::DoWrite() {
+  auto next_message = messages_.front();
+  ws_.async_write(
+      boost::asio::buffer(*next_message),
+      boost::asio::bind_executor(strand_, [message_string = std::move(next_message), shared_this = shared_from_this()](
+                                              boost::beast::error_code ec, const size_t bytes_transferred) {
+        shared_this->OnWrite(ec, bytes_transferred);
+      }));
+}
+
+void Session::OnWrite(boost::beast::error_code ec, size_t /*bytes_transferred*/) {
+  messages_.pop_front();
+
+  if (ec) {
+    return LogError(ec, "write");
+  }
+
+  if (!messages_.empty()) {
+    DoWrite();
+  }
+}
+
+void Session::DoRead() {
+  ws_.async_read(
+      buffer_, boost::asio::bind_executor(strand_, [shared_this = shared_from_this()](boost::beast::error_code ec,
+                                                                                      const size_t bytes_transferred) {
+        shared_this->OnRead(ec, bytes_transferred);
+      }));
+}
+
+void Session::OnRead(boost::beast::error_code ec, size_t /*bytes_transferred*/) {
+  if (ec == boost::beast::websocket::error::closed) {
+    messages_.clear();
+    connected_.store(false, std::memory_order_relaxed);
+    return;
+  }
+
+  buffer_.consume(buffer_.size());
+  DoRead();
+}
+
+}  // namespace communication::websocket
