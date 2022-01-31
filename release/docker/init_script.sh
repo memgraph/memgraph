f18479a3164d0da80ca8290e780c65e451c014da
jmatak <josip.matak@hotmail.com>
2022-01-31 17:37:11 +0100

    Working example of init script

--- release/docker/init_script.sh
+++ release/docker/init_script.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
+ARGS=$@
+
 _init() {
-    DATA_DIR="/tmp/lib/memgraph/init"
+    # Wait till Memgraph is started
+    cnt=0
+    max_wait=10
+    initialized=0
+    until [ $cnt -gt $max_wait ]; do
+        sleep 2
+        if [[ $(ps -u memgraph | grep memgraph) ]]; then
+            initialized=1
+            break
+        fi
+        ((cnt++))
+    done
+
+    # Return if Memgraph is not initialized in max_wait * 2 seconds
+    if [[ $initialized = 0 ]]; then
+        return
+    fi
+
+    # Initialize with Cypher data from /usr/lib/memgraph/init
+    DATA_DIR="/usr/lib/memgraph/init"
     if [[ -d "$DATA_DIR" ]]
     then
         for file in $DATA_DIR/*.cypherl; do
+            echo "Loading $file ..."
             mgconsole < $file
         done
     fi
 
 }
 
-
 _main() {
-    # TODO: Include arguments from
-    /usr/lib/memgraph/memgraph
-    # TODO: Should wait till the Memgraph has started
-    _init
+    _init &
+    /usr/lib/memgraph/memgraph $ARGS
 }
 
 _main
