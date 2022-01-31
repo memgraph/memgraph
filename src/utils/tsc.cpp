ea9f2b024a0975853b11ce931bcba70895eff2f7
Antonio Andelic <antonio.andelic@memgraph.io>
2022-01-31 17:12:16 +0100

    use lib for TSC

---
+++ src/utils/tsc.cpp
@@ -0,0 +1,25 @@
+// Copyright 2022 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+extern "C" {
+#include <librdtsc/rdtsc.h>
+}
+
+#include "utils/tsc.hpp"
+
+namespace utils {
+uint64_t ReadTSC() { return rdtsc(); }
+
+std::optional<double> GetTSCFrequency() {
+  static auto result = std::invoke([] { return rdtsc_init(); });
+  return result == 0 ? std::optional{rdtsc_get_tsc_hz()} : std::nullopt;
+}
+}  // namespace utils
