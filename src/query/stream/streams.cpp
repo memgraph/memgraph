6c2763b49231a1dfa4ce295639148b072c0fb34a
János Benjamin Antal <benjamin.antal@memgraph.io>
2022-01-31 16:09:49 +0100

    Abort the transaction in case of serialization error

--- src/query/stream/streams.cpp
+++ src/query/stream/streams.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 Memgraph Ltd.
+// Copyright 2022 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -484,6 +484,7 @@ Streams::StreamsMap::iterator Streams::CreateConsumer(StreamsMap &map, const std
         result.rows.clear();
         break;
       } catch (const query::TransactionSerializationException &e) {
+        interpreter->Abort();
         if (i == total_retries) {
           throw;
         }
