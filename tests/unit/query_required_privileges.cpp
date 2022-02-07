609d75e427d78bd64a1f16d6154999aa42a7bf68
Antonio Andelic <antonio.andelic@memgraph.io>
2022-02-07 11:10:22 +0100

    Add tests for show version query

--- tests/unit/query_required_privileges.cpp
+++ tests/unit/query_required_privileges.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 Memgraph Ltd.
+// Copyright 2022 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -185,4 +185,9 @@ TEST_F(TestPrivilegeExtractor, StreamQuery) {
 TEST_F(TestPrivilegeExtractor, SettingQuery) {
   auto *query = storage.Create<SettingQuery>();
   EXPECT_THAT(GetRequiredPrivileges(query), UnorderedElementsAre(AuthQuery::Privilege::CONFIG));
+}
+
+TEST_F(TestPrivilegeExtractor, ShowVersion) {
+  auto *query = storage.Create<VersionQuery>();
+  EXPECT_THAT(GetRequiredPrivileges(query), UnorderedElementsAre(AuthQuery::Privilege::STATS));
 }
