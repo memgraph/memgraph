4d861b30f65c8e05f7424decb5c0420d9a764dd0
jeremy <jeremy.bailleux@memgraph.io>
2022-06-09 17:51:07 +0200

    Expanding test to cover cases when replicas are droped + killed

--- tests/e2e/replication/show.py
+++ tests/e2e/replication/show.py
@@ -9,9 +9,14 @@
 # by the Apache License, Version 2.0, included in the file
 # licenses/APL.txt.
 
+from pickle import FALSE
 import sys
 
+import os
 import pytest
+import signal
+import time
+
 from common import execute_and_fetch_all
 
 
@@ -27,18 +32,51 @@ def test_show_replication_role(port, role, connection):
 
 
 def test_show_replicas(connection):
+    # Goal of this test is to check the SHOW REPLICAS command.
+    # 1/ We check that all replicas have the correct state: they should all be ready.
+    # 2/ We drop one replica. It should not appear anymore in the SHOW REPLICAS command.
+    # 3/ We kill another replica. It should become invalid in the SHOW REPLICAS command.
     cursor = connection(7687, "main").cursor()
+    EXPECTED_COLUMN_NAMES = {"name", "socket_address", "sync_mode", "timeout", "state"}
+
+    # 1/
     actual_data = set(execute_and_fetch_all(cursor, "SHOW REPLICAS;"))
 
     expected_column_names = {"name", "socket_address", "sync_mode", "timeout", "state"}
     actual_column_names = {x.name for x in cursor.description}
-    assert expected_column_names == actual_column_names
-
+    assert EXPECTED_COLUMN_NAMES == actual_column_names
     expected_data = {
         ("replica_1", "127.0.0.1:10001", "sync", 0, "ready"),
         ("replica_2", "127.0.0.1:10002", "sync", 1.0, "ready"),
         ("replica_3", "127.0.0.1:10003", "async", None, "ready"),
     }
+    assert expected_data == actual_data
+
+    # 2/
+    execute_and_fetch_all(cursor, "DROP REPLICA replica_2")
+    actual_data = set(execute_and_fetch_all(cursor, "SHOW REPLICAS;"))
+    expected_data = {
+        ("replica_1", "127.0.0.1:10001", "sync", 0, "ready"),
+        ("replica_3", "127.0.0.1:10003", "async", None, "ready"),
+    }
+    assert expected_data == actual_data
+
+    # 3/
+    for line in os.popen("ps ax | grep -E '7688|7690' | grep -E 'replica1.log|replica3.log' | grep -v grep"):
+        try:
+            fields = line.split()
+            pid = fields[0]
+            os.kill(int(pid), signal.SIGKILL)
+        except OSError as ex:
+            assert FALSE
+
+    time.sleep(2)
+    actual_data = set(execute_and_fetch_all(cursor, "SHOW REPLICAS;"))
+    expected_data = {
+        ("replica_1", "127.0.0.1:10001", "sync", 0, "invalid"),
+        ("replica_3", "127.0.0.1:10003", "async", None, "invalid"),
+    }
+    print(actual_data)
     assert expected_data == actual_data
 
 
