51371398ce0040827a5194e4d83872401cdfa384
Tyler Neely <tylerneely@gmail.com>
2022-07-27 17:27:11 +0200

    Update src/io/future.hpp
    Co-authored-by: János Benjamin Antal <antaljanosbenjamin@users.noreply.github.com>

--- src/io/future.hpp
+++ src/io/future.hpp
@@ -148,11 +148,13 @@ class Future {
     consumed_or_moved_ = old.consumed_or_moved_;
     old.consumed_or_moved_ = true;
   }
+
   Future &operator=(Future &&old) {
-    shared_ = std::move(old.shared_);
     MG_ASSERT(!old.consumed_or_moved_, "Future moved from after already being moved from or consumed.");
+    shared_ = std::move(old.shared_);
     old.consumed_or_moved_ = true;
   }
+
   Future(const Future &) = delete;
   Future &operator=(const Future &) = delete;
   ~Future() = default;
