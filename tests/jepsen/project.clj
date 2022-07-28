17d20316666636532cc565d4427a405fcbc5a3be
jeremy <jeremy.bailleux@memgraph.io>
2022-07-28 14:36:57 +0200

    Revert jepsen version

--- tests/jepsen/project.clj
+++ tests/jepsen/project.clj
@@ -5,7 +5,7 @@
             :url "https://github.com/memgraph/memgraph/blob/master/release/LICENSE_ENTERPRISE.md"}
   :main jepsen.memgraph.core
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.2.4-SNAPSHOT"]
+                 [jepsen "0.2.1-SNAPSHOT"]
                  [gorillalabs/neo4j-clj "4.1.0"]]
   :profiles {:test {:dependencies [#_[org.neo4j.test/neo4j-harness "4.1.0"]]}}
   :repl-options {:init-ns jepsen.memgraph.core})
