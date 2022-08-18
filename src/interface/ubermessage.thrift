b5cff914c0fd984efa6017bbd2a6886bd33397d9
Tyler Neely <t@jujit.su>
2022-08-18 09:56:13 +0000

    Check-in ubermessage-related thrift specs

---
+++ src/interface/ubermessage.thrift
@@ -0,0 +1,47 @@
+//include "address.thrift"
+
+struct HeartbeatRequest {
+  1: bool test;
+}
+
+struct HeartbeatResponse {
+  1: bool test;
+}
+
+struct ScanAllRequest{
+  1: bool test;
+}
+
+struct ScanAllResponse {
+  1: bool test;
+}
+
+union ToStorageEngine{
+  1: ScanAllRequest scan_all_request;
+  2: HeartbeatRequest heartbeat_request;
+}
+
+union ToQueryEngine {
+  1: ScanAllResponse scan_all_response;
+}
+
+union ToCoordinator {
+  1: HeartbeatResponse heartbeat_response;
+}
+
+union HighLevelUnion {
+    1: ToStorageEngine to_storage_engine;
+    2: ToQueryEngine to_query_engine;
+    3: ToCoordinator to_coordinator;
+}
+
+struct UberMessage {
+    //1: address.Address to_address;
+    //2: address.Address from_address;
+    3: i64 request_id;
+    4: HighLevelUnion high_level_union;
+}
+
+service UberServer {
+    oneway void ReceiveUberMessage(1: UberMessage uber_message)
+}
