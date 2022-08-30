32a1d3958ab4a8cf4fc5809c5fb44e65ba07b956
Josip Mrden <josip.mrden@memgraph.io>
2022-08-30 15:35:12 +0200

    Added integration tests for update query modules

---
+++ tests/e2e/lba_procedures/procedures/update.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Memgraph Ltd.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+# License, and you may not use this file except in compliance with the Business Source License.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0, included in the file
+# licenses/APL.txt.
+
+import mgp
+
+
+@mgp.write_proc
+def set_property(ctx: mgp.ProcCtx, object: mgp.Any) -> mgp.Record():
+    object.properties.set("prop", 2)
+    return mgp.Record()
