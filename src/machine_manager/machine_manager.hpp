4694beb30daa98c24ded18b3c4d16b31de836370
Tyler Neely <t@jujit.su>
2022-08-31 15:24:34 +0000

    Check in mostly skeletal MachineManager::Run method

--- src/machine_manager/machine_manager.hpp
+++ src/machine_manager/machine_manager.hpp
@@ -24,6 +24,7 @@ using memgraph::io::rsm::RsmClient;
 /// The MachineManager is responsible for:
 /// * starting the entire system and ensuring that high-level
 ///   operational requirements continue to be met
+/// * acting as a machine's single caller of Io::Receive
 /// * routing incoming messages from the Io interface to the
 ///   appropriate Coordinator or to the StorageManager
 ///   (it's not necessary to route anything in this layer
@@ -36,11 +37,35 @@ using memgraph::io::rsm::RsmClient;
 template <typename IoImpl>
 class MachineManager {
   Io<IoImpl> io_;
+  Coordinator coordinator_;
+  ShardManager shard_manager_;
 
  public:
   MachineManager(Io<IoImpl> io, MachineConfig config) : io_(io) {
     for (const auto &initial_shard : config.initial_shards) {
       // TODO(tyler) initialize shard
+    }
+  }
+
+ private:
+  void Run() {
+    Time last_cron = io_.Now();
+
+    while (!io_.ShouldShutDown()) {
+      const auto now = io_.Now();
+
+      /*
+      auto request_result =
+          io_.template ReceiveWithTimeout<ReadRequest<ReadOperation>, AppendRequest<WriteOperation>, AppendResponse,
+                                          WriteRequest<WriteOperation>, VoteRequest, VoteResponse>(receive_timeout);
+      if (request_result.HasError()) {
+        continue;
+      }
+
+      auto request = std::move(request_result.GetValue());
+
+      Handle(std::move(request.message), request.request_id, request.from_address);
+      */
     }
   }
 };
