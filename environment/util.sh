689707ab7429797f163460fe4bebc711bb01c7ee
Jure Bajic <jure.bajic@memgraph.com>
2022-10-06 15:55:23 +0200

    Fix architecture check (#583)

--- environment/util.sh
+++ environment/util.sh
@@ -22,7 +22,7 @@ check_architecture() {
     for arch in "$@"; do
         if [ "$(architecture)" = "$arch" ]; then
             echo "The right architecture!"
-            exit 0
+            return 0
         fi
     done
     echo "Not the right architecture!"
