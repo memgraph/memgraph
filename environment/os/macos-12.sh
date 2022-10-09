795845ccf741b452c57cc072e6c04f0218ef8e9e
Marko Budiselic <marko.budiselic@memgraph.com>
2022-10-09 09:28:31 +0200

    Add more stuff to the env mac

--- environment/os/macos-12.sh
+++ environment/os/macos-12.sh
@@ -6,5 +6,18 @@ brew install clisp sbcl
 brew install boost gflags fmt jemalloc openssl
 brew install openssl@1.1
 
-# TODO(gitbuda): memgraph::utils::SpinLock -> complete rewrite
-# TODO(gitbuda): memgraph::utils::AsyncTimer -> complete rewrite
+brew install cyrus-sasl
+# cyrus-sasl is keg-only, which means it was not symlinked into /opt/homebrew,
+# because macOS already provides this software and installing another version in
+# parallel can cause all kinds of trouble.
+# If you need to have cyrus-sasl first in your PATH, run:
+#   echo 'export PATH="/opt/homebrew/opt/cyrus-sasl/sbin:$PATH"' >> ~/.zshrc
+# For compilers to find cyrus-sasl you may need to set:
+#   export LDFLAGS="-L/opt/homebrew/opt/cyrus-sasl/lib"
+#   export CPPFLAGS="-I/opt/homebrew/opt/cyrus-sasl/include"
+
+# TODO(gitbuda): memgraph::utils::SpinLock
+# TODO(gitbuda): memgraph::utils::AsyncTimer
+# TODO(gitbuda): memgraph::utils::RWLock
+# TODO(gitbuda): memgraph::utils::ThreadSetName
+# TODO(gitbuda): RocksDB 7.7.2 compiles fine
