112bb88ffe3313bfc09c385a54deed81ee3ea495
antejavor <ante.javor@memgraph.io>
2022-11-03 16:03:26 +0100

    Add dataset option, and all configurations

--- tests/mgbench/graph_bench.py
+++ tests/mgbench/graph_bench.py
@@ -15,36 +15,116 @@ def parse_arguments():
         help="Forward name and paths to vendors binary"
         "Example: --vendor memgraph /path/to/binary --vendor neo4j /path/to/binary",
     )
+    parser.add_argument(
+        "--dataset-size",
+        default="small",
+        choices=["small", "medium", "large"],
+        help="Pick a dataset size (small, medium, large)",
+    )
     args = parser.parse_args()
 
     return args
 
 
-def run_full_benchmarks(vendor, binary):
-    subprocess.run(
-        args=[
-            "python3",
-            "benchmark.py",
-            "--vendor-binary",
-            binary,
-            "--vendor-name",
-            vendor,
-            "--num-workers-for-benchmark",
-            "12",
+def run_full_benchmarks(vendor, binary, dataset_size):
+    # All run configurations
+    configurations = [
+        # Basic full group test cold
+        [
             "--export-results",
-            "out.json",
-            "pokec/small/mixed/*/*",
+            vendor + "_cold.json",
+        ],
+        # Basic full group test warm
+        [
+            "--export-results",
+            vendor + "_warm.json",
+            "--warmup-run",
+        ],
+        # Mixed workload per query, 30% write cold
+        [
+            "--export-results",
+            vendor + "_cold_mixed_per_query.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
+            "--mixed-workload",
+            "100 30 0 0 0 70",
+        ],
+        # Mixed workload per query, 30% write warm
+        [
+            "--export-results",
+            vendor + "_warm_mixed_per_query.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
             "--warmup-run",
             "--mixed-workload",
-            "100",
-            "30",
-            "0",
-            "0",
-            "0",
-            "70",
+            "100 30 0 0 0 70",
         ],
-        check=True,
-    )
+        # Mixed workload per full group 30% write, 70% read cold
+        [
+            "--export-results",
+            vendor + "_cold_mixed_group.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
+            "--mixed-workload",
+            "100 30 70 0 0",
+        ],
+        # Mixed workload per full group 30% write, 70% read warm
+        [
+            "--export-results",
+            vendor + "_warm_mixed_group.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
+            "--warmup-run",
+            "--mixed-workload",
+            "100 30 70 0 0",
+        ],
+        # Mixed workload per full group 50% write, 50% read cold
+        [
+            "--export-results",
+            vendor + "_warm_mixed_group.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
+            "--mixed-workload",
+            "100 50 50 0 0",
+        ],
+        # Mixed workload per full group 50% write, 50% read warm
+        [
+            "--export-results",
+            vendor + "_warm_mixed_group.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
+            "--warmup-run",
+            "--mixed-workload",
+            "100 50 50 0 0",
+        ],
+        # Mixed workload per full group 70% write, 30% read cold
+        [
+            "--export-results",
+            vendor + "_warm_mixed_group.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
+            "--mixed-workload",
+            "100 70 30 0 0",
+        ],
+        # Mixed workload per full group 70% write, 30% read cold
+        [
+            "--export-results",
+            vendor + "_warm_mixed_group.json",
+            "pokec/" + dataset_size + "/mixed/*/*",
+            "--warmup-run",
+            "--mixed-workload",
+            "100 70 30 0 0",
+        ],
+    ]
+
+    default_args = [
+        "python3",
+        "benchmark.py",
+        "--vendor-binary",
+        binary,
+        "--vendor-name",
+        vendor,
+        "--num-workers-for-benchmark",
+        "12",
+        "pokec/small/mixed/*/*",
+    ]
+
+    for config in configurations:
+        default_args.extend(config)
+        subprocess.run(args=default_args, check=True)
 
 
 if __name__ == "__main__":
@@ -53,7 +133,7 @@ if __name__ == "__main__":
     for vendor_name, vendor_binary in args.vendor:
         path = Path(vendor_binary)
         if vendor_name.lower() in vendor_names and path.is_file():
-            run_full_benchmarks(vendor_name, vendor_binary)
+            run_full_benchmarks(vendor_name, vendor_binary, args.dataset_size)
         else:
             raise Exception(
                 "Check that vendor: {} is supported and you are passing right path: {} to binary.".format(
