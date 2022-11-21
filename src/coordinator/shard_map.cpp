0f66ae31dd831f222984588efc3e6ecf520134f3
Tyler Neely <t@jujit.su>
2022-11-21 11:11:39 +0000

    Use explicit unsigned integer in right shift operation

--- src/coordinator/shard_map.cpp
+++ src/coordinator/shard_map.cpp
@@ -9,7 +9,6 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-#include <cstdint>
 #include <optional>
 #include <unordered_map>
 #include <vector>
@@ -258,13 +257,13 @@ boost::uuids::uuid NewShardUuid(uint64_t shard_id) {
                             0,
                             0,
                             0,
-                            static_cast<unsigned char>(shard_id >> UINT8_C(56)),
-                            static_cast<unsigned char>(shard_id >> UINT8_C(48)),
-                            static_cast<unsigned char>(shard_id >> UINT8_C(40)),
-                            static_cast<unsigned char>(shard_id >> UINT8_C(32)),
-                            static_cast<unsigned char>(shard_id >> UINT8_C(24)),
-                            static_cast<unsigned char>(shard_id >> UINT8_C(16)),
-                            static_cast<unsigned char>(shard_id >> UINT8_C(8)),
+                            static_cast<unsigned char>(shard_id >> 56u),
+                            static_cast<unsigned char>(shard_id >> 48u),
+                            static_cast<unsigned char>(shard_id >> 40u),
+                            static_cast<unsigned char>(shard_id >> 32u),
+                            static_cast<unsigned char>(shard_id >> 24u),
+                            static_cast<unsigned char>(shard_id >> 16u),
+                            static_cast<unsigned char>(shard_id >> 8u),
                             static_cast<unsigned char>(shard_id)};
 }
 
