00a4127e4e563eac97d4dda487fceac9ebab7dce
jeremy <jeremy.bailleux@memgraph.io>
2022-11-29 17:15:52 +0100

    Remove incorrect = default

--- src/query/v2/multiframe.hpp
+++ src/query/v2/multiframe.hpp
@@ -185,7 +185,7 @@ class ValidFramesConsumer {
  public:
   explicit ValidFramesConsumer(MultiFrame &multiframe);
 
-  ~ValidFramesConsumer() = default;
+  ~ValidFramesConsumer();
   ValidFramesConsumer(const ValidFramesConsumer &other) = delete;                 // copy constructor
   ValidFramesConsumer(ValidFramesConsumer &&other) noexcept = delete;             // move constructor
   ValidFramesConsumer &operator=(const ValidFramesConsumer &other) = delete;      // copy assignment
