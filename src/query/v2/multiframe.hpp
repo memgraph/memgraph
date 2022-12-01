452722f4f8067831996a2d576eb48ec62b98ad90
Jeremy B <97525434+42jeremy@users.noreply.github.com>
2022-12-01 14:52:30 +0100

    Update src/query/v2/multiframe.hpp
    Co-authored-by: Kostas Kyrimis  <kostaskyrim@gmail.com>

--- src/query/v2/multiframe.hpp
+++ src/query/v2/multiframe.hpp
@@ -159,7 +159,7 @@ class ValidFramesModifier {
     Iterator &operator++() {
       do {
         ptr_++;
-      } while (*this != iterator_wrapper_.end() && !this->ptr_->IsValid());
+      } while (*this != iterator_wrapper_.end() && ptr_->IsValid());
 
       return *this;
     }
