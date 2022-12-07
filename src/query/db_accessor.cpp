40142bff156d2a83c5f84be9e8a753c07d610e43
niko4299 <niko.krvavica@gmail.com>
2022-12-07 17:06:48 +0100

    Pr fix

--- src/query/db_accessor.cpp
+++ src/query/db_accessor.cpp
@@ -115,7 +115,7 @@ auto SubgraphVertexAccessor::OutEdges(storage::View view) const -> decltype(impl
   auto maybe_edges = impl_.impl_.OutEdges(view, {});
   if (maybe_edges.HasError()) return maybe_edges.GetError();
   auto edges = std::move(*maybe_edges);
-  auto &graph_edges = graph_->edges();
+  const auto &graph_edges = graph_->edges();
 
   std::vector<storage::EdgeAccessor> filteredOutEdges;
   for (auto &edge : edges) {
@@ -132,7 +132,7 @@ auto SubgraphVertexAccessor::InEdges(storage::View view) const -> decltype(impl_
   auto maybe_edges = impl_.impl_.InEdges(view, {});
   if (maybe_edges.HasError()) return maybe_edges.GetError();
   auto edges = std::move(*maybe_edges);
-  auto &graph_edges = graph_->edges();
+  const auto &graph_edges = graph_->edges();
 
   std::vector<storage::EdgeAccessor> filteredOutEdges;
   for (auto &edge : edges) {
