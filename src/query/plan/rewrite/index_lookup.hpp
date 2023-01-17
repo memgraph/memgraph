8c8b92fefc2aec79b9093afbe85aff7043a6c263
Josip Mrden <josip.mrden@memgraph.io>
2023-01-17 16:11:07 +0100

    Added fix for rewriting query plan to use indexes on optional match and foreach

--- src/query/plan/rewrite/index_lookup.hpp
+++ src/query/plan/rewrite/index_lookup.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Memgraph Ltd.
+// Copyright 2023 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -444,7 +444,7 @@ class IndexLookupRewriter final : public HierarchicalLogicalOperatorVisitor {
 
   bool PreVisit(Foreach &op) override {
     prev_ops_.push_back(&op);
-    return false;
+    return true;
   }
 
   bool PostVisit(Foreach &) override {
