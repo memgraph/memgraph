7fb828bca38ab8e8929bc40494255f70645797b7
János Benjamin Antal <benjamin.antal@memgraph.io>
2023-01-17 20:32:00 +0100

    Update outdated comments

--- src/query/v2/plan/operator.cpp
+++ src/query/v2/plan/operator.cpp
@@ -217,9 +217,7 @@ class DistributedCreateNodeCursor : public Cursor {
     msgs::NewVertex rqst;
     MG_ASSERT(!node_info_.labels.empty(), "Cannot determine primary label");
     const auto primary_label = node_info_.labels[0];
-    // TODO(jbajic) Fix properties not send,
-    // suggestion: ignore distinction between properties and primary keys
-    // since schema validation is done on storage side
+    // TODO(jbajic) Send also the properties that are not part of primary key
     ExpressionEvaluator evaluator(&frame, context.symbol_table, context.evaluation_context, nullptr,
                                   storage::v3::View::NEW);
     if (const auto *node_info_properties = std::get_if<PropertiesMapList>(&node_info_.properties)) {
@@ -272,9 +270,7 @@ class DistributedCreateNodeCursor : public Cursor {
       MG_ASSERT(!node_info_.labels.empty(), "Cannot determine primary label");
       const auto primary_label = node_info_.labels[0];
       MG_ASSERT(context.request_router->IsPrimaryLabel(primary_label), "First label has to be a primary label!");
-      // TODO(jbajic) Fix properties not send,
-      // suggestion: ignore distinction between properties and primary keys
-      // since schema validation is done on storage side
+      // TODO(jbajic) Send also the properties that are not part of primary key
       ExpressionEvaluator evaluator(&frame, context.symbol_table, context.evaluation_context, nullptr,
                                     storage::v3::View::NEW);
       if (const auto *node_info_properties = std::get_if<PropertiesMapList>(&node_info_.properties)) {
