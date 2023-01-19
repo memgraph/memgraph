e7f10ec8f482d7c99d94b203ecdff54ad0f2a21c
János Benjamin Antal <benjamin.antal@memgraph.io>
2023-01-19 17:44:03 +0100

    Remove duplicated definition

--- src/query/v2/multiframe.cpp
+++ src/query/v2/multiframe.cpp
@@ -52,10 +52,6 @@ bool MultiFrame::HasInvalidFrame() const noexcept {
   return std::any_of(frames_.rbegin(), frames_.rend(), [](const auto &frame) { return !frame.IsValid(); });
 }
 
-bool MultiFrame::HasInvalidFrame() const noexcept {
-  return std::any_of(frames_.begin(), frames_.end(), [](auto &frame) { return !frame.IsValid(); });
-}
-
 // NOLINTNEXTLINE (bugprone-exception-escape)
 void MultiFrame::DefragmentValidFrames() noexcept {
   /*
