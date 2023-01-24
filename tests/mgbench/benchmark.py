a91830c4916f0f7d990762ed84c91a0764c8d0f4
Marko Budiselic <marko.budiselic@memgraph.com>
2023-01-24 15:53:45 +0100

    Additionl --datasets-path help description note

--- tests/mgbench/benchmark.py
+++ tests/mgbench/benchmark.py
@@ -109,7 +109,10 @@ parser.add_argument("--no-properties-on-edges", action="store_true", help="disab
 parser.add_argument("--bolt-port", default=7687, help="memgraph bolt port")
 
 parser.add_argument(
-    "--datasets-path", default="datasets", help="path to a single Python file where additional datasets are located"
+    "--datasets-path",
+    default="datasets",
+    help="path to a single Python file where additional datasets are located"
+    "NOTE: give the file unique name compared to the core module names",
 )
 parser.add_argument("--test-system-args", default="")
 
