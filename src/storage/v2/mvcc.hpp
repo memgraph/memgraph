85d1acce1ad798688440f5009d71629838424a40
antoniofilipovic <filipovicantonio1998@gmail.com>
2023-02-08 16:43:42 +0100

    remove deltas completely

--- src/storage/v2/mvcc.hpp
+++ src/storage/v2/mvcc.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Memgraph Ltd.
+// Copyright 2023 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -104,31 +104,31 @@ inline Delta *CreateDeleteObjectDelta(Transaction *transaction) {
 /// @throw std::bad_alloc
 template <typename TObj, class... Args>
 inline void CreateAndLinkDelta(Transaction *transaction, TObj *object, Args &&...args) {
-  transaction->EnsureCommitTimestampExists();
-  auto delta = &transaction->deltas.emplace_back(std::forward<Args>(args)..., transaction->commit_timestamp.get(),
-                                                 transaction->command_id);
+  // transaction->EnsureCommitTimestampExists();
+  // auto delta = &transaction->deltas.emplace_back(std::forward<Args>(args)..., transaction->commit_timestamp.get(),
+  //                                                transaction->command_id);
 
-  // The operations are written in such order so that both `next` and `prev`
-  // chains are valid at all times. The chains must be valid at all times
-  // because garbage collection (which traverses the chains) is done
-  // concurrently (as well as other execution threads).
+  // // The operations are written in such order so that both `next` and `prev`
+  // // chains are valid at all times. The chains must be valid at all times
+  // // because garbage collection (which traverses the chains) is done
+  // // concurrently (as well as other execution threads).
 
-  // 1. We need to set the next delta of the new delta to the existing delta.
-  delta->next.store(object->delta, std::memory_order_release);
-  // 2. We need to set the previous delta of the new delta to the object.
-  delta->prev.Set(object);
-  // 3. We need to set the previous delta of the existing delta to the new
-  // delta. After this point the garbage collector will be able to see the new
-  // delta but won't modify it until we are done with all of our modifications.
-  if (object->delta) {
-    object->delta->prev.Set(delta);
-  }
-  // 4. Finally, we need to set the object's delta to the new delta. The garbage
-  // collector and other transactions will acquire the object lock to read the
-  // delta from the object. Because the lock is held during the whole time this
-  // modification is being done, everybody else will wait until we are fully
-  // done with our modification before they read the object's delta value.
-  object->delta = delta;
+  // // 1. We need to set the next delta of the new delta to the existing delta.
+  // delta->next.store(object->delta, std::memory_order_release);
+  // // 2. We need to set the previous delta of the new delta to the object.
+  // delta->prev.Set(object);
+  // // 3. We need to set the previous delta of the existing delta to the new
+  // // delta. After this point the garbage collector will be able to see the new
+  // // delta but won't modify it until we are done with all of our modifications.
+  // if (object->delta) {
+  //   object->delta->prev.Set(delta);
+  // }
+  // // 4. Finally, we need to set the object's delta to the new delta. The garbage
+  // // collector and other transactions will acquire the object lock to read the
+  // // delta from the object. Because the lock is held during the whole time this
+  // // modification is being done, everybody else will wait until we are fully
+  // // done with our modification before they read the object's delta value.
+  // object->delta = delta;
 }
 
 }  // namespace memgraph::storage
