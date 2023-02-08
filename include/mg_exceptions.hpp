c4c5f8f47c2c4c7a93516f8efa46c0f11164a53a
Marko Budiselic <marko.budiselic@memgraph.com>
2023-02-08 12:07:30 +0100

    Add more info to the query modules error messages

--- include/mg_exceptions.hpp
+++ include/mg_exceptions.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Memgraph Ltd.
+// Copyright 2023 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -13,6 +13,8 @@
 
 #include <exception>
 #include <iostream>
+
+// TODO(gitbuda): Add more info how to deal with exceptions.
 
 namespace mg_exception {
 struct NotEnoughMemoryException : public std::exception {
