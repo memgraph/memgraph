a4e46b5945b65361265e204d6186d8f054b1a4db
Marko Budiselic <marko.budiselic@memgraph.com>
2023-02-09 17:32:13 +0100

    Add init impl

--- import/n2mg_cypherl.sh
+++ import/n2mg_cypherl.sh
@@ -1,3 +1,23 @@
-!/bin/bash
+#!/bin/bash -e
 
-echo "TODO(gitbuda): Write the magic"
+# TODO(gitbuda): Test with mgconsole
+# TODO(gitbuda): Add help and colors!
+
+INPUT="$1"
+OUTPUT="$2"
+
+echo ""
+echo "NOTE: BEGIN and COMMIT are required because variables share the same name (e.g. row)"
+echo "NOTE: CONSTRAINTS are just skipped -> please create them manually if needed"
+echo ""
+
+sed -e 's/^:begin/BEGIN/g; s/^:commit/COMMIT/g;' \
+    -e '/^CALL/d; /^SCHEMA AWAIT/d;' \
+    -e 's/CREATE RANGE INDEX FOR (n:/CREATE INDEX ON :/g;' \
+    -e 's/) ON (n./(/g;' \
+    -e '/^CREATE CONSTRAINT/d;' "$INPUT" > "$OUTPUT"
+
+echo ""
+echo "DONE! Please find Memgraph compatible cypherl|.cypher file under $OUTPUT"
+echo ""
+echo "Please import data by executing \`cat $OUTPUT | mgconsole\`"
