bccd8d71655f0b0e20f9942ad4355c7692f67ba1
Marko Budiselic <marko.budiselic@memgraph.com>
2023-02-12 18:30:48 +0100

    Remove duplication and add a note

--- include/mg_exceptions.hpp
+++ include/mg_exceptions.hpp
@@ -15,45 +15,70 @@
 #include <iostream>
 
 // TODO(gitbuda): Add more info how to deal with exceptions.
+// TODO(gitbuda): An issue here is that the message.hpp also has to be shipped.
+// fmt dependency is a further problem.
+// PROPOSAL: Format all manually to avoid any additional client complexity.
+#include "utils/message.hpp"
 
 namespace mg_exception {
+
 struct NotEnoughMemoryException : public std::exception {
   const char *what() const throw() { return "Not enough memory!"; }
 };
+
 struct UnknownException : public std::exception {
   const char *what() const throw() { return "Unknown exception!"; }
 };
+
 struct AllocationException : public std::exception {
-  const char *what() const throw() { return "Could not allocate memory!"; }
+  AllocationException()
+      : message_(memgraph::utils::MessageWithLink("Could not allocate memory.",
+                                                  "https://memgraph.com/docs/memgraph/reference-guide/"
+                                                  "memory-control#controlling-the-memory-usage-of-a-procedure")) {}
+  const char *what() const throw() { return message_.c_str(); }
+
+ private:
+  std::string message_;
 };
+
 struct InsufficientBufferException : public std::exception {
   const char *what() const throw() { return "Buffer is not sufficient to process procedure!"; }
 };
+
 struct OutOfRangeException : public std::exception {
   const char *what() const throw() { return "Index out of range!"; }
 };
+
 struct LogicException : public std::exception {
   const char *what() const throw() { return "Logic exception, check the procedure signature!"; }
 };
+
 struct DeletedObjectException : public std::exception {
   const char *what() const throw() { return "Object is deleted!"; }
 };
+
 struct InvalidArgumentException : public std::exception {
   const char *what() const throw() { return "Invalid argument!"; }
 };
+
 struct InvalidIDException : public std::exception {
   const char *what() const throw() { return "Invalid ID!"; }
 };
+
 struct KeyAlreadyExistsException : public std::exception {
   const char *what() const throw() { return "Key you are trying to set already exists!"; }
 };
+
 struct ImmutableObjectException : public std::exception {
   const char *what() const throw() { return "Object you are trying to change is immutable!"; }
 };
+
 struct ValueConversionException : public std::exception {
   const char *what() const throw() { return "Error in value conversion!"; }
 };
+
 struct SerializationException : public std::exception {
   const char *what() const throw() { return "Error in serialization!"; }
 };
+
 }  // namespace mg_exception
--- include/mgp.hpp
+++ include/mgp.hpp
@@ -19,6 +19,7 @@
 #include <vector>
 
 #include "_mgp.hpp"
+#include "mg_exceptions.hpp"
 #include "mg_procedure.h"
 
 namespace mgp {
@@ -48,11 +49,6 @@ class NotFoundException : public std::exception {
 
  private:
   std::string message_;
-};
-
-class NotEnoughMemoryException : public std::exception {
- public:
-  const char *what() const throw() { return "Not enough memory!"; }
 };
 
 // Forward declarations
@@ -2316,7 +2312,7 @@ inline bool Node::HasLabel(std::string_view label) const {
 inline Relationships Node::InRelationships() const {
   auto relationship_iterator = mgp::vertex_iter_in_edges(ptr_, memory);
   if (relationship_iterator == nullptr) {
-    throw NotEnoughMemoryException();
+    throw mg_exception::NotEnoughMemoryException();
   }
   return Relationships(relationship_iterator);
 }
@@ -2324,7 +2320,7 @@ inline Relationships Node::InRelationships() const {
 inline Relationships Node::OutRelationships() const {
   auto relationship_iterator = mgp::vertex_iter_out_edges(ptr_, memory);
   if (relationship_iterator == nullptr) {
-    throw NotEnoughMemoryException();
+    throw mg_exception::NotEnoughMemoryException();
   }
   return Relationships(relationship_iterator);
 }
@@ -3238,7 +3234,7 @@ inline RecordFactory::RecordFactory(mgp_result *result) : result_(result) {}
 inline const Record RecordFactory::NewRecord() const {
   auto record = mgp::result_new_record(result_);
   if (record == nullptr) {
-    throw NotEnoughMemoryException();
+    throw mg_exception::NotEnoughMemoryException();
   }
   return Record(record);
 }
