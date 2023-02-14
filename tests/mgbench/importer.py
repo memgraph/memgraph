d3efa5fc186243540be1fbc48824d88f51e40451
antejavor <javor.ante@gmail.com>
2023-02-14 16:30:11 +0100

    Add Neo4j LDBC import.

--- tests/mgbench/importer.py
+++ tests/mgbench/importer.py
@@ -6,33 +6,33 @@ import helpers
 from runners import Memgraph, Neo4j, Runners
 from workload.dataset import Dataset
 
-# Removed speaks/email
+# Removed speaks/email from person header
 HEADERS_INTERACTIVE = {
     "static/organisation": "id:ID(Organisation)|:LABEL|name:STRING|url:STRING",
     "static/place": "id:ID(Place)|name:STRING|url:STRING|:LABEL",
     "static/tagclass": "id:ID(TagClass)|name:STRING|url:STRING",
     "static/tag": "id:ID(Tag)|name:STRING|url:STRING",
-    "static/tagclass_isSubclassOf_tagclass": "START_ID(TagClass)|:END_ID(TagClass)",
+    "static/tagclass_isSubclassOf_tagclass": ":START_ID(TagClass)|:END_ID(TagClass)",
     "static/tag_hasType_tagclass": ":START_ID(Tag)|:END_ID(TagClass)",
     "static/organisation_isLocatedIn_place": ":START_ID(Organisation)|:END_ID(Place)",
     "static/place_isPartOf_place": ":START_ID(Place)|:END_ID(Place)",
-    "dynamic/comment": "id:ID(Comment)|creationDate:LONG|locationIP:STRING|browserUsed:STRING|content:STRING|length:INT",
-    "dynamic/forum": "id:ID(Forum)|title:STRING|creationDate:LONG",
-    "dynamic/person": "id:ID(Person)|firstName:STRING|lastName:STRING|gender:STRING|birthday:LONG|creationDate:LONG|locationIP:STRING|browserUsed:STRING",
-    "dynamic/post": "id:ID(Post)|imageFile:STRING|creationDate:LONG|locationIP:STRING|browserUsed:STRING|language:STRING|content:STRING|length:INT",
+    "dynamic/comment": "id:ID(Comment)|creationDate:DATETIME|locationIP:STRING|browserUsed:STRING|content:STRING|length:INT",
+    "dynamic/forum": "id:ID(Forum)|title:STRING|creationDate:DATETIME",
+    "dynamic/person": "id:ID(Person)|firstName:STRING|lastName:STRING|gender:STRING|birthday:DATETIME|creationDate:DATETIME|locationIP:STRING|browserUsed:STRING",
+    "dynamic/post": "id:ID(Post)|imageFile:STRING|creationDate:DATETIME|locationIP:STRING|browserUsed:STRING|language:STRING|content:STRING|length:INT",
     "dynamic/comment_hasCreator_person": ":START_ID(Comment)|:END_ID(Person)",
     "dynamic/comment_isLocatedIn_place": ":START_ID(Comment)|:END_ID(Place)",
     "dynamic/comment_replyOf_comment": ":START_ID(Comment)|:END_ID(Comment)",
     "dynamic/comment_replyOf_post": ":START_ID(Comment)|:END_ID(Post)",
     "dynamic/forum_containerOf_post": ":START_ID(Forum)|:END_ID(Post)",
-    "dynamic/forum_hasMember_person": ":START_ID(Forum)|:END_ID(Person)|joinDate:LONG",
+    "dynamic/forum_hasMember_person": ":START_ID(Forum)|:END_ID(Person)|joinDate:DATETIME",
     "dynamic/forum_hasModerator_person": ":START_ID(Forum)|:END_ID(Person)",
     "dynamic/forum_hasTag_tag": ":START_ID(Forum)|:END_ID(Tag)",
     "dynamic/person_hasInterest_tag": ":START_ID(Person)|:END_ID(Tag)",
     "dynamic/person_isLocatedIn_place": ":START_ID(Person)|:END_ID(Place)",
-    "dynamic/person_knows_person": ":START_ID(Person)|:END_ID(Person)|creationDate:LONG",
-    "dynamic/person_likes_comment": ":START_ID(Person)|:END_ID(Comment)|creationDate:LONG",
-    "dynamic/person_likes_post": ":START_ID(Person)|:END_ID(Post)|creationDate:LONG",
+    "dynamic/person_knows_person": ":START_ID(Person)|:END_ID(Person)|creationDate:DATETIME",
+    "dynamic/person_likes_comment": ":START_ID(Person)|:END_ID(Comment)|creationDate:DATETIME",
+    "dynamic/person_likes_post": ":START_ID(Person)|:END_ID(Post)|creationDate:DATETIME",
     "dynamic/person_studyAt_organisation": ":START_ID(Person)|:END_ID(Organisation)|classYear:INT",
     "dynamic/person_workAt_organisation": ":START_ID(Person)|:END_ID(Organisation)|workFrom:INT",
     "dynamic/post_hasCreator_person": ":START_ID(Post)|:END_ID(Person)",
@@ -59,7 +59,7 @@ class Importer:
                 dump_dir = helpers.unpack_tar_zst(Path(downloaded_file))
 
             input_files = {}
-            for file in dump_dir.glob("*/*.csv"):
+            for file in dump_dir.glob("*/*0.csv"):
                 parts = file.parts[-2:]
                 key = parts[0] + "/" + parts[1][:-8]
                 input_files[key] = file
@@ -69,34 +69,65 @@ class Importer:
                 output = file.parent / (file.stem + "_neo" + ".csv")
                 if not output.exists():
                     with file.open("r") as input_f, output.open("a") as output_f:
-                        header = next(input_f).strip().split("|")
                         reader = csv.reader(input_f, delimiter="|")
+                        next(reader)
                         writer = csv.writer(output_f, delimiter="|")
                         if key in HEADERS_INTERACTIVE.keys():
-                            updated_header = HEADERS_INTERACTIVE[key]
+                            updated_header = HEADERS_INTERACTIVE[key].split("|")
                             writer.writerow(updated_header)
                         for line in reader:
-                            writer.writerow(line)
+                            if "organisation_0_0.csv" == file.name:
+                                writer.writerow([line[0], line[1].capitalize(), line[2], line[3]])
+                            elif "place_0_0.csv" == file.name:
+                                writer.writerow([line[0], line[1], line[2], line[3].capitalize()])
+                            else:
+                                writer.writerow(line)
+                output_files[key] = output.as_posix()
 
-                else:
-                    output.unlink()
-
-                    # output_files[key] = out
-
-            # subprocess.run(
-            #     args=[
-            #         vendor._neo4j_admin,
-            #         "database",
-            #         "import",
-            #         "full",
-            #         "--nodes=",
-            #         nodes,
-            #         "--relationships=",
-            #         relationships,
-            #         "neo4j",
-            #     ],
-            #     check=True,
-            # )
+            subprocess.run(
+                args=[
+                    vendor._neo4j_admin,
+                    "database",
+                    "import",
+                    "full",
+                    "--id-type=INTEGER",
+                    "--nodes=Place=" + output_files["static/place"],
+                    "--nodes=Organisation=" + output_files["static/organisation"],
+                    "--nodes=TagClass=" + output_files["static/tagclass"],
+                    "--nodes=Tag=" + output_files["static/tag"],
+                    "--nodes=Comment:Message=" + output_files["dynamic/comment"],
+                    "--nodes=Forum=" + output_files["dynamic/forum"],
+                    "--nodes=Person=" + output_files["dynamic/person"],
+                    "--nodes=Post:Message=" + output_files["dynamic/post"],
+                    "--relationships=IS_PART_OF=" + output_files["static/place_isPartOf_place"],
+                    "--relationships=IS_SUBCLASS_OF=" + output_files["static/tagclass_isSubclassOf_tagclass"],
+                    "--relationships=IS_LOCATED_IN=" + output_files["static/organisation_isLocatedIn_place"],
+                    "--relationships=HAS_TYPE=" + output_files["static/tag_hasType_tagclass"],
+                    "--relationships=HAS_CREATOR=" + output_files["dynamic/comment_hasCreator_person"],
+                    "--relationships=IS_LOCATED_IN=" + output_files["dynamic/comment_isLocatedIn_place"],
+                    "--relationships=REPLY_OF=" + output_files["dynamic/comment_replyOf_comment"],
+                    "--relationships=REPLY_OF=" + output_files["dynamic/comment_replyOf_post"],
+                    "--relationships=CONTAINER_OF=" + output_files["dynamic/forum_containerOf_post"],
+                    "--relationships=HAS_MEMBER=" + output_files["dynamic/forum_hasMember_person"],
+                    "--relationships=HAS_MODERATOR=" + output_files["dynamic/forum_hasModerator_person"],
+                    "--relationships=HAS_TAG=" + output_files["dynamic/forum_hasTag_tag"],
+                    "--relationships=HAS_INTEREST=" + output_files["dynamic/person_hasInterest_tag"],
+                    "--relationships=IS_LOCATED_IN=" + output_files["dynamic/person_isLocatedIn_place"],
+                    "--relationships=KNOWS=" + output_files["dynamic/person_knows_person"],
+                    "--relationships=LIKES=" + output_files["dynamic/person_likes_comment"],
+                    "--relationships=LIKES=" + output_files["dynamic/person_likes_post"],
+                    "--relationships=HAS_CREATOR=" + output_files["dynamic/post_hasCreator_person"],
+                    "--relationships=HAS_TAG=" + output_files["dynamic/comment_hasTag_tag"],
+                    "--relationships=HAS_TAG=" + output_files["dynamic/post_hasTag_tag"],
+                    "--relationships=IS_LOCATED_IN=" + output_files["dynamic/post_isLocatedIn_place"],
+                    "--relationships=STUDY_AT=" + output_files["dynamic/person_studyAt_organisation"],
+                    "--relationships=WORK_AT=" + output_files["dynamic/person_workAt_organisation"],
+                    "--delimiter",
+                    "|",
+                    "final",
+                ],
+                check=True,
+            )
 
             return True
 
