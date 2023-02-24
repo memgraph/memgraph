e2e5a6032cefd8915d677bae4f1347e16ef49e27
Tyler Neely <t@jujit.su>
2023-02-24 11:18:39 +0000

    Split cluster property test file into bulk_load.cpp and random_workload.cpp

--- tests/simulation/random_workload.cpp
+++ tests/simulation/random_workload.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Memgraph Ltd.
+// Copyright 2023 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -33,7 +33,8 @@ using io::Time;
 using io::simulator::SimulatorConfig;
 using storage::v3::kMaximumCronInterval;
 
-RC_GTEST_PROP(RandomClusterConfig, HappyPath, (ClusterConfig cluster_config, NonEmptyOpVec ops, uint64_t rng_seed)) {
+RC_GTEST_PROP(RandomClusterConfig, RandomWorkload,
+              (ClusterConfig cluster_config, NonEmptyOpVec ops, uint64_t rng_seed)) {
   spdlog::cfg::load_env_levels();
 
   SimulatorConfig sim_config{
@@ -48,53 +49,6 @@ RC_GTEST_PROP(RandomClusterConfig, HappyPath, (ClusterConfig cluster_config, Non
 
   auto [sim_stats_1, latency_stats_1] = RunClusterSimulation(sim_config, cluster_config, ops.ops);
   auto [sim_stats_2, latency_stats_2] = RunClusterSimulation(sim_config, cluster_config, ops.ops);
-
-  if (latency_stats_1 != latency_stats_2 || sim_stats_1 != sim_stats_2) {
-    spdlog::error("simulator stats diverged across runs");
-    spdlog::error("run 1 simulator stats: {}", sim_stats_1);
-    spdlog::error("run 2 simulator stats: {}", sim_stats_2);
-    spdlog::error("run 1 latency:\n{}", latency_stats_1.SummaryTable());
-    spdlog::error("run 2 latency:\n{}", latency_stats_2.SummaryTable());
-    RC_ASSERT(latency_stats_1 == latency_stats_2);
-    RC_ASSERT(sim_stats_1 == sim_stats_2);
-  }
-
-  spdlog::trace("passed stats comparison - all good!");
-}
-
-RC_GTEST_PROP(RandomClusterConfig, BulkLoadAndSplit,
-              (ClusterConfig cluster_config, uint8_t inserts, uint64_t rng_seed)) {
-  spdlog::cfg::load_env_levels();
-
-  // This is a static workload that just inserts vertices and reads them back, which implicitly triggers concurrent
-  // splits
-  std::vector<Op> ops{};
-
-  for (int key = 0; key < inserts; key++) {
-    Op op1 = {.inner = AssertShardsSplit{}};
-    ops.emplace_back(std::move(op1));
-
-    Op op2 = {.inner = CreateVertex{.first = 0, .second = key}};
-    ops.emplace_back(std::move(op2));
-  }
-
-  Op op1 = {.inner = AssertShardsSplit{}};
-  ops.emplace_back(std::move(op1));
-
-  ops.emplace_back(Op{.inner = ScanAll{}});
-
-  SimulatorConfig sim_config{
-      .drop_percent = 0,
-      .perform_timeouts = true,
-      .scramble_messages = true,
-      .rng_seed = rng_seed,
-      .start_time = Time::min(),
-      // TODO(tyler) set abort_time to something more restrictive than Time::max()
-      .abort_time = Time::max(),
-  };
-
-  auto [sim_stats_1, latency_stats_1] = RunClusterSimulation(sim_config, cluster_config, ops);
-  auto [sim_stats_2, latency_stats_2] = RunClusterSimulation(sim_config, cluster_config, ops);
 
   if (latency_stats_1 != latency_stats_2 || sim_stats_1 != sim_stats_2) {
     spdlog::error("simulator stats diverged across runs");
