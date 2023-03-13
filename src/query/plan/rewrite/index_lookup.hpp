aa77707e5fddfb5d5e8d0c56d4eba4c51b0e2c64
Josip Mrden <josip.mrden@memgraph.io>
2023-03-13 17:08:47 +0100

    Fix plan generation with index lookups

--- src/query/plan/rewrite/index_lookup.hpp
+++ src/query/plan/rewrite/index_lookup.hpp
@@ -464,6 +464,16 @@ class IndexLookupRewriter final : public HierarchicalLogicalOperatorVisitor {
     return true;
   }
 
+  bool PreVisit(Apply &op) override {
+    prev_ops_.push_back(&op);
+    return true;
+  }
+
+  bool PostVisit(Apply & /*op*/) override {
+    prev_ops_.pop_back();
+    return true;
+  }
+
   std::shared_ptr<LogicalOperator> new_root_;
 
  private:
