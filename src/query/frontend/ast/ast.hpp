f4cbdbc1b8df18382227cb15bb8b5788a5879a2a
Andi Skrgat <andi8647@gmail.com>
2023-03-17 11:00:14 +0100

    Lisp changes

--- src/query/frontend/ast/ast.hpp
+++ src/query/frontend/ast/ast.hpp
@@ -2699,7 +2699,8 @@ class AuthQuery : public memgraph::query::Query {
     STREAM,
     MODULE_READ,
     MODULE_WRITE,
-    WEBSOCKET
+    WEBSOCKET,
+    TRANSACTION_MANAGEMENT
   };
 
   enum class FineGrainedPrivilege { NOTHING, READ, UPDATE, CREATE_DELETE };
@@ -3199,6 +3200,26 @@ class ShowConfigQuery : public memgraph::query::Query {
 
   ShowConfigQuery *Clone(AstStorage *storage) const override {
     ShowConfigQuery *object = storage->Create<ShowConfigQuery>();
+    return object;
+  }
+};
+
+class TransactionQueueQuery : public memgraph::query::Query {
+ public:
+  static const utils::TypeInfo kType;
+  const utils::TypeInfo &GetTypeInfo() const override { return kType; }
+
+  enum class Action { SHOW_TRANSACTIONS, TERMINATE_TRANSACTIONS };
+
+  TransactionQueueQuery() = default;
+
+  DEFVISITABLE(QueryVisitor<void>);
+
+  memgraph::query::TransactionQueueQuery::Action action_;
+  std::vector<Expression *> transaction_id_list_;
+
+  TransactionQueueQuery *Clone(AstStorage *storage) const override {
+    auto *object = storage->Create<TransactionQueueQuery>();
     return object;
   }
 };
