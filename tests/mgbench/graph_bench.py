3260918cbb99b8df2410b55d2d55a65136ac5af6
antejavor <javor.ante@gmail.com>
2023-03-19 23:56:36 +0100

    Update graph bench runner.

--- tests/mgbench/graph_bench.py
+++ tests/mgbench/graph_bench.py
@@ -16,14 +16,20 @@ def parse_arguments():
         help="Forward name and paths to vendors binary"
         "Example: --vendor memgraph /path/to/binary --vendor neo4j /path/to/binary",
     )
+
     parser.add_argument(
-        "--dataset-size",
-        default="small",
-        choices=["small", "medium", "large"],
-        help="Pick a dataset size (small, medium, large)",
+        "--dataset-name",
+        default="",
+        help="Dataset name you wish to execute",
     )
 
-    parser.add_argument("--dataset-group", default="basic", help="Select a group of queries")
+    parser.add_argument(
+        "--dataset-size",
+        default="",
+        help="Pick a dataset variant you wish to execute",
+    )
+
+    parser.add_argument("--dataset-group", default="", help="Select a group of queries")
 
     parser.add_argument(
         "--realistic",
@@ -53,88 +59,146 @@ def parse_arguments():
     return args
 
 
-def run_full_benchmarks(vendor, binary, dataset_size, dataset_group, realistic, mixed):
+def run_full_benchmarks(vendor, binary, dataset, dataset_size, dataset_group, realistic, mixed):
 
     configurations = [
-        # Basic full group test cold
+        # Basic isolated test cold
         [
             "--export-results",
-            vendor + "_" + dataset_size + "_cold_isolated.json",
+            vendor + "_" + dataset + "_" + dataset_size + "_cold_isolated.json",
         ],
-        # Basic full group test hot
+        # Basic isolated test hot
         [
             "--export-results",
-            vendor + "_" + dataset_size + "_hot_isolated.json",
-            "--warmup-run",
+            vendor + "_" + dataset + "_" + dataset_size + "_hot_isolated.json",
+            "--warm-up",
+            "hot",
+        ],
+        # Basic isolated test vulcanic
+        [
+            "--export-results",
+            vendor + "_" + dataset + "_" + dataset_size + "_vulcanic_isolated.json",
+            "--warm-up",
+            "vulcanic",
         ],
     ]
 
-    # Configurations for full workload
-    for count, write, read, update, analytical in realistic:
-        cold = [
-            "--export-results",
-            vendor
-            + "_"
-            + dataset_size
-            + "_cold_realistic_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical),
-            "--mixed-workload",
-            count,
-            write,
-            read,
-            update,
-            analytical,
-        ]
+    if realistic:
+        # Configurations for full workload
+        for count, write, read, update, analytical in realistic:
+            cold = [
+                "--export-results",
+                vendor
+                + "_"
+                + dataset
+                + "_"
+                + dataset_size
+                + "_cold_realistic_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical),
+                "--workload-realistic",
+                count,
+                write,
+                read,
+                update,
+                analytical,
+            ]
 
-        hot = [
-            "--export-results",
-            vendor
-            + "_"
-            + dataset_size
-            + "_hot_realistic_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical),
-            "--warmup-run",
-            "--mixed-workload",
-            count,
-            write,
-            read,
-            update,
-            analytical,
-        ]
-        configurations.append(cold)
-        configurations.append(hot)
+            hot = [
+                "--export-results",
+                vendor
+                + "_"
+                + dataset
+                + "_"
+                + dataset_size
+                + "_hot_realistic_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical),
+                "--warm-up",
+                "hot",
+                "--workload-realistic",
+                count,
+                write,
+                read,
+                update,
+                analytical,
+            ]
 
-    # Configurations for workload per query
-    for count, write, read, update, analytical, query in mixed:
-        cold = [
-            "--export-results",
-            vendor
-            + "_"
-            + dataset_size
-            + "_cold_mixed_{}_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical, query),
-            "--mixed-workload",
-            count,
-            write,
-            read,
-            update,
-            analytical,
-            query,
-        ]
-        hot = [
-            "--export-results",
-            vendor
-            + "_"
-            + dataset_size
-            + "_hot_mixed_{}_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical, query),
-            "--warmup-run",
-            "--mixed-workload",
-            count,
-            write,
-            read,
-            update,
-            analytical,
-            query,
-        ]
-        configurations.append(cold)
-        configurations.append(hot)
+            vulcanic = [
+                "--export-results",
+                vendor
+                + "_"
+                + dataset
+                + "_"
+                + dataset_size
+                + "_vulcanic_realistic_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical),
+                "--warm-up",
+                "vulcanic",
+                "--workload-realistic",
+                count,
+                write,
+                read,
+                update,
+                analytical,
+            ]
+            configurations.append(cold)
+            configurations.append(hot)
+            configurations.append(vulcanic)
+
+    if mixed:
+        # Configurations for workload per query
+        for count, write, read, update, analytical, query in mixed:
+            cold = [
+                "--export-results",
+                vendor
+                + "_"
+                + dataset
+                + "_"
+                + dataset_size
+                + "_cold_mixed_{}_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical, query),
+                "--workload-mixed",
+                count,
+                write,
+                read,
+                update,
+                analytical,
+                query,
+            ]
+            hot = [
+                "--export-results",
+                vendor
+                + "_"
+                + dataset
+                + "_"
+                + dataset_size
+                + "_hot_mixed_{}_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical, query),
+                "--warm-up",
+                "hot",
+                "--workload-mixed",
+                count,
+                write,
+                read,
+                update,
+                analytical,
+                query,
+            ]
+            vulcanic = [
+                "--export-results",
+                vendor
+                + "_"
+                + dataset
+                + "_"
+                + dataset_size
+                + "_hot_mixed_{}_{}_{}_{}_{}_{}.json".format(count, write, read, update, analytical, query),
+                "--warm-up",
+                "vulcanic" "--workload-mixed",
+                count,
+                write,
+                read,
+                update,
+                analytical,
+                query,
+            ]
+
+            configurations.append(cold)
+            configurations.append(hot)
+            configurations.append(vulcanic)
 
     default_args = [
         "python3",
@@ -146,9 +210,7 @@ def run_full_benchmarks(vendor, binary, dataset_size, dataset_group, realistic, 
         "--num-workers-for-benchmark",
         "12",
         "--no-authorization",
-        "pokec/" + dataset_size + "/" + dataset_group + "/*",
-        "--tail-latency",
-        "100",
+        dataset + "/" + dataset_size + "/" + dataset_group + "/*",
     ]
 
     for config in configurations:
@@ -157,11 +219,11 @@ def run_full_benchmarks(vendor, binary, dataset_size, dataset_group, realistic, 
         subprocess.run(args=full_config, check=True)
 
 
-def collect_all_results(vendor_name, dataset_size, dataset_group):
+def collect_all_results(vendor_name, dataset, dataset_size, dataset_group):
     working_directory = Path().absolute()
     print(working_directory)
-    results = sorted(working_directory.glob(vendor_name + "_" + dataset_size + "_*.json"))
-    summary = {"pokec": {dataset_size: {dataset_group: {}}}}
+    results = sorted(working_directory.glob(vendor_name + "_" + dataset + "_" + dataset_size + "_*.json"))
+    summary = {dataset: {dataset_size: {dataset_group: {}}}}
 
     for file in results:
         if "summary" in file.name:
@@ -169,19 +231,22 @@ def collect_all_results(vendor_name, dataset_size, dataset_group):
         f = file.open()
         data = json.loads(f.read())
         if data["__run_configuration__"]["condition"] == "hot":
-            for key, value in data["pokec"][dataset_size][dataset_group].items():
+            for key, value in data[dataset][dataset_size][dataset_group].items():
                 key_condition = key + "_hot"
-                summary["pokec"][dataset_size][dataset_group][key_condition] = value
+                summary[dataset][dataset_size][dataset_group][key_condition] = value
         elif data["__run_configuration__"]["condition"] == "cold":
-            for key, value in data["pokec"][dataset_size][dataset_group].items():
+            for key, value in data[dataset][dataset_size][dataset_group].items():
                 key_condition = key + "_cold"
-                summary["pokec"][dataset_size][dataset_group][key_condition] = value
-
+                summary[dataset][dataset_size][dataset_group][key_condition] = value
+        elif data["__run_configuration__"]["condition"] == "vulcanic":
+            for key, value in data[dataset][dataset_size][dataset_group].items():
+                key_condition = key + "_vulcanic"
+                summary[dataset][dataset_size][dataset_group][key_condition] = value
     print(summary)
 
     json_object = json.dumps(summary, indent=4)
     print(json_object)
-    with open(vendor_name + "_" + dataset_size + "_summary.json", "w") as f:
+    with open(vendor_name + "_" + dataset + "_" + dataset_size + "_summary.json", "w") as f:
         json.dump(summary, f)
 
 
@@ -194,16 +259,17 @@ if __name__ == "__main__":
     vendor_names = {"memgraph", "neo4j"}
     for vendor_name, vendor_binary in args.vendor:
         path = Path(vendor_binary)
-        if vendor_name.lower() in vendor_names and (path.is_file() or path.is_dir()):
+        if vendor_name.lower() in vendor_names:
             run_full_benchmarks(
                 vendor_name,
                 vendor_binary,
+                args.dataset_name,
                 args.dataset_size,
                 args.dataset_group,
                 realistic,
                 mixed,
             )
-            collect_all_results(vendor_name, args.dataset_size, args.dataset_group)
+            collect_all_results(vendor_name, args.dataset_name, args.dataset_size, args.dataset_group)
         else:
             raise Exception(
                 "Check that vendor: {} is supported and you are passing right path: {} to binary.".format(
