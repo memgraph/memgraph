18fe8ee7555443cb5b33c8811e641ad290cadf91
Marko Budiselic <marko.budiselic@memgraph.com>
2023-04-04 11:53:53 +0000

    Improve SharedLibraryHandle

--- src/query/procedure/module.hpp
+++ src/query/procedure/module.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Memgraph Ltd.
+// Copyright 2023 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -144,13 +144,18 @@ class ModuleRegistry final {
     }
 
    private:
+    // TODO(gitbuda): If handle is not there -> warning that some modules might
+    // not work as expected. Explained below. It's possible to run Memgraph on
+    // older systems (CentOS 7). The problem is how to know if there is the
+    // version of libstdc++, also what is libc++ if used?
+    //
     void *handle_;
   };
 
 #if __has_feature(address_sanitizer)
   // This is why we need RTLD_NODELETE and we must not use RTLD_DEEPBIND with
   // ASAN: https://github.com/google/sanitizers/issues/89
-  SharedLibraryHandle libstd_handle{"libstdc++.so.6", RTLD_NOW | RTLD_LOCAL | RTLD_NODELETE};
+  // SharedLibraryHandle libstd_handle{"libstdc++.so.6", RTLD_NOW | RTLD_LOCAL | RTLD_NODELETE};
 #else
   // The reason behind opening share library during runtime is to avoid issues
   // with loading symbols from stdlib. We have encounter issues with locale
@@ -161,7 +166,7 @@ class ModuleRegistry final {
   // mentioned library will be first performed in the already existing binded
   // libraries and then the global namespace.
   // RTLD_DEEPBIND => https://linux.die.net/man/3/dlopen
-  SharedLibraryHandle libstd_handle{"libstdc++.so.6", RTLD_NOW | RTLD_LOCAL | RTLD_DEEPBIND};
+  // SharedLibraryHandle libstd_handle{"libstdc++.so.6", RTLD_NOW | RTLD_LOCAL | RTLD_DEEPBIND};
 #endif
   std::vector<std::filesystem::path> modules_dirs_;
   std::filesystem::path internal_module_dir_;
