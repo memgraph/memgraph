7d0eb85381ae0187d7681d5e79f11bc082a374ee
Marko Budiselic <marko.budiselic@memgraph.com>
2023-04-09 00:16:16 +0000

    Add OS test.sh script

---
+++ environment/os/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$DIR/../util.sh"
+
+echo "TODO(gitbuda): Test all XYZ_DEPS could be installed"
+exit 1
