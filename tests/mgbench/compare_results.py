15bae4734e91a76dbb22d3ac46f940a62f2e3a2f
antejavor <javor.ante@gmail.com>
2023-04-15 15:47:08 +0200

    Add memgraph licenses.

--- tests/mgbench/compare_results.py
+++ tests/mgbench/compare_results.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2022 Memgraph Ltd.
+# Copyright 2023 Memgraph Ltd.
 #
 # Use of this software is governed by the Business Source License
 # included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
