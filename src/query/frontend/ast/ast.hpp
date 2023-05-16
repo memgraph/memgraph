6abd5a97888bc30d6f6969497dfb1194c2885f41
antoniofilipovic <filipovicantonio1998@gmail.com>
2023-05-16 14:34:21 +0200

    format ast.hpp

--- src/query/frontend/ast/ast.hpp
+++ src/query/frontend/ast/ast.hpp
@@ -1135,10 +1135,8 @@ class MapProjectionLiteral : public memgraph::query::BaseLiteral {
   }
 
  protected:
-
-explicit MapProjectionLiteral(Expression *map_variable, std::unordered_map<PropertyIx, Expression *> &&elements)
+  explicit MapProjectionLiteral(Expression *map_variable, std::unordered_map<PropertyIx, Expression *> &&elements)
       : map_variable_(map_variable), elements_(std::move(elements)) {}
-
 
  private:
   friend class AstStorage;
