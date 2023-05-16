dc0d8a4fe37d7d73f93f2274a20854ae290393d3
Josip Mrden <josip.mrden@memgraph.io>
2023-05-16 18:11:43 +0200

    Get back antlr runtime

--- src/query/frontend/ast/cypher_main_visitor.hpp
+++ src/query/frontend/ast/cypher_main_visitor.hpp
@@ -15,6 +15,8 @@
 #include <unordered_set>
 #include <utility>
 
+#include <antlr4-runtime.h>
+
 #include "query/frontend/ast/ast.hpp"
 #include "query/frontend/opencypher/generated/MemgraphCypherBaseVisitor.h"
 #include "utils/exceptions.hpp"
