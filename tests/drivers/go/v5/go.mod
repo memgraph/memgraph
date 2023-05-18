085faab7eaa25f8183966aa5898cfa4a7285418a
Andreja Tonev <andreja.tonev@memgraph.io>
2023-05-18 14:43:40 +0200

    Using a specific version of GO in tests

--- tests/drivers/go/v5/go.mod
+++ tests/drivers/go/v5/go.mod
@@ -2,4 +2,7 @@ module bolt-test
 
 go 1.18
 
-require github.com/neo4j/neo4j-go-driver/v5 v5.6.0 // indirect
+require (
+	github.com/neo4j/neo4j-go-driver/v5 v5.8.1 // indirect
+	golang.org/dl v0.0.0-20230502172222-5216546bad51 // indirect
+)
--- tests/drivers/go/v5/run.sh
+++ tests/drivers/go/v5/run.sh
@@ -8,6 +8,16 @@ for i in go; do
   fi
 done
 
-go get github.com/neo4j/neo4j-go-driver/v5
+# setup go 1.18.2
+go get golang.org/dl/go1.18.2
+go install golang.org/dl/go1.18.2
+if [ -z ${GOPATH+x} ]; then
+  export GO=$HOME/go/bin/go1.18.2
+else
+  export GO=$GOPATH/bin/go1.18.2
+fi
+$GO download
 
-go run docs_quick_start.go
+# run tests using go 1.18.2
+$GO get github.com/neo4j/neo4j-go-driver/v5
+$GO run docs_quick_start.go
