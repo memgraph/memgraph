fa7cbb669b2c6ef557a28485ec3732e5bc25f95b
Andreja Tonev <andreja.tonev@memgraph.io>
2023-05-24 17:18:04 +0200

    Driver tests bugfixes

--- tests/drivers/go/v4/metadata.go
+++ tests/drivers/go/v4/metadata.go
@@ -19,11 +19,16 @@ func check_md(result neo4j.Result, err error) {
       log.Fatal("Failed to read metadata!")
     }
     md_map, ok := md.(map[string]interface{})
-    if ok && md_map["ver"].(string) == "session" && md_map["str"].(string) == "aha" && md_map["num"].(int64) == 123{
-      n++
-    }
-    else if ok && md_map["ver"].(string) == "transaction" && md_map["str"].(string) == "oho" && md_map["num"].(int64) == 456{
-      n++
+    if ok {
+      ver_val, ver_ok := md_map["ver"]
+      str_val, str_ok := md_map["str"]
+      num_val, num_ok := md_map["num"]
+      if (ver_ok && str_ok && num_ok) {
+        if ((ver_val.(string) == "session" && str_val.(string) == "aha" && num_val.(int64) == 123) ||
+            (ver_val.(string) == "transaction" && str_val.(string) == "oho" && num_val.(int64) == 456)) {
+          n++
+        }
+      }
     }
   }
   if n == 0 {
@@ -39,7 +44,7 @@ func check_tx(driver neo4j.Driver) {
   sessionConfig := neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite}
   session, err := driver.NewSession(sessionConfig)
   if err != nil {
-    log.Fatal("An error occured while creating a session: %s", err)
+    log.Fatal("An error occurred while creating a session: %s", err)
   }
 
   defer session.Close()
@@ -61,16 +66,18 @@ func main() {
   sessionConfig := neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite}
   session, err := driver.NewSession(sessionConfig)
   if err != nil {
-    log.Fatal("An error occured while creating a session: %s", err)
+    log.Fatal("An error occurred while creating a session: %s", err)
   }
 
   defer session.Close()
 
   // Implicit transaction
+  fmt.Println("Checking implicit transaction metadata...")
   result, err := session.Run("SHOW TRANSACTIONS", nil, neo4j.WithTxMetadata(map[string]interface{}{"ver":"session", "str":"aha", "num":123}))
   check_md(result, err)
 
   // Explicit transaction
+  fmt.Println("Checking explicit transaction metadata...")
   tx, err := session.BeginTransaction(neo4j.WithTxMetadata(map[string]interface{}{"ver":"transaction", "str":"oho", "num":456}))
   if err != nil {
     handle_error(err)
