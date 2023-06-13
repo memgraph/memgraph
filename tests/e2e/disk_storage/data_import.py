20630b72a1adab972301df17a5255f0cabef1578
Andi Skrgat <andi8647@gmail.com>
2023-06-13 17:27:12 +0200

    Add e2e tests for batched import

--- tests/e2e/disk_storage/data_import.py
+++ tests/e2e/disk_storage/data_import.py
@@ -11,38 +11,31 @@
 
 import sys
 
-import mgclient
 import pytest
 from common import connect, execute_and_fetch_all
 
-# TODO: andi You should add configuration flag that you can start Memgraph with disk storage as default
-# def test_disk_import_fail(connect):
-#     cursor = connect.cursor()
-#     execute_and_fetch_all(cursor, "STORAGE MODE ON_DISK_TRANSACTIONAL")
-#     # TODO:andi when merged master to this epic, tes that show storage info shows the correct storage mode
-#     try:
-#         execute_and_fetch_all(cursor, "FOREACH (i IN range(1, 100000) | CREATE (n:DiskLabel {id: i}));")
-#         assert True
-#     except:
-#         assert False
+num_entries = 100000
 
 
-# TODO: andi You should add configuration flag that you can start Memgraph with disk storage as default
-# TODO: this should also be tested with unit tests
+def test_disk_import_fail(connect):
+    cursor = connect.cursor()
+    execute_and_fetch_all(cursor, "STORAGE MODE ON_DISK_TRANSACTIONAL")
+    try:
+        execute_and_fetch_all(cursor, "FOREACH (i IN range(1, {num_entries}) | CREATE (n:DiskLabel {{id: i}}));")
+        assert False
+    except:
+        assert True
+
+
 def test_batched_disk_import_passes(connect):
-    num_entries = 1000000
-    step = int(num_entries / 100)
+    step = int(num_entries / 5)
     for i in range(1, num_entries, step):
         cursor = connect.cursor()
         execute_and_fetch_all(cursor, "STORAGE MODE ON_DISK_TRANSACTIONAL")
         query = "FOREACH (i IN range({i}, {i} + {step}) | CREATE (n:DiskLabel {{id: {i}}}));".format(i=i, step=step)
-        print(f"Query: {query}")
-        tx = (i - 1) / step + 1
         try:
             execute_and_fetch_all(cursor, query)
-            print(f"Finished tx: {tx}/{num_entries/step}")
         except:
-            print(f"Failed in tx: {tx}/{num_entries/step}")
             assert False
         cursor.close()
     assert True
