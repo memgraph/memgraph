c7d6709c56868c5c530e1e36f7a115e06ae99db5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-14 10:08:18 -0400

    spelling: unique
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/storage/v2/storage.cpp
+++ src/storage/v2/storage.cpp
@@ -916,7 +916,7 @@ utils::BasicResult<StorageDataManipulationError, void> Storage::Accessor::Commit
       }
     }
 
-    // Result of validating the vertex against unqiue constraints. It has to be
+    // Result of validating the vertex against unique constraints. It has to be
     // declared outside of the critical section scope because its value is
     // tested for Abort call which has to be done out of the scope.
     std::optional<ConstraintViolation> unique_constraint_violation;
