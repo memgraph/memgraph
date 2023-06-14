786204f99c39fb6e66436f1c38efb4ac8d702d41
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-13 20:16:14 -0400

    spelling: the
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/utils/async_timer.hpp
+++ src/utils/async_timer.hpp
@@ -39,7 +39,7 @@ class AsyncTimer {
   void ReleaseResources();
 
   // If the expiration_flag_ is nullptr, then the object is not associated with any timer, therefore no clean up
-  // is necessary. Furthermore, the the POSIX API doesn't specify any value as "invalid" for timer_t, so the timer_id_
+  // is necessary. Furthermore, the POSIX API doesn't specify any value as "invalid" for timer_t, so the timer_id_
   // cannot be used to determine whether the object is associated with any timer or not.
   std::shared_ptr<std::atomic<bool>> expiration_flag_;
   uint64_t flag_id_;
