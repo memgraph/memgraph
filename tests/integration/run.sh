f995e42ce8a69076f5a957a42b4f1731b436e90b
Marko Budiselic <marko.budiselic@memgraph.com>
2023-06-15 19:37:53 +0000

    Add picker

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# TODO(gitbuda): Add the picker to it's possible to run a single test.
+print_help() {
+  echo -e "$0             => run all under tests/integration"
+  echo -e "$0 folder_name => run single test under tests/integration"
+}
 
-cd $DIR
-for name in *; do
-  if [ ! -d $name ]; then continue; fi
-  pushd $name >/dev/null
-  echo "Running: $name"
+run_single_test() {
+  cd "$DIR"
+  integration_test_folder_name="$1"
+  pushd "$integration_test_folder_name" >/dev/null
+  echo "Running: $integration_test_folder_name"
   if [ -x prepare.sh ]; then
     ./prepare.sh
   fi
@@ -18,4 +21,26 @@ for name in *; do
   fi
   echo
   popd >/dev/null
-done
+}
+
+test_all() {
+  cd "$DIR"
+  for name in *; do
+    if [ ! -d "$name" ]; then continue; fi
+    run_single_test "$name"
+  done
+}
+
+if [ "$#" -eq 0 ]; then
+  test_all
+else
+  if [ "$#" -gt 1 ]; then
+    print_help
+  else
+    if [ -d "$DIR/$1" ]; then
+      run_single_test "$1"
+    else
+      print_help
+    fi
+  fi
+fi
