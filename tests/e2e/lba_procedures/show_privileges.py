7a48c6a31eb442298430aeb7ca8a49cf8b2d8116
Andreja Tonev <andreja.tonev@memgraph.io>
2023-06-29 09:32:50 +0200

    e2e test updated to support MULTI_DATABASE priv

--- tests/e2e/lba_procedures/show_privileges.py
+++ tests/e2e/lba_procedures/show_privileges.py
@@ -63,7 +63,7 @@ def test_lba_procedures_show_privileges_first_user():
     cursor = connect(username="Josip", password="").cursor()
     result = execute_and_fetch_all(cursor, "SHOW PRIVILEGES FOR Josip;")
 
-    assert len(result) == 33
+    assert len(result) == 34
 
     fine_privilege_results = [res for res in result if res[0] not in BASIC_PRIVILEGES]
 
