6c8de94a37f13dd98b2f5d2e370d97aec38de0b0
Josip Mrden <josip.mrden@memgraph.io>
2023-07-19 11:23:15 +0200

    Add database mode config parameter

--- tests/stress/test_config.py
+++ tests/stress/test_config.py
@@ -1,13 +1,33 @@
+import itertools
+from typing import List, Tuple
+
+
 class StorageModeConstants:
     IN_MEMORY_TRANSACTIONAL = "IN_MEMORY_TRANSACTIONAL"
     IN_MEMORY_ANALYTICAL = "IN_MEMORY_ANALYTICAL"
     ON_DISK_TRANSACTIONAL = "ON_DISK_TRANSACTIONAL"
+
+    @classmethod
+    def to_list(cls) -> List[str]:
+        return [cls.IN_MEMORY_TRANSACTIONAL, cls.IN_MEMORY_ANALYTICAL, cls.ON_DISK_TRANSACTIONAL]
 
 
 class IsolationModeConstants:
     READ_UNCOMMITED = "READ_UNCOMMITED"
     READ_COMMITED = "READ_COMMITED"
     SNAPSHOT_SERIALIZATION = "SNAPSHOT_SERIALIZATION"
+
+    @classmethod
+    def to_list(cls) -> List[str]:
+        return [cls.SNAPSHOT_SERIALIZATION, cls.READ_COMMITED, cls.READ_UNCOMMITED]
+
+
+def get_default_database_mode() -> Tuple[str, str]:
+    return (StorageModeConstants.IN_MEMORY_TRANSACTIONAL, IsolationModeConstants.SNAPSHOT_SERIALIZATION)
+
+
+def get_all_database_modes() -> List[Tuple[str, str]]:
+    return list(itertools.product(StorageModeConstants.to_list(), IsolationModeConstants.to_list()))
 
 
 # dataset calibrated for running on Apollo (total 4min)
@@ -20,26 +40,31 @@ SMALL_DATASET = [
         "test": "bipartite.py",
         "options": ["--u-count", "100", "--v-count", "100"],
         "timeout": 5,
+        "mode": [get_default_database_mode()],
     },
     {
         "test": "create_match.py",
         "options": ["--vertex-count", "40000", "--create-pack-size", "100"],
         "timeout": 5,
+        "mode": [get_default_database_mode()],
     },
     {
         "test": "parser.cpp",
         "options": ["--per-worker-query-count", "1000"],
         "timeout": 5,
+        "mode": [get_default_database_mode()],
     },
     {
         "test": "long_running.cpp",
         "options": ["--vertex-count", "1000", "--edge-count", "5000", "--max-time", "1", "--verify", "20"],
         "timeout": 5,
+        "mode": [get_default_database_mode()],
     },
     {
         "test": "long_running.cpp",
         "options": ["--vertex-count", "10000", "--edge-count", "50000", "--max-time", "2", "--verify", "30"],
         "timeout": 5,
+        "mode": [get_default_database_mode()],
     },
 ]
 
@@ -53,11 +78,13 @@ LARGE_DATASET = (
             "test": "bipartite.py",
             "options": ["--u-count", "300", "--v-count", "300"],
             "timeout": 30,
+            "mode": [get_default_database_mode()],
         },
         {
             "test": "create_match.py",
             "options": ["--vertex-count", "500000", "--create-pack-size", "500"],
             "timeout": 30,
+            "mode": [get_default_database_mode()],
         },
     ]
     + [
@@ -65,6 +92,7 @@ LARGE_DATASET = (
             "test": "long_running.cpp",
             "options": ["--vertex-count", "10000", "--edge-count", "40000", "--max-time", "5", "--verify", "60"],
             "timeout": 16,
+            "mode": [get_default_database_mode()],
         },
     ]
     * 6
@@ -73,6 +101,7 @@ LARGE_DATASET = (
             "test": "long_running.cpp",
             "options": ["--vertex-count", "200000", "--edge-count", "1000000", "--max-time", "480", "--verify", "300"],
             "timeout": 500,
+            "mode": [get_default_database_mode()],
         },
     ]
 )
