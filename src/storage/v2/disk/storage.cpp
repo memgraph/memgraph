34c444298539c9157d7a71f86a4095f051c225f0
Josip Mrden <josip.mrden@memgraph.io>
2023-07-27 17:09:51 +0200

    Adapt assert

--- src/storage/v2/disk/storage.cpp
+++ src/storage/v2/disk/storage.cpp
@@ -787,7 +787,7 @@ Result<std::optional<VertexAccessor>> DiskStorage::DiskAccessor::DeleteVertex(Ve
 
   const auto &[vertices, edges] = *value;
 
-  MG_ASSERT(vertices.size() == 1, "The number of deleted vertices is not equal to 1!");
+  MG_ASSERT(vertices.size() <= 1, "The number of deleted vertices is not less or equal to 1!");
 
   return std::make_optional<VertexAccessor>(vertices[0]);
 }
