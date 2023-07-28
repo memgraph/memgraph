066b2695e19ebe5a0eff14556ff86ac2e618abc1
Marko Budiselic <marko.budiselic@memgraph.com>
2023-07-28 11:13:02 +0000

    Unify node version setup

--- tests/e2e/graphql/setup.sh
+++ tests/e2e/graphql/setup.sh
@@ -3,29 +3,7 @@ set -Eeuo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
-if [ -f "$HOME/.nvm/nvm.sh" ]; then
-  . "$HOME/.nvm/nvm.sh"
-  nvm install 14
-  nvm use 14
-fi
-
-if ! command -v node >/dev/null; then
-  echo "Could NOT node. Make sure node is installed."
-  exit 1
-fi
-if ! command -v npm >/dev/null; then
-  echo "Could NOT npm. Make sure npm is installed."
-  exit 1
-fi
-node_version=$(node --version)
-npm_version=$(npm --version)
-echo "NODE VERSION: $node_version"
-echo "NPM  VERSION: $npm_version"
-node_major_version=${node_version##v}
-node_major_version=${node_major_version%%.*}
-if [ ! "$node_major_version" -ge 14 ]; then
-  echo "ERROR: It's required to have node >= 14."
-  exit 1
-fi
+source "$SCRIPT_DIR/../../util.sh"
+setup_node
 
 npm i
