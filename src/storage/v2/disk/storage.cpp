c57068f07ed30c3726a5d642cbae3f0a7c3e5947
Andi Skrgat <andi8647@gmail.com>
2023-08-05 15:00:53 +0200

    Remove delta.hpp include

--- src/storage/v2/disk/storage.cpp
+++ src/storage/v2/disk/storage.cpp
@@ -25,7 +25,6 @@
 #include "kvstore/kvstore.hpp"
 #include "spdlog/spdlog.h"
 #include "storage/v2/constraints/unique_constraints.hpp"
-#include "storage/v2/delta.hpp"
 #include "storage/v2/disk/rocksdb_storage.hpp"
 #include "storage/v2/disk/storage.hpp"
 #include "storage/v2/disk/unique_constraints.hpp"
