8c8397aba81cbfd0e9abf1706d812c86f0e31f5c
Josip Mrden <josip.mrden@memgraph.io>
2023-09-15 23:18:01 +0200

    Add nested property parsing

--- query_modules/generic_transformation.cpp
+++ query_modules/generic_transformation.cpp
@@ -17,6 +17,47 @@
 static constexpr std::string_view kQuery = "query";
 static constexpr std::string_view kParameters = "parameters";
 
+template <typename T>
+void ReplaceQueryParameters(T object, std::string key, std::string &query) {
+  for (auto &el : object.items()) {
+    auto new_key = fmt::format("{}__{}", key, el.key());
+
+    if (el.value().type() == nlohmann::json::value_t::object) {
+      ReplaceQueryParameters(el.value(), new_key, query);
+      continue;
+    }
+
+    if (el.value().type() == nlohmann::json::value_t::array) {
+      continue;
+    }
+
+    size_t pos;
+    while ((pos = query.find(new_key)) != std::string::npos) {
+      query.replace(pos, new_key.size(), el.value());
+    }
+  }
+}
+
+void ReplaceQueryParameters(nlohmann::json &json, std::string &query) {
+  for (auto &el : json.items()) {
+    auto key = fmt::format("${}", el.key());
+
+    if (el.value().type() == nlohmann::json::value_t::object) {
+      ReplaceQueryParameters(el.value(), key, query);
+      continue;
+    }
+
+    if (el.value().type() == nlohmann::json::value_t::array) {
+      continue;
+    }
+
+    size_t pos;
+    while ((pos = query.find(key)) != std::string::npos) {
+      query.replace(pos, key.size(), el.value());
+    }
+  }
+}
+
 void Transformation(struct mgp_messages *messages, mgp_trans_context *ctx, mgp_result *result, mgp_memory *memory) {
   mgp::MemoryDispatcherGuard guard(memory);
   auto record_factory = mgp::RecordFactory(result);
@@ -27,15 +68,9 @@ void Transformation(struct mgp_messages *messages, mgp_trans_context *ctx, mgp_r
       auto record = record_factory.NewRecord();
 
       auto payload = nlohmann::json::parse(message.Payload());
-      std::string generic_query = transformation_context.Query();
-      for (auto &el : payload.items()) {
-        auto key = fmt::format("${}", el.key());
-        size_t pos;
-        while ((pos = generic_query.find(key)) != std::string::npos) {
-          generic_query.replace(pos, key.size(), el.value());
-        }
-      }
 
+      std::string generic_query = transformation_context.Query();
+      ReplaceQueryParameters(payload, generic_query);
       auto query_value = mgp::Value(generic_query.data());
 
       record.Insert(kQuery.data(), query_value);
