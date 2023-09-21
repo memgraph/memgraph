b050e43265487d1ddecfeb03dc63eaf73364d51a
Andi Skrgat <andi8647@gmail.com>
2023-09-21 15:33:30 +0200

    Default config test

--- tests/e2e/configuration/default_config.py
+++ tests/e2e/configuration/default_config.py
@@ -156,7 +156,11 @@ startup_config_dict = {
         "100000",
         "Issue a 'fsync' call after this amount of transactions are written to the WAL file. Set to 1 for fully synchronous operation.",
     ),
-    "storage_mode": ("IN_MEMORY_TRANSACTIONAL", "IN_MEMORY_TRANSACTIONAL", "Storage mode used by Memgraph"),
+    "storage_mode": (
+        "IN_MEMORY_TRANSACTIONAL",
+        "IN_MEMORY_TRANSACTIONAL",
+        "Storage mode Memgraph uses. Allowed values: IN_MEMORY_TRANSACTIONAL, IN_MEMORY_ANALYTICAL, ON_DISK_TRANSACTIONAL",
+    ),
     "storage_wal_file_size_kib": ("20480", "20480", "Minimum file size of each WAL file."),
     "storage_delete_on_drop": (
         "true",
