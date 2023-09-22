efdf7baea0e6ead04b2f74f44ed5e33beeb97b7a
Andi <andi8647@gmail.com>
2023-09-22 19:05:16 +0200

    Refactor mgbench

---
+++ tests/mgbench/workload_mode.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Memgraph Ltd.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+# License, and you may not use this file except in compliance with the Business Source License.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0, included in the file
+# licenses/APL.txt.
+
+BENCHMARK_MODE_MIXED = "Mixed"
+BENCHMARK_MODE_REALISTIC = "Realistic"
+BENCHMARK_MODE_ISOLATED = "Isolated"
