dab3ffd0f6fa3a513bae59471fb871ab5c0c8b9d
DavIvek <david.ivekovic@memgraph.io>
2023-10-12 15:28:45 +0200

    fix crash caused by deleting non-existing edge

--- src/storage/v2/storage.cpp
+++ src/storage/v2/storage.cpp
@@ -363,6 +363,9 @@ EdgeInfoForDeletion Storage::Accessor::PrepareDeletableEdges(const std::unordere
 
   // also add edges which we want to delete from the query
   for (const auto &edge_accessor : edges) {
+    if (edge_accessor->from_vertex_->deleted || edge_accessor->to_vertex_->deleted) {
+      continue;
+    }
     partial_src_vertices.insert(edge_accessor->from_vertex_);
     partial_dest_vertices.insert(edge_accessor->to_vertex_);
 
