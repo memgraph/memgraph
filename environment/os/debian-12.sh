298884535d6d227ad85104762037f325beb7dbe4
Marko Budiselic <mbudiselicbuda@gmail.com>
2023-10-16 17:45:46 +0000

    Remove sources.list because it's redundant on Debian 12

--- environment/os/debian-12.sh
+++ environment/os/debian-12.sh
@@ -99,16 +99,6 @@ check() {
 }
 
 install() {
-    cat >/etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian bookworm main
-deb-src http://deb.debian.org/debian bookworm main
-
-deb http://deb.debian.org/debian-security/ bookworm-security main
-deb-src http://deb.debian.org/debian-security/ bookworm-security main
-
-deb http://deb.debian.org/debian bookworm-updates main
-deb-src http://deb.debian.org/debian bookworm-updates main
-EOF
     cd "$DIR"
     apt update
     # If GitHub Actions runner is installed, append LANG to the environment.
