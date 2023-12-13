a9558765f6f42ba65f48921e5b30eec4a6f40f5e
Andi Skrgat <andi8647@gmail.com>
2023-12-13 14:35:04 +0100

    Change localhost to kafka host

--- tests/e2e/streams/conftest.py
+++ tests/e2e/streams/conftest.py
@@ -60,7 +60,7 @@ def kafka_topics():
 
 @pytest.fixture(scope="function")
 def kafka_producer():
-    yield KafkaProducer(bootstrap_servers="localhost:9092", api_version=(0, 10, 1))
+    yield KafkaProducer(bootstrap_servers="kafka:9092", api_version=(0, 10, 1))
 
 
 @pytest.fixture(scope="function")
