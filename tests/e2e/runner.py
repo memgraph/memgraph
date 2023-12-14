6c985db80dc240d241754a20d681350e02cbf62d
Gareth Andrew Lloyd <gareth.lloyd@memgraph.io>
2023-12-14 13:36:33 +0000

    Cleanup filesystem after e2e tests (#1584)

--- tests/e2e/runner.py
+++ tests/e2e/runner.py
@@ -53,8 +53,8 @@ def run(args):
 
         # Setup.
         @atexit.register
-        def cleanup():
-            interactive_mg_runner.stop_all()
+        def cleanup(keep_directories=True):
+            interactive_mg_runner.stop_all(keep_directories)
 
         if "pre_set_workload" in workload:
             binary = os.path.join(BUILD_DIR, workload["pre_set_workload"])
@@ -92,7 +92,7 @@ def run(args):
                     data = mg_instance.query(validation["query"], conn)[0][0]
                     assert data == validation["expected"]
                 conn.close()
-        cleanup()
+        cleanup(keep_directories=False)
         log.info("%s PASSED.", workload_name)
 
 
