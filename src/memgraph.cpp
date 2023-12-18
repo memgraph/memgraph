8aba4c46b6d7737797e9aba8f8b507279177ab01
antoniofilipovic <filipovicantonio1998@gmail.com>
2023-12-18 15:07:03 +0100

    remove include

--- src/memgraph.cpp
+++ src/memgraph.cpp
@@ -9,7 +9,6 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-#include <chrono>
 #include <cstdint>
 #include "audit/log.hpp"
 #include "communication/metrics.hpp"
