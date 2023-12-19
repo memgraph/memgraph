0f566d8058976c6e67c5f6bf5276798d09530ca0
Deda <marko.barisic@memgraph.io>
2023-12-19 14:38:04 +0100

    Add debug output

--- release/binary/debian-10/run.sh
+++ release/binary/debian-10/run.sh
@@ -8,9 +8,13 @@ echo -e "\n-----THREADS=$MG_THREADS-----\n"
 
 cd memgraph
 source /opt/toolchain-v4/activate
+
+echo -e "\n---INIT---\n"
 ./init
+
+echo -e "\n---CMAKE---\n"
 cd build
 cmake -DCMAKE_BUILD_TYPE=$MG_BUILD_TYPE -DMG_ENTERPRISE=OFF ..
-make -j$MG_THREADS
 
-sleep eternity
+echo -e "\n---MAKE---\n"
+make -j$MG_THREADS
