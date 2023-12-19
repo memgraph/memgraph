b36f4a6e4b70cd7c95dcb33c92e8fa0899cbe8d1
Deda <marko.barisic@memgraph.io>
2023-12-19 13:21:38 +0100

    Add run.sh to binary builder container

---
+++ release/binary/debian-10/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eo
+
+MG_BUILD_TYPE=${MG_BUILD_TYPE:-RelWithDebInfo}
+MG_THREADS=${MG_THREADS:-24}
+
+echo -e "\n-----BUILD_TYPE=$MG_BUILD_TYPE-----\n"
+echo -e "\n-----THREADS=$MG_THREADS-----\n"
+
+cd memgraph
+source /opt/toolchain-v4/activate
+./init
+cd build
+cmake -DCMAKE_BUILD_TYPE=$MG_BUILD_TYPE -DMG_ENTERPRISE=OFF ..
+make -j$MG_THREADS
+
