cf45b36ae01140e8d17bebb32fc5e088ef3ef455
Deda <marko.barisic@memgraph.io>
2023-12-20 21:23:40 +0100

    Make mg builder v2

--- release/binary/debian-10/run.sh
+++ release/binary/debian-10/run.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 
-set -eo
+set -eo pipefail
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 MG_BUILD_TYPE=${MG_BUILD_TYPE:-RelWithDebInfo}
 MG_THREADS=${MG_THREADS:-24}
 
-git config --global --add safe.directory /memgraph
-
-echo -e "\n-----BUILD_TYPE=$MG_BUILD_TYPE-----\n"
-echo -e "\n-----THREADS=$MG_THREADS-----\n"
-
-cd memgraph
 source /opt/toolchain-v4/activate
-
-echo -e "\n---INIT---\n"
-./init
-
-echo -e "\n---CMAKE---\n"
-cd build
+cd ../../../build
 cmake -DCMAKE_BUILD_TYPE=$MG_BUILD_TYPE -DMG_ENTERPRISE=OFF ..
-
-echo -e "\n---MAKE---\n"
 make -j$MG_THREADS
