424bd3596ede363754f6668f886223d86839b4a7
imilinovic <ivan.milinovic123@gmail.com>
2024-01-02 14:54:42 +0100

    fix storage mode flag

--- src/storage/v2/inmemory/storage.cpp
+++ src/storage/v2/inmemory/storage.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Memgraph Ltd.
+// Copyright 2024 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -153,7 +153,7 @@ InMemoryStorage::InMemoryStorage(Config config, StorageMode storage_mode)
   }
 }
 
-InMemoryStorage::InMemoryStorage(Config config) : InMemoryStorage(config, StorageMode::IN_MEMORY_TRANSACTIONAL) {}
+InMemoryStorage::InMemoryStorage(Config config) : InMemoryStorage(config, config.storage_mode) {}
 
 InMemoryStorage::~InMemoryStorage() {
   if (config_.gc.type == Config::Gc::Type::PERIODIC) {
