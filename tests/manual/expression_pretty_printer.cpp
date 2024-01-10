ade96472a669eab042411291c4015aa2fa670f48
DavIvek <david.ivekovic@memgraph.io>
2024-01-10 15:08:21 +0100

    Add query parameters support for labels (#1602)

--- tests/manual/expression_pretty_printer.cpp
+++ tests/manual/expression_pretty_printer.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Memgraph Ltd.
+// Copyright 2023 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -17,6 +17,7 @@
 #include "query/frontend/ast/cypher_main_visitor.hpp"
 #include "query/frontend/ast/pretty_print.hpp"
 #include "query/frontend/opencypher/parser.hpp"
+#include "query/parameters.hpp"
 
 std::string AssembleQueryString(const std::string &expression_string) {
   return "return " + expression_string + " as expr";
@@ -24,8 +25,9 @@ std::string AssembleQueryString(const std::string &expression_string) {
 
 memgraph::query::Query *ParseQuery(const std::string &query_string, memgraph::query::AstStorage *ast_storage) {
   memgraph::query::frontend::ParsingContext context;
+  memgraph::query::Parameters parameters;
   memgraph::query::frontend::opencypher::Parser parser(query_string);
-  memgraph::query::frontend::CypherMainVisitor visitor(context, ast_storage);
+  memgraph::query::frontend::CypherMainVisitor visitor(context, ast_storage, &parameters);
 
   visitor.visit(parser.tree());
   return visitor.query();
