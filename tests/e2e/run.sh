97f8822eac908a91ed9a02529c1965b2173f31bc
Deda <marko.barisic@memgraph.io>
2024-01-25 14:47:54 +0100

    Debug e2e tests

--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -25,6 +25,7 @@ if [ "$#" -eq 0 ]; then
   # NOTE: If you want to run all tests under specific folder/section just
   # replace the dot (root directory below) with the folder name, e.g.
   # `--workloads-root-directory replication`.
+  echo "-----RUNNING RUNNER.PY-----"
   python3 runner.py --workloads-root-directory "$SCRIPT_DIR/../../build"
 elif [ "$#" -eq 1 ]; then
   if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
