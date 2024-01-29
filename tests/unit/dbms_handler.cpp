ff44d688437ce2842de655515ff2dfca01079e21
andrejtonev <29177572+andrejtonev@users.noreply.github.com>
2024-01-29 13:52:32 +0100

    Simplify auth::Auth (#1663)
    Moved various auth flags under a single config
    Moved all regex logic under auth::Auth

--- tests/unit/dbms_handler.cpp
+++ tests/unit/dbms_handler.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Memgraph Ltd.
+// Copyright 2024 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -66,7 +66,7 @@ class TestEnvironment : public ::testing::Environment {
     }
     auth =
         std::make_unique<memgraph::utils::Synchronized<memgraph::auth::Auth, memgraph::utils::WritePrioritizedRWLock>>(
-            storage_directory / "auth");
+            storage_directory / "auth", memgraph::auth::Auth::Config{/* default */});
     ptr_ = std::make_unique<memgraph::dbms::DbmsHandler>(storage_conf, auth.get(), false);
   }
 
