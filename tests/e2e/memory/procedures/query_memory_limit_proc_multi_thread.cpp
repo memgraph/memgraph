be8b75567331805beae1c6750b7b803baf2640a8
Gareth Andrew Lloyd <gareth.lloyd@memgraph.io>
2024-02-16 15:35:08 +0000

    Fixup memory e2e tests (#1715)
    - Remove the e2e that did concurrent mgp_* calls on the same transaction
      (ATM this is unsupported)
    - Fix up the concurrent mgp_global_alloc test to be testing it more precisely
    - Reduce the memory limit on detach delete test due to recent memory
      optimizations around deltas.
    - No longer throw from hook, through jemalloc C, to our C++ on other
      side. This cause mutex unlocks to not happen.
    - No longer allocate error messages while inside the hook. This caused
      recursive entry back inside jamalloc which would try to relock a
      non-recursive mutex.

--- tests/e2e/memory/procedures/query_memory_limit_proc_multi_thread.cpp
+++ tests/e2e/memory/procedures/query_memory_limit_proc_multi_thread.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Memgraph Ltd.
+// Copyright 2024 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -13,73 +13,122 @@
 #include <cassert>
 #include <exception>
 #include <functional>
+#include <future>
 
+#include <exception>
+#include <latch>
+#include <list>
+#include <memory>
 #include <mutex>
-#include <sstream>
 #include <string>
 #include <thread>
-#include <utility>
 #include <vector>
 
 #include "mg_procedure.h"
 #include "mgp.hpp"
+#include "utils/memory_tracker.hpp"
 #include "utils/on_scope_exit.hpp"
 
-enum mgp_error Alloc(void *ptr) {
-  const size_t mb_size_268 = 1 << 28;
+using safe_ptr = std::unique_ptr<void, decltype([](void *p) { mgp_global_free(p); })>;
+enum class AllocFuncRes { NoIssues, UnableToAlloc, Unexpected };
+using result_t = std::pair<AllocFuncRes, std::list<safe_ptr>>;
 
-  return mgp_global_alloc(mb_size_268, (void **)(&ptr));
-}
+constexpr auto N_THREADS = 2;
+static_assert(N_THREADS > 0 && (N_THREADS & (N_THREADS - 1)) == 0);
 
-// change communication between threads with feature and promise
-std::atomic<int> num_allocations{0};
-std::vector<void *> ptrs_;
+constexpr auto mb_size_512 = 1 << 29;
+constexpr auto mb_size_16 = 1 << 24;
 
-void AllocFunc(mgp_graph *graph) {
+static_assert(mb_size_512 % N_THREADS == 0);
+static_assert(mb_size_16 % N_THREADS == 0);
+static_assert(mb_size_512 % mb_size_16 == 0);
+
+void AllocFunc(std::latch &start_latch, std::promise<result_t> promise, mgp_graph *graph) {
   [[maybe_unused]] const enum mgp_error tracking_error = mgp_track_current_thread_allocations(graph);
-  void *ptr = nullptr;
+  auto on_exit = memgraph::utils::OnScopeExit{[&]() {
+    [[maybe_unused]] const enum mgp_error untracking_error = mgp_untrack_current_thread_allocations(graph);
+  }};
 
-  ptrs_.emplace_back(ptr);
+  std::list<safe_ptr> ptrs;
+
+  // Ensure test would concurrently run these allocations, wait until both are ready
+  start_latch.arrive_and_wait();
+
   try {
-    enum mgp_error alloc_err { mgp_error::MGP_ERROR_NO_ERROR };
-    alloc_err = Alloc(ptr);
-    if (alloc_err != mgp_error::MGP_ERROR_UNABLE_TO_ALLOCATE) {
-      num_allocations.fetch_add(1, std::memory_order_relaxed);
-    }
-    if (alloc_err != mgp_error::MGP_ERROR_NO_ERROR) {
-      assert(false);
+    constexpr auto allocation_limit = mb_size_512 / N_THREADS;
+    // many allocation to increase chance of seeing any concurent issues
+    for (auto total = 0; total < allocation_limit; total += mb_size_16) {
+      void *ptr = nullptr;
+      auto alloc_err = mgp_global_alloc(mb_size_16, &ptr);
+      if (alloc_err != mgp_error::MGP_ERROR_UNABLE_TO_ALLOCATE && ptr != nullptr) {
+        ptrs.emplace_back(ptr);
+      } else if (alloc_err == mgp_error::MGP_ERROR_UNABLE_TO_ALLOCATE) {
+        // this is expected, the test checks N threads allocating to a limit of 512MB
+        promise.set_value({AllocFuncRes::UnableToAlloc, std::move(ptrs)});
+        return;
+      } else {
+        promise.set_value({AllocFuncRes::Unexpected, std::move(ptrs)});
+        return;
+      }
     }
   } catch (const std::exception &e) {
-    assert(false);
+    promise.set_value({AllocFuncRes::Unexpected, std::move(ptrs)});
+    return;
   }
-
-  [[maybe_unused]] const enum mgp_error untracking_error = mgp_untrack_current_thread_allocations(graph);
+  promise.set_value({AllocFuncRes::NoIssues, std::move(ptrs)});
+  return;
 }
 
 void DualThread(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   mgp::MemoryDispatcherGuard guard{memory};
   const auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
-  num_allocations.store(0, std::memory_order_relaxed);
+
+  // 1 byte allocation to
+  auto ptr = std::invoke([&] {
+    void *ptr;
+    [[maybe_unused]] auto alloc_err = mgp_global_alloc(1, &ptr);
+    return safe_ptr{ptr};
+  });
+
   try {
-    std::vector<std::thread> threads;
-
-    for (int i = 0; i < 2; i++) {
-      threads.emplace_back(AllocFunc, memgraph_graph);
-    }
-
-    for (int i = 0; i < 2; i++) {
-      threads[i].join();
-    }
-    for (void *ptr : ptrs_) {
-      if (ptr != nullptr) {
-        mgp_global_free(ptr);
+    auto futures = std::vector<std::future<result_t>>{};
+    futures.reserve(N_THREADS);
+    std::latch start_latch{N_THREADS};
+    {
+      auto threads = std::vector<std::jthread>{};
+      threads.reserve(N_THREADS);
+      for (int i = 0; i < N_THREADS; i++) {
+        auto promise = std::promise<result_t>{};
+        futures.emplace_back(promise.get_future());
+        threads.emplace_back([&, promise = std::move(promise)]() mutable {
+          AllocFunc(start_latch, std::move(promise), memgraph_graph);
+        });
       }
+    }  // ~jthread will join
+
+    int alloc_errors = 0;
+    int unexpected_errors = 0;
+    for (auto &x : futures) {
+      auto [res, ptrs] = x.get();
+      alloc_errors += (res == AllocFuncRes::UnableToAlloc);
+      unexpected_errors += (res == AllocFuncRes::Unexpected);
+      // regardless of outcome, we want this thread to do the deallocation
+      ptrs.clear();
+    }
+
+    if (unexpected_errors != 0) {
+      record_factory.SetErrorMessage("Unanticipated error happened");
+      return;
+    }
+
+    if (alloc_errors < 1) {
+      record_factory.SetErrorMessage("Didn't hit the QUERY MEMORY LIMIT we expected");
+      return;
     }
 
     auto new_record = record_factory.NewRecord();
-
-    new_record.Insert("allocated_all", num_allocations.load(std::memory_order_relaxed) == 2);
+    new_record.Insert("test_passed", true);
   } catch (std::exception &e) {
     record_factory.SetErrorMessage(e.what());
   }
@@ -90,7 +139,7 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
     mgp::memory = memory;
 
     AddProcedure(DualThread, std::string("dual_thread").c_str(), mgp::ProcedureType::Read, {},
-                 {mgp::Return(std::string("allocated_all").c_str(), mgp::Type::Bool)}, module, memory);
+                 {mgp::Return(std::string("test_passed").c_str(), mgp::Type::Bool)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
