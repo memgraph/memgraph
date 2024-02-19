a809acfd82ac185cffbbf45fa2f61262283ba177
Marko Budiselic <mbudiselicbuda@gmail.com>
2024-02-19 11:17:01 +0000

    Revert copyright only changes

--- src/audit/log.cpp
+++ src/audit/log.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Memgraph Ltd.
+// Copyright 2023 Memgraph Ltd.
 //
 // Licensed as a Memgraph Enterprise file under the Memgraph Enterprise
 // License (the "License"); by using this file, you agree to be bound by the terms of the License, and you may not use
