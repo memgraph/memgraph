67e76eac0524db1a39ed4a1e8ce7ee2cf1444f9f
Deda <marko.barisic@memgraph.io>
2024-02-27 16:43:07 +0100

    Add pip install networkx

--- tests/setup.sh
+++ tests/setup.sh
@@ -34,6 +34,7 @@ PYTHON_MINOR=$(python3 -c 'import sys; print(sys.version_info[:][1])')
 
 # install pulsar-client
 pip --timeout 1000 install "pulsar-client==3.1.0"
+pip --timeout 1000 install "networkx"
 for pkg in "${PIP_DEPS[@]}"; do
     pip --timeout 1000 install "$pkg"
 done
