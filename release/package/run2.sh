fa9024597681c657eb44b99c28859ac112b5a358
Deda <marko.barisic@memgraph.io>
2024-02-27 13:11:35 +0100

    Add alias for kafka and pulsar containers

--- release/package/run2.sh
+++ release/package/run2.sh
@@ -405,8 +405,8 @@ test_memgraph() {
     ;;
     e2e)
       local setup_hostnames="export KAFKA_HOSTNAME=kafka && PULSAR_HOSTNAME=pulsar"
-      docker network connect package_default kafka_kafka_1
-      docker network connect package_default pulsar_pulsar_1
+      docker network connect --alias kafka package_default kafka_kafka_1  > /dev/null 2>&1 || echo "Kafka container already inside correct network"
+      docker network connect --alias pulsar package_default pulsar_pulsar_1  > /dev/null 2>&1 || echo "Kafka container already inside correct network"
       docker exec -u mg $build_container bash -c "$EXPORT_LICENSE && $EXPORT_ORG_NAME && $setup_hostnames && cd $MGBUILD_ROOT_DIR/tests && $ACTIVATE_VENV && source ve3/bin/activate_e2e && cd $MGBUILD_ROOT_DIR/tests/e2e "'&& ./run.sh'
     ;;
     *)
