0076654b8de08cb6bc1262787f78bf0547218483
antoniofilipovic <filipovicantonio1998@gmail.com>
2024-02-28 12:02:51 +0100

    revert storage recover on startup

--- src/memgraph.cpp
+++ src/memgraph.cpp
@@ -311,7 +311,7 @@ int main(int argc, char **argv) {
              .interval = std::chrono::seconds(FLAGS_storage_gc_cycle_sec)},
 
       .durability = {.storage_directory = FLAGS_data_directory,
-                     .recover_on_startup = FLAGS_data_recovery_on_startup,
+                     .recover_on_startup = FLAGS_storage_recover_on_startup || FLAGS_data_recovery_on_startup,
                      .snapshot_retention_count = FLAGS_storage_snapshot_retention_count,
                      .wal_file_size_kibibytes = FLAGS_storage_wal_file_size_kib,
                      .wal_file_flush_every_n_tx = FLAGS_storage_wal_file_flush_every_n_tx,
