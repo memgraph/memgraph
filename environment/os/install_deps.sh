29c7e1d09686c266875f1ed832778e66941d2496
n0vakovic <milan.kupovina@gmail.com>
2024-03-25 23:54:50 +0000

    checking for supported OS and passing all the script args

--- environment/os/install_deps.sh
+++ environment/os/install_deps.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-# Initialize a variable to track if the --build-deps or --toolchain-deps flag is passed
-build_deps_flag=false
-toolchain_deps_flag=false
+SUPPORTED_OS=(
+    all
+    amzn-2
+    centos-7 centos-9
+    debian-10 debian-11 debian-11-arm debian-12 debian-12-arm
+    fedora-36 fedora-38 fedora-39
+    rocky-9.3
+    ubuntu-18.04 ubuntu-20.04 ubuntu-22.04 ubuntu-22.04-arm
+)
 
-# Display help if no arguments are passed
-if [ "$#" -eq 0 ]; then
+
+# Check for help argument before processing others
+if [[ "$#" -eq 1 && ("$1" == "-h" || "$1" == "--help") ]]; then
     cat <<EOF
-Please specify one or both of these config flags:
-    --build-deps        for installing Memgraph build dependencies
-    --toolchain-deps    for installing toolchain runtime dependencies
+Usage:
+    ./install_deps.sh [command] [dependency_set]
 
-If you run into issues with this script, please run corresponding commands manually and open a github issue:
-    sudo ./environment/os/debian-10.sh install TOOLCHAIN_RUN_DEPS
-    sudo ./environment/os/debian-10.sh install MEMGRAPH_BUILD_DEPS
+Commands:
+    install       Install the specified dependencies.
+    check         Check the specified dependencies.
+
+Dependency sets:
+    TOOLCHAIN_RUN_DEPS       Dependencies required for the toolchain runtime.
+    MEMGRAPH_BUILD_DEPS      Dependencies required for building Memgraph.
+
+Examples:
+    ./install_deps.sh install TOOLCHAIN_RUN_DEPS
+    ./install_deps.sh check TOOLCHAIN_RUN_DEPS
+    ./install_deps.sh install MEMGRAPH_BUILD_DEPS
+    ./install_deps.sh check MEMGRAPH_BUILD_DEPS
     
 Link to create new github issue: https://github.com/memgraph/memgraph/issues/new?title=install-deps.sh%20...&assignee=gitbuda&body=%0A%0A%0A---%0AI%27m+a+human.+Please+be+nice.
 EOF
     exit 0
 fi
 
-# Parse command-line arguments
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --build-deps) build_deps_flag=true ;;
-        --toolchain-deps) toolchain_deps_flag=true ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
-
 # Function to check and install dependencies for a given distribution script
 run_script() {
     local distro_script=$1
-    echo "Running script for $distro_script"
-    if [ "$toolchain_deps_flag" = true ]; then
-        sudo ./environment/os/$distro_script check TOOLCHAIN_RUN_DEPS
-        sudo ./environment/os/$distro_script install TOOLCHAIN_RUN_DEPS
-    fi
-    if [ "$build_deps_flag" = true ]; then
-        sudo ./environment/os/$distro_script install MEMGRAPH_BUILD_DEPS
-    fi
+    echo "Running script for $@"
+    sudo ./environment/os/"$@"
 }
 
 # Detect OS, version, and architecture
@@ -76,65 +76,27 @@ esac
 # Normalize OS name to lowercase
 OS=$(echo "$OS" | tr '[:upper:]' '[:lower:]')
 
-# Map OS, version, and architecture to script name
-case "$OS" in
-    debian)
-        case "$VER" in
-            10) run_script "debian-10.sh" ;;
-            11) 
-                if [ "$ARCH" = "arm" ]; then
-                    run_script "debian-11-arm.sh"
-                else
-                    run_script "debian-11.sh"
-                fi
-                ;;
-            12) 
-                if [ "$ARCH" = "arm" ]; then
-                    run_script "debian-12-arm.sh"
-                else
-                    run_script "debian-12.sh"
-                fi
-                ;;
-            *) echo "Unsupported Debian version: $VER"; exit 1 ;;
-        esac
-        ;;
-    ubuntu)
-        case "$VER" in
-            18.04) run_script "ubuntu-18.04.sh" ;;
-            20.04) run_script "ubuntu-20.04.sh" ;;
-            22.04) 
-                if [ "$ARCH" = "arm" ]; then
-                    run_script "ubuntu-22.04-arm.sh"
-                else
-                    run_script "ubuntu-22.04.sh"
-                fi
-                ;;
-            *) echo "Unsupported Ubuntu version: $VER"; exit 1 ;;
-        esac
-        ;;
-    centos)
-        case "$VER" in
-            7) run_script "centos-7.sh" ;;
-            9) run_script "centos-9.sh" ;;
-            *) echo "Unsupported CentOS version: $VER"; exit 1 ;;
-        esac
-        ;;
-    fedora)
-        case "$VER" in
-            36) run_script "fedora-36.sh" ;;
-            38) run_script "fedora-38.sh" ;;
-            39) run_script "fedora-39.sh" ;;
-            *) echo "Unsupported Fedora version: $VER"; exit 1 ;;
-        esac
-        ;;
-    "amzn")
-        case "$VER" in
-            2) run_script "amzn-2.sh" ;;
-            *) echo "Unsupported Amazon Linux version: $VER"; exit 1 ;;
-        esac
-        ;;
-    *)
-        echo "Unsupported OS: $OS"
-        exit 1
-        ;;
-esac
+if [[ -z "$ARCH" ]]; then
+    OS_ARCH="${OS}-${VER}"
+else
+    OS_ARCH="${OS}-${VER}-${ARCH}"
+fi
+
+OS_ARCH_SCRIPT="${OS_ARCH}.sh"
+
+# Check if OS_ARCH is in the SUPPORTED_OS array
+is_supported=false
+for supported_os in "${SUPPORTED_OS[@]}"; do
+    if [[ "$OS_ARCH" == "$supported_os" ]]; then
+        is_supported=true
+        break
+    fi
+done
+
+# If supported, run the script with all original arguments
+if [[ "$is_supported" == true ]]; then
+    run_script "$OS_ARCH_SCRIPT" "$@"
+else
+    echo "Unsupported OS/Architecture combination: $OS_ARCH"
+    exit 1
+fi
