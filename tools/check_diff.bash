dcfe0eebcd6ef8b65b9bfd798c17b5ff6c5b2e3a
Marko Barišić <marko.barisic@memgraph.io>
2024-04-19 17:01:22 +0200

    Improve comments in check_diff.bash

--- tools/check_diff.bash
+++ tools/check_diff.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-# This script checks if the diff between the current branch and the master branch
-# contains only changes to the files in the .github/workflows directory.
-# If so, it returns exit code 1, otherwise it returns exit code 0.
-# Additionally, if there are changes to files starting with 'diff' it returns 0.
+# This script checks if the diff workflow should be run.
+# Exit code 0 means the workflow should run, exit code 1 means it should not.
+# If only the files in .github/workflows directory are changed, the workflow should not run.
+# The only exception is the diff workflow itself and the workflows that are used to run the diff workflow.
 
 diff=$(git diff --name-only origin/master)
 regex_workflow='^\.github/workflows/.*\.yaml$'
 regex_diff='^\.github/workflows/diff.*\.yaml$'
-echo $diff
+echo "Changed files in comparison to master: $diff"
 for file in $diff; do
   if [[ $file =~ $regex_workflow ]] && ! [[ $file =~ $regex_diff ]]; then
     continue
