9e9a56879e39a680fe1b1f052f3a65a004446f9a
Marko Barišić <marko.barisic@memgraph.io>
2024-04-19 15:15:37 +0200

    Add check for modified files

---
+++ tools/check_diff.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script checks if the diff between the current branch and the master branch
+# contains only changes to the files in the .github/workflows directory.
+# If so, it returns exit code 1, otherwise it returns exit code 0.
+# Additionally, if there are changes to files starting with 'diff' it returns 0.
+
+diff=$(git diff --name-only master)
+regex_workflow='^\.github/workflows/.*\.yaml$'
+regex_diff='^\.github/workflows/diff.*\.yaml$'
+echo $diff
+for file in $diff; do
+  if [[ $file =~ $regex_workflow ]] && ! [[ $file =~ $regex_diff ]]; then
+    continue
+  else
+    exit 0
+  fi
+done
+exit 1
