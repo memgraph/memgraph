40b06ad409e2f37fe53f6f3a2170c204006e1044
Marko Barišić <marko.barisic@memgraph.io>
2024-04-22 08:43:37 +0200

    Rename check_diff.py to diff_setup.py

--- tools/diff_setup.py
+++ tools/diff_setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import json
 import os
 import sys
@@ -8,7 +7,7 @@ import sys
 class GHContext:
     def __init__(self, gh_context_path: str):
         self._gh_context_path = gh_context_path
-        with open(self._gh_context_path, 'r') as gh_context_file:
+        with open(self._gh_context_path, "r") as gh_context_file:
             self._gh_context = json.load(gh_context_file)
 
     def get_event_name(self) -> str:
@@ -25,7 +24,8 @@ def echo_outputs(tests: dict) -> None:
     for build, tests in tests.items():
         for test, run in tests.items():
             print(f"run_{build}_{test}={run}")
-            os.popen(f'echo run_{build}_{test}={run} >> $GITHUB_OUTPUT')
+            os.popen(f"echo run_{build}_{test}={run} >> $GITHUB_OUTPUT")
+
 
 def check_diff_workflow(base_branch: str) -> bool:
     for file in os.popen(f"git diff --name-only {base_branch}").read().splitlines():
@@ -36,35 +36,39 @@ def check_diff_workflow(base_branch: str) -> bool:
             return True
     return False
 
-def get_default_tests(value: bool=False) -> dict:
-    return {"community": {"core": value},
-            "coverage": {"core": value},
-            "debug": {"core": value, "integration": value},
-            "jepsen": {"core": value},
-            "release": {"core": value, "benchmark": value, "e2e": value, "stress": value}}
+
+def get_default_tests(value: bool = False) -> dict:
+    return {
+        "community": {"core": value},
+        "coverage": {"core": value},
+        "debug": {"core": value, "integration": value},
+        "jepsen": {"core": value},
+        "release": {"core": value, "benchmark": value, "e2e": value, "stress": value},
+    }
+
 
 def check_tests(gh_context: GHContext) -> dict:
-    event_name=gh_context.get_event_name()
-    build_tests=get_default_tests(False)
-    
-    print(f"Event name: {event_name}")
-    if event_name=="merge_group":
-        build_tests=get_default_tests(True)
+    event_name = gh_context.get_event_name()
+    build_tests = get_default_tests(False)
 
-    elif event_name=="pull_request":
-        pr_labels=gh_context.get_pr_labels()
+    print(f"Event name: {event_name}")
+    if event_name == "merge_group":
+        build_tests = get_default_tests(True)
+
+    elif event_name == "pull_request":
+        pr_labels = gh_context.get_pr_labels()
         print(f"PR labels: {pr_labels}")
         for build, tests in build_tests.items():
             for test in tests.keys():
                 if f"CI -build={build} -test={test}" in pr_labels:
                     build_tests[build][test] = True
-    
-    elif event_name=="workflow_dispatch":
-        workflow_dispatch_inputs=gh_context.get_workflow_dispatch_inputs()
+
+    elif event_name == "workflow_dispatch":
+        workflow_dispatch_inputs = gh_context.get_workflow_dispatch_inputs()
         print(f"Workflow dispatch inputs: {workflow_dispatch_inputs}")
         for build, tests in build_tests.items():
             for test in tests.keys():
-                if workflow_dispatch_inputs.get(f"{build}_{test}") == 'true':
+                if workflow_dispatch_inputs.get(f"{build}_{test}") == "true":
                     build_tests[build][test] = True
     else:
         print("Invalid event name")
@@ -74,11 +78,11 @@ def check_tests(gh_context: GHContext) -> dict:
 
 
 if __name__ == "__main__":
-    run_diff=check_diff_workflow(base_branch="origin/master")
+    run_diff = check_diff_workflow(base_branch="origin/master")
     if run_diff:
-        gh_context=GHContext(gh_context_path=sys.argv[1])
-        tests=check_tests(gh_context=gh_context)
+        gh_context = GHContext(gh_context_path=sys.argv[1])
+        tests = check_tests(gh_context=gh_context)
     else:
-        tests=get_default_tests(False)
+        tests = get_default_tests(False)
     echo_outputs(tests)
     sys.exit(0)
