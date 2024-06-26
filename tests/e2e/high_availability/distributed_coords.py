1823b7cba2874e9bcde153ad0fa697ea0a893c9f
antoniofilipovic <filipovicantonio1998@gmail.com>
2024-06-26 13:09:05 +0200

    make all runs go

--- tests/e2e/high_availability/distributed_coords.py
+++ tests/e2e/high_availability/distributed_coords.py
@@ -3764,4 +3764,4 @@ def test_one_coord_down_with_durability_resume():
 
 
 if __name__ == "__main__":
-    sys.exit(pytest.main([__file__, "-k", "test_old_main_comes_back_on_new_leader_as_replica", "-vv"]))
+    sys.exit(pytest.main([__file__, "-rA"]))
