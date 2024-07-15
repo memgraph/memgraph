e15b9bfa641e529bd614e007f4fdb44850144e05
DavIvek <david.ivekovic@memgraph.io>
2024-07-15 23:58:07 +0200

    fix durability tests

--- tests/e2e/durability/durability_with_property_compression_used.py
+++ tests/e2e/durability/durability_with_property_compression_used.py
@@ -11,24 +11,13 @@
 
 import os
 import sys
+import tempfile
+import time
 from typing import Any, Dict
 
 import interactive_mg_runner
-import mgclient
 import pytest
-from mg_utils import mg_sleep_and_assert_collection
-
-
-def execute_and_fetch_all(cursor, query: str) -> Any:
-    cursor.execute(query)
-    return cursor.fetchall()
-
-
-def connect(**kwargs) -> mgclient.Connection:
-    connection = mgclient.connect(**kwargs)
-    connection.autocommit = True
-    return connection
-
+from common import execute_and_fetch_all
 
 interactive_mg_runner.SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 interactive_mg_runner.PROJECT_DIR = os.path.normpath(
@@ -47,6 +36,8 @@ def test_durability_with_compression_on(connection):
     # 4/ Start MAIN
     # 5/ Validate property is present
 
+    data_directory = tempfile.TemporaryDirectory()
+
     MEMGRAPH_INSTANCE_DESCRIPTION_MANUAL = {
         "main": {
             "args": [
@@ -54,13 +45,14 @@ def test_durability_with_compression_on(connection):
                 "--storage-property-store-compression-enabled=true",
                 "--data-recovery-on-startup=true",
             ],
-            "log_file": "main.log",
+            "log_file": "main_durability_with_compression_on.log",
+            "data_directory": data_directory.name,
         },
     }
 
     # 0/
-    interactive_mg_runner.start(MEMGRAPH_INSTANCE_DESCRIPTION_MANUAL)
-    cursor = connection.cursor()
+    interactive_mg_runner.start(MEMGRAPH_INSTANCE_DESCRIPTION_MANUAL, "main")
+    cursor = connection(7687, "main").cursor()
 
     # 1/
     execute_and_fetch_all(
@@ -76,19 +68,19 @@ def test_durability_with_compression_on(connection):
     assert properties == [("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",)]
 
     # 3/
-    interactive_mg_runner.kill("main")
+    interactive_mg_runner.kill(MEMGRAPH_INSTANCE_DESCRIPTION_MANUAL, "main")
 
     # 4/
-    interactive_mg_runner.start(MEMGRAPH_INSTANCE_DESCRIPTION_MANUAL)
+    interactive_mg_runner.start(MEMGRAPH_INSTANCE_DESCRIPTION_MANUAL, "main")
+    cursor = connection(7687, "main").cursor()
 
     # 5/
-    cursor = connection.cursor()
     properties = get_properties(cursor)
 
     assert len(properties) == 1
     assert properties == [("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",)]
 
-    interactive_mg_runner.stop("main")
+    interactive_mg_runner.stop(MEMGRAPH_INSTANCE_DESCRIPTION_MANUAL, "main")
 
 
 if __name__ == "__main__":
