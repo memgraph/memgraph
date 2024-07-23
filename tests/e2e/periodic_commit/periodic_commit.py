1e6f2b471b7cd59a30667b2a5656f59e72a9ed9d
Josip Mrden <josip.mrden@memgraph.io>
2024-07-23 13:11:11 +0200

    Add periodic e2e test

---
+++ tests/e2e/periodic_commit/periodic_commit.py
@@ -0,0 +1,45 @@
+# Copyright 2023 Memgraph Ltd.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+# License, and you may not use this file except in compliance with the Business Source License.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0, included in the file
+# licenses/APL.txt.
+
+import sys
+
+import pytest
+from common import memgraph
+
+
+@pytest.mark.parametrize("ba_commit", ["BEFORE COMMIT", "AFTER COMMIT"])
+def test_create_trigger_on_create_periodic_commit(ba_commit, memgraph):
+    """
+    Args:
+        ba_commit (str): BEFORE OR AFTER commit
+    """
+
+    QUERY_TRIGGER_CREATE = f"""
+        CREATE TRIGGER CreateOnCreateTrigger
+        ON () CREATE
+        {ba_commit}
+        EXECUTE
+        CREATE (n:TriggerCreated)
+    """
+
+    # Setup queries
+    memgraph.execute(QUERY_TRIGGER_CREATE)
+    memgraph.execute("UNWIND range(1, 10) as x CALL { CREATE (n:PeriodicCommitCreated) } IN TRANSACTIONS OF 1 ROWS;")
+
+    actual = list(memgraph.execute_and_fetch("MATCH (n:PeriodicCommitCreated) RETURN count(n) as cnt"))[0]["cnt"]
+    assert actual == 10
+
+    actual = list(memgraph.execute_and_fetch("MATCH (n:TriggerCreated) RETURN count(n) as cnt"))[0]["cnt"]
+    assert actual == 10
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__, "-rA"]))
