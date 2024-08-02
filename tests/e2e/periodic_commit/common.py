bc674be2f635cf36414af489264d9ce79db759fd
Josip Mrden <josip.mrden@memgraph.io>
2024-08-02 16:00:54 +0200

    Add periodic commit abort test

--- tests/e2e/periodic_commit/common.py
+++ tests/e2e/periodic_commit/common.py
@@ -9,13 +9,37 @@
 # by the Apache License, Version 2.0, included in the file
 # licenses/APL.txt.
 
+import typing
+
+import mgclient
 import pytest
 from gqlalchemy import Memgraph
+
+
+def execute_and_fetch_all(cursor: mgclient.Cursor, query: str, params: dict = {}) -> typing.List[tuple]:
+    cursor.execute(query, params)
+    return cursor.fetchall()
+
+
+def connect(**kwargs) -> mgclient.Connection:
+    connection = mgclient.connect(host="localhost", port=7687, **kwargs)
+    connection.autocommit = False
+    return connection
+
+
+def connect_with_autocommit(**kwargs) -> mgclient.Connection:
+    connection = mgclient.connect(host="localhost", port=7687, **kwargs)
+    connection.autocommit = True
+    return connection
 
 
 @pytest.fixture
 def memgraph(**kwargs) -> Memgraph:
     memgraph = Memgraph()
+
+    memgraph.drop_indexes()
+    memgraph.ensure_constraints([])
+    memgraph.drop_database()
 
     yield memgraph
 
--- tests/e2e/periodic_commit/periodic_commit.py
+++ tests/e2e/periodic_commit/periodic_commit.py
@@ -12,40 +12,66 @@
 import sys
 
 import pytest
-from common import memgraph
+from common import connect, connect_with_autocommit, execute_and_fetch_all, memgraph
+from mgclient import DatabaseError
+
+# def test_create_trigger_on_create_periodic_commit(memgraph):
+#     QUERY_TRIGGER_CREATE = f"""
+#         CREATE TRIGGER CreateOnCreateTrigger
+#         ON () CREATE
+#         BEFORE COMMIT
+#         EXECUTE
+#         UNWIND createdVertices AS createdVertex
+#         CREATE (n:TriggerCreated)
+#     """
+
+#     # Setup queries
+#     memgraph.execute(QUERY_TRIGGER_CREATE)
+#     memgraph.execute("UNWIND range(1, 10) as x CALL { CREATE (n:PeriodicCommitCreated) } IN TRANSACTIONS OF 1 ROWS;")
+
+#     actual = list(memgraph.execute_and_fetch("MATCH (n:PeriodicCommitCreated) RETURN count(n) as cnt"))[0]["cnt"]
+#     assert actual == 10
+
+#     actual = list(memgraph.execute_and_fetch("MATCH (n:TriggerCreated) RETURN count(n) as cnt"))[0]["cnt"]
+#     assert actual == 10
 
 
-def test_create_trigger_on_create_periodic_commit(memgraph):
-    QUERY_TRIGGER_CREATE = f"""
-        CREATE TRIGGER CreateOnCreateTrigger
-        ON () CREATE
-        BEFORE COMMIT
-        EXECUTE
-        UNWIND createdVertices AS createdVertex
-        CREATE (n:TriggerCreated)
-    """
+# def test_periodic_commit_uses_same_iterator_like_initial_transaction(memgraph):
+#     memgraph.execute("CREATE INDEX ON :Node(id)")
+#     memgraph.execute("CREATE (:Node {id: 1})")
+#     memgraph.execute("CREATE (:Node {id: 2})")
 
-    # Setup queries
-    memgraph.execute(QUERY_TRIGGER_CREATE)
-    memgraph.execute("UNWIND range(1, 10) as x CALL { CREATE (n:PeriodicCommitCreated) } IN TRANSACTIONS OF 1 ROWS;")
+#     memgraph.execute("USING PERIODIC COMMIT 1 UNWIND range(1, 3) as i MATCH (n:Node {id: i}) SET n.id = i + 2")
+#     results = list(memgraph.execute_and_fetch("MATCH (n) RETURN n.id AS id ORDER BY id"))
 
-    actual = list(memgraph.execute_and_fetch("MATCH (n:PeriodicCommitCreated) RETURN count(n) as cnt"))[0]["cnt"]
-    assert actual == 10
-
-    actual = list(memgraph.execute_and_fetch("MATCH (n:TriggerCreated) RETURN count(n) as cnt"))[0]["cnt"]
-    assert actual == 10
+#     assert results[0]["id"] == 3
+#     assert results[1]["id"] == 4
 
 
-def test_periodic_commit_uses_same_iterator_like_initial_transaction(memgraph):
-    memgraph.execute("CREATE INDEX ON :Node(id)")
-    memgraph.execute("CREATE (:Node {id: 1})")
-    memgraph.execute("CREATE (:Node {id: 2})")
+def test_periodic_commit_acid_guarantee_in_batches(memgraph):
+    memgraph.execute("CREATE ({id: 1})")
+    memgraph.execute("CREATE ({id: 2})")
+    memgraph.execute("CREATE ({id: 3})")
+    memgraph.execute("CREATE ({id: 4})")
 
-    memgraph.execute("USING PERIODIC COMMIT 1 UNWIND range(1, 3) as i MATCH (n:Node {id: i}) SET n.id = i + 2")
-    results = list(memgraph.execute_and_fetch("MATCH (n) RETURN n.id AS id ORDER BY id"))
+    connection1 = connect()
+    connection2 = connect_with_autocommit()
+    cursor1 = connection1.cursor()
+    cursor2 = connection2.cursor()
 
-    assert results[0]["id"] == 3
-    assert results[1]["id"] == 4
+    execute_and_fetch_all(cursor1, "MATCH (n {id: 3}) SET n.id = 5")
+
+    with pytest.raises(DatabaseError):
+        execute_and_fetch_all(cursor2, "USING PERIODIC COMMIT 1 MATCH (n) SET n.id = n.id + 100")
+
+    connection1.commit()
+
+    results = list(memgraph.execute_and_fetch("MATCH (n) RETURN n.id as id"))
+    assert len(results) == 4
+    assert results[0]["id"] == 101
+    assert results[1]["id"] == 102
+    assert results[2]["id"] == 5
+    assert results[3]["id"] == 4
 
 
 if __name__ == "__main__":
