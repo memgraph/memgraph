463318e4ca4b94903a43b9400f62283eefb9d033
Josip Mrden <josip.mrden@memgraph.io>
2024-08-02 12:54:33 +0200

    Refactor replication tests

--- tests/e2e/replication/replicate_periodic_commit.py
+++ tests/e2e/replication/replicate_periodic_commit.py
@@ -9,14 +9,20 @@
 # by the Apache License, Version 2.0, included in the file
 # licenses/APL.txt.
 
+import atexit
 import os
+import shutil
 import sys
+import tempfile
+import time
+from functools import partial
+from typing import Any, Dict
 
 import interactive_mg_runner
 import mgclient
 import pytest
 from common import execute_and_fetch_all
-from mg_utils import mg_sleep_and_assert_collection
+from mg_utils import mg_sleep_and_assert, mg_sleep_and_assert_collection
 
 interactive_mg_runner.SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 interactive_mg_runner.PROJECT_DIR = os.path.normpath(
@@ -26,8 +32,8 @@ interactive_mg_runner.BUILD_DIR = os.path.normpath(os.path.join(interactive_mg_r
 interactive_mg_runner.MEMGRAPH_BINARY = os.path.normpath(os.path.join(interactive_mg_runner.BUILD_DIR, "memgraph"))
 
 
-BOLT_PORTS = {"main": 7687, "replica_1": 7688}
-REPLICATION_PORTS = {"replica_1": 10001}
+BOLT_PORTS = {"main": 7687, "replica_1": 7688, "replica_2": 7689}
+REPLICATION_PORTS = {"replica_1": 10001, "replica_2": 10002}
 
 
 def show_replicas_func(cursor):
@@ -44,34 +50,8 @@ def test_periodic_commit_replication(connection):
     # 2/ Validate data has arrived at REPLICA
     # 3/ Validate you can't periodically commit to replica
 
-    MEMGRAPH_INSTANCES_DESCRIPTION_MANUAL = {
-        "replica_1": {
-            "args": [
-                "--bolt-port",
-                f"{BOLT_PORTS['replica_1']}",
-                "--log-level=TRACE",
-            ],
-            "log_file": "replica1.log",
-            "setup_queries": [
-                f"SET REPLICATION ROLE TO REPLICA WITH PORT {REPLICATION_PORTS['replica_1']};",
-            ],
-        },
-        "main": {
-            "args": [
-                "--bolt-port",
-                f"{BOLT_PORTS['main']}",
-                "--log-level=TRACE",
-            ],
-            "log_file": "main.log",
-            "setup_queries": [
-                f"REGISTER REPLICA replica_1 SYNC TO '127.0.0.1:{REPLICATION_PORTS['replica_1']}';",
-            ],
-        },
-    }
-
     # 0/
-    interactive_mg_runner.start_all(MEMGRAPH_INSTANCES_DESCRIPTION_MANUAL)
-    cursor = connection(BOLT_PORTS["main"], "main").cursor()
+    cursor = connection(7687, "main").cursor()
 
     # 1/
     execute_and_fetch_all(cursor, "USING PERIODIC COMMIT 1 UNWIND range(1, 3) AS x CREATE (:A {id: x})")
@@ -80,11 +60,18 @@ def test_periodic_commit_replication(connection):
     expected_data = [
         (
             "replica_1",
-            f"127.0.0.1:{REPLICATION_PORTS['replica_1']}",
+            "127.0.0.1:10001",
             "sync",
             {"behind": None, "status": "ready", "ts": 0},
             {"memgraph": {"behind": 0, "status": "ready", "ts": 6}},
-        )
+        ),
+        (
+            "replica_2",
+            "127.0.0.1:10002",
+            "async",
+            {"behind": None, "status": "ready", "ts": 0},
+            {"memgraph": {"behind": 0, "status": "ready", "ts": 6}},
+        ),
     ]
 
     mg_sleep_and_assert_collection(expected_data, show_replicas_func(cursor))
@@ -92,15 +79,13 @@ def test_periodic_commit_replication(connection):
     def get_periodic_commit_data(cursor):
         return execute_and_fetch_all(cursor, f"MATCH (n) RETURN n.id AS id;")
 
-    cursor_replica = connection(BOLT_PORTS["replica_1"], "replica").cursor()
+    cursor_replica = connection(7688, "replica").cursor()
     replica_1_data = get_periodic_commit_data(cursor_replica)
     assert replica_1_data == [(1,), (2,), (3,)]
 
-    with pytest.raises(mgclient.DatabaseError):
-        execute_and_fetch_all(cursor_replica, "USING PERIODIC COMMIT 1 CREATE (n)")
-
-    with pytest.raises(mgclient.DatabaseError):
-        execute_and_fetch_all(cursor_replica, "USING PERIODIC COMMIT 1 CALL { CREATE (n) } IN TRANSACTIONS OF 1 ROWS")
+    cursor_replica_2 = connection(7689, "replica").cursor()
+    replica_2_data = get_periodic_commit_data(cursor_replica_2)
+    assert replica_2_data == [(1,), (2,), (3,)]
 
 
 if __name__ == "__main__":
