91575bb9120ee81b80bfd2c8f1f1a7a4d1baf681
DavIvek <david.ivekovic@memgraph.io>
2024-09-06 19:07:28 +0200

    renaming

--- src/storage/v2/inmemory/storage.cpp
+++ src/storage/v2/inmemory/storage.cpp
@@ -1356,10 +1356,10 @@ void InMemoryStorage::InMemoryAccessor::Abort() {
                   if (!current_value.IsNull()) {
                     for (const auto &edge_type : edge_types->second) {
                       auto *from_vertex = current->property.out_vertex;
-                      for (const auto &in_edge : from_vertex->out_edges) {
-                        if (std::get<0>(in_edge) == edge_type) {
+                      for (const auto &out_edge : from_vertex->out_edges) {
+                        if (std::get<0>(out_edge) == edge_type) {
                           edge_property_cleanup[std::make_pair(edge_type, current->property.key)].emplace_back(
-                              from_vertex, std::get<1>(in_edge), edge, std::move(current_value));
+                              from_vertex, std::get<1>(out_edge), edge, std::move(current_value));
                         }
                       }
                     }
