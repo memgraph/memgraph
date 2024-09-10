54331b0c39dc58cdcc0b387733fb7e908dabbc3b
Gareth Andrew Lloyd <gareth.lloyd@memgraph.io>
2024-09-10 11:51:13 +0100

    Fix perf regression (#2316)
    The logging code for every session had an expensive call, even when no
    logging was being done.
    
    ---------
    
    Co-authored-by: imilinovic <ivan.milinovic123@gmail.com>

--- src/communication/bolt/v1/states/handlers.hpp
+++ src/communication/bolt/v1/states/handlers.hpp
@@ -296,16 +296,17 @@ State HandleRunV4(TSession &session, const State state, const Marker marker) {
   } catch (const std::exception &e) {
     return HandleFailure(session, e);
   }
-
+  if (spdlog::should_log(spdlog::level::debug)) {
 #if MG_ENTERPRISE
-  if (memgraph::license::global_license_checker.IsEnterpriseValidFast()) {
-    spdlog::debug("[Run - {}] '{}'", session.GetCurrentDB(), logging::MaskSensitiveInformation(query.ValueString()));
-  } else {
-    spdlog::debug("[Run - {}] '{}'", session.GetCurrentDB(), query.ValueString());
-  }
+    if (memgraph::license::global_license_checker.IsEnterpriseValidFast()) {
+      spdlog::debug("[Run - {}] '{}'", session.GetCurrentDB(), logging::MaskSensitiveInformation(query.ValueString()));
+    } else {
+      spdlog::debug("[Run - {}] '{}'", session.GetCurrentDB(), query.ValueString());
+    }
 #else
-  spdlog::debug("[Run] '{}'", query.ValueString());
+    spdlog::debug("[Run] '{}'", query.ValueString());
 #endif
+  }
 
   // Increment number of queries in the metrics
   IncrementQueryMetrics(session);
