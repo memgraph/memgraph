7212d30d0de247d166c2cb1f0f5632cc72438e90
Andi Skrgat <andi8647@gmail.com>
2024-09-25 08:50:21 +0200

    Stop_all -> kill_all for replication tests

--- tests/e2e/replication/show_while_creating_invalid_state.py
+++ tests/e2e/replication/show_while_creating_invalid_state.py
@@ -33,7 +33,7 @@ def cleanup_after_test():
     # Run the test
     yield
     # Stop + delete directories after running the test
-    interactive_mg_runner.stop_all(keep_directories=False)
+    interactive_mg_runner.kill_all(keep_directories=False)
 
 
 @pytest.fixture
--- tests/e2e/system_replication/auth.py
+++ tests/e2e/system_replication/auth.py
@@ -42,7 +42,7 @@ def cleanup_after_test():
     # Run the test
     yield
     # Stop + delete directories after running the test
-    interactive_mg_runner.stop_all(keep_directories=False)
+    interactive_mg_runner.kill_all(keep_directories=False)
 
 
 def update_to_main(cursor):
