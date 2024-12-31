f2f6f4eac5c0ba6faa2e5fce2c9292c38b4e9b03
Marko Budiselic <mbudiselicbuda@gmail.com>
2024-12-31 18:20:35 +0000

    Add the cleanup stuff for kafka client

--- environment/toolchain/v6/clean.sh
+++ environment/toolchain/v6/clean.sh
@@ -46,6 +46,7 @@ PREFIX=/opt/toolchain-v6
 # rm -rf "$PREFIX/lib/linuraft.a"
 # rm -rf "$PREFIX/lib/libantlr4-runtime.a"
 # rm -rf "$PREFIX/lib/libprotobuf.a"
-# rm -rf "$PREFIX/lib/libpulsarwithdeps.a
+# rm -rf "$PREFIX/lib/libpulsarwithdeps.a"
+# rm -rf "$PREFIX/lib/librdkafka++.a"
 
 # rm -rf "$PREFIX"
