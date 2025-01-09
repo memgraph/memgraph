8313709859b701315a46c9736d09a634d33eb57a
andrejtonev <29177572+andrejtonev@users.noreply.github.com>
2025-01-09 17:45:06 +0100

    Remove EdgeSetFrom, EdgeSetTo, EdgeChangeType (#2530)
    These have been deemed low value and add extra complexity to other parts
    of the code base.
    Its an important invariant that an edge during its lifetime is only ever
    be owned by same vertices as when the edge was created.
    This is important for the correct handling of edges. Among other things,
    indexing and edge deletion require that the edge's type, source and
    destination are invariant.
    
    ---------
    
    Co-authored-by: Gareth Lloyd <gareth.lloyd@memgraph.io>

--- src/storage/v2/inmemory/storage.hpp
+++ src/storage/v2/inmemory/storage.hpp
@@ -283,12 +283,6 @@ class InMemoryStorage final : public Storage {
     std::optional<EdgeAccessor> FindEdge(Gid gid, View view, EdgeTypeId edge_type, VertexAccessor *from_vertex,
                                          VertexAccessor *to_vertex) override;
 
-    Result<EdgeAccessor> EdgeSetFrom(EdgeAccessor *edge, VertexAccessor *new_from) override;
-
-    Result<EdgeAccessor> EdgeSetTo(EdgeAccessor *edge, VertexAccessor *new_to) override;
-
-    Result<EdgeAccessor> EdgeChangeType(EdgeAccessor *edge, EdgeTypeId new_edge_type) override;
-
     bool LabelIndexExists(LabelId label) const override {
       return static_cast<InMemoryStorage *>(storage_)->indices_.label_index_->IndexExists(label);
     }
