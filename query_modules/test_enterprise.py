12dcd9651702d34a471f0b2634e5b43173555532
imilinovic <ivan.milinovic123@gmail.com>
2025-01-13 15:09:31 +0100

    Don't load enterprise modules

--- query_modules/test_enterprise.py
+++
@@ -1,8 +0,0 @@
-import mgp
-
-@mgp.read_proc
-def is_enterprise_valid(context: mgp.ProcCtx) -> mgp.Record(valid=bool):
-    """
-    A procedure that checks if the enterprise license is valid.
-    """
-    return mgp.Record(valid=mgp.is_enterprise_valid())
