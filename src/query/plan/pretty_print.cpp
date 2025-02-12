1380a22461976f52e733d12090ccd5763b812838
Colin Barry <colin.barry@memgraph.io>
2025-02-12 17:53:03 +0000

    test: Fix tests for plan pretty print
    This reverts the public facing name of the `arg1` and `arg2` aggregation
    parameters, instead referring to them as they were before, i.e.:
    - `key`, `value` in the case of `map(,)`
    - `value` otherwise
    
    And for the new function:
    - `nodes`, `relationships` in the case of `project(,)`

--- src/query/plan/pretty_print.cpp
+++ src/query/plan/pretty_print.cpp
@@ -441,12 +441,26 @@ json ToJson(const EdgeCreationInfo &edge_info, const DbAccessor &dba) {
 
 json ToJson(const Aggregate::Element &elem, const DbAccessor &dba) {
   json json;
-  if (elem.arg1) {
-    json["arg1"] = ToJson(elem.arg1, dba);
+  if (elem.op == Aggregation::Op::PROJECT_LISTS) {
+    if (elem.arg1) {
+      json["nodes"] = ToJson(elem.arg1, dba);
+    }
+    if (elem.arg2) {
+      json["relationships"] = ToJson(elem.arg2, dba);
+    }
+  } else if (elem.op == Aggregation::Op::COLLECT_MAP) {
+    if (elem.arg1) {
+      json["value"] = ToJson(elem.arg1, dba);
+    }
+    if (elem.arg2) {
+      json["key"] = ToJson(elem.arg2, dba);
+    }
+  } else {
+    if (elem.arg1) {
+      json["value"] = ToJson(elem.arg1, dba);
+    }
   }
-  if (elem.arg2) {
-    json["arg2"] = ToJson(elem.arg2, dba);
-  }
+
   json["op"] = utils::ToLowerCase(Aggregation::OpToString(elem.op));
   json["output_symbol"] = ToJson(elem.output_sym);
   json["distinct"] = elem.distinct;
