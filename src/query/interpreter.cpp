fdbb97bf839234776b993794b8d4665f9ea7cf3f
Andreja Tonev <andreja.tonev@memgraph.io>
2025-02-26 19:43:16 +0100

    Expand TTL to edges

--- src/query/interpreter.cpp
+++ src/query/interpreter.cpp
@@ -3516,6 +3516,9 @@ PreparedQuery PrepareTtlQuery(ParsedQuery parsed_query, bool in_explicit_transac
 
           if (!ttl.Enabled()) {
             (void)dba->CreateIndex(label, prop);  // Only way to fail is to try to create an already existant index
+            if (db_acc->config().salient.items.property_store_compression_enabled) {
+              (void)dba->CreateGlobalEdgeIndex(prop);  // Only way to fail is to try to create an already existant index
+            }
             ttl.Enable();
             std::invoke(invalidate_plan_cache);
           }
@@ -3535,6 +3538,9 @@ PreparedQuery PrepareTtlQuery(ParsedQuery parsed_query, bool in_explicit_transac
       handler = [db_acc = std::move(db_acc), dba, label, prop,
                  invalidate_plan_cache = std::move(invalidate_plan_cache)](Notification &notification) mutable {
         (void)dba->DropIndex(label, prop);  // Only way to fail is to try to drop a non-existant index
+        if (db_acc->config().salient.items.property_store_compression_enabled) {
+          (void)dba->DropGlobalEdgeIndex(prop);  // Only way to fail is to try to drop a non-existant index
+        }
         const utils::OnScopeExit invalidator(invalidate_plan_cache);
         db_acc->ttl().Disable();
         notification.code = NotificationCode::DISABLE_TTL;
