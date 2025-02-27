6d7e1428163cb965f67b0b59aa846dd974bacfcb
Gareth Lloyd <gareth.lloyd@memgraph.io>
2025-02-27 16:18:25 +0000

    wip: adding composite index definitions

--- tests/concurrent/storage_indices.cpp
+++ tests/concurrent/storage_indices.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Memgraph Ltd.
+// Copyright 2025 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -119,7 +119,7 @@ TEST(Storage, LabelPropertyIndex) {
   auto prop = store->NameToProperty("prop");
   {
     auto unique_acc = store->UniqueAccess();
-    ASSERT_FALSE(unique_acc->CreateIndex(label, prop).HasError());
+    ASSERT_FALSE(unique_acc->CreateIndex(label, {prop}).HasError());
   }
 
   std::vector<std::thread> verifiers;
