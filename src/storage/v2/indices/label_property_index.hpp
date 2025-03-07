e1c8fc42263ac7c53e2f2594e4f8056c4c159c0f
Gareth Lloyd <gareth.lloyd@memgraph.io>
2025-03-07 16:04:05 +0000

    wip: change index hints + index existance in prep for composite

--- src/storage/v2/indices/label_property_index.hpp
+++ src/storage/v2/indices/label_property_index.hpp
@@ -18,6 +18,13 @@
 
 namespace memgraph::storage {
 
+// These possitions are in refernce to the
+// labels + properties passed into `RelevantLabelPropertiesIndicesInfo`
+struct LabelPropertiesIndicesInfo {
+  std::size_t label_pos_;
+  std::vector<long> properties_pos_;  // -1 means missing
+};
+
 class LabelPropertyIndex {
  public:
   struct IndexStats {
@@ -44,6 +51,10 @@ class LabelPropertyIndex {
   virtual bool DropIndex(LabelId label, std::vector<PropertyId> const &properties) = 0;
 
   virtual bool IndexExists(LabelId label, PropertyId property /*TODO*/) const = 0;
+
+  virtual auto RelevantLabelPropertiesIndicesInfo(std::span<LabelId const> labels,
+                                                  std::span<PropertyId const> properties) const
+      -> std::vector<LabelPropertiesIndicesInfo> = 0;
 
   virtual std::vector<std::pair<LabelId, PropertyId /*TODO*/>> ListIndices() const = 0;
   virtual std::vector<std::pair<LabelId, std::vector<PropertyId>>> ListIndicesNew() const = 0;
