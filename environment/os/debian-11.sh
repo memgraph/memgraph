225e19ddf6330049f7f8061dee8aede3a5626721
matt <mattkjames7@gmail.com>
2025-03-10 16:44:40 +0000

    added tzdata to debian 11 run deps

--- environment/os/debian-11.sh
+++ environment/os/debian-11.sh
@@ -39,6 +39,7 @@ TOOLCHAIN_RUN_DEPS=(
     libreadline8 # for cmake and llvm
     libffi7 libxml2 # for llvm
     libssl-dev # for libevent
+    tzdata # for timezone bugfix
 )
 
 MEMGRAPH_BUILD_DEPS=(
@@ -165,6 +166,9 @@ EOF
                 apt-get install -y apt-transport-https dotnet-sdk-3.1
             fi
             continue
+        fi
+        if [ "$pkg" == tzdata ]; then
+            apt install -y tzdata=2024a-0+deb11u1 # specific version for timezone bug
         fi
         apt install -y "$pkg"
     done
