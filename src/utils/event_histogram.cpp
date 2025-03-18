a6a4fb5b3164672be6207cd13da41d87efc8a108
Andi Skrgat <andi8647@gmail.com>
2025-03-18 10:27:28 +0100

    Move socket connect to the general

--- src/utils/event_histogram.cpp
+++ src/utils/event_histogram.cpp
@@ -24,7 +24,7 @@
   M(InstanceSuccCallback_us, HighAvailability, "Instance success callback in microseconds", 50, 90, 99)           \
   M(InstanceFailCallback_us, HighAvailability, "Instance failure callback in microseconds", 50, 90, 99)           \
   M(ChooseMostUpToDateInstance_us, HighAvailability, "Latency of choosing next main in microseconds", 50, 90, 99) \
-  M(SocketConnect_us, HighAvailability, "Latency of Socket::Connect in microseconds", 50, 90, 99)                 \
+  M(SocketConnect_us, General, "Latency of Socket::Connect in microseconds", 50, 90, 99)                 \
   M(ReplicaStream_us, HighAvailability, "Latency of creating replica stream in microseconds", 50, 90, 99)         \
   GenerateRpcTimer(PromoteToMainRpc)                                                                              \
   GenerateRpcTimer(DemoteMainToReplicaRpc)                                                                        \
