dc840f5933d3ce84ff0c58474bc52c9dfe6117ff
imilinovic <ivan.milinovic123@gmail.com>
2025-03-19 21:36:33 +0100

    Move path to unique_ptr

--- src/query/typed_value.hpp
+++ src/query/typed_value.hpp
@@ -34,7 +34,7 @@ class Graph;  // fwd declare
 
 namespace {
 template <typename T>
-concept TypedValueValidPrimativeType =
+concept TypedValueValidPrimitiveType =
     std::is_same_v<T, bool> || std::is_same_v<T, int> || std::is_same_v<T, int64_t> || std::is_same_v<T, double> ||
     std::is_same_v<T, storage::Enum> || std::is_same_v<T, utils::Date> || std::is_same_v<T, utils::LocalTime> ||
     std::is_same_v<T, utils::LocalDateTime> || std::is_same_v<T, utils::ZonedDateTime> ||
@@ -251,7 +251,7 @@ class TypedValue {
   }
 
   template <class T>
-  requires TypedValueValidPrimativeType<T>
+  requires TypedValueValidPrimitiveType<T>
   explicit TypedValue(const std::vector<T> &value, utils::MemoryResource *memory = utils::NewDeleteResource())
       : memory_(memory), type_(Type::List) {
     new (&list_v) TVector(value.begin(), value.end(), memory_);
@@ -313,7 +313,8 @@ class TypedValue {
 
   explicit TypedValue(const Path &path, utils::MemoryResource *memory = utils::NewDeleteResource())
       : memory_(memory), type_(Type::Path) {
-    new (&path_v) Path(path, memory_);
+    auto *path_ptr = utils::Allocator<Path>(memory_).new_object<Path>(path);
+    path_v = std::unique_ptr<Path>(path_ptr);
   }
 
   /** Construct a copy using default utils::NewDeleteResource() */
@@ -431,16 +432,15 @@ class TypedValue {
    * utils::MemoryResource is obtained from path. After the move, path will be
    * left empty.
    */
-  explicit TypedValue(Path &&path) noexcept : TypedValue(std::move(path), path.GetMemoryResource()) {}
+  explicit TypedValue(Path &&path);
+  // explicit TypedValue(Path &&path) noexcept : TypedValue(std::move(path), path.GetMemoryResource()) {}
 
   /**
    * Construct with the value of path and use the given MemoryResource.
    * If `*path.GetMemoryResource() != *memory`, this call will perform an
    * element-wise move and path is not guaranteed to be empty.
    */
-  TypedValue(Path &&path, utils::MemoryResource *memory) : memory_(memory), type_(Type::Path) {
-    new (&path_v) Path(std::move(path), memory_);
-  }
+  TypedValue(Path &&path, utils::MemoryResource *memory);
 
   /**
    * Construct with the value of graph.
@@ -541,7 +541,7 @@ class TypedValue {
   DECLARE_VALUE_AND_TYPE_GETTERS(TMap, Map, map_v)
   DECLARE_VALUE_AND_TYPE_GETTERS(VertexAccessor, Vertex, vertex_v)
   DECLARE_VALUE_AND_TYPE_GETTERS(EdgeAccessor, Edge, edge_v)
-  DECLARE_VALUE_AND_TYPE_GETTERS(Path, Path, path_v)
+  DECLARE_VALUE_AND_TYPE_GETTERS(Path, Path, *path_v)
 
   DECLARE_VALUE_AND_TYPE_GETTERS(utils::Date, Date, date_v)
   DECLARE_VALUE_AND_TYPE_GETTERS(utils::LocalTime, LocalTime, local_time_v)
@@ -591,7 +591,7 @@ class TypedValue {
     TMap map_v;
     VertexAccessor vertex_v;
     EdgeAccessor edge_v;
-    Path path_v;
+    std::unique_ptr<Path> path_v;
     utils::Date date_v;
     utils::LocalTime local_time_v;
     utils::LocalDateTime local_date_time_v;
