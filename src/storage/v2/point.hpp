d16df664a36430f95269b49d853629f809e7dd06
Gareth Lloyd <gareth.lloyd@memgraph.io>
2025-03-19 17:14:01 +0000

    wip three way cmp on property value

--- src/storage/v2/point.hpp
+++ src/storage/v2/point.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Memgraph Ltd.
+// Copyright 2025 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -136,12 +136,8 @@ struct Point2d {
   friend bool operator==(Point2d const &, Point2d const &) = default;
 
   friend auto operator<=>(Point2d const &lhs, Point2d const &rhs) -> std::partial_ordering {
-    if (lhs.crs_ != rhs.crs_) return std::partial_ordering::unordered;
-
-    const auto cmp_x = lhs.x_longitude_ <=> rhs.x_longitude_;
-    const auto cmp_y = lhs.y_latitude_ <=> rhs.y_latitude_;
-
-    return (cmp_x == cmp_y) ? cmp_x : std::partial_ordering::unordered;
+    return std::tie(lhs.crs_, lhs.x_longitude_, lhs.y_latitude_) <=>
+           std::tie(rhs.crs_, rhs.x_longitude_, rhs.y_latitude_);
   }
 
  private:
@@ -169,13 +165,8 @@ struct Point3d {
   friend bool operator==(Point3d const &A, Point3d const &B) = default;
 
   friend auto operator<=>(Point3d const &lhs, Point3d const &rhs) -> std::partial_ordering {
-    if (lhs.crs_ != rhs.crs_) return std::partial_ordering::unordered;
-
-    const auto cmp_x = lhs.x_longitude_ <=> rhs.x_longitude_;
-    const auto cmp_y = lhs.y_latitude_ <=> rhs.y_latitude_;
-    const auto cmp_z = lhs.z_height_ <=> rhs.z_height_;
-
-    return (cmp_x == cmp_y && cmp_x == cmp_z) ? cmp_x : std::partial_ordering::unordered;
+    return std::tie(lhs.crs_, lhs.x_longitude_, lhs.y_latitude_, lhs.z_height_) <=>
+           std::tie(rhs.crs_, rhs.x_longitude_, rhs.y_latitude_, rhs.z_height_);
   }
 
  private:
