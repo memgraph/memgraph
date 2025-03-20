aa4f6552f3b45e0b978a3b6029613f6d539c8896
matt <mattkjames7@gmail.com>
2025-03-20 16:34:38 +0000

    added some missing dependencies

--- environment/os/ubuntu-20.04.sh
+++ environment/os/ubuntu-20.04.sh
@@ -25,6 +25,10 @@ TOOLCHAIN_BUILD_DEPS=(
     gperf # for proxygen
     libssl-dev
     libedit-dev libpcre2-dev libpcre3-dev automake bison # for swig
+    custom-rust
+    libtool # for protobuf
+    pkg-config # for pulsar
+    libsasl2-dev # for librdkafka
 )
 
 TOOLCHAIN_RUN_DEPS=(
@@ -36,6 +40,7 @@ TOOLCHAIN_RUN_DEPS=(
     libreadline8 # for cmake and llvm
     libffi7 libxml2 # for llvm
     libssl-dev # for libevent
+    tzdata # for timezone bug
 )
 
 MEMGRAPH_BUILD_DEPS=(
@@ -58,6 +63,7 @@ MEMGRAPH_BUILD_DEPS=(
     autoconf # for jemalloc code generation
     libtool  # for protobuf code generation
     libsasl2-dev
+    ninja-build
 )
 
 MEMGRAPH_TEST_DEPS="${MEMGRAPH_BUILD_DEPS[*]}"
@@ -88,6 +94,12 @@ check() {
               missing="$pkg $missing"
             fi
             continue
+        fi
+        if [ "$pkg" == custom-rust ]; then
+            if [ ! -x "$HOME/.cargo/bin/rustup" ]; then
+                missing="$pkg $missing"
+            fi
+	        continue
         fi
         if ! dpkg -s "$pkg" >/dev/null 2>/dev/null; then
             missing="$pkg $missing"
