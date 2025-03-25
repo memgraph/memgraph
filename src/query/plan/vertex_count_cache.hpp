008440b1b6421d8509fb08c1906cc927ed0bdb87
Colin Barry <colin.barry@memgraph.io>
2025-03-25 09:24:48 +0000

    wip: Cache vertex count for label + properties

--- src/query/plan/vertex_count_cache.hpp
+++ src/query/plan/vertex_count_cache.hpp
@@ -49,27 +49,23 @@ class VertexCountCache {
     return label_vertex_count_.at(label);
   }
 
-  int64_t VerticesCount(storage::LabelId label, storage::PropertyId property) {
-    auto key = std::make_pair(label, property);
-    if (label_property_vertex_count_.find(key) == label_property_vertex_count_.end())
-      label_property_vertex_count_[key] = db_->VerticesCount(label, std::array{property});
-    return label_property_vertex_count_.at(key);
-  }
-
   int64_t VerticesCount(storage::LabelId label, std::span<storage::PropertyId const> properties) {
-    // TODO(composite_index) Not caching: direct read from the db. Does that
-    // matter?
-    return db_->VerticesCount(label, properties);
+    auto key = std::make_pair(label, std::vector(properties.begin(), properties.end()));
+    if (label_properties_vertex_count_.find(key) == label_properties_vertex_count_.end())
+      label_properties_vertex_count_[key] = db_->VerticesCount(label, properties);
+    return label_properties_vertex_count_.at(key);
   }
 
   int64_t VerticesCount(storage::LabelId label, std::span<storage::PropertyId const> properties,
                         std::span<storage::PropertyValue const> values) {
+    // TODO(composite_index) cache count based on properties + values
     return db_->VerticesCount(label, properties, values);
   }
 
   int64_t VerticesCount(storage::LabelId label, std::span<storage::PropertyId const> properties,
                         std::span<std::optional<utils::Bound<storage::PropertyValue>> const> lowers,
                         std::span<std::optional<utils::Bound<storage::PropertyValue>> const> uppers) {
+    // TODO(composite_index) cache count based on properties + bounds
     return db_->VerticesCount(label, properties, lowers, uppers);
   }
 
@@ -208,11 +204,18 @@ class VertexCountCache {
 
  private:
   using LabelPropertyKey = std::pair<storage::LabelId, storage::PropertyId>;
+  using LabelPropertiesKey = std::pair<storage::LabelId, std::vector<storage::PropertyId>>;
   using EdgeTypePropertyKey = std::pair<storage::EdgeTypeId, storage::PropertyId>;
 
   struct LabelPropertyHash {
     size_t operator()(const LabelPropertyKey &key) const {
       return utils::HashCombine<storage::LabelId, storage::PropertyId>{}(key.first, key.second);
+    }
+  };
+
+  struct LabelPropertiesHash {
+    size_t operator()(const LabelPropertiesKey &key) const {
+      return utils::HashCombine<storage::LabelId, std::vector<storage::PropertyId>>{}(key.first, key.second);
     }
   };
 
@@ -256,7 +259,7 @@ class VertexCountCache {
   std::optional<int64_t> vertices_count_;
   std::unordered_map<storage::LabelId, int64_t> label_vertex_count_;
   std::unordered_map<storage::EdgeTypeId, int64_t> edge_type_edge_count_;
-  std::unordered_map<LabelPropertyKey, int64_t, LabelPropertyHash> label_property_vertex_count_;
+  std::unordered_map<LabelPropertiesKey, int64_t, LabelPropertiesHash> label_properties_vertex_count_;
   std::unordered_map<LabelPropertyKey, std::optional<int64_t>, LabelPropertyHash> label_property_vertex_point_count_;
   std::unordered_map<EdgeTypePropertyKey, int64_t, EdgeTypePropertyHash> edge_type_property_edge_count_;
   std::unordered_map<storage::PropertyId, int64_t> edge_property_edge_count_;
