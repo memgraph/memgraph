a22f7193a286c07f87d89e5d8a567ead6644cfd3
DavIvek <david.ivekovic@memgraph.io>
2025-04-09 17:31:40 +0200

    remove unused headers

--- tests/unit/integrations_kafka_consumer.cpp
+++ tests/unit/integrations_kafka_consumer.cpp
@@ -9,10 +9,7 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-#include <atomic>
 #include <chrono>
-#include <condition_variable>
-#include <mutex>
 #include <optional>
 #include <string>
 #include <string_view>
