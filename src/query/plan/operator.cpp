09f04521aeb22e3939df4571bd87df29d33ad741
imilinovic <ivan.milinovic123@gmail.com>
2025-04-10 15:01:08 +0200

    fix failure when not all qm results were yielded

--- src/query/plan/operator.cpp
+++ src/query/plan/operator.cpp
@@ -5734,6 +5734,14 @@ class CallProcedureCursor : public Cursor {
       result_.signature.emplace(self_->result_fields_[i],
                                 ResultsMetadata(signature_it->second.first, signature_it->second.second, i));
     }
+    if (proc_->results.size() == self_->result_fields_.size()) return;
+    // Not all results were yielded but they still need to be inserted inside the signature
+    int index = self_->result_fields_.size();
+    for (auto const &[name, signature] : proc_->results) {
+      if (result_.signature.find(name) == result_.signature.end()) {
+        result_.signature.emplace(name, ResultsMetadata(signature.first, signature.second, index++));
+      }
+    }
   }
 
   bool Pull(Frame &frame, ExecutionContext &context) override {
