9296a9a8f0da7a77708ec50c53f7ba8f9c293cee
Gareth Lloyd <gareth.lloyd@memgraph.io>
2025-04-11 20:52:11 +0100

    Backwards compat for Cartesian_3d srid bug
    kSrid_Cartesian_3D for v2.19.0, v2.20.0, and v2.20.1 was wrong. For
    compatabilty we allow the incorrect value `9757` to also work for now.

--- src/storage/v2/durability/version.hpp
+++ src/storage/v2/durability/version.hpp
@@ -32,6 +32,7 @@ const uint64_t kEnumsVersion{18};
 // We prematurely bumped the version when making the point datatype as part of 2.19
 const uint64_t kAccidentalVersionBump1{19};
 const uint64_t kPointIndexAndTypeConstraints{20};
+const uint64_t kSridCartesian3DCorrected{21};
 const uint64_t kEdgeSetDeltaWithVertexInfo{21};
 const uint64_t kVectorIndex{22};
 const uint64_t kDurableTS{23};
