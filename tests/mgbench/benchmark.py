6f82794772d1ad6e652b3b5e1d36229dcf657083
Josip Mrden <josip.mrden@memgraph.io>
2025-06-02 10:04:20 +0200

    Fix condition

--- tests/mgbench/benchmark.py
+++ tests/mgbench/benchmark.py
@@ -949,7 +949,11 @@ if __name__ == "__main__":
         vendor_name=args.vendor_name,
         installation_type=args.installation_type,
         # Client binary present in native and external installation types
-        client_binary=args.client_binary if args.installation_type != BenchmarkInstallationType.NATIVE else None,
+        client_binary=(
+            args.client_binary
+            if args.installation_type in [BenchmarkInstallationType.NATIVE, BenchmarkInstallationType.EXTERNAL]
+            else None
+        ),
         client_language=args.client_language,
         databases=args.databases,
         client_bolt_address=args.client_bolt_address,
