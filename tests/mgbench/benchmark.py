6dcd7fa73816f1ad3469e3af418824bab4d2c550
Josip Mrden <josip.mrden@memgraph.io>
2025-06-02 12:32:26 +0200

    Add fixes for successfully starting PostgreSQL

--- tests/mgbench/benchmark.py
+++ tests/mgbench/benchmark.py
@@ -459,7 +459,11 @@ def mixed_workload(
 
 
 def warmup(condition: str, client, queries: list = None):
-    if condition == DATABASE_CONDITION_HOT:
+    if condition == DATABASE_CONDITION_HOT and client._vendor in [
+        GraphVendors.MEMGRAPH,
+        GraphVendors.NEO4J,
+        GraphVendors.FALKORDB,
+    ]:
         log.log("Execute warm-up to match condition: {} ".format(condition))
         warmup_to_hot_queries = get_warmup_to_hot_queries(client)
         if len(queries) > 0:
