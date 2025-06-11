80a56ae058523ab310b2329167876ae99a66f67e
Andi Skrgat <andi8647@gmail.com>
2025-06-11 15:47:23 +0200

    Remove networking overhead from the flaky Raft state test (#3046)
    The important part of testing is preserved by isolating routing table
    creation into utils method.

--- src/coordination/include/coordination/raft_state.hpp
+++ src/coordination/include/coordination/raft_state.hpp
@@ -18,6 +18,7 @@
 #include "coordination/coordinator_communication_config.hpp"
 #include "coordination/coordinator_state_machine.hpp"
 #include "coordination/coordinator_state_manager.hpp"
+#include "coordination/utils.hpp"
 #include "coordination_observer.hpp"
 
 #include <libnuraft/logger.hxx>
@@ -30,7 +31,6 @@ struct DataInstanceConfig;
 
 using BecomeLeaderCb = std::function<void()>;
 using BecomeFollowerCb = std::function<void()>;
-using RoutingTable = std::vector<std::pair<std::vector<std::string>, std::string>>;
 
 using nuraft::asio_service;
 using nuraft::buffer;
