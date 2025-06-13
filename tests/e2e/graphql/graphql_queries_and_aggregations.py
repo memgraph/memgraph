b081b4d168b8d028a4a53c9afe9e003f6db2d6b3
Colin Barry <colin.barry@memgraph.io>
2025-06-13 10:55:27 +0100

    test: Add additional `query` test

--- tests/e2e/graphql/graphql_queries_and_aggregations.py
+++ tests/e2e/graphql/graphql_queries_and_aggregations.py
@@ -13,7 +13,86 @@ def query_server() -> GraphQLServer:
 def test_return_all_user_nodes_from_their_id_and_name(query_server):
     query_server.send_query("mutation { setup }")
 
-    # cleanup_user_and_posts(query_server, user_uuid)
+    query = dedent(
+        """\
+        query {
+            users {
+                id
+                name
+            }
+        }
+        """
+    ).strip()
+
+    gotten = query_server.send_query(query)
+    expected_result = dedent(
+        """\
+        {
+            "data":{
+                "users":[
+                    {
+                        "id":"51f65ea1-b612-47e6-8cc1-c13735168130",
+                        "name":"Alice"
+                    },
+                    {
+                        "id":"02bae290-1943-49e6-8be8-f15c1a0c5923",
+                        "name":"Bob"
+                    }
+                ]
+            }
+        }
+        """
+    ).strip()
+    assert server_returned_expected(expected_result, gotten)
+
+    query_server.send_query("mutation { teardown }")
+
+
+def test_query_user_and_return_all_their_posts(query_server):
+    query_server.send_query("mutation { setup }")
+
+    query = dedent(
+        """\
+        query {
+            users(where: { name: { eq: "Alice" } }) {
+                id
+                name
+                posts {
+                    content
+                }
+            }
+        }
+        """
+    ).strip()
+
+    gotten = query_server.send_query(query)
+    expected_result = dedent(
+        """\
+        {
+            "data":{
+                "users":[
+                    {
+                        "name":"Alice",
+                        "id":"51f65ea1-b612-47e6-8cc1-c13735168130",
+                        "posts":[
+                            {
+                                "content":"First post"
+                            },
+                            {
+                                "content":"Second post"
+                            },
+                            {
+                                "content":"Third post"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        """
+    ).strip()
+    assert server_returned_expected(expected_result, gotten)
+
     query_server.send_query("mutation { teardown }")
 
 
