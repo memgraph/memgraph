50ea62fbe29b83a4ca86f57c1ebaa2a5a4a88af6
Colin Barry <colin.barry@memgraph.io>
2025-06-13 15:21:31 +0100

    test: Use date conversions query module in tests

--- tests/e2e/graphql/graphql_library_config/queries_and_aggregations.js
+++ tests/e2e/graphql/graphql_library_config/queries_and_aggregations.js
@@ -30,10 +30,10 @@ type Mutation {
       CREATE
        (a:User { name: "Alice", id: "51f65ea1-b612-47e6-8cc1-c13735168130", age: 23 }),
        (b:User { name: "Bob", id: "02bae290-1943-49e6-8be8-f15c1a0c5923", age: 42 }),
-       (a)-[:HAS_POST]->(:Post {id: "5cd0d311-16d8-4e88-9cde-e815c7623a6c", content: "First post"}),
-       (a)-[:HAS_POST]->(:Post {id: "fb2a6146-6511-45ae-8956-bc8977f586cc", content: "Second post"}),
-       (a)-[:HAS_POST]->(:Post {id: "0dc886aa-8c65-48f1-8e46-fd628d695831", content: "Third one"}),
-       (b)-[:HAS_POST]->(:Post {id: "94f5a303-8776-4272-ba82-01c1755a43ad", content: "Fourth one"})
+       (a)-[:HAS_POST {date: "2011-12-03T10:15:30+01:00[Europe/Paris]"}]->(:Post {id: "5cd0d311-16d8-4e88-9cde-e815c7623a6c", content: "First post", createdAt: "2011-12-03T10:15:30+01:00[Europe/Paris]"}),
+       (a)-[:HAS_POST {date: "2011-12-04T10:15:30+01:00[Europe/Paris]"}]->(:Post {id: "fb2a6146-6511-45ae-8956-bc8977f586cc", content: "Second post", createAt: "2011-12-04T10:15:30+01:00[Europe/Paris]"}),
+       (a)-[:HAS_POST {date: "2011-12-05T10:15:30+01:00[Europe/Paris]"}]->(:Post {id: "0dc886aa-8c65-48f1-8e46-fd628d695831", content: "Third one", createdAt: "2011-12-05T10:15:30+01:00[Europe/Paris]"}),
+       (b)-[:HAS_POST {date: "2011-12-06T10:15:30+01:00[Europe/Paris]"}]->(:Post {id: "94f5a303-8776-4272-ba82-01c1755a43ad", content: "Fourth one", createdAt: "2011-12-06T10:15:30+01:00[Europe/Paris]"})
       RETURN true AS success
     """
     columnName: "success"
