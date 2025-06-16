caa07620d80ed5dd65871e8679638b89b62d7277
Colin Barry <colin.barry@memgraph.io>
2025-06-16 16:41:12 +0100

    test: Add mutation tests for mathematical operators

--- tests/e2e/graphql/temporary_procedures/mgps.py
+++ tests/e2e/graphql/temporary_procedures/mgps.py
@@ -6,3 +6,11 @@ import mgp
 @mgp.read_proc
 def components(context: mgp.ProcCtx) -> mgp.Record(versions=list, edition=str):
     return mgp.Record(versions=["4.3"], edition="4.3.2")
+
+
+@mgp.function
+def validate_predicate(predicate: bool, message: str, params: list):
+    if predicate:
+        raise Exception(message % tuple(params))
+    else:
+        return True
