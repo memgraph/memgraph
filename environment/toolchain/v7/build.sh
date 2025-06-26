54903d4dac677b1d615bdcd585d5cddb21377d0a
Matt James <mattkjames7@gmail.com>
2025-06-26 12:18:52 +0100

    update pip install

--- environment/toolchain/v7/build.sh
+++ environment/toolchain/v7/build.sh
@@ -914,8 +914,7 @@ popd
 
 pushd build
 source $PREFIX/activate
-PIP_BREAK_SYSTEM_PACKAGES=1
-pip install conan==2.17.1
+PIP_BREAK_SYSTEM_PACKAGES=1 && pip install conan==2.17.1
 export CC=$PREFIX/bin/clang
 export CXX=$PREFIX/bin/clang++
 export CFLAGS="$CFLAGS -fPIC"
