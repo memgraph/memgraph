40bb7d86b9d88ea69aa98edb137a20e0980fd3d2
matt <mattkjames7@gmail.com>
2025-06-26 22:12:22 +0000

    questionable hacks

--- src/utils/scheduler.hpp
+++ src/utils/scheduler.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Memgraph Ltd.
+// Copyright 2025 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -15,6 +15,7 @@
 #include <condition_variable>
 #include <functional>
 #include <thread>
+#include <variant>
 
 #include "utils/synchronized.hpp"
 #include "utils/variant_helpers.hpp"
