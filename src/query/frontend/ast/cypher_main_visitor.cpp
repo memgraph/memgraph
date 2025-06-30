24e90016fc850a04cfd7550b1c37dc2184149927
Josip Mrden <josip.mrden@memgraph.io>
2025-06-30 11:35:04 +0200

    Clang tidy

--- src/query/frontend/ast/cypher_main_visitor.cpp
+++ src/query/frontend/ast/cypher_main_visitor.cpp
@@ -3148,7 +3148,8 @@ antlrcpp::Any CypherMainVisitor::visitExistsExpression(MemgraphCypher::ExistsExp
   }
 
   // Ensure only one of pattern_ or subquery_ is set
-  const bool only_one_member_set = exists->pattern_ ? exists->subquery_ : !exists->subquery_;
+  const bool only_one_member_set =
+      exists->pattern_ ? exists->subquery_ : !exists->subquery_;  // NOLINT(clang-diagnostic-error)
   if (!only_one_member_set) {
     throw SyntaxException("EXISTS must have exactly one of pattern or subquery set.");
   }
@@ -3199,7 +3200,8 @@ antlrcpp::Any CypherMainVisitor::visitExistsSubquery(MemgraphCypher::ExistsSubqu
   }
 
   // Ensure only one of pattern_ or subquery_ is set
-  const bool only_one_member_set = exists->pattern_ ? exists->subquery_ : !exists->subquery_;
+  const bool only_one_member_set =
+      exists->pattern_ ? exists->subquery_ : !exists->subquery_;  // NOLINT(clang-diagnostic-error)
   if (!only_one_member_set) {
     throw SyntaxException("EXISTS must have exactly one of pattern or subquery set.");
   }
