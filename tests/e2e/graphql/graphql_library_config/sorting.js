09d506876b6cffc961558270b1f6391854a9ebe5
colinbarry <colin.barry@memgraph.io>
2025-07-02 12:26:05 +0100

    Support latest GraphQL middleware (#3027)
    Update Memgraph to support the latest Neo4j GraphQL middleware.
    
    - Cypher compatiblity fixes:
      - `WITH *` can now be used even if no variables are in scope.
    - Subqueries no longer advance the transaction frame, fixing an issue
    with vertex visibility in outer queries.
    - New apoc compatible functions:
    - `mgps.validate_predicate` replaces `apoc.util.validatePredicate`. This
    method is used to evaluate a predicate and either error on failure or
    return `true` on success.
    - `date.convert_format` replaces `apoc.date.convertFormat`, and is used
    to convert DateTimes to a GraphQL compatible format. (Currently, only
    `iso_zoned_date_time` is supported, as this is all that GraphQL needs.)
     - Update test suite to use the latest GraphQL middleware:
       - Add extensive tests for all GraphQL mutations and queries.
    - Remove the previous tests (as these were testing behaviours covered by
    the new tests. No point having duplication here.)
    - Remove temporary procedures used for testing. Because these query
    modules are now implemented directly in Memgraph, we don't need to stub
    them in test code.
    
    ---------
    
    Co-authored-by: Dr Matt James <mattkjames7@gmail.com>
    Co-authored-by: DavIvek <david.ivekovic@memgraph.io>

---
+++ tests/e2e/graphql/graphql_library_config/sorting.js
@@ -0,0 +1,48 @@
+const { gql } = require("graphql-tag");
+
+module.exports.typeDefs = gql`
+type Movie @node {
+    title: String!
+    runtime: Int!
+    actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
+}
+
+type Actor @node {
+    surname: String!
+    movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+}
+
+type Mutation {
+  setup: Boolean
+    @cypher(
+    statement: """
+    CREATE
+      (inception:Movie {title: "Inception", runtime: 148}),
+      (matrix:Movie {title: "The Matrix", runtime: 136}),
+      (interstellar:Movie {title: "Interstellar", runtime: 169}),
+      (parasite:Movie {title: "Parasite", runtime: 132}),
+      (spirited:Movie {title: "Spirited Away", runtime: 125}),
+      (:Actor {surname: "DiCaprio"})-[:ACTED_IN]->(inception),
+      (:Actor {surname: "Gordon-Levitt"})-[:ACTED_IN]->(inception),
+      (:Actor {surname: "Page"})-[:ACTED_IN]->(inception),
+      (:Actor {surname: "Reeves"})-[:ACTED_IN]->(matrix),
+      (:Actor {surname: "Moss"})-[:ACTED_IN]->(matrix),
+      (:Actor {surname: "Fishburne"})-[:ACTED_IN]->(matrix),
+      (:Actor {surname: "McConaughey"})-[:ACTED_IN]->(interstellar),
+      (:Actor {surname: "Song"})-[:ACTED_IN]->(parasite),
+      (:Actor {surname: "Hiiragi"})-[:ACTED_IN]->(spirited)
+      RETURN true AS success
+    """
+    columnName: "success"
+    )
+
+  teardown: Int
+    @cypher(
+      statement: """
+        MATCH (x)
+        DETACH DELETE x
+        RETURN COUNT(x) AS num_deleted_nodes
+      """
+      columnName: "num_deleted_nodes"
+    )
+}`
