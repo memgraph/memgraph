dcf29e5dc6b111bfcc5da053512ccd32b8c281dd
Colin Barry <colin.barry@memgraph.io>
2025-07-17 13:51:19 +0100

    test: Refactor test code

--- tests/e2e/serialization/serialization.py
+++ tests/e2e/serialization/serialization.py
@@ -14,99 +14,29 @@ import time
 from threading import Barrier, Thread
 
 import pytest
-from common import connect, execute_and_fetch_all, make_connection
+from common import serialization
 
 
-def test_wait_on_creating_edges(connect, make_connection):
-    c = connect.cursor()
-    execute_and_fetch_all(c, "CREATE (:L1), (:L2)")
-    connect.commit()
+def test_serialization_on_creating_edges(serialization):
+    serialization.setup("CREATE (:L1), (:L2)")
 
-    has_error = False
-
-    barrier = Barrier(2)
-
-    # TODO maybe make these global modifier functions, as they will be reused
-    # throughout the tests
-    def add_edge_with_barrier(edge_label, precommit_duration):
-        nonlocal has_error
-        try:
-            connection = make_connection()
-            cursor = connection.cursor()
-            cursor.execute("MATCH (m:L1), (n:L2) CREATE (m)-[:$edge_label]->(n)", {"edge_label": edge_label})
-            barrier.wait()
-            if precommit_duration:
-                time.sleep(precommit_duration)
-            connection.commit()
-        except Exception as e:
-            print(str(e))
-            has_error = True
-
-    def add_edge(edge_label):
-        nonlocal has_error
-        try:
-            connection = make_connection()
-            cursor = connection.cursor()
-            cursor.execute("MATCH (m:L1), (n:L2) CREATE (m)-[:$edge_label]->(n)", {"edge_label": edge_label})
-            connection.commit()
-        except Exception as e:
-            print(str(e))
-            has_error = True
-
-    t1 = Thread(target=add_edge_with_barrier, args=("ALFA", 3))
-    t2 = Thread(target=add_edge, args=("BRAVO",))
-    t1.start()
-    barrier.wait()
-    t2.start()
-    t1.join()
-    t2.join()
-
-    assert not has_error
+    serialization.run(
+        [
+            {"query": "MATCH (m:L1), (n:L2) CREATE (m)-[:$edge_label]->(n)", "args": {"edge_label": "alfa"}, "wait": 3},
+            {"query": "MATCH (m:L1), (n:L2) CREATE (m)-[:$edge_label]->(n)", "args": {"edge_label": "bravo"}},
+        ]
+    )
 
 
-def test_wait_on_updating_props(connect, make_connection):
-    c = connect.cursor()
-    execute_and_fetch_all(c, "CREATE (:L1)")
-    connect.commit()
+def test_serialization_on_updating_props(serialization):
+    serialization.setup("CREATE (:L1)")
 
-    has_error = False
-
-    barrier = Barrier(2)
-
-    def set_prop_with_barrier(prop_value, precommit_duration):
-        nonlocal has_error
-        try:
-            connection = make_connection()
-            cursor = connection.cursor()
-            cursor.execute("MATCH (m:L1) SET m.prop_value = $prop_value", {"prop_value": prop_value})
-            barrier.wait()
-            if precommit_duration:
-                time.sleep(precommit_duration)
-            connection.commit()
-        except Exception as e:
-            print(str(e))
-            has_error = True
-
-    def set_prop(prop_value):
-        nonlocal has_error
-        try:
-            connection = make_connection()
-            cursor = connection.cursor()
-            cursor.execute("MATCH (m:L1) SET m.prop_value = $prop_value", {"prop_value": prop_value})
-            connection.commit()
-        except Exception as e:
-            print(str(e))
-            has_error = True
-
-    t1 = Thread(target=set_prop_with_barrier, args=("ALFA", 3))
-    t2 = Thread(target=set_prop, args=("BRAVO",))
-    t1.start()
-    barrier.wait()
-    t2.start()
-    t1.join()
-    t2.join()
-
-    assert not has_error
+    serialization.run(
+        [
+            {"query": "MATCH (m:L1) SET m.prop_value = $prop_value", "args": {"prop_value": "first"}, "wait": 3},
+            {"query": "MATCH (m:L1) SET m.prop_value = $prop_value", "args": {"prop_value": "second"}},
+        ]
+    )
 
 
 if __name__ == "__main__":
