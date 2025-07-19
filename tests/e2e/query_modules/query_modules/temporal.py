10d185e30d805fbdefad19cf1ce3cc68a2d42f67
Colin Barry <colin.barry@memgraph.io>
2025-07-19 15:47:49 +0100

    test: Add more tests for accepting a `ZonedDateTime`

--- tests/e2e/query_modules/query_modules/temporal.py
+++ tests/e2e/query_modules/query_modules/temporal.py
@@ -9,9 +9,23 @@
 # by the Apache License, Version 2.0, included in the file
 # licenses/APL.txt.
 
+from datetime import datetime, timedelta, timezone
+
 import mgp
 
 
 @mgp.function
 def zoneddatetime_to_string(val):
     return str(val)
+
+
+@mgp.function
+def make_zdt(year, month, day, hour, minute, second, offset_minutes):
+    return datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second).astimezone(
+        timezone(timedelta(minutes=offset_minutes))
+    )
+
+
+# @mgp.function
+# def make_dt(year, month, day, hour, min, second):
+#     return datetime(year=year, month=month, day=day, hour=hour, minute=min, second=second)
