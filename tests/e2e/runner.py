4a4fa75d63b64ca494da15b868d726e72af5f237
Matt James <mattkjames7@gmail.com>
2025-07-31 20:58:21 +0100

    reenable the other tests

--- tests/e2e/runner.py
+++ tests/e2e/runner.py
@@ -54,20 +54,11 @@ def cleanup(workload, keep_directories=True):
         interactive_mg_runner.stop_all(keep_directories)
 
 
-stream_workloads = [
-    "Kafka streams start, stop and show",
-    "Streams with users",
-    "Pulsar streams start, stop and show",
-]
-
-
 def run(args):
     workloads = load_workloads(args.workloads_root_directory)
     for workload in workloads:
         workload_name = workload["name"]
-        if (
-            args.workload_name is not None and args.workload_name != workload_name
-        ) or workload_name not in stream_workloads:
+        if args.workload_name is not None and args.workload_name != workload_name:
             continue
         log.info("%s STARTED.", workload_name)
 
