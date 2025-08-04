7a440ee5d6917ba39cc290699404d040b3aefc70
DavIvek <david.ivekovic@memgraph.io>
2025-08-04 23:10:04 +0200

    add text index utils

---
+++ src/storage/v2/indices/text_index_utils.cpp
@@ -0,0 +1,123 @@
+// Copyright 2025 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+#include "storage/v2/indices/text_index_utils.hpp"
+
+#include <cctype>
+#include <filesystem>
+#include <map>
+
+#include <nlohmann/json.hpp>
+#include "storage/v2/name_id_mapper.hpp"
+#include "storage/v2/property_value.hpp"
+#include "utils/string.hpp"
+
+namespace r = ranges;
+
+namespace memgraph::storage {
+
+std::string ToLowerCasePreservingBooleanOperators(std::string_view input) {
+  if (input.empty()) return {};
+
+  std::string result;
+  result.reserve(input.length());
+
+  const auto *it = input.cbegin();
+  while (it != input.cend()) {
+    if (std::isspace(*it)) {
+      result += *it++;
+      continue;
+    }
+
+    const auto *word_start = it;
+    it = r::find_if(word_start, input.cend(), [](char c) { return std::isspace(c); });
+
+    // Extract the word
+    auto word = input.substr(word_start - input.cbegin(), it - word_start);
+    auto uppercase_word = utils::ToUpperCase(word);
+
+    // Check if it's a boolean operator (case-insensitive)
+    if (uppercase_word == kBooleanAnd || uppercase_word == kBooleanOr || uppercase_word == kBooleanNot) {
+      // Preserve the boolean operator in uppercase
+      result += uppercase_word;
+    } else {
+      result += utils::ToLowerCase(word);
+    }
+  }
+
+  return result;
+}
+
+std::string MakeIndexPath(const std::string &base_path, std::string_view index_name) {
+  return (std::filesystem::path(base_path) / index_name).string();
+}
+
+nlohmann::json SerializeProperties(const std::map<PropertyId, PropertyValue> &properties,
+                                   NameIdMapper *name_id_mapper) {
+  // Property types that are indexed in Tantivy are Bool, Int, Double, and String.
+  nlohmann::json serialized_properties = nlohmann::json::value_t::object;
+  for (const auto &[prop_id, prop_value] : properties) {
+    switch (prop_value.type()) {
+      case PropertyValue::Type::Bool:
+        serialized_properties[name_id_mapper->IdToName(prop_id.AsUint())] = prop_value.ValueBool();
+        break;
+      case PropertyValue::Type::Int:
+        serialized_properties[name_id_mapper->IdToName(prop_id.AsUint())] = prop_value.ValueInt();
+        break;
+      case PropertyValue::Type::Double:
+        serialized_properties[name_id_mapper->IdToName(prop_id.AsUint())] = prop_value.ValueDouble();
+        break;
+      case PropertyValue::Type::String:
+        serialized_properties[name_id_mapper->IdToName(prop_id.AsUint())] = prop_value.ValueString();
+        break;
+      case PropertyValue::Type::Null:
+      case PropertyValue::Type::List:
+      case PropertyValue::Type::Map:
+      case PropertyValue::Type::TemporalData:
+      case PropertyValue::Type::ZonedTemporalData:
+      default:
+        continue;
+    }
+  }
+  return serialized_properties;
+}
+
+std::string StringifyProperties(const std::map<PropertyId, PropertyValue> &properties) {
+  // Property types that are indexed in Tantivy are Bool, Int, Double, and String.
+  std::vector<std::string> indexable_properties_as_string;
+  for (const auto &[_, prop_value] : properties) {
+    switch (prop_value.type()) {
+      case PropertyValue::Type::Bool:
+        indexable_properties_as_string.emplace_back(prop_value.ValueBool() ? "true" : "false");
+        break;
+      case PropertyValue::Type::Int:
+        indexable_properties_as_string.push_back(std::to_string(prop_value.ValueInt()));
+        break;
+      case PropertyValue::Type::Double:
+        indexable_properties_as_string.push_back(std::to_string(prop_value.ValueDouble()));
+        break;
+      case PropertyValue::Type::String:
+        indexable_properties_as_string.push_back(prop_value.ValueString());
+        break;
+      // NOTE: As the following types aren't indexed in Tantivy, they don't appear in the property value string either.
+      case PropertyValue::Type::Null:
+      case PropertyValue::Type::List:
+      case PropertyValue::Type::Map:
+      case PropertyValue::Type::TemporalData:
+      case PropertyValue::Type::ZonedTemporalData:
+      default:
+        continue;
+    }
+  }
+  return utils::Join(indexable_properties_as_string, " ");
+}
+
+}  // namespace memgraph::storage
---
+++ src/storage/v2/indices/text_index_utils.hpp
@@ -0,0 +1,55 @@
+// Copyright 2025 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+#pragma once
+
+#include <map>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "nlohmann/json_fwd.hpp"
+#include "storage/v2/id_types.hpp"
+#include "storage/v2/property_value.hpp"
+
+namespace memgraph::storage {
+
+class NameIdMapper;
+
+inline constexpr std::string_view kTextIndicesDirectory = "text_indices";
+inline constexpr bool kDoSkipCommit = true;
+
+// Boolean operators that should be preserved in uppercase for Tantivy
+inline constexpr std::string_view kBooleanAnd = "AND";
+inline constexpr std::string_view kBooleanOr = "OR";
+inline constexpr std::string_view kBooleanNot = "NOT";
+
+struct TextIndexInfo {
+  inline bool operator==(const TextIndexInfo &other) const = default;
+
+  std::string index_name_;
+  LabelId label_;
+  std::vector<PropertyId> properties_;
+};
+
+// Convert text to lowercase while preserving boolean operators
+std::string ToLowerCasePreservingBooleanOperators(std::string_view input);
+
+// Make index path from base directory and index name
+std::string MakeIndexPath(const std::string &base_path, std::string_view index_name);
+
+// Serialize properties to JSON format
+nlohmann::json SerializeProperties(const std::map<PropertyId, PropertyValue> &properties, NameIdMapper *name_id_mapper);
+
+// Convert properties to string representation
+std::string StringifyProperties(const std::map<PropertyId, PropertyValue> &properties);
+
+}  // namespace memgraph::storage
