75e4a4e04316269c08425be6eeff434e2177eee7
Andreja Tonev <andreja.tonev@memgraph.io>
2025-08-05 17:05:05 +0200

    clang-tidy

--- src/dbms/replication_handlers.cpp
+++ src/dbms/replication_handlers.cpp
@@ -188,7 +188,7 @@ void RenameDatabaseHandler(memgraph::system::ReplicaHandlerAccessToState &system
     }
   } catch (...) {
     // Failure
-    spdlog::trace("RenameDatabaseHandler: Failed to rename database \"{}\" to \"{}\".", req.old_name, req.new_name);
+    spdlog::trace(R"(RenameDatabaseHandler: Failed to rename database "{}" to "{}".)", req.old_name, req.new_name);
   }
 
   rpc::SendFinalResponse(res, res_builder);
