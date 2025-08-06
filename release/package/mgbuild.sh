001b983ba338320323237db9e5ecdfd3b658c33b
Matt James <mattkjames7@gmail.com>
2025-08-06 19:52:42 +0100

    update rpmlint

--- release/package/mgbuild.sh
+++ release/package/mgbuild.sh
@@ -500,6 +500,9 @@ package_memgraph() {
   local package_command=""
 
   if [[ "$os" == "centos-10" ]]; then
+      # install much newer rpmlint than what ships with centos-10
+      docker exec -u root "$build_container" bash -c "dnf remove -y rpmlint"
+      docker exec -u mg "$build_container" bash -c "pip install rpmlint==2.8.0"
       package_command=" cpack -G RPM --config ../CPackConfig.cmake && rpmlint --file='../../release/rpm/rpmlintrc_centos10' memgraph*.rpm "
   elif [[ "$os" =~ ^"fedora".* ]]; then
       docker exec -u root "$build_container" bash -c "yum -y update"
