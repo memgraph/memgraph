2e7bdf16d011f1b8ccc60a28e7a1c5dbee93853d
Andi Skrgat <andi8647@gmail.com>
2025-08-06 10:47:52 +0200

    Add Conan build (#3074)
    ```
    source ./venv/bin/activate
    MG_TOOLCHAIN_ROOT=/opt/toolchain-v7/ conan install . --build=missing -pr ./memgraph_template_profile -s build_type=RelWithDebInfo
    source build/RelWithDebInfo/generators/conanbuild.sh
    cmake --preset conan-relwithdebinfo
    cmake --build --preset conan-relwithdebinfo --target memgraph
    ```
    
    **Problems with**:
    - Sasl for Kafka has issues, should be fixed with latest master but not
    yet released https://github.com/conan-io/conan-center-index/pull/27911
       - currently there is a problem with aclocal
    - Built new recipe for the newest NuRaft release
    https://github.com/conan-io/conan-center-index/pull/27919
    - libbcrypt not supported
    - protobuf is used by Pulsar, once we figure out Pulsar's recipe
    protobuf will be included there. No need to have Protobuf as a MG
    dependency
    - Rapidcheck problems with gtest
    - librtdsc -> built new recipe
    https://github.com/memgraph/memgraph/pull/3097
    - jemalloc won't be touched since we may remove it
    - built recipe for mgconsole
    - what to with neo4j package?
    - mcxx recipe needed
      - what do we do with cargo?
      - progress https://github.com/memgraph/mgcxx-conan
    - usearch recipe needed
    - nlohmann json?

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,25 +1,36 @@
 set(CMAKE_SYSTEM_NAME Linux)
 execute_process(
-    COMMAND uname -m
-    OUTPUT_VARIABLE uname_result
-    OUTPUT_STRIP_TRAILING_WHITESPACE
+     COMMAND uname -m
+     OUTPUT_VARIABLE uname_result
+     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 set(CMAKE_SYSTEM_PROCESSOR "${uname_result}")
 
 set(tools "/opt/toolchain-v7")
 
+# Optional: paths for find_package(), find_library(), etc.
+set(CMAKE_FIND_ROOT_PATH "${tools}")
+# Configure how the find_* commands search
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 
-#set(CMAKE_SYSROOT "${tools}")
-set(CMAKE_PREFIX_PATH "${tools}")
+
 set(MG_TOOLCHAIN_ROOT "${tools}")
-message(STATUS "Using toolchain file at: ${CMAKE_TOOLCHAIN_FILE}" )
+SET(MG_TOOLCHAIN_VERSION 7)
 
-set(CMAKE_C_COMPILER "${tools}/bin/clang")
-set(CMAKE_CXX_COMPILER "${tools}/bin/clang++")
-#set(CMAKE_C_FLAGS "-isystem '${tools}/include'")
-#set(CMAKE_CXX_FLAGS "-isystem '${tools}/include'")
+# Set compiler
+set(CMAKE_C_COMPILER   "${MG_TOOLCHAIN_ROOT}/bin/clang"   CACHE STRING "" FORCE)
+set(CMAKE_CXX_COMPILER "${MG_TOOLCHAIN_ROOT}/bin/clang++" CACHE STRING "" FORCE)
 
-#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_LINKER_TYPE LLD)
+
+# Enable ccache
+find_program(CCACHE_PROGRAM ccache)
+
+if(CCACHE_PROGRAM)
+    message(STATUS "Using ccache: ${CCACHE_PROGRAM}")
+    set(CMAKE_C_COMPILER_LAUNCHER   ${CCACHE_PROGRAM} CACHE STRING "" FORCE)
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM} CACHE STRING "" FORCE)
+endif()
