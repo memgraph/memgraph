4a37b32ebd9584e3880d7f5286b694e2c54ccc6f
as51340 <andi8647@gmail.com>
2025-08-07 07:34:48 +0200

    Trigger merge

--- tests/unit/replication_persistence_helper.cpp
+++ tests/unit/replication_persistence_helper.cpp
@@ -27,10 +27,10 @@ using memgraph::utils::UUID;
 
 TEST(ReplicationDurability, V3Main) {
   auto const role_entry = ReplicationRoleEntry{.version = DurabilityVersion::V3, .role = MainRole{.main_uuid = UUID{}}};
-  nlohmann::json j;
-  to_json(j, role_entry);
+  nlohmann::json json_data;
+  to_json(json_data, role_entry);
   ReplicationRoleEntry deser;
-  from_json(j, deser);
+  from_json(json_data, deser);
   ASSERT_EQ(role_entry, deser);
 }
 
