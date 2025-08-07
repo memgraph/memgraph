378ecc929730d8012ccd90c2191e78fdab5b3303
andrejtonev <29177572+andrejtonev@users.noreply.github.com>
2025-08-07 19:13:19 +0200

    Return snapshot path (#3166)
    `CREATE SNAPSHOT` now returns the path of the created snapshot.
    User query also cannot fail because there is no change (snapshot is
    forced)

--- src/storage/v2/durability/snapshot.hpp
+++ src/storage/v2/durability/snapshot.hpp
@@ -77,11 +77,11 @@ RecoveredSnapshot LoadSnapshot(std::filesystem::path const &path, utils::SkipLis
                                memgraph::storage::SharedSchemaTracking *schema_info,
                                std::optional<SnapshotObserverInfo> const &snapshot_info = std::nullopt);
 
-bool CreateSnapshot(Storage *storage, Transaction *transaction, const std::filesystem::path &snapshot_directory,
-                    const std::filesystem::path &wal_directory, utils::SkipList<Vertex> *vertices,
-                    utils::SkipList<Edge> *edges, utils::UUID const &uuid,
-                    const memgraph::replication::ReplicationEpoch &epoch,
-                    const std::deque<std::pair<std::string, uint64_t>> &epoch_history,
-                    utils::FileRetainer *file_retainer, std::atomic_bool *abort_snapshot = nullptr);
+std::optional<std::filesystem::path> CreateSnapshot(
+    Storage *storage, Transaction *transaction, const std::filesystem::path &snapshot_directory,
+    const std::filesystem::path &wal_directory, utils::SkipList<Vertex> *vertices, utils::SkipList<Edge> *edges,
+    utils::UUID const &uuid, const memgraph::replication::ReplicationEpoch &epoch,
+    const std::deque<std::pair<std::string, uint64_t>> &epoch_history, utils::FileRetainer *file_retainer,
+    std::atomic_bool *abort_snapshot = nullptr);
 
 }  // namespace memgraph::storage::durability
