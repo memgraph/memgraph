28fe8de290b9462b68847f78a333aca98637a69d
DavIvek <david.ivekovic@memgraph.io>
2025-08-12 11:35:05 +0200

    setup script update

--- libs/setup.sh
+++ libs/setup.sh
@@ -301,7 +301,7 @@ cd ..
 
 # abseil 20250512.1
 absl_ref="20250512.1"
-repo_clone_try_double "${primary_urls[absl]}" "${secondary_urls[absl]}" "absl" "$absl_ref"
+repo_clone_try_double "${primary_urls[absl]}" "${secondary_urls[absl]}" "absl" "$absl_ref" true
 
 # if [ -z "${MG_TOOLCHAIN_VERSION}" ]; then
 # jemalloc ea6b3e973b477b8061e0076bb257dbd7f3faa756
