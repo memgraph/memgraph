9c99c4097ba64fe5ebd6e6b48c35e40d772015e1
Memgraph <josip.mrden@memgraph.io>
2025-08-18 14:01:49 +0200

    Add syntax tests

--- tests/unit/cypher_main_visitor.cpp
+++ tests/unit/cypher_main_visitor.cpp
@@ -6266,3 +6266,44 @@ TEST_P(CypherMainVisitorTest, TestShowActiveUsersInfo) {
     EXPECT_EQ(query->info_type_, SystemInfoQuery::InfoType::ACTIVE_USERS);
   }
 }
+
+TEST_P(CypherMainVisitorTest, TestNestedPropertyUpdate) {
+  {
+    auto &ast_generator = *GetParam();
+    auto *query = dynamic_cast<CypherQuery *>(ast_generator.ParseQuery("MATCH (n) SET n.details.age = 21"));
+    ASSERT_FALSE(query, nullptr);
+
+    ASSERT_EQ(query->single_query_->clauses_.size(), 2);
+    auto *set_property = dynamic_cast<SetProperty *>(single_query->clauses_[1]);
+    ASSERT_FALSE(set_property, nullptr);
+
+    ASSERT_EQ(set_property->property_lookup_->property_path_.size(), 2);
+    ASSERT_EQ(set_property->property_lookup_->lookup_mode_, PropertyLookup::LookupMode::REPLACE);
+  }
+
+  {
+    auto &ast_generator = *GetParam();
+    auto *query =
+        dynamic_cast<CypherQuery *>(ast_generator.ParseQuery("MATCH (n) SET n.details.details2 += {age: 21}"));
+    ASSERT_FALSE(query, nullptr);
+
+    ASSERT_EQ(query->single_query_->clauses_.size(), 2);
+    auto *set_property = dynamic_cast<SetProperty *>(single_query->clauses_[1]);
+    ASSERT_FALSE(set_property, nullptr);
+
+    ASSERT_EQ(set_property->property_lookup_->property_path_.size(), 2);
+    ASSERT_EQ(set_property->property_lookup_->lookup_mode_, PropertyLookup::LookupMode::APPEND);
+  }
+
+  {
+    auto &ast_generator = *GetParam();
+    auto *query = dynamic_cast<CypherQuery *>(ast_generator.ParseQuery("MATCH (n) REMOVE n.details.age"));
+    ASSERT_FALSE(query, nullptr);
+
+    ASSERT_EQ(query->single_query_->clauses_.size(), 2);
+    auto *remove_property = dynamic_cast<RemoveProperty *>(single_query->clauses_[1]);
+    ASSERT_FALSE(remove_property, nullptr);
+
+    ASSERT_EQ(remove_property->property_lookup_->property_path_.size(), 2);
+  }
+}
