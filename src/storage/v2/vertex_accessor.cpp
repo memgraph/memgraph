5540e8a8819b831c7a41c1b8bf051b035b8e708b
Colin Barry <colin.barry@memgraph.io>
2025-08-22 13:55:24 +0100

    fix: Only use conflict detection in edge creation case

--- src/storage/v2/vertex_accessor.cpp
+++ src/storage/v2/vertex_accessor.cpp
@@ -136,8 +136,7 @@ Result<bool> VertexAccessor::AddLabel(LabelId label) {
   }
   auto guard = std::unique_lock{vertex_->lock};
 
-  if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-    return Error::SERIALIZATION_ERROR;
+  if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
   if (vertex_->deleted) return Error::DELETED_OBJECT;
 
   // Now that the vertex is locked, we can check if it has any edges and if it does, we can upgrade the accessor
@@ -149,8 +148,7 @@ Result<bool> VertexAccessor::AddLabel(LabelId label) {
                  *SchemaInfoUniqueAccessor(storage_, transaction_));
       guard.lock();
       // Need to re-check for serialization errors
-      if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-        return Error::SERIALIZATION_ERROR;
+      if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
       if (vertex_->deleted) return Error::DELETED_OBJECT;
     }
   }
@@ -209,8 +207,7 @@ Result<bool> VertexAccessor::RemoveLabel(LabelId label) {
   }
   auto guard = std::unique_lock{vertex_->lock};
 
-  if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-    return Error::SERIALIZATION_ERROR;
+  if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
   if (vertex_->deleted) return Error::DELETED_OBJECT;
 
   // Now that the vertex is locked, we can check if it has any edges and if it does, we can upgrade the accessor
@@ -222,8 +219,7 @@ Result<bool> VertexAccessor::RemoveLabel(LabelId label) {
                  *SchemaInfoUniqueAccessor(storage_, transaction_));
       guard.lock();
       // Need to re-check for serialization errors
-      if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-        return Error::SERIALIZATION_ERROR;
+      if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
       if (vertex_->deleted) return Error::DELETED_OBJECT;
     }
   }
@@ -359,8 +355,7 @@ Result<PropertyValue> VertexAccessor::SetProperty(PropertyId property, const Pro
   auto schema_acc = SchemaInfoAccessor(storage_, transaction_);
   auto guard = std::unique_lock{vertex_->lock};
 
-  if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-    return Error::SERIALIZATION_ERROR;
+  if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
 
   if (vertex_->deleted) return Error::DELETED_OBJECT;
 
@@ -428,8 +423,7 @@ Result<bool> VertexAccessor::InitProperties(const std::map<storage::PropertyId, 
   auto schema_acc = SchemaInfoAccessor(storage_, transaction_);
   auto guard = std::unique_lock{vertex_->lock};
 
-  if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-    return Error::SERIALIZATION_ERROR;
+  if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
 
   if (vertex_->deleted) return Error::DELETED_OBJECT;
   bool result{false};
@@ -486,8 +480,7 @@ Result<std::vector<std::tuple<PropertyId, PropertyValue, PropertyValue>>> Vertex
   auto schema_acc = SchemaInfoAccessor(storage_, transaction_);
   auto guard = std::unique_lock{vertex_->lock};
 
-  if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-    return Error::SERIALIZATION_ERROR;
+  if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
 
   if (vertex_->deleted) return Error::DELETED_OBJECT;
 
@@ -540,8 +533,7 @@ Result<std::map<PropertyId, PropertyValue>> VertexAccessor::ClearProperties() {
   auto schema_acc = SchemaInfoAccessor(storage_, transaction_);
   auto guard = std::unique_lock{vertex_->lock};
 
-  if (!PrepareForWriteWithRetry(transaction_, vertex_, storage_->transaction_dependencies_))
-    return Error::SERIALIZATION_ERROR;
+  if (!PrepareForWrite(transaction_, vertex_)) return Error::SERIALIZATION_ERROR;
 
   if (vertex_->deleted) return Error::DELETED_OBJECT;
 
