1514a012a882aba990ce293f9be52df4477c0ac5
as51340 <andi8647@gmail.com>
2025-09-03 14:18:09 +0200

    refactor: Add file_replication_handler.hpp

---
+++ src/rpc/file_replication_handler.hpp
@@ -0,0 +1,37 @@
+// Copyright 2025 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+#pragma once
+
+#include "utils/file.hpp"
+
+namespace memgraph::rpc {
+class FileReplicationHandler final {
+ public:
+  FileReplicationHandler(const uint8_t *data, size_t size);
+  ~FileReplicationHandler();
+
+  FileReplicationHandler(const FileReplicationHandler &) = delete;
+  FileReplicationHandler &operator=(const FileReplicationHandler &) = delete;
+
+  FileReplicationHandler(FileReplicationHandler &&) = default;
+  FileReplicationHandler &operator=(FileReplicationHandler &&) = default;
+
+  void WriteToFile(const uint8_t *data, size_t size);
+
+  // TODO: (andi) make private
+  utils::OutputFile file_;
+  uint64_t file_size_;
+  // How many bytes we wrote to the file
+  uint64_t written_;
+  std::vector<std::filesystem::path> paths_;
+};
+}  // namespace memgraph::rpc
