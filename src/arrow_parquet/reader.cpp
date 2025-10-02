7e4c5394407a9b41825db14922eec0344918038a
as51340 <andi8647@gmail.com>
2025-10-02 12:46:51 +0200

    feat: Use RecordBatchReader

--- src/arrow_parquet/reader.cpp
+++ src/arrow_parquet/reader.cpp
@@ -13,9 +13,15 @@
 
 #include <chrono>
 #include <optional>
-#include <sstream>
+
+#include <arrow/acero/exec_plan.h>
+#include <arrow/dataset/dataset.h>
+#include <arrow/dataset/file_parquet.h>
+#include <arrow/filesystem/api.h>
 
 #include "spdlog/spdlog.h"
+
+constexpr int64_t batch_rows = 1U << 18U;
 
 namespace memgraph::arrow {
 
@@ -37,14 +43,11 @@ struct RowRef {
 class RowIterator {
  public:
   RowIterator() = default;
-  explicit RowIterator(std::shared_ptr<::arrow::Table> table, int num_columns, int64_t const batch_rows = 1UL << 16U)
-      : num_columns_(num_columns) {
-    cols_.reserve(table->num_columns());
-    tbr_.emplace(std::move(table));
-    tbr_->set_chunksize(batch_rows);
+  explicit RowIterator(std::unique_ptr<::arrow::RecordBatchReader> rbr, int const num_columns)
+      : num_columns_(num_columns), rbr_(std::move(rbr)) {
+    cols_.reserve(num_columns_);
   }
 
-  // Delete copy operations since TableBatchReader can't be copied
   RowIterator(const RowIterator &) = delete;
   RowIterator &operator=(const RowIterator &) = delete;
   RowIterator(RowIterator &&) = delete;
@@ -54,9 +57,11 @@ class RowIterator {
   std::optional<RowRef> Next() {
     // Need to load another batch
     if (!batch_ || row_in_batch_ >= batch_->num_rows()) {
-      if (const auto status = tbr_->ReadNext(&batch_); !status.ok()) {
+      auto const res = rbr_->Next();
+      if (!res.ok()) {
         return std::nullopt;
       }
+      batch_ = *res;
       // Check if we actually got a batch (end of data)
       if (!batch_) {
         return std::nullopt;
@@ -74,7 +79,7 @@ class RowIterator {
 
  private:
   int num_columns_;
-  std::optional<::arrow::TableBatchReader> tbr_;
+  std::unique_ptr<::arrow::RecordBatchReader> rbr_;
   // A record batch is table-like data structure that is semantically a sequence of fields, each a contiguous Arrow
   // array
   std::shared_ptr<::arrow::RecordBatch> batch_;
@@ -83,22 +88,31 @@ class RowIterator {
 };
 
 struct ParquetReader::impl {
-  explicit impl(std::shared_ptr<::arrow::Table> table);
+  explicit impl(std::unique_ptr<parquet::arrow::FileReader> file_reader,
+                std::unique_ptr<::arrow::RecordBatchReader> rbr);
 
   auto GetNextRow() -> std::optional<Row>;
 
+  auto GetSchema() -> std::shared_ptr<::arrow::Schema>;
+
  private:
+  std::unique_ptr<parquet::arrow::FileReader> file_reader_;  // Keep FileReader alive
+  std::shared_ptr<::arrow::Schema> schema_;
   int num_columns_;
-  std::string file_;
   RowIterator row_it_;
 };
 
-ParquetReader::impl::impl(std::shared_ptr<::arrow::Table> table)
-    : num_columns_(table->num_columns()), row_it_(RowIterator(std::move(table), num_columns_)) {}
+ParquetReader::impl::impl(std::unique_ptr<parquet::arrow::FileReader> file_reader,
+                          std::unique_ptr<::arrow::RecordBatchReader> rbr)
+    : file_reader_(std::move(file_reader)),
+      schema_(rbr->schema()),
+      num_columns_(schema_->num_fields()),
+      row_it_(RowIterator(std::move(rbr), num_columns_)) {}
 
 // TODO: (andi)
 // Next steps are optimizing this or trying Scanner
 // Try to improve it by using Scanner -> it should be faster when the data is on disk, and ours it is
+// ParquetFileReader?
 
 auto ParquetReader::impl::GetNextRow() -> std::optional<Row> {
   auto const maybe_arrow_row = row_it_.Next();
@@ -108,7 +122,7 @@ auto ParquetReader::impl::GetNextRow() -> std::optional<Row> {
   Row row;
   row.reserve(num_columns_);
 
-  for (size_t i = 0; i < num_columns_; i++) {
+  for (int i = 0; i < num_columns_; i++) {
     // TODO: (andi) Not sure if this is good ValueOrDie
     auto cell = arrow_row[i].ToScalar().ValueOrDie()->ToString();
     row.push_back(std::move(cell));
@@ -117,17 +131,51 @@ auto ParquetReader::impl::GetNextRow() -> std::optional<Row> {
   return row;
 }
 
+auto ParquetReader::impl::GetSchema() -> std::shared_ptr<::arrow::Schema> { return schema_; }
+
 ParquetReader::ParquetReader(std::string const &file) {
   auto const start = std::chrono::high_resolution_clock::now();
-  std::shared_ptr<::arrow::io::ReadableFile> infile;
-  std::unique_ptr<parquet::arrow::FileReader> reader;
-  PARQUET_ASSIGN_OR_THROW(infile, ::arrow::io::ReadableFile::Open(file, ::arrow::default_memory_pool()));
-  PARQUET_ASSIGN_OR_THROW(reader, parquet::arrow::OpenFile(infile, ::arrow::default_memory_pool()));
-  PARQUET_THROW_NOT_OK(reader->ReadTable(&table_));
+  ::arrow::MemoryPool *pool = ::arrow::default_memory_pool();
+
+  try {
+    // Open the file
+    std::shared_ptr<::arrow::io::ReadableFile> infile;
+    PARQUET_ASSIGN_OR_THROW(infile, ::arrow::io::ReadableFile::Open(file, pool));
+
+    // Configure general Parquet reader settings
+    auto reader_properties = parquet::ReaderProperties(pool);
+    reader_properties.set_buffer_size(4096 * 4);
+    reader_properties.enable_buffered_stream();
+
+    // Configure Arrow-specific Parquet reader settings
+    auto arrow_reader_props = parquet::ArrowReaderProperties();
+    arrow_reader_props.set_batch_size(batch_rows);
+    arrow_reader_props.set_use_threads(true);
+
+    // Build the reader
+    parquet::arrow::FileReaderBuilder reader_builder;
+    PARQUET_THROW_NOT_OK(reader_builder.Open(infile, reader_properties));
+    reader_builder.memory_pool(pool);
+    reader_builder.properties(arrow_reader_props);
+
+    std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
+    PARQUET_THROW_NOT_OK(reader_builder.Build(&arrow_reader));
+
+    // Get the RecordBatchReader
+    auto res = arrow_reader->GetRecordBatchReader();
+    if (!res.ok()) {
+      throw std::runtime_error(res.status().message());
+    }
+
+    pimpl_ = std::make_unique<impl>(std::move(arrow_reader), std::move(*res));
+  } catch (const std::exception &e) {
+    spdlog::error("Failed to open parquet file '{}': {}", file, e.what());
+    throw;
+  }
+
   auto const duration =
       std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
-  spdlog::trace("Time spent on reading table: {}ms", duration.count());
-  pimpl_ = std::make_unique<impl>(table_);
+  spdlog::trace("Time spent on initializing parquet reader: {}ms", duration.count());
 }
 
 // Destructor must be defined here where impl is complete
@@ -136,7 +184,7 @@ ParquetReader::~ParquetReader() = default;
 auto ParquetReader::GetNextRow() const -> std::optional<Row> { return pimpl_->GetNextRow(); }
 
 auto ParquetReader::GetHeader() const -> Row {
-  auto const schema = table_->schema();
+  auto const schema = pimpl_->GetSchema();
   Row header;
   auto const header_size = schema->num_fields();
   header.reserve(header_size);
