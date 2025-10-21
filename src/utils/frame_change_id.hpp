674a70f6c54d050076a4d5d70431e80329880615
Gareth Andrew Lloyd <gareth.lloyd@memgraph.io>
2025-10-21 14:12:44 +0100

    InList optimisation on more expressions (#3347)
    Now always generating a FrameChangeId for each InList operator.
    
    This means we now support the optimisation for queries like `x IN
    range(0,1000)`.

--- src/utils/frame_change_id.hpp
+++ src/utils/frame_change_id.hpp
@@ -18,8 +18,7 @@
 namespace memgraph::utils {
 
 struct FrameChangeId {
-  explicit FrameChangeId(query::ListLiteral *list_literal)
-      : kind_(Kind::ListExpr), hash_{std::hash<void *>{}(list_literal)}, list_expr_{list_literal} {}
+  explicit FrameChangeId(query::Expression *expr) : kind_(Kind::Expr), hash_{std::hash<void *>{}(expr)}, expr_{expr} {}
 
   explicit FrameChangeId(query::Symbol::Position_t const &symbol_pos)
       : kind_(Kind::Sym), hash_{std::hash<int32_t>{}(symbol_pos)}, symbol_pos_{symbol_pos} {}
@@ -27,8 +26,8 @@ struct FrameChangeId {
   friend bool operator==(const FrameChangeId &lhs, const FrameChangeId &rhs) {
     if (lhs.kind_ != rhs.kind_) return false;
     switch (lhs.kind_) {
-      case Kind::ListExpr:
-        return lhs.list_expr_ == rhs.list_expr_;
+      case Kind::Expr:
+        return lhs.expr_ == rhs.expr_;
       case Kind::Sym:
         return lhs.symbol_pos_ == rhs.symbol_pos_;
     }
@@ -37,28 +36,28 @@ struct FrameChangeId {
   size_t hash() const { return hash_; }
 
  private:
-  enum struct Kind : uint8_t { ListExpr, Sym };
+  enum struct Kind : uint8_t { Expr, Sym };
   Kind kind_;
   std::size_t hash_;
   union {
     query::Symbol::Position_t symbol_pos_;
-    query::ListLiteral *list_expr_;
+    query::Expression *expr_;
   };
 };
 
 // Get ID by which FrameChangeCollector struct can cache in_list.expression2_
-inline std::optional<FrameChangeId> GetFrameChangeId(memgraph::query::InListOperator &in_list) {
-  if (in_list.expression2_->GetTypeInfo() == memgraph::query::ListLiteral::kType) {
-    auto *list_literal = utils::Downcast<memgraph::query::ListLiteral>(in_list.expression2_);
-    return FrameChangeId{list_literal};
-  }
+inline FrameChangeId GetFrameChangeId(const memgraph::query::InListOperator &in_list) {
+  // Identifiers refer to a list already stored on the frame
+  // We only want to build one set for that identifier, hence key on that symbol
+  // e.g. `x IN lst AND y IN lst` that is 2 InListOperator that both use the same `lst`
   if (in_list.expression2_->GetTypeInfo() == memgraph::query::Identifier::kType) {
-    auto *identifier = utils::Downcast<memgraph::query::Identifier>(in_list.expression2_);
+    const auto *identifier = utils::Downcast<memgraph::query::Identifier>(in_list.expression2_);
     MG_ASSERT(identifier->symbol_pos_ != -1);
     return FrameChangeId{identifier->symbol_pos_};
   }
-  return {};
-};
+  // Otherwise we uniquely cache this expression
+  return FrameChangeId{in_list.expression2_};
+}
 
 }  // namespace memgraph::utils
 
