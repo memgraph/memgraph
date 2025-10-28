7479cfca1863fe686c43ffaabf74869310cb439e
Andreja Tonev <andreja.tonev@memgraph.io>
2025-10-28 16:50:16 +0100

    Bigfix: Edge index RemoveObsoleteEntries broke MVCC isolation

--- src/storage/v2/inmemory/edge_type_index.cpp
+++ src/storage/v2/inmemory/edge_type_index.cpp
@@ -233,8 +233,6 @@ void InMemoryEdgeTypeIndex::RemoveObsoleteEntries(uint64_t oldest_active_start_t
         continue;
       }
 
-      const bool vertices_deleted = it->from_vertex->deleted || it->to_vertex->deleted;
-      const bool edge_deleted = it->edge->deleted;
       const bool has_next = next_it != edges_acc.end();
 
       // When we update specific entries in the index, we don't delete the previous entry.
@@ -243,7 +241,7 @@ void InMemoryEdgeTypeIndex::RemoveObsoleteEntries(uint64_t oldest_active_start_t
       // should be removed here.
       const bool redundant_duplicate = has_next && it->from_vertex == next_it->from_vertex &&
                                        it->to_vertex == next_it->to_vertex && it->edge == next_it->edge;
-      if (redundant_duplicate || vertices_deleted || edge_deleted) {
+      if (redundant_duplicate) {
         edges_acc.remove(*it);
       }
 
