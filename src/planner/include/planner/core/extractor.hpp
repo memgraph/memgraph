cab6c17705db4f22548cbfac8c3fe28993a2e9f7
Gareth Lloyd <gareth.lloyd@memgraph.io>
2025-11-07 15:46:07 +0000

    working on extractor

--- src/planner/include/planner/core/extractor.hpp
+++ src/planner/include/planner/core/extractor.hpp
@@ -35,43 +35,57 @@ struct Cost {
 // Standalone function: Process an e-graph to find the cheapest e-node for each e-class
 template <typename Symbol, typename Analysis, typename Func>
 requires std::is_invocable_r_v < double, Func, ENode<Symbol>
-const & > auto ProcessEGraph(EGraph<Symbol, Analysis> const &egraph, Func const &cost_function, EClassId id,
-                             std::unordered_map<EClassId, Cost> &cheapest_enode) -> double {
-  // if (!is_eclass) {
-  //   // enode
-  //   const auto &enode = egraph.get_enode(id);
-  //   return std::accumulate(enode.children().begin(), enode.children().end(), 0.0,
-  //                          [&](double acc, EClassId child_eclass_id) {
-  //                            return acc + ProcessEGraph(egraph, cost_function, child_eclass_id, cheapest_enode);
-  //                          });
-  // }
+const & > auto ProcessCosts(EGraph<Symbol, Analysis> const &egraph, Func const &cost_function, EClassId eclass_id,
+                            std::unordered_map<EClassId, Cost> &cheapest_enode) -> double {
+  DMG_ASSERT(!egraph.needs_rebuild(), "to avoid internal cost of getting canonical looking up we should");
 
-  // eclass
-  auto it = cheapest_enode.find(id);
+  const auto it = cheapest_enode.find(eclass_id);
   if (it != cheapest_enode.end()) {
     // If cost is infinity, we're currently processing this e-class (cycle detected)
     // If cost is finite, we've already computed it
     return it->second.cost;
   }
 
+  auto const &eclass = egraph.eclass(eclass_id);
+
   // Mark this e-class as "in progress" with infinity cost to detect cycles
-  cheapest_enode.emplace(id, Cost(ENodeId{0}, std::numeric_limits<double>::infinity()));
+  auto [tmp_it, _] =
+      cheapest_enode.emplace(eclass_id, Cost(*eclass.nodes().begin(), std::numeric_limits<double>::infinity()));
 
-  double best_cost = std::numeric_limits<double>::max();
-  ENodeId best_node{0};
-  for (auto const &enode_id : egraph.eclass(id).nodes()) {
-    auto cost = cost_function(egraph.get_enode(enode_id)) +
-                ProcessEGraph(egraph, cost_function, egraph.find(enode_id), cheapest_enode);
+  auto best_node = std::optional<Cost>{};
 
-    if (cost < best_cost) {
-      best_cost = cost;
-      best_node = enode_id;
+  for (auto const &enode_id : eclass.nodes()) {
+    auto &enode = egraph.get_enode(enode_id);
+    auto cost_value =
+        std::accumulate(enode.children().begin(), enode.children().end(), cost_function(enode),
+                        [&](double acc, EClassId child_eclass_id) {
+                          return acc + ProcessCosts(egraph, cost_function, child_eclass_id, cheapest_enode);
+                        });
+
+    // ignore enodes who have infinate cost
+    if (cost_value == std::numeric_limits<double>::infinity()) {
+      continue;
+    }
+
+    auto cost = Cost{enode_id, cost_value};
+    if (best_node) {
+      if (cost.cost < best_node->cost) {
+        best_node = cost;
+      }
+    } else {
+      best_node = cost;
     }
   }
 
   // Update with the actual computed cost
-  cheapest_enode[id] = Cost(best_node, best_cost);
-  return best_cost;
+  if (best_node) {
+    tmp_it->second = *best_node;
+    return best_node->cost;
+  }
+
+  // Remove infinate cycle case
+  cheapest_enode.erase(tmp_it);
+  return std::numeric_limits<double>::infinity();
 }
 
 // Standalone function: Collect dependencies and compute in-degrees for topological sort
@@ -94,13 +108,14 @@ auto CollectDependencies(EGraph<Symbol, Analysis> const &egraph,
 // Standalone function: Perform topological sort to produce ordered output
 template <typename Symbol, typename Analysis>
 auto TopologicalSort(EGraph<Symbol, Analysis> const &egraph, std::unordered_map<EClassId, Cost> const &cheapest_enode,
-                     std::unordered_map<EClassId, int> in_degree, EClassId eclass_id,
-                     std::vector<std::pair<EClassId, ENodeId>> &result) -> void {
+                     std::unordered_map<EClassId, int> in_degree, EClassId eclass_id)
+    -> std::vector<std::pair<EClassId, ENodeId>> {
+  auto result = std::vector<std::pair<EClassId, ENodeId>>{};
+  result.reserve(in_degree.size() + 1);
   // egraph is an acyclic graph so we can use topological sort
 
   // root should be the only eclass with in_degree 0
   std::queue<EClassId> queue{{eclass_id}};
-  result.reserve(in_degree.size() + 1);
 
   while (!queue.empty()) {
     EClassId current_id = queue.front();
@@ -119,6 +134,7 @@ auto TopologicalSort(EGraph<Symbol, Analysis> const &egraph, std::unordered_map<
       }
     }
   }
+  return result;
 }
 
 template <typename Symbol, typename Analysis, typename Func>
@@ -131,20 +147,17 @@ const & > struct Extractor {
   cost_function_(std::forward<FuncInner>(cost_function)) {}
 
   auto Extract(EClassId const root_id) -> std::vector<std::pair<EClassId, ENodeId>> {
-    ProcessEGraph(egraph_, cost_function_, root_id, cheapest_enode_);
-    CollectDependencies(egraph_, cheapest_enode_, root_id, in_degree_);
-    TopologicalSort(egraph_, cheapest_enode_, in_degree_, root_id, result_);
-    return result_;
+    auto cheapest_enode = std::unordered_map<EClassId, Cost>{};
+    ProcessCosts(egraph_, cost_function_, root_id, cheapest_enode);
+    auto in_degree = std::unordered_map<EClassId, int>{};
+    CollectDependencies(egraph_, cheapest_enode, root_id, in_degree);
+    return TopologicalSort(egraph_, cheapest_enode, in_degree, root_id);
   }
-  auto GetResult() -> std::vector<std::pair<EClassId, ENodeId>> const & { return result_; }
 
  private:
   // NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
   EGraph<Symbol, Analysis> const &egraph_;
   Func cost_function_;
-  std::vector<std::pair<EClassId, ENodeId>> result_;
-  std::unordered_map<EClassId, Cost> cheapest_enode_;
-  std::unordered_map<EClassId, int> in_degree_;
 };
 
 template <typename Symbol, typename Analysis, typename Func>
