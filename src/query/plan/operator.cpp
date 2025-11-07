f38df457ab93103a7aea7261a95efd4a50d45281
as51340 <andi8647@gmail.com>
2025-11-07 10:44:29 +0100

    feat: Add jsonl cpp module

--- src/query/plan/operator.cpp
+++ src/query/plan/operator.cpp
@@ -32,6 +32,7 @@
 
 #include "csv/parsing.hpp"
 #include "license/license.hpp"
+import memgraph.query.jsonl.reader;
 #include "query/arrow_parquet/parquet_file_config.hpp"
 #include "query/arrow_parquet/reader.hpp"
 #include "query/context.hpp"
@@ -7887,6 +7888,8 @@ class LoadJsonlCursor : public Cursor {
   const LoadJsonl *self_;
   const UniqueCursorPtr input_cursor_;
   bool did_pull_{false};
+  std::optional<JsonlReader> reader_;
+  Row row_;
 
  public:
   LoadJsonlCursor(const LoadJsonl *self, utils::MemoryResource *mem)
@@ -7897,8 +7900,14 @@ class LoadJsonlCursor : public Cursor {
     SCOPED_PROFILE_OP_BY_REF(*self_);
     AbortCheck(context);
 
-    // auto frame_writer = frame.GetFrameWriter(context.frame_change_collector, context.evaluation_context.memory);
-    // auto *mem = context.evaluation_context.memory;
+    auto *mem = context.evaluation_context.memory;
+    auto frame_writer = frame.GetFrameWriter(context.frame_change_collector, context.evaluation_context.memory);
+
+    if (UNLIKELY(!reader_.has_value())) {
+      auto evaluator = PrimitiveLiteralExpressionEvaluator{context.evaluation_context};
+      auto maybe_file = self_->file_->Accept(evaluator).ValueString();
+      reader_.emplace(std::string{maybe_file}, mem);
+    }
 
     if (input_cursor_->Pull(frame, context)) {
       if (did_pull_) {
@@ -7908,6 +7917,18 @@ class LoadJsonlCursor : public Cursor {
       }
       did_pull_ = true;
     }
+
+    if (!reader_->GetNextRow(row_)) {
+      return false;
+    }
+
+    frame_writer.Modify(self_->row_var_, [&](TypedValue &value) {
+      if (value.IsMap()) {
+        std::swap(value.ValueMap(), row_);
+      } else {
+        value = TypedValue(std::move(row_), mem);
+      }
+    });
 
     if (context.frame_change_collector) {
       context.frame_change_collector->ResetInListCache(self_->row_var_);
