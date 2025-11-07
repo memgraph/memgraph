747d98b399f889656162ebf333e05bfa75326b6e
Colin Barry <colin.barry@memgraph.io>
2025-11-07 14:42:14 +0000

    refactor: Change call to static member func to silence clang-tidy

--- src/storage/v2/inmemory/storage.cpp
+++ src/storage/v2/inmemory/storage.cpp
@@ -634,7 +634,7 @@ Result<EdgeAccessor> InMemoryStorage::InMemoryAccessor::CreateEdge(VertexAccesso
     transaction_.manyDeltasCache.Invalidate(to_vertex, edge_type, EdgeDirection::IN);
 
     // Update indices if they exist.
-    storage_->indices_.UpdateOnEdgeCreation(from_vertex, to_vertex, edge, edge_type, transaction_);
+    Indices::UpdateOnEdgeCreation(from_vertex, to_vertex, edge, edge_type, transaction_);
 
     // Increment edge count.
     storage_->edge_count_.fetch_add(1, std::memory_order_acq_rel);
@@ -764,7 +764,7 @@ Result<EdgeAccessor> InMemoryStorage::InMemoryAccessor::CreateEdgeEx(VertexAcces
     transaction_.manyDeltasCache.Invalidate(to_vertex, edge_type, EdgeDirection::IN);
 
     // Update indices if they exist.
-    storage_->indices_.UpdateOnEdgeCreation(from_vertex, to_vertex, edge, edge_type, transaction_);
+    Indices::UpdateOnEdgeCreation(from_vertex, to_vertex, edge, edge_type, transaction_);
 
     // Increment edge count.
     storage_->edge_count_.fetch_add(1, std::memory_order_acq_rel);
