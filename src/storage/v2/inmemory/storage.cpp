8e30afb8cac2b588741ee04eb8577da6561b00d2
Colin Barry <colin.barry@memgraph.io>
2025-11-10 15:58:44 +0000

    fix: Break WAL interleaved delta iteration for EDGE

--- src/storage/v2/inmemory/storage.cpp
+++ src/storage/v2/inmemory/storage.cpp
@@ -3080,6 +3080,8 @@ bool InMemoryStorage::InMemoryAccessor::HandleDurabilityAndReplicate(uint64_t du
       auto prev = delta.prev.Get();
       MG_ASSERT(prev.type != PreviousPtr::Type::NULL_PTR, "Invalid pointer!");
 
+      if (prev.type == PreviousPtr::Type::EDGE) continue;
+
       bool const is_subchain_start =
           (prev.type == PreviousPtr::Type::VERTEX) ||
           (prev.type == PreviousPtr::Type::DELTA &&
@@ -3112,6 +3114,8 @@ bool InMemoryStorage::InMemoryAccessor::HandleDurabilityAndReplicate(uint64_t du
     for (const auto &delta : transaction_.deltas) {
       auto prev = delta.prev.Get();
       MG_ASSERT(prev.type != PreviousPtr::Type::NULL_PTR, "Invalid pointer!");
+
+      if (prev.type == PreviousPtr::Type::EDGE) continue;
 
       bool const is_subchain_start =
           (prev.type == PreviousPtr::Type::VERTEX) ||
@@ -3169,6 +3173,8 @@ bool InMemoryStorage::InMemoryAccessor::HandleDurabilityAndReplicate(uint64_t du
       auto prev = delta.prev.Get();
       MG_ASSERT(prev.type != PreviousPtr::Type::NULL_PTR, "Invalid pointer!");
 
+      if (prev.type == PreviousPtr::Type::EDGE) continue;
+
       bool const is_subchain_start =
           (prev.type == PreviousPtr::Type::VERTEX) ||
           (prev.type == PreviousPtr::Type::DELTA &&
@@ -3200,6 +3206,8 @@ bool InMemoryStorage::InMemoryAccessor::HandleDurabilityAndReplicate(uint64_t du
     for (const auto &delta : transaction_.deltas) {
       auto prev = delta.prev.Get();
       MG_ASSERT(prev.type != PreviousPtr::Type::NULL_PTR, "Invalid pointer!");
+
+      if (prev.type == PreviousPtr::Type::EDGE) continue;
 
       bool const is_subchain_start =
           (prev.type == PreviousPtr::Type::VERTEX) ||
