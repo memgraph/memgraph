a03e87f9d4ba1168318754a4ad31513020fe0a43
matt <mattkjames7@gmail.com>
2025-11-11 15:29:02 +0000

    Daily ISSU test

---
+++ tools/get_daily_issu_images.py
@@ -0,0 +1,27 @@
+import os
+from typing import List
+
+from aggregate_build_tests import list_daily_release_packages
+
+
+def get_daily_issu_images(date: int) -> List[str]:
+    """
+    Get the Releasse docker images for x86_64 and arm64 for the given date
+    """
+    packages = list_daily_release_packages(date, return_url=False)
+    amd_docker = f"https://s3.eu-west-1.amazonaws.com/deps.memgraph.io/{packages['docker']['x86_64']}"
+    arm_docker = f"https://s3.eu-west-1.amazonaws.com/deps.memgraph.io/{packages['docker']['arm64']}"
+    return amd_docker, arm_docker
+
+
+def main() -> None:
+    """
+    Print images to be used in ISSU test
+    """
+    date = int(os.getenv("CURRENT_BUILD_DATE"))
+    amd_docker, arm_docker = get_daily_issu_images(date)
+    print(f"{amd_docker} {arm_docker}")
+
+
+if __name__ == "__main__":
+    main()
