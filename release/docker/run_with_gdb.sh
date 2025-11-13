769f09ec374476f3fd56075dbe395818d62f068d
matt <mattkjames7@gmail.com>
2025-11-13 11:26:13 +0000

    change core output directory

--- release/docker/run_with_gdb.sh
+++ release/docker/run_with_gdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script for launching Memgraph with GDB inside a container. Override the entrypoint to use this script.
-
+mkdir -p /tmp/cores
 exec gdb -ex "set confirm off" -ex "set pagination off" -ex run -ex "thread apply all bt full" \
-    -ex "generate-core-file /var/lib/memgraph/core" \
+    -ex "generate-core-file /tmp/cores/core" \
     --args /usr/lib/memgraph/memgraph "$@"
