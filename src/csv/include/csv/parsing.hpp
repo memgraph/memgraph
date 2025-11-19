6a37375db48cbf7603c4416fdb98b5bf0ad39f45
as51340 <andi8647@gmail.com>
2025-11-19 09:54:14 +0100

    feat: Add support for downloading files using https?/ftp

--- src/csv/include/csv/parsing.hpp
+++ src/csv/include/csv/parsing.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Memgraph Ltd.
+// Copyright 2025 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -24,7 +24,6 @@
 #include <optional>
 #include <string>
 #include <variant>
-#include <vector>
 
 #include "utils/exceptions.hpp"
 #include "utils/pmr/string.hpp"
