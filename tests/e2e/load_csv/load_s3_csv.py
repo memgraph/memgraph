d16d563c2c27c91cb6183346217c47c82c1b4117
as51340 <andi8647@gmail.com>
2025-11-24 11:32:39 +0100

    fixup! refactor: LOAD CSV parsing

--- tests/e2e/load_csv/load_s3_csv.py
+++ tests/e2e/load_csv/load_s3_csv.py
@@ -19,8 +19,8 @@ from mgclient import DatabaseError
 
 # LocalStack configuration
 # NOTE: If you are testing this locally change localstack-s3 to localhost
-# AWS_ENDPOINT_URL = "http://localstack-s3:4566"
-AWS_ENDPOINT_URL = "http://localhost:4566"
+AWS_ENDPOINT_URL = "http://localstack-s3:4566"
+# AWS_ENDPOINT_URL = "http://localhost:4566"
 AWS_ACCESS_KEY_ID = "test"
 AWS_SECRET_ACCESS_KEY = "test"
 AWS_REGION = "us-east-1"
