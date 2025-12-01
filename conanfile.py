e35c2cedd323fae95491abd737886a10519f9543
matt <mattkjames7@gmail.com>
2025-12-01 20:43:56 +0000

    static link to zlib

--- conanfile.py
+++ conanfile.py
@@ -86,8 +86,7 @@ class Memgraph(ConanFile):
         self.requires("snappy/1.2.1", override=True)
         self.requires("spdlog/1.15.3")
         self.requires("strong_type/v15")
-        self.requires("zlib/1.3.1", override=True)
-        self.requires("openssl/3.5.4", override=True, options={"shared": True})
+        self.requires("zlib/1.3.1", override=True, options={"shared": False})
 
     def build_requirements(self):
         self.tool_requires("cmake/4.1.2")
