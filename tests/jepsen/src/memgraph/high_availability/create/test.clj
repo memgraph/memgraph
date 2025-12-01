ed52522c190d49a5f8894974fc7bb74599483313
as51340 <andi8647@gmail.com>
2025-12-01 08:49:28 +0100

    testiung: Randomize batch size

--- tests/jepsen/src/memgraph/high_availability/create/test.clj
+++ tests/jepsen/src/memgraph/high_availability/create/test.clj
@@ -35,10 +35,12 @@
 
 ; By modifying batch-size, we are testing the configuration in a different replication sitauations.
 ; Each situation won't last equally because of a different number of deltas.
-(def batch-size
+(defn batch-size
   "Number of nodes to import in the batch."
-  (rand-nth [100 1000 5000 10000 50000])
-  )
+  []
+  (let [res (rand-nth [100 1000 5000 10000 50000])]
+       (info "Batch size:" res)
+    res))
 
 (def delay-requests-sec 5)
 (defn hamming-sim
@@ -270,7 +272,7 @@
                                    max-idx (atom nil)]
                                (try
                                  (utils/with-session bolt-routing-conn session
-                                   (let [local-idx (->> (mgquery/add-nodes session {:batchSize batch-size})
+                                   (let [local-idx (->> (mgquery/add-nodes session {:batchSize (batch-size)})
                                                         (map :id)
                                                         (reduce conj [])
                                                         first)]
