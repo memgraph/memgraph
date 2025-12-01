1d7239905a037658e5abf99b942fcc09be62df9c
DavIvek <david.ivekovic@memgraph.io>
2025-12-01 14:41:47 +0100

    resolve conflicts

--- tests/drivers/run_cluster.sh
+++ tests/drivers/run_cluster.sh
@@ -236,12 +236,12 @@ fi
 
 echo "All instances registered successfully."
 
+DISABLE_NODE=${DISABLE_NODE:-false}
 code_test=0
 for lang in *; do
     if [ ! -d $lang ]; then continue; fi
 
-    # Skip Node.js driver tests in CI
-    if [ "$lang" = "node" ]; then
+    if [ "$lang" = "node" ] && [ "$DISABLE_NODE" = "true" ]; then
         echo "Skipping Node.js driver cluster tests in CI"
         continue
     fi
