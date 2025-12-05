f991a651bd4fd1faa739e0410b5041ac1a89b721
Andi Skrgat <andi8647@gmail.com>
2025-12-05 14:43:43 +0100

    feat: Modularize raft state (#3518)

--- src/coordination/include/coordination/coordinator_instance.hpp
+++ src/coordination/include/coordination/coordinator_instance.hpp
@@ -21,7 +21,6 @@
 
 #include "coordination/coordinator_instance_connector.hpp"
 #include "coordination/coordinator_instance_management_server.hpp"
-#include "coordination/raft_state.hpp"
 #include "coordination/replication_instance_connector.hpp"
 #include "utils/resource_lock.hpp"
 #include "utils/thread_pool.hpp"
@@ -29,8 +28,10 @@
 #include <list>
 
 import memgraph.coordination.coordinator_communication_config;
+import memgraph.coordination.coordinator_instance_aux;
 import memgraph.coordination.coordinator_ops_status;
 import memgraph.coordination.instance_status;
+import memgraph.coordination.raft_state;
 
 namespace memgraph::coordination {
 
--- tests/unit/coordinator_raft_state.cpp
+++ tests/unit/coordinator_raft_state.cpp
@@ -9,18 +9,20 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-#include "coordination/raft_state.hpp"
 #include "io/network/endpoint.hpp"
 #include "replication_coordination_glue/mode.hpp"
 #include "replication_coordination_glue/role.hpp"
+#include "utils/uuid.hpp"
 
 #include <gflags/gflags.h>
 #include <gtest/gtest.h>
+#include <libnuraft/nuraft.hxx>
 #include <nlohmann/json.hpp>
 
 import memgraph.coordination.coordinator_communication_config;
 import memgraph.coordination.coordinator_instance_context;
 import memgraph.coordination.data_instance_context;
+import memgraph.coordination.raft_state;
 import memgraph.coordination.utils;
 
 using memgraph::coordination::CoordinatorInstanceContext;
