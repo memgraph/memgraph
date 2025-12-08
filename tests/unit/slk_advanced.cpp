05ef15fd166c678a28a4674aaec078e7bef907a8
as51340 <andi8647@gmail.com>
2025-12-08 17:55:14 +0100

    feat: Modularize coord slk

--- tests/unit/slk_advanced.cpp
+++ tests/unit/slk_advanced.cpp
@@ -11,7 +11,6 @@
 
 #include <gtest/gtest.h>
 
-#include "coordination/coordinator_slk.hpp"
 #include "io/network/endpoint.hpp"
 #include "replication_coordination_glue/mode.hpp"
 #include "slk_common.hpp"
@@ -22,6 +21,7 @@
 #include "utils/uuid.hpp"
 
 import memgraph.coordination.coordinator_communication_config;
+import memgraph.coordination.coordinator_slk;
 
 using memgraph::io::network::Endpoint;
 
