8ec32355eebf58f1a354896695967ffb24054efc
Andreja Tonev <andreja.tonev@memgraph.io>
2025-12-10 14:43:59 +0100

    SharedQuota implementation and updated Skip/Limit/HopsLimit to use it (single threade for now)

---
+++ src/utils/shared_quota.hpp
@@ -0,0 +1,80 @@
+// Copyright 2025 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <memory>
+#include <optional>
+
+namespace memgraph::utils {
+
+class QuotaCoordinator {
+  std::atomic<int64_t> remaining_quota_;
+  std::atomic<int> active_holders_;
+  std::atomic<uint32_t> epoch_{0};  // Incremented whenever the state changes in a way waiters care about.
+
+ public:
+  explicit QuotaCoordinator(int64_t total_limit);
+
+  class QuotaHandle {
+    QuotaCoordinator *coord_;
+    int64_t amount_;
+
+   public:
+    QuotaHandle(QuotaCoordinator *coord, int64_t amount);
+    ~QuotaHandle() { ReturnUnused(); }
+
+    QuotaHandle(const QuotaHandle &) = delete;
+    QuotaHandle &operator=(const QuotaHandle &) = delete;
+    QuotaHandle(QuotaHandle &&other) noexcept;
+    QuotaHandle &operator=(QuotaHandle &&other) noexcept;
+
+    int64_t Count() const { return amount_; }
+    // Returns the amount that was actually consumed.
+    int64_t Consume(int64_t amount);
+    void ReturnUnused();
+  };
+
+  std::optional<QuotaHandle> Acquire(int64_t desired_batch_size);
+
+ private:
+  friend class QuotaHandle;
+  void ReturnQuota(int64_t amount);
+  void NotifyWaiters();
+};
+
+class SharedQuota {
+  std::shared_ptr<QuotaCoordinator> coord_;
+  int64_t desired_batch_size_;
+  std::optional<QuotaCoordinator::QuotaHandle> handle_;
+
+ public:
+  explicit SharedQuota(int64_t limit, int64_t n_batches = 1);
+  ~SharedQuota() = default;
+
+  SharedQuota(const SharedQuota &other) noexcept;
+  SharedQuota &operator=(const SharedQuota &other) noexcept;
+  SharedQuota(SharedQuota &&other) noexcept;
+  SharedQuota &operator=(SharedQuota &&other) noexcept;
+
+  // Returns true if the quota was successfully incremented.
+  // Generally, it returns true if the quota was incremented by the requested amount.
+  // However, if the quota is exhausted, it returns false (and the quota was not incremented or partially incremented).
+  // The caller should handle the case where the quota is exhausted.
+  // BUT the previous implementation returned bool, so to keep it compatible with the previous implementation
+  // we will change it to return the amount incremented.
+  int64_t Increment(int64_t amount = 1);
+  void Reset();
+};
+
+}  // namespace memgraph::utils
