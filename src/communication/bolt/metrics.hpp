decb47533583325e23be61be163980dc25552dbe
Gareth Lloyd <gareth.lloyd@memgraph.io>
2025-12-15 12:44:50 +0000

    Restore .value() in bolt/metrics.hpp for exception safety
    UpdateNewSession and IncrementQueryMetrics assume session.metrics_
    is already set from a previous call. Using .value() preserves the
    exception safety - if the invariant is violated, bad_optional_access
    is thrown instead of undefined behavior.
    
    RegisterNewSession keeps (*x)-> since it sets metrics_ immediately above.

--- src/communication/bolt/metrics.hpp
+++ src/communication/bolt/metrics.hpp
@@ -38,18 +38,18 @@ inline void TouchNewSession(TSession &session, Value &metadata) {
 template <typename TSession>
 inline void UpdateNewSession(TSession &session, Value &metadata) {
   auto &data = metadata.ValueMap();
-  (*session.metrics_)->bolt_v = fmt::format("{}.{}", session.version_.major, session.version_.minor);
-  (*session.metrics_)->supported_bolt_v = session.client_supported_bolt_versions_;
-  ++(*session.metrics_)->sessions;
+  session.metrics_.value()->bolt_v = fmt::format("{}.{}", session.version_.major, session.version_.minor);
+  session.metrics_.value()->supported_bolt_v = session.client_supported_bolt_versions_;
+  ++session.metrics_.value()->sessions;
   auto conn_type = (!data.contains("scheme") || data["scheme"].ValueString() == "none")
                        ? BoltMetrics::ConnectionType::kAnonymous
                        : BoltMetrics::ConnectionType::kBasic;
-  ++(*session.metrics_)->connection_types[(int)conn_type];
+  ++session.metrics_.value()->connection_types[(int)conn_type];
 }
 
 template <typename TSession>
 inline void IncrementQueryMetrics(TSession &session) {
-  ++(*session.metrics_)->queries;
+  ++session.metrics_.value()->queries;
 }
 
 }  // namespace memgraph::communication::bolt
