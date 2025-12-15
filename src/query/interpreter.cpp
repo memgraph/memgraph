ac06f5c5f2a905469b7df926508ab494a69e7d14
imilinovic <ivan.milinovic123@gmail.com>
2025-12-15 23:16:13 +0100

    clang tidy

--- src/query/interpreter.cpp
+++ src/query/interpreter.cpp
@@ -2892,9 +2892,9 @@ PreparedQuery PrepareCypherQuery(ParsedQuery parsed_query, std::map<std::string,
       .priority = utils::Priority::LOW};  // Default to LOW priority for all Cypher queries
 }
 
-PreparedQuery PrepareExplainQuery(ParsedQuery parsed_query, std::map<std::string, TypedValue> *summary,
-                                  std::vector<Notification> *notifications, InterpreterContext *interpreter_context,
-                                  Interpreter &interpreter, CurrentDB &current_db) {
+PreparedQuery PrepareExplainQuery(ParsedQuery parsed_query, std::vector<Notification> *notifications,
+                                  InterpreterContext *interpreter_context, Interpreter &interpreter,
+                                  CurrentDB &current_db) {
   const std::string kExplainQueryStart = "explain ";
   MG_ASSERT(
       utils::StartsWith(utils::ToLowerCase(parsed_query.stripped_query.stripped_query().str()), kExplainQueryStart),
@@ -4559,6 +4559,7 @@ Callback CreateTrigger(TriggerQuery *trigger_query, const storage::ExternalPrope
   // Make a copy of the user and pass it to the subsystem
   auto owner = interpreter_context->auth_checker->GenQueryUser(user_or_role->username(), user_or_role->rolenames());
   return {.header = {},
+          // NOLINTNEXTLINE(bugprone-exception-escape)
           .fn = [trigger_name = std::move(trigger_query->trigger_name_),
                  trigger_statement = std::move(trigger_query->statement_), event_type = trigger_query->event_type_,
                  before_commit = trigger_query->before_commit_, trigger_store, interpreter_context, dba,
@@ -4578,6 +4579,7 @@ Callback CreateTrigger(TriggerQuery *trigger_query, const storage::ExternalPrope
 
 Callback DropTrigger(TriggerQuery *trigger_query, TriggerStore *trigger_store) {
   return {.header = {},
+          // NOLINTNEXTLINE(bugprone-exception-escape)
           .fn = [trigger_name = std::move(trigger_query->trigger_name_),
                  trigger_store]() -> std::vector<std::vector<TypedValue>> {
             trigger_store->DropTrigger(trigger_name);
@@ -7495,8 +7497,8 @@ Interpreter::PrepareResult Interpreter::Prepare(ParseRes parse_res, UserParamete
 #endif
       );
     } else if (utils::Downcast<ExplainQuery>(parsed_query.query)) {
-      prepared_query = PrepareExplainQuery(std::move(parsed_query), &query_execution->summary,
-                                           &query_execution->notifications, interpreter_context_, *this, current_db_);
+      prepared_query = PrepareExplainQuery(std::move(parsed_query), &query_execution->notifications,
+                                           interpreter_context_, *this, current_db_);
     } else if (utils::Downcast<ProfileQuery>(parsed_query.query)) {
       prepared_query =
           PrepareProfileQuery(std::move(parsed_query), in_explicit_transaction_, &query_execution->summary,
