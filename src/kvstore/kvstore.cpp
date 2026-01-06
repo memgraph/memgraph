f32147e33a22a7ef473ad0da4136403a39894039
Gareth Andrew Lloyd <gareth.lloyd@memgraph.io>
2026-01-06 15:19:27 +0000

    Add RocksDB tuning flags for reduced syscall overhead (#3609)
    Add two new flags to control RocksDB behavior:
    - --storage-rocksdb-info-log-level: Controls log verbosity (default:
    ERROR_LEVEL)
    - --storage-rocksdb-enable-thread-tracking: Controls thread status API
    (default: false)
    
    Applied to all RocksDB instances: KVStore (auth, replication,
    coordination), and ON_DISK storage components (main storage, indexes,
    constraints).
    
    Impact: Reduces gettimeofday/gettid syscalls from RocksDB. Users will
    see less RocksDB logging by default. To restore verbose logging, use:
      --storage-rocksdb-info-log-level=INFO_LEVEL

--- src/kvstore/kvstore.cpp
+++ src/kvstore/kvstore.cpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Memgraph Ltd.
+// Copyright 2026 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -13,6 +13,7 @@
 #include <rocksdb/options.h>
 
 #include "kvstore/kvstore.hpp"
+#include "kvstore/rocksdb_utils.hpp"
 #include "utils/file.hpp"
 #include "utils/logging.hpp"
 
@@ -29,6 +30,7 @@ KVStore::KVStore(std::filesystem::path storage) : pimpl_(std::make_unique<impl>(
   if (!utils::EnsureDir(pimpl_->storage))
     throw KVStoreError("Folder for the key-value store " + pimpl_->storage.string() + " couldn't be initialized!");
   pimpl_->options.create_if_missing = true;
+  utils::ApplyRocksDBConfigFlags(pimpl_->options);
   rocksdb::DB *db = nullptr;
   auto s = rocksdb::DB::Open(pimpl_->options, pimpl_->storage.c_str(), &db);
   if (!s.ok())
