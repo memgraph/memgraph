4fc0a48b6ab66506caeb7cd65cfc5faaebc96ecc
Gareth Lloyd <gareth.lloyd@memgraph.io>
2026-01-09 15:09:09 +0000

    Change --storage-rocksdb-info-log-level
    We need to investigate more but for some reason doing more work in
    this rocksDB diagnostic log is somehow helping throughput for the rest
    of memgraph's queries.

--- src/flags/general.cpp
+++ src/flags/general.cpp
@@ -147,10 +147,9 @@ DEFINE_bool(storage_delta_on_identical_property_update, true,
 
 // RocksDB flags
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-DEFINE_string(storage_rocksdb_info_log_level, "ERROR_LEVEL",
+DEFINE_string(storage_rocksdb_info_log_level, "INFO_LEVEL",
               "RocksDB info log level. Options: DEBUG_LEVEL, INFO_LEVEL, WARN_LEVEL, ERROR_LEVEL, "
-              "FATAL_LEVEL, HEADER_LEVEL. Default is ERROR_LEVEL for reduced syscall overhead. "
-              "Use INFO_LEVEL or DEBUG_LEVEL when debugging disk storage issues.");
+              "FATAL_LEVEL, HEADER_LEVEL. Default is INFO_LEVEL.");
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 DEFINE_bool(storage_rocksdb_enable_thread_tracking, false,
             "Enable RocksDB thread status tracking. Default is false for reduced syscall overhead. "
