cf2695693f173ab0a83756e2722b373906998c8d
matt <mattkjames7@gmail.com>
2026-01-12 09:47:59 +0000

    fix issu image setup for daily build

--- tools/ci/get_daily_issu_images.py
+++ tools/ci/get_daily_issu_images.py
@@ -8,7 +8,7 @@ def get_daily_issu_images(date: int, mock: bool = False) -> List[str]:
     """
     Get the Releasse docker images for x86_64 and arm64 for the given date
     """
-    packages = list_daily_release_packages(date, return_url=False, mock=mock)
+    packages = list_daily_release_packages(date, image_type="memgraph", return_url=False, mock=mock)
     amd_docker = f"https://s3.eu-west-1.amazonaws.com/deps.memgraph.io/{packages['docker']['x86_64']}"
     arm_docker = f"https://s3.eu-west-1.amazonaws.com/deps.memgraph.io/{packages['docker']['arm64']}"
     return amd_docker, arm_docker
