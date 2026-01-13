8b497e7046d1441f3f3afc650a8685005719e7f4
colinbarry <colin.barry@memgraph.io>
2026-01-13 16:40:50 +0000

    [temp] Deduplicate query modules (#3623)
    Temp PR so I can work on this without breaking things for Matt whilst he
    is working on the main PR.

--- query_modules/date.py
+++ query_modules/date.py
@@ -1,9 +1,129 @@
+import datetime
 import re
-from datetime import datetime
 from enum import IntEnum
 from zoneinfo import ZoneInfo
 
 import mgp
+import pytz
+from mage.date.constants import Conversion, Epoch
+from mage.date.unit_conversion import to_int, to_timedelta
+
+
+def getOffset(timezone, date):
+    offset = pytz.timezone(timezone).utcoffset(date)
+    if offset.days == 1:
+        return (
+            datetime.timedelta(
+                minutes=offset.seconds // Conversion.SECONDS_IN_MINUTE
+                + Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR
+            ),
+            False,
+        )
+    elif offset.days == -1:
+        return (
+            datetime.timedelta(
+                minutes=Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR
+                - offset.seconds // Conversion.SECONDS_IN_MINUTE
+            ),
+            True,
+        )
+    return (
+        datetime.timedelta(minutes=offset.seconds // Conversion.SECONDS_IN_MINUTE),
+        False,
+    )
+
+
+@mgp.read_proc
+def parse(
+    time: str,
+    unit: str = "ms",
+    format: str = "%Y-%m-%d %H:%M:%S",
+    timezone: str = "UTC",
+) -> mgp.Record(parsed=int):
+    first_date = Epoch.UNIX_EPOCH
+    input_date = datetime.datetime.strptime(time, format)
+
+    if timezone not in pytz.all_timezones:
+        raise Exception("Timezone doesn't exist. Check documentation to see available timezones.")
+
+    offset, add = getOffset(timezone, input_date)
+    tz_input = input_date + offset if add else input_date - offset
+
+    time_since = tz_input - first_date
+
+    if unit == "ms":
+        parsed = (
+            time_since.days
+            * Conversion.HOURS_IN_DAY
+            * Conversion.MINUTES_IN_HOUR
+            * Conversion.SECONDS_IN_MINUTE
+            * Conversion.MILLISECONDS_IN_SECOND
+            + time_since.seconds * Conversion.MILLISECONDS_IN_SECOND
+        )
+    elif unit == "s":
+        parsed = (
+            time_since.days * Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR * Conversion.SECONDS_IN_MINUTE
+            + time_since.seconds
+        )
+    elif unit == "m":
+        parsed = (
+            time_since.days * Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR
+            + time_since.seconds // Conversion.SECONDS_IN_MINUTE
+        )
+    elif unit == "h":
+        parsed = (
+            time_since.days * Conversion.HOURS_IN_DAY
+            + time_since.seconds // Conversion.SECONDS_IN_MINUTE // Conversion.MINUTES_IN_HOUR
+        )
+    elif unit == "d":
+        parsed = time_since.days
+    else:
+        raise Exception("Unit doesn't exist. Check documentation to see available units.")
+
+    return mgp.Record(parsed=parsed)
+
+
+@mgp.read_proc
+def format(
+    time: int,
+    unit: str = "ms",
+    format: str = "%Y-%m-%d %H:%M:%S %Z",
+    timezone: str = "UTC",
+) -> mgp.Record(formatted=str):
+    first_date = Epoch.UNIX_EPOCH
+
+    if unit == "ms":
+        new_date = first_date + datetime.timedelta(milliseconds=time)
+    elif unit == "s":
+        new_date = first_date + datetime.timedelta(seconds=time)
+    elif unit == "m":
+        new_date = first_date + datetime.timedelta(minutes=time)
+    elif unit == "h":
+        new_date = first_date + datetime.timedelta(hours=time)
+    elif unit == "d":
+        new_date = first_date + datetime.timedelta(days=time)
+    else:
+        raise Exception("Unit doesn't exist. Check documentation to see available units.")
+
+    if timezone not in pytz.all_timezones:
+        raise Exception("Timezone doesn't exist. Check documentation to see available timezones.")
+    offset, subtract = getOffset(timezone, new_date)
+    tz_new = new_date - offset if subtract else new_date + offset
+
+    return mgp.Record(formatted=pytz.timezone(timezone).localize(tz_new).strftime(format))
+
+
+@mgp.function
+def add(
+    time: int,
+    unit: str,
+    add_value: int,
+    add_unit: str,
+) -> int:
+    return to_int(
+        to_timedelta(time=time, unit=unit) + to_timedelta(time=add_value, unit=add_unit),
+        unit=unit,
+    )
 
 
 class FormatLength(IntEnum):
@@ -72,26 +192,26 @@ def convert_format(temporal: mgp.Nullable[str], current_format: str, convert_to:
 
             if "." in temporal_without_zone:
                 temporal_without_zone = re.sub(r"(\.\d{6})\d*", r"\1", temporal_without_zone)
-                dt = datetime.strptime(temporal_without_zone, "%Y-%m-%dT%H:%M:%S.%f%z")
+                dt = datetime.datetime.strptime(temporal_without_zone, "%Y-%m-%dT%H:%M:%S.%f%z")
             else:
-                dt = datetime.strptime(temporal_without_zone, "%Y-%m-%dT%H:%M:%S%z")
+                dt = datetime.datetime.strptime(temporal_without_zone, "%Y-%m-%dT%H:%M:%S%z")
         elif current_format.lower() == "iso_date":
             # iso_date can have optional offset, try parsing with offset first
             try:
-                dt = datetime.strptime(temporal, "%Y-%m-%d%z")
+                dt = datetime.datetime.strptime(temporal, "%Y-%m-%d%z")
             except ValueError:
-                dt = datetime.strptime(temporal, "%Y-%m-%d")
+                dt = datetime.datetime.strptime(temporal, "%Y-%m-%d")
         elif current_format.lower() == "iso_time":
             # iso_time can have optional offset
             try:
-                dt = datetime.strptime(temporal, "%H:%M:%S%z")
+                dt = datetime.datetime.strptime(temporal, "%H:%M:%S%z")
             except ValueError:
-                dt = datetime.strptime(temporal, "%H:%M:%S")
+                dt = datetime.datetime.strptime(temporal, "%H:%M:%S")
         else:
             try:
-                dt = datetime.fromisoformat(temporal)
+                dt = datetime.datetime.fromisoformat(temporal)
             except Exception:
-                dt = datetime.strptime(temporal, current_formatter)
+                dt = datetime.datetime.strptime(temporal, current_formatter)
 
         if convert_to.lower() in ["iso_offset_date", "iso_offset_time", "iso_offset_date_time"] and dt.tzinfo is None:
             raise Exception("missing timezone")
