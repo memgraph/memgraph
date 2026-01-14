510c2567525feaa1407133b56eb7786d1e4008ed
Gareth Lloyd <gareth.lloyd@memgraph.io>
2026-01-14 11:53:10 +0000

    Update expectation to account for SHOW INDEX INFO

--- tests/e2e/replication/show_while_creating_invalid_state.py
+++ tests/e2e/replication/show_while_creating_invalid_state.py
@@ -1672,7 +1672,7 @@ def test_attempt_to_create_indexes_on_main_when_async_replica_is_down(connection
 
     # 5/
     expected_data = [
-        ("async_replica1", "async", -4, "invalid"),
+        ("async_replica1", "async", -3, "invalid"),
         ("async_replica2", "async", 0, "ready"),
     ]
 
@@ -1808,14 +1808,14 @@ def test_attempt_to_create_indexes_on_main_when_sync_replica_is_down(connection,
             "127.0.0.1:10001",
             "sync",
             {"ts": 0, "behind": None, "status": "invalid"},
-            {"memgraph": {"ts": 2, "behind": -4, "status": "invalid"}},
+            {"memgraph": {"ts": 2, "behind": -3, "status": "invalid"}},
         ),
         (
             "sync_replica2",
             "127.0.0.1:10002",
             "sync",
             {"ts": 0, "behind": None, "status": "ready"},
-            {"memgraph": {"ts": 6, "behind": 0, "status": "ready"}},
+            {"memgraph": {"ts": 5, "behind": 0, "status": "ready"}},
         ),
     ]
     res_from_main = interactive_mg_runner.MEMGRAPH_INSTANCES["main"].query(QUERY_TO_CHECK)
