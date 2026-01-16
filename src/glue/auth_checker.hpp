e2f237747aff1fc32b6794aa7746b7b5161105d2
Andreja Tonev <andreja.tonev@memgraph.io>
2026-01-16 15:07:41 +0100

    Fixed FineGrainedAuthChecker to copy + deep copy of users for triggers

--- src/glue/auth_checker.hpp
+++ src/glue/auth_checker.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Memgraph Ltd.
+// Copyright 2026 Memgraph Ltd.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
@@ -31,7 +31,7 @@ class AuthChecker : public query::AuthChecker {
   std::shared_ptr<query::QueryUserOrRole> GenEmptyUser() const override;
 
 #ifdef MG_ENTERPRISE
-  std::unique_ptr<query::FineGrainedAuthChecker> GetFineGrainedAuthChecker(std::shared_ptr<query::QueryUserOrRole> user,
+  std::unique_ptr<query::FineGrainedAuthChecker> GetFineGrainedAuthChecker(const query::QueryUserOrRole &user,
                                                                            const query::DbAccessor *dba) const override;
 #endif
 
