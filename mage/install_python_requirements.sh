f9ef6f2ada3d94163bd7fb293ba573d3e8680590
matt <mattkjames7@gmail.com>
2026-01-28 17:43:41 +0000

    updated requirements to use new torch version and new packages

--- mage/install_python_requirements.sh
+++ mage/install_python_requirements.sh
@@ -42,50 +42,55 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [[ "$CUDA" == true && "$ARCH" != "amd64" ]]; then
-    echo "CUDA is only supported on amd64 architecture."
-    exit 1
+  echo "CUDA is only supported on amd64 architecture."
+  exit 1
 fi
 
 export PIP_BREAK_SYSTEM_PACKAGES=1
 if [[ "$CUDA" == true && "$DEB_PACKAGE" == "false" ]]; then
-    requirements_file="requirements-gpu.txt"
+  requirements_file="requirements-gpu.txt"
 else
-    requirements_file="requirements.txt"
+  requirements_file="requirements.txt"
 fi
 
 if [ "$CI" = true ]; then
-    # for building the docker image
-    python3 -m pip install --no-cache-dir -r "/tmp/${requirements_file}"
-    python3 -m pip install --no-cache-dir -r /tmp/auth_module-requirements.txt
+  # for building the docker image
+  python3 -m pip install --no-cache-dir -r "/tmp/${requirements_file}"
+  python3 -m pip install --no-cache-dir -r /tmp/auth_module-requirements.txt
 elif [[ "$DEB_PACKAGE" == "true" ]]; then
-    # for installing python deps during deb package installation
-    python3 -m pip install --no-cache-dir -r "/usr/lib/memgraph/mage-requirements.txt"
-    python3 -m pip install --no-cache-dir -r "/usr/lib/memgraph/auth_module/requirements.txt"
+  # for installing python deps during deb package installation
+  python3 -m pip install --no-cache-dir -r "/usr/lib/memgraph/mage-requirements.txt"
+  python3 -m pip install --no-cache-dir -r "/usr/lib/memgraph/auth_module/requirements.txt"
 else
-    # for installing locally, from within the memgraph repo, under the mage directory
-    python3 -m pip install --no-cache-dir -r "$(pwd)/python/${requirements_file}"
-    python3 -m pip install --no-cache-dir -r "$(pwd)/../src/auth/reference_modules/requirements.txt"
+  # for installing locally, from within the memgraph repo, under the mage directory
+  python3 -m pip install --no-cache-dir -r "$(pwd)/python/${requirements_file}"
+  python3 -m pip install --no-cache-dir -r "$(pwd)/../src/auth/reference_modules/requirements.txt"
 fi
 
 if [ "$ARCH" = "arm64" ]; then
-    if [ "$CACHE_PRESENT" = "true" ]; then
-        echo "Using cached torch packages"
-        python3 -m pip install --no-index --find-links=$WHEEL_CACHE_DIR torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter
-    else
-        python3 -m pip install --no-cache-dir torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter -f https://data.pyg.org/whl/torch-2.6.0+cpu.html
-    fi
-    curl -o dgl-2.5.0-cp312-cp312-linux_aarch64.whl https://s3.eu-west-1.amazonaws.com/deps.memgraph.io/wheels/arm64/dgl-2.5.0-cp312-cp312-linux_aarch64.whl
-    python3 -m pip install --no-cache-dir dgl-2.5.0-cp312-cp312-linux_aarch64.whl
-    rm dgl-2.5.0-cp312-cp312-linux_aarch64.whl
+  if [ "$CACHE_PRESENT" = "true" ]; then
+    echo "Using cached torch packages"
+    python3 -m pip install --no-index --find-links=$WHEEL_CACHE_DIR torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter dgl
+  else
+    python3 -m pip install --no-cache-dir torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter -f https://data.pyg.org/whl/torch-2.9.0+cpu.html
+
+    curl -o dgl-2.5-cp312-cp312-linux_aarch64.whl https://s3.eu-west-1.amazonaws.com/deps.memgraph.io/wheels/arm64/dgl-2.5-cp312-cp312-linux_aarch64.whl
+    python3 -m pip install --no-cache-dir dgl-2.5-cp312-cp312-linux_aarch64.whl
+    rm dgl-2.5-cp312-cp312-linux_aarch64.whl
+  fi
 else
+  if [ "$CACHE_PRESENT" = "true" ]; then
+      echo "Using cached torch packages"
+      python3 -m pip install --no-index --find-links=$WHEEL_CACHE_DIR torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter dgl
+  else
     if [[ "$CUDA" == true ]]; then
-      python3 -m pip install --no-cache-dir torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter -f https://data.pyg.org/whl/torch-2.6.0+cu126.html
-    elif [ "$CACHE_PRESENT" = "true" ]; then
-        echo "Using cached torch packages"
-        python3 -m pip install --no-index --find-links=$WHEEL_CACHE_DIR torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter
+      python3 -m pip install --no-cache-dir torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter -f https://data.pyg.org/whl/torch-2.9.0+cu130.html
     else
-        python3 -m pip install --no-cache-dir torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter -f https://data.pyg.org/whl/torch-2.6.0+cpu.html
+      python3 -m pip install --no-cache-dir torch-sparse torch-cluster torch-spline-conv torch-geometric torch-scatter -f https://data.pyg.org/whl/torch-2.9.0+cpu.html
     fi
-    python3 -m pip install --no-cache-dir dgl==2.5.0 -f https://data.dgl.ai/wheels/torch-2.6/repo.html
+    curl -o dgl-2.5-cp312-cp312-linux_x86_64.whl https://s3.eu-west-1.amazonaws.com/deps.memgraph.io/wheels/amd64/dgl-2.5-cp312-cp312-linux_x86_64.whl
+    python3 -m pip install --no-cache-dir dgl-2.5-cp312-cp312-linux_x86_64.whl
+    rm dgl-2.5-cp312-cp312-linux_x86_64.whl
+  fi
 fi
 rm -fr /home/memgraph/.cache/pip
