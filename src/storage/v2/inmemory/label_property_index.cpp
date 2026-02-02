b4d0c60985167757e79c2c490f48d63a2bfd35c2
Colin Barry <colin.barry@memgraph.io>
2026-02-02 14:56:42 +0000

    fix: Use correct lock scope for delta-reads in indices

--- src/storage/v2/inmemory/label_property_index.cpp
+++ src/storage/v2/inmemory/label_property_index.cpp
@@ -71,19 +71,17 @@ bool CurrentVersionHasLabelProperties(const Vertex &vertex, LabelId label, Prope
   bool has_label = false;
   auto current_values_equal_to_value = std::vector<bool>{};
   const Delta *delta = nullptr;
-  {
-    auto guard = std::shared_lock{vertex.lock};
-    delta = vertex.delta;
-    deleted = vertex.deleted;
-    if (!delta && deleted) return false;
-    has_label = std::ranges::contains(vertex.labels, label);
-    if (!delta && !has_label) return false;
-    current_values_equal_to_value = helper.MatchesValues(vertex.properties, values);
+  auto guard = std::shared_lock{vertex.lock};
+  delta = vertex.delta;
+  deleted = vertex.deleted;
+  if (!delta && deleted) return false;
+  has_label = std::ranges::contains(vertex.labels, label);
+  if (!delta && !has_label) return false;
+  current_values_equal_to_value = helper.MatchesValues(vertex.properties, values);
 
-    // If vertex has non-sequential deltas, hold lock while applying them
-    if (!vertex.has_uncommitted_non_sequential_deltas) {
-      guard.unlock();
-    }
+  // If vertex has non-sequential deltas, hold lock while applying them
+  if (!vertex.has_uncommitted_non_sequential_deltas) {
+    guard.unlock();
   }
 
   // Checking cache has a cost, only do it if we have any deltas
