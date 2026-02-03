670353544136afb141968ecccf5dc5260ca812b9
Josip Mrđen <josip.mrden@memgraph.io>
2026-02-03 17:03:21 +0100

    Add latest memgraph image

--- tests/stress/docker_ha/deployment/deployment.sh
+++ tests/stress/docker_ha/deployment/deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Docker image to use
-MEMGRAPH_IMAGE="${MEMGRAPH_IMAGE:-memgraph/memgraph:3.7.2}"
+MEMGRAPH_IMAGE="${MEMGRAPH_IMAGE:-memgraph/memgraph:latest}"
 
 # Container name prefix
 CONTAINER_PREFIX="memgraph_stress"
