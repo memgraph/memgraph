ecc78e32d0a841b2bbb50c990b44d5a56829491f
matt <mattkjames7@gmail.com>
2026-02-09 13:23:44 +0000

    added mgconsole check to smoke tests

--- mage/tests/smoke-release-testing/test_single.bash
+++ mage/tests/smoke-release-testing/test_single.bash
@@ -61,6 +61,7 @@ test_load_csv
 test_load_jsonl
 test_load_parquet
 test_parallel_runtime
+test_mgconsole "1.5"
 
 # NOTE: If the testing container is NOT restarted (each test having their own
 # container), all the auth test have to come after all tests that assume there
