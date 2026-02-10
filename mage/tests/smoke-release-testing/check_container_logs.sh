98d9251954cfa7645fdd5da4dd592911dfc42d78
matt <mattkjames7@gmail.com>
2026-02-10 10:36:22 +0000

    fix smoke test

--- mage/tests/smoke-release-testing/check_container_logs.sh
+++ mage/tests/smoke-release-testing/check_container_logs.sh
@@ -5,7 +5,7 @@ source "$SCRIPT_DIR/utils.bash"
 check_container_logs() {
     # Check container logs for `[error]` while loading query modules
     echo "Checking container logs for errors..."
-    logs="$(docker exec -it -u root memgraph_next_data bash -c 'cat /var/log/memgraph/*')"
+    logs="$(docker exec -u root memgraph_next_data bash -c 'cat /var/log/memgraph/*')"
     if echo "$logs" | grep -q "\[error\]"; then
         echo "Error(s) found in container logs:"
         echo "$logs" | grep "\[error\]"
