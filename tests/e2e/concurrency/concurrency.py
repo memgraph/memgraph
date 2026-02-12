849d85e5c7f249a10b19ecffc4b2472a73eec294
Ivan Milinović <44698587+imilinovic@users.noreply.github.com>
2026-02-12 15:27:40 +0100

    refactor: better error messages on storage access timeout (#3778)
    better error message on storage access timeout

--- tests/e2e/concurrency/concurrency.py
+++ tests/e2e/concurrency/concurrency.py
@@ -94,10 +94,7 @@ def test_concurrency_read_only_v_shared_storage_acc(first_connection, second_con
     try:
         execute_and_fetch_all(m2c, "CREATE INDEX ON :L")
     except Exception as e:
-        assert (
-            str(e)
-            == "Cannot get read only access to the storage. Try stopping other queries that are running in parallel."
-        )
+        assert str(e).startswith("Cannot get read only access to the storage.")
         m2c_timeout = True
 
     first_connection.commit()
@@ -118,10 +115,7 @@ def test_concurrency_unique_v_shared_storage_acc(first_connection, second_connec
     try:
         execute_and_fetch_all(m2c, "DROP GRAPH")
     except Exception as e:
-        assert (
-            str(e)
-            == "Cannot get unique access to the storage. Try stopping other queries that are running in parallel."
-        )
+        assert str(e).startswith("Cannot get unique access to the storage.")
         m2c_timeout = True
 
     first_connection.commit()
