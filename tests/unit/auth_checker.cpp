6dd7a40934d59621aed2db31d09542147415a355
Colin Barry <colin.barry@memgraph.io>
2026-02-19 16:16:48 +0000

    feat: Remove `GRANT NOTHING` and `REVOKE NOTHING` from grammar

--- tests/unit/auth_checker.cpp
+++ tests/unit/auth_checker.cpp
@@ -119,8 +119,7 @@ TYPED_TEST(FineGrainedAuthCheckerFixture, DeniedAllLabels) {
 
 TYPED_TEST(FineGrainedAuthCheckerFixture, DeniedAllEdgeTypes) {
   memgraph::auth::User user{"test"};
-  user.fine_grained_access_handler().edge_type_permissions().GrantGlobal(
-      memgraph::auth::FineGrainedPermission::NOTHING);
+  user.fine_grained_access_handler().edge_type_permissions().DenyGlobal(memgraph::auth::kAllPermissions);
   memgraph::glue::FineGrainedAuthChecker auth_checker{user, &this->dba};
 
   ASSERT_FALSE(auth_checker.Has(this->r1, memgraph::query::AuthQuery::FineGrainedPrivilege::READ));
@@ -202,8 +201,7 @@ TYPED_TEST(FineGrainedAuthCheckerFixture, GrantEdgeType) {
 
 TYPED_TEST(FineGrainedAuthCheckerFixture, DenyEdgeType) {
   memgraph::auth::User user{"test"};
-  user.fine_grained_access_handler().edge_type_permissions().Grant({"edge_type_1"},
-                                                                   memgraph::auth::FineGrainedPermission::NOTHING);
+  user.fine_grained_access_handler().edge_type_permissions().Deny({"edge_type_1"}, memgraph::auth::kAllPermissions);
   memgraph::glue::FineGrainedAuthChecker auth_checker{user, &this->dba};
 
   ASSERT_FALSE(auth_checker.Has(this->r1, memgraph::query::AuthQuery::FineGrainedPrivilege::READ));
@@ -212,8 +210,7 @@ TYPED_TEST(FineGrainedAuthCheckerFixture, DenyEdgeType) {
 TYPED_TEST(FineGrainedAuthCheckerFixture, GrantAndDenySpecificEdgeTypes) {
   memgraph::auth::User user{"test"};
   user.fine_grained_access_handler().edge_type_permissions().Grant({"edge_type_1"}, memgraph::auth::kAllPermissions);
-  user.fine_grained_access_handler().edge_type_permissions().Grant({"edge_type_2"},
-                                                                   memgraph::auth::FineGrainedPermission::NOTHING);
+  user.fine_grained_access_handler().edge_type_permissions().Deny({"edge_type_2"}, memgraph::auth::kAllPermissions);
   memgraph::glue::FineGrainedAuthChecker auth_checker{user, &this->dba};
 
   ASSERT_TRUE(auth_checker.Has(this->r1, memgraph::query::AuthQuery::FineGrainedPrivilege::READ));
