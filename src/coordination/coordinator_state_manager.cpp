f9a6837c447248d3f44327ae914c27a5aa046dbc
as51340 <andi8647@gmail.com>
2026-03-02 08:55:38 +0100

    fix: Data race in state manager

--- src/coordination/coordinator_state_manager.cpp
+++ src/coordination/coordinator_state_manager.cpp
@@ -18,9 +18,10 @@
 #include "utils/logging.hpp"
 
 #include <spdlog/spdlog.h>
-
 #include <nlohmann/json.hpp>
 #include <range/v3/view.hpp>
+
+#include <mutex>
 
 namespace memgraph::coordination {
 using nuraft::cluster_config;
@@ -125,6 +126,7 @@ CoordinatorStateManager::CoordinatorStateManager(CoordinatorStateManagerConfig c
 // only after the gap is small enough so it could happen that for the short time window
 // the config doesn't contain this coordinator. This should be temporary and the log which
 // contains the current coordinator should be after shortly accepted.
+// Caller is responsible for obtainig the lock for updating cluster_config_
 void CoordinatorStateManager::TryUpdateClusterConfigFromDisk() {
   auto const maybe_cluster_config = durability_.Get(kClusterConfigKey);
   if (!maybe_cluster_config) {
@@ -143,32 +145,14 @@ void CoordinatorStateManager::TryUpdateClusterConfigFromDisk() {
 
 // Called when application is starting up
 auto CoordinatorStateManager::load_config() -> std::shared_ptr<cluster_config> {
-  spdlog::trace("Got request to update config from disk");
+  auto lock = std::shared_lock{config_mutex_};
   TryUpdateClusterConfigFromDisk();
   return cluster_config_;
 }
 
-auto CoordinatorStateManager::GetCoordinatorInstancesAux() const -> std::vector<CoordinatorInstanceAux> {
-  auto const &cluster_config_servers = cluster_config_->get_servers();
-  std::vector<CoordinatorInstanceAux> coord_instances_aux;
-  coord_instances_aux.reserve(cluster_config_servers.size());
-
-  try {
-    std::ranges::transform(cluster_config_servers,
-                           std::back_inserter(coord_instances_aux),
-                           [](auto const &server) -> CoordinatorInstanceAux {
-                             auto j = nlohmann::json::parse(server->get_aux());
-                             return j.template get<CoordinatorInstanceAux>();
-                           });
-  } catch (std::exception const &e) {
-    LOG_FATAL("Error occurred while parsing aux field {}", e.what());
-  }
-
-  return coord_instances_aux;
-}
-
 auto CoordinatorStateManager::save_config(cluster_config const &config) -> void {
-  spdlog::trace("Got request to save config.");
+  auto lock = std::lock_guard{config_mutex_};
+
   std::shared_ptr<buffer> const buf = config.serialize();
   cluster_config_ = cluster_config::deserialize(*buf);
   nlohmann::json json;
@@ -178,10 +162,8 @@ auto CoordinatorStateManager::save_config(cluster_config const &config) -> void 
     throw StoreClusterConfigException("Failed to store cluster config in RocksDb");
   }
 
-  spdlog::trace("Successfully saved cluster config to the durable storage.");
-
-  NotifyObserver(GetCoordinatorInstancesAux());
-  spdlog::trace("Successfully notified observer about changes in the cluster configuration.");
+  // Lock already taken hence not needed
+  NotifyObserver(GetCoordinatorInstancesAux<false>());
 }
 
 void CoordinatorStateManager::NotifyObserver(std::vector<CoordinatorInstanceAux> const &coord_instances_aux) const {
