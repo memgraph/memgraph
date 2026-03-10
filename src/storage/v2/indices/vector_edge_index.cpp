33234357073c8de0eb76418959229b4a2e835ba4
as51340 <andi8647@gmail.com>
2026-03-10 08:52:41 +0100

    fix: Clang-tidy

--- src/storage/v2/indices/vector_edge_index.cpp
+++ src/storage/v2/indices/vector_edge_index.cpp
@@ -300,7 +300,7 @@ bool VectorEdgeIndex::Empty() const { return pimpl->edge_index_.empty(); }
 
 void VectorEdgeIndex::RemoveEdges(std::list<Gid> const &deleted_edge_gids) const {
   auto as_uint = deleted_edge_gids | std::views::transform([](auto const &g) { return g.AsUint(); });
-  std::unordered_set<uint64_t> gids_to_remove(as_uint.begin(), as_uint.end());
+  std::unordered_set<uint64_t> const gids_to_remove(as_uint.begin(), as_uint.end());
 
   for (auto &[_, index_item] : pimpl->edge_index_) {
     auto &[mg_index, spec] = index_item;
