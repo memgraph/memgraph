e51597cdb0149493aad5f8bb7d1a503dae57aa06
Colin Barry <colin.barry@memgraph.io>
2026-03-13 15:01:57 +0000

    fix: Unify notifications when users and roles are created

--- src/query/interpreter.cpp
+++ src/query/interpreter.cpp
@@ -1093,8 +1093,9 @@ Callback HandleAuthQuery(AuthQuery *auth_query, InterpreterContext *interpreter_
                 username);
           }
           spdlog::warn("User '{}' already exists.", username);
-          runtime_notifications->emplace_back(
-              SeverityLevel::WARNING, NotificationCode::CREATE_USER, fmt::format("User {} already exists.", username));
+          runtime_notifications->emplace_back(SeverityLevel::WARNING,
+                                              NotificationCode::CREATE_USER,
+                                              fmt::format("User '{}' already exists.", username));
           return std::vector<std::vector<TypedValue>>{};
         }
 
@@ -1120,7 +1121,7 @@ Callback HandleAuthQuery(AuthQuery *auth_query, InterpreterContext *interpreter_
                                &*interpreter->system_transaction_);
           runtime_notifications->emplace_back(SeverityLevel::INFO,
                                               NotificationCode::CREATE_USER,
-                                              fmt::format("User {} created. All privileges granted.", username));
+                                              fmt::format("User '{}' created. All privileges granted.", username));
           return std::vector<std::vector<TypedValue>>{};
         }
 
@@ -1130,15 +1131,16 @@ Callback HandleAuthQuery(AuthQuery *auth_query, InterpreterContext *interpreter_
             runtime_notifications->emplace_back(
                 SeverityLevel::INFO,
                 NotificationCode::CREATE_USER,
-                fmt::format("User {} created. Built-in roles created: admin, readwrite, readonly. User {} assigned "
-                            "built-in role admin, with full access to all data and privileges.",
-                            username,
-                            username));
+                fmt::format(
+                    "User '{}' created. Built-in roles created: 'admin', 'readwrite', 'readonly'. User '{}' assigned "
+                    "built-in role 'admin', with full access to all data and privileges.",
+                    username,
+                    username));
           } else {
             runtime_notifications->emplace_back(
                 SeverityLevel::INFO,
                 NotificationCode::CREATE_USER,
-                fmt::format("User {} created. Assigned built-in role admin, with full access to all data and "
+                fmt::format("User '{}' created. Assigned built-in role 'admin', with full access to all data and "
                             "privileges.",
                             username));
           }
@@ -1146,7 +1148,7 @@ Callback HandleAuthQuery(AuthQuery *auth_query, InterpreterContext *interpreter_
           runtime_notifications->emplace_back(
               SeverityLevel::INFO,
               NotificationCode::CREATE_USER,
-              fmt::format("User {} created. No roles or privileges assigned.", username));
+              fmt::format("User '{}' created. No roles or privileges assigned.", username));
         }
         return std::vector<std::vector<TypedValue>>{};
       };
@@ -1226,11 +1228,12 @@ Callback HandleAuthQuery(AuthQuery *auth_query, InterpreterContext *interpreter_
                 rolename);
           }
           spdlog::warn("Role '{}' already exists.", rolename);
-          runtime_notifications->emplace_back(
-              SeverityLevel::WARNING, NotificationCode::CREATE_ROLE, fmt::format("Role {} already exists.", rolename));
+          runtime_notifications->emplace_back(SeverityLevel::WARNING,
+                                              NotificationCode::CREATE_ROLE,
+                                              fmt::format("Role '{}' already exists.", rolename));
         } else {
           runtime_notifications->emplace_back(
-              SeverityLevel::INFO, NotificationCode::CREATE_ROLE, fmt::format("Role {} created.", rolename));
+              SeverityLevel::INFO, NotificationCode::CREATE_ROLE, fmt::format("Role '{}' created.", rolename));
         }
         return std::vector<std::vector<TypedValue>>();
       };
