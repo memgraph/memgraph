288e507508e2c640b6aba1bed71a6e04db23988e
Colin Barry <colin.barry@memgraph.io>
2026-03-16 11:20:44 +0000

    feat: Add basic OpenMetrics endpoint with test counter

--- src/memgraph.cpp
+++ src/memgraph.cpp
@@ -38,6 +38,7 @@
 #include "flags/general.hpp"
 #include "flags/logging.hpp"
 #include "glue/MonitoringServerT.hpp"
+#include "glue/PrometheusServerT.hpp"
 #include "glue/ServerT.hpp"
 #include "glue/auth_checker.hpp"
 #include "glue/auth_handler.hpp"
@@ -926,6 +927,9 @@ int main(int argc, char **argv) {
   memgraph::glue::MonitoringServerT metrics_server{
       {FLAGS_metrics_address, static_cast<uint16_t>(FLAGS_metrics_port)}, db_acc->storage(), &bolt_server_context};
   spdlog::trace("Metrics server created.");
+  // TODO: replace hardcoded port 9092 with FLAGS_metrics_port once old metrics server is removed
+  memgraph::glue::PrometheusServerT prometheus_server{{"localhost", 9092}, &dbms_handler, &context};
+  spdlog::trace("Prometheus metrics server created.");
 #endif
 
   // Handler for regular termination signals
@@ -933,6 +937,7 @@ int main(int argc, char **argv) {
 #ifdef MG_ENTERPRISE
                       &coordinator_state,
                       &metrics_server,
+                      &prometheus_server,
 #endif
                       &websocket_server,
                       &server,
@@ -982,6 +987,7 @@ int main(int argc, char **argv) {
     websocket_server.Shutdown();
 #ifdef MG_ENTERPRISE
     metrics_server.Shutdown();
+    prometheus_server.Shutdown();
     if (coordinator_state && coordinator_state->IsCoordinator()) {
       // Coordinator instance destruction will handle the complete shutdown
       coordinator_state.reset();
@@ -1001,6 +1007,8 @@ int main(int argc, char **argv) {
 #ifdef MG_ENTERPRISE
   metrics_server.Start();
   spdlog::trace("Metrics server started");
+  prometheus_server.Start();
+  spdlog::trace("Prometheus metrics server started");
 #endif
 
   if (!FLAGS_init_data_file.empty()) {
@@ -1030,6 +1038,7 @@ int main(int argc, char **argv) {
   memgraph::memory::UnsetHooks();
 #ifdef MG_ENTERPRISE
   metrics_server.AwaitShutdown();
+  prometheus_server.AwaitShutdown();
 #endif
   try {
     memgraph::query::procedure::gModuleRegistry.UnloadAllModules();
