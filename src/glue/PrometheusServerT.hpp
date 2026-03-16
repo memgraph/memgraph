25d3d6bcf521c6c9402dbcdcbb5cb73faf808c60
Colin Barry <colin.barry@memgraph.io>
2026-03-16 11:20:44 +0000

    feat: Add basic OpenMetrics endpoint with test counter

---
+++ src/glue/PrometheusServerT.hpp
@@ -0,0 +1,26 @@
+// Copyright 2026 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+#pragma once
+
+#include "communication/http/server.hpp"
+#include "dbms/dbms_handler.hpp"
+#include "http_handlers/prometheus_service.hpp"
+
+extern template class memgraph::communication::http::Server<memgraph::http::PrometheusRequestHandler,
+                                                            memgraph::dbms::DbmsHandler>;
+
+namespace memgraph::glue {
+
+using PrometheusServerT =
+    memgraph::communication::http::Server<memgraph::http::PrometheusRequestHandler, memgraph::dbms::DbmsHandler>;
+
+}  // namespace memgraph::glue
