7a339071c0ebb37512df9e3c72ddc103b835bab5
as51340 <andi8647@gmail.com>
2026-03-17 08:02:35 +0100

    feat: Handle txn asked to abort err message

--- tests/jepsen/src/memgraph/replication/large.clj
+++ tests/jepsen/src/memgraph/replication/large.clj
@@ -76,6 +76,7 @@
                   (catch Exception e
                     (if (or (utils/sync-replica-down? e)
                             (utils/main-reached-rpc-timeout? e)
+                            (utils/asked-to-abort-shutdown? e)
                             (utils/cannot-get-shared-access? e))
                       (assoc op :type :ok :value (str e)); Exception due to down sync replica is accepted/expected. Here we return ok because
                       ; data will still get replicated since Memgraph doesn't have SYNC replication by the book.
