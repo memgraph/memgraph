0d6feaad32beb674427fce48e003d3be047be8eb
Colin Barry <colin.barry@memgraph.io>
2026-03-24 17:15:23 +0000

    feat: Add TTL metrics

--- src/storage/v2/storage.cpp
+++ src/storage/v2/storage.cpp
@@ -789,6 +789,7 @@ void Storage::SetMetricHandles(metrics::DatabaseMetricHandles *metric_handles) {
   metric_handles_ = metric_handles;
   indices_.SetMetricHandles(metric_handles);
   constraints_.SetMetricHandles(metric_handles);
+  ttl_.SetMetricHandles(metric_handles);
 }
 
 }  // namespace memgraph::storage
