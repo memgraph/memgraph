710ef191447eeb9e2556309913b5fd2406658089
as51340 <andi8647@gmail.com>
2026-04-07 06:54:36 +0200

    fix: shutdown() on socket before client_ destruction

--- src/replication/replication_client.cpp
+++ src/replication/replication_client.cpp
@@ -31,6 +31,9 @@ ReplicationClient::ReplicationClient(const ReplicationClientConfig &config)
 
 void ReplicationClient::Shutdown() {
   replica_checker_.Stop();
+  // Abort is needed to break possible ongoing task in thread pool. Doesn't destroy underlying object, just invokes
+  // socket shutdown operation
+  rpc_client_.Abort();
   thread_pool_.ShutDown();
   rpc_client_.Shutdown();
 }
