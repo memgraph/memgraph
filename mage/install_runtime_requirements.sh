a54832ec77e0cb3a2ea6e6d9b8084806f0d5c714
DavIvek <david.ivekovic@memgraph.io>
2026-04-20 18:16:12 +0200

    Add kerberos build/runtime deps to build and image environments
    Python gssapi has no prebuilt arm64 wheel, so pip builds it from source.
    That requires krb5-config and libkrb5 headers.
    
    - environment/os/*.sh: add libkrb5-dev / krb5-devel to MEMGRAPH_BUILD_DEPS
      and libkrb5-3 / krb5-libs to MEMGRAPH_RUN_DEPS, per distro family
    - mage Dockerfile.release / Dockerfile.cugraph python-base stage:
      install gcc, python3-dev, libkrb5-dev so the pip install of
      auth_module-requirements.txt can build gssapi
    - mage install_runtime_requirements.sh: install libkrb5-3 so the compiled
      gssapi module can dlopen libkrb5.so.3 at runtime

--- mage/install_runtime_requirements.sh
+++ mage/install_runtime_requirements.sh
@@ -56,7 +56,7 @@ rm packages-microsoft-prod.deb
 apt-get update
 ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
 apt-get install -y libcurl4 libpython${PY_VERSION} openssl python3 \
-    libgomp1 libaio1t64 libatomic1 --no-install-recommends
+    libgomp1 libaio1t64 libatomic1 libkrb5-3 --no-install-recommends
 ln -s /usr/bin/$(ls /usr/bin | grep perf) /usr/bin/perf
 
 # In CI, we clean up the apt cache to save space in the Docker container.
