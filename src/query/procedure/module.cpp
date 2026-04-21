f765e96a4c9dd821ae6684f9bb88381267c7a711
Josip Mrđen <josip.mrden@memgraph.io>
2026-04-21 16:31:46 +0200

    Add mgp to no removals

--- src/query/procedure/module.cpp
+++ src/query/procedure/module.cpp
@@ -46,7 +46,8 @@ namespace memgraph::query::procedure {
 
 constexpr const char *func_code =
     "import ast\n\n"
-    "no_removals = ['collections', 'abc', 'sys', 'torch', 'torch_geometric', 'igraph', 'dgl', 'numpy']\n"
+    "no_removals = ['collections', 'abc', 'sys', 'torch', 'torch_geometric', 'igraph', 'dgl', 'numpy', 'mgp', "
+    "'_mgp']\n"
     "modules = set()\n\n"
     "def visit_Import(node):\n"
     "  for name in node.names:\n"
