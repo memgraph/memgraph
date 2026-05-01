5bfdacb221339b4dba08c0b76559a68aad223913
Gareth Lloyd <gareth.lloyd@memgraph.io>
2026-05-01 15:00:43 +0100

    conan: trim_conandata in remaining recipes for stable revisions
    Aligns gflags, libbcrypt, nuraft, pulsar-client-cpp, rocksdb, and usearch
    with the existing pattern in boost/ccache/jemalloc/librdtsc/nlohmann_json
    so recipe revisions are stable across `conan export` and the
    local-recipes-index remote.

--- conan_recipes/recipes/nuraft/all/conanfile.py
+++ conan_recipes/recipes/nuraft/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, patch, rm, rmdir
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, patch, rm, rmdir, trim_conandata
 from conan.tools.scm import Version
 import os
 
@@ -29,6 +29,10 @@ class NuRaftConan(ConanFile):
         "fPIC": True,
         "asio": "standalone",
     }
+
+    def export(self):
+        # Stabilise recipe revision across conan export and local-recipes-index
+        trim_conandata(self)
 
     def export_sources(self):
         export_conandata_patches(self)
