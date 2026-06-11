b077f1659bcc1b4de26e4440b08a71813e93d0a0
Gareth Lloyd <gareth.lloyd@memgraph.io>
2026-06-12 01:25:43 +0200

    test: bind the fifth PrepareResult field at stale destructure sites
    Interpreter::Prepare returns a five-field result; the projection-schema table
    is the fifth field. Several unit-test call sites still destructured four
    elements and no longer compiled. Bind a fifth placeholder so they build again.

--- tests/unit/query_dump.cpp
+++ tests/unit/query_dump.cpp
@@ -339,7 +339,7 @@ auto Execute(memgraph::query::InterpreterContext *context, memgraph::dbms::Datab
   interpreter.SetUser(auth_checker.GenQueryUser(std::nullopt, {}));
   ResultStreamFaker stream(db->storage());
 
-  auto [header, _1, qid, _2] = interpreter.Prepare(query, memgraph::query::no_params_fn, {});
+  auto [header, _1, qid, _2, _3] = interpreter.Prepare(query, memgraph::query::no_params_fn, {});
   stream.Header(header);
   auto summary = interpreter.PullAll(&stream);
   stream.Summary(summary);
@@ -1608,7 +1608,7 @@ class StatefulInterpreter {
   auto Execute(const std::string &query) {
     ResultStreamFaker stream(interpreter_.current_db_.db_acc_->get()->storage());
 
-    auto [header, _1, qid, _2] = interpreter_.Prepare(query, memgraph::query::no_params_fn, {});
+    auto [header, _1, qid, _2, _3] = interpreter_.Prepare(query, memgraph::query::no_params_fn, {});
     stream.Header(header);
     auto summary = interpreter_.PullAll(&stream);
     stream.Summary(summary);
