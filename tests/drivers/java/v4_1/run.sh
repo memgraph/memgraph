e78e5b9aa0ae75bc8e70119414c7e6dc46b85e41
Matt <mattkjames7@gmail.com>
2026-09-01 15:38:38 +0000

    harden Python driver test download

--- tests/drivers/java/v4_1/run.sh
+++ tests/drivers/java/v4_1/run.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
+source "$DIR/../../wget_retry.sh"
 
 for i in java javac; do
     if ! which $i >/dev/null; then
@@ -15,11 +16,11 @@ REACTIVE_STREAM_DEP=reactive-streams.jar
 
 if [ ! -f $DRIVER ]; then
     # Driver downloaded from: http://central.maven.org/maven2/org/neo4j/driver/neo4j-java-driver/1.5.2/neo4j-java-driver-1.5.2.jar
-    wget -nv https://repo1.maven.org/maven2/org/neo4j/driver/neo4j-java-driver/4.1.1/neo4j-java-driver-4.1.1.jar -O $DRIVER || exit 1
+    wget_retry https://repo1.maven.org/maven2/org/neo4j/driver/neo4j-java-driver/4.1.1/neo4j-java-driver-4.1.1.jar -O $DRIVER || exit 1
 fi
 
 if [ ! -f $REACTIVE_STREAM_DEP ]; then
-    wget -nv https://repo1.maven.org/maven2/org/reactivestreams/reactive-streams/1.0.3/reactive-streams-1.0.3.jar -O $REACTIVE_STREAM_DEP || exit 1
+    wget_retry https://repo1.maven.org/maven2/org/reactivestreams/reactive-streams/1.0.3/reactive-streams-1.0.3.jar -O $REACTIVE_STREAM_DEP || exit 1
 fi
 
 # CentOS 7 doesn't have Java version that supports var keyword
