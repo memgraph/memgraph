3e347df27c4cc4e9642999446ca3a274daf77bdd
Gareth Andrew Lloyd <gareth.lloyd@memgraph.io>
2026-09-24 10:57:21 +0100

    test(storage): share the setup the chunked empty-range tests repeat

--- tests/unit/db_accessor_chunked_test.cpp
+++ tests/unit/db_accessor_chunked_test.cpp
@@ -66,6 +66,28 @@ class DbAccessorChunkedTest : public ::testing::Test {
     }
   }
 
+  void CreateNumberedEdges(int count) {
+    auto acc = storage_->Access(storage::StorageAccessType::WRITE);
+    auto dba = DbAccessor(acc.get());
+    auto v1 = dba.InsertVertex();
+    auto v2 = dba.InsertVertex();
+    for (int i = 0; i < count; ++i) {
+      auto e = dba.InsertEdge(&v1, &v2, type_id_);
+      ASSERT_TRUE(e.has_value());
+      ASSERT_TRUE(e->SetProperty(prop_id_, PropertyValue(static_cast<int64_t>(i))).has_value());
+    }
+    ASSERT_TRUE(dba.Commit(memgraph::tests::MakeMainCommitArgs()).has_value());
+  }
+
+  static size_t CountEdges(auto &&chunks) {
+    size_t count = 0;
+    for (size_t i = 0; i < chunks.size(); ++i) {
+      auto chunk = chunks.get_chunk(i);
+      for ([[maybe_unused]] auto const &edge : chunk) ++count;
+    }
+    return count;
+  }
+
   std::unique_ptr<storage::Storage> storage_;
   storage::Config config_{.salient = {.items = {.properties_on_edges = true}}};
 
@@ -431,57 +453,19 @@ TEST_F(DbAccessorChunkedTest, PropertyChunkIteratorAppliesTheValuePredicate) {
 }
 
 TEST_F(DbAccessorChunkedTest, PropertyChunkIteratorScansNothingForAnEmptyRange) {
-  {
-    auto acc = storage_->Access(storage::StorageAccessType::WRITE);
-    auto dba = DbAccessor(acc.get());
-    auto v1 = dba.InsertVertex();
-    auto v2 = dba.InsertVertex();
-    for (int i = 0; i < 100; ++i) {
-      auto e = dba.InsertEdge(&v1, &v2, type_id_);
-      ASSERT_TRUE(e.has_value());
-      ASSERT_TRUE(e->SetProperty(prop_id_, PropertyValue(static_cast<int64_t>(i))).has_value());
-    }
-    ASSERT_TRUE(dba.Commit(memgraph::tests::MakeMainCommitArgs()).has_value());
-  }
-
+  CreateNumberedEdges(100);
   auto acc = storage_->Access(storage::StorageAccessType::WRITE);
   auto dba = DbAccessor(acc.get());
 
-  auto chunks = dba.ChunkedEdges(View::OLD, prop_id_, storage::PropertyValueRange::Empty(), 4);
-
-  size_t read = 0;
-  for (size_t i = 0; i < chunks.size(); ++i) {
-    auto chunk = chunks.get_chunk(i);
-    for ([[maybe_unused]] auto edge : chunk) ++read;
-  }
-  EXPECT_EQ(read, 0);
+  EXPECT_EQ(CountEdges(dba.ChunkedEdges(View::OLD, prop_id_, storage::PropertyValueRange::Empty(), 4)), 0);
 }
 
 TEST_F(DbAccessorChunkedTest, TypePropertyChunkIteratorScansNothingForAnEmptyRange) {
-  {
-    auto acc = storage_->Access(storage::StorageAccessType::WRITE);
-    auto dba = DbAccessor(acc.get());
-    auto v1 = dba.InsertVertex();
-    auto v2 = dba.InsertVertex();
-    for (int i = 0; i < 100; ++i) {
-      auto e = dba.InsertEdge(&v1, &v2, type_id_);
-      ASSERT_TRUE(e.has_value());
-      ASSERT_TRUE(e->SetProperty(prop_id_, PropertyValue(static_cast<int64_t>(i))).has_value());
-    }
-    ASSERT_TRUE(dba.Commit(memgraph::tests::MakeMainCommitArgs()).has_value());
-  }
-
+  CreateNumberedEdges(100);
   auto acc = storage_->Access(storage::StorageAccessType::WRITE);
   auto dba = DbAccessor(acc.get());
 
-  auto chunks = dba.ChunkedEdges(View::OLD, type_id_, prop_id_, storage::PropertyValueRange::Empty(), 4);
-
-  size_t read = 0;
-  for (size_t i = 0; i < chunks.size(); ++i) {
-    auto chunk = chunks.get_chunk(i);
-    for ([[maybe_unused]] auto edge : chunk) ++read;
-  }
-  EXPECT_EQ(read, 0);
+  EXPECT_EQ(CountEdges(dba.ChunkedEdges(View::OLD, type_id_, prop_id_, storage::PropertyValueRange::Empty(), 4)), 0);
 }
 
 TEST_F(DbAccessorChunkedTest, PropertyRangeChunkIterator) {
